a65f8888d46c0aebc04c92181cd0e5959653cb3a
Max Howell <max.howell@apple.com>
2015-11-11 16:26:57 -0800

    A dummy XCTestCaseProvider protocol for Darwin
    Linux XCTest requires XCTestCaseProvider, but Darwin does not. Providing a dummy means we compile for both without further modification.

---
+++ Tests/XCTestCaseProvider.swift
@@ -0,0 +1,16 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright 2015 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+
+ -------------------------------------------------------------------------
+ XCTestCaseProvider is required on Linux: this way we compile on Darwin.
+ */
+
+#if os(OSX)
+    protocol XCTestCaseProvider {}
+#endif
