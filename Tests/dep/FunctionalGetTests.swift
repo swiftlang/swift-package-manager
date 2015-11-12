7daf35a07d04d69e9e965bb1716d10bcbbc9cf8e
Max Howell <max.howell@apple.com>
2015-11-12 15:23:40 -0800

    Improve fixture machinery
    This is more flexible and allowed me to write some new swift-build tests.

--- Tests/dep/FunctionalGetTests.swift
+++ Tests/dep/FunctionalGetTests.swift
@@ -8,30 +8,46 @@
  See http://swift.org/CONTRIBUTORS.txt for Swift project authors
 */
 
-@testable import dep
-import sys
+import func POSIX.popen
 import XCTest
 
-class FunctionalGetTests: SandboxTestCase, XCTestCaseProvider {
+class FunctionalGetTests: XCTestCase, XCTestCaseProvider {
 
     var allTests : [(String, () -> ())] {
         return [
-            ("testDealerBuild", testDealerBuild),
-            ("testExternalDeps", testExternalDeps)
+            ("test_get_ExternalDeps", test_get_ExternalDeps),
+            ("test_get_DealerBuild", test_get_DealerBuild),
+            ("test_get_DealerBuildOutput", test_get_DealerBuildOutput)
         ]
     }
 
-    func testDealerBuild() {
-        testSwiftGet(fixtureName: "101_mattts_dealer") { prefix, baseURL, executeSwiftGet in
-            XCTAssertEqual(try! executeSwiftGet("\(baseURL)/app"), 0)
+    func test_get_ExternalDeps() {
+        fixture(name: "100_external_deps") { prefix in
+            mktmpdir {
+                XCTAssertNotNil(try? executeSwiftGet("\(prefix)/Bar"))
+                XCTAssertTrue("Foo-1.2.3".isDirectory)
+                XCTAssertTrue("Bar-1.2.3".isDirectory)
+            }
         }
     }
 
-    func testExternalDeps() {
-        testSwiftGet(fixtureName: "100_external_deps") { prefix, baseURL, executeSwiftGet in
-            XCTAssertEqual(try! executeSwiftGet("\(baseURL)/Bar"), 0)
-            XCTAssertTrue(Path.join(prefix, "Foo-1.2.3").isDirectory)
-            XCTAssertTrue(Path.join(prefix, "Bar-1.2.3").isDirectory)
+    // 25: Build Mattt's Dealer
+    func test_get_DealerBuild() {
+        fixture(name: "101_mattts_dealer") { prefix in
+            mktmpdir {
+                XCTAssertNotNil(try? executeSwiftGet("\(prefix)/app"))
+            }
+        }
+    }
+
+    // 25: Build Mattt's Dealer
+    func test_get_DealerBuildOutput() {
+        fixture(name: "102_mattts_dealer") { prefix in
+            mktmpdir {
+                XCTAssertNotNil(try? executeSwiftGet("\(prefix)/app"))
+                let output = try POSIX.popen(["app-1.2.3/Dealer"])
+                XCTAssertEqual(output, "♣︎K\n♣︎Q\n♣︎J\n♣︎10\n♣︎9\n♣︎8\n♣︎7\n♣︎6\n♣︎5\n♣︎4\n")
+            }
         }
     }
 }
