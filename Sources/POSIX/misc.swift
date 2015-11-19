d6dd97b56c3c5579ed103aea8ca96051f0cdd456
Max Howell <max.howell@apple.com>
2015-11-19 14:00:17 -0800

    Simplify prettyArguments

--- Sources/POSIX/misc.swift
+++ Sources/POSIX/misc.swift
@@ -11,15 +11,8 @@
 /**
  For an array of String arguments that would be passed to system() or
  popen(), returns a pretty-printed string that is user-readable and
- could be typed into a Terminal to reattempt execution.
+ could be typed into a Terminal to re-attempt execution.
 */
 public func prettyArguments(args: [String]) -> String {
-    return args.map { arg in
-        let chars = arg.characters
-        if chars.contains(" ") {
-            return chars.split(" ").map(String.init).joinWithSeparator("\\ ")
-        } else {
-            return arg
-        }
-    }.joinWithSeparator(" ")
+    return args.map { $0.characters.split(" ").map(String.init).joinWithSeparator("\\ ") }.joinWithSeparator(" ")
 }
