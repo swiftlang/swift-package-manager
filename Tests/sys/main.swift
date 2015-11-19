a95a7aa64a2f75fb98424ce3a6507d2c70eef4d3
Daniel Dunbar <daniel_dunbar@apple.com>
2015-11-19 12:11:23 -0800

    [tests] Give test targets an implementation of XCTestProvider, when not on Linux.

--- Tests/sys/main.swift
+++ Tests/sys/main.swift
@@ -1,5 +1,12 @@
 import XCTest
 
+// Support building the "enumerated"-tests style, on OS X.
+#if !os(Linux)
+public protocol XCTestCaseProvider {
+    var allTests : [(String, () -> ())] { get }
+}
+#endif
+
 // POSIXTests.swift
 PathTests().invokeTest()
 WalkTests().invokeTest()
