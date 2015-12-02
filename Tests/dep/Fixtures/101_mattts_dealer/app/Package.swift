21ef8ba24163c4b5c8b6e957799fe27224e03cd1
Max Howell <max.howell@apple.com>
2015-12-01 17:09:22 -0800

    Test recursive dependencies in these testcases
    We *should* test both scenarios, I will need to go through our functional suite and write out a test matrix.

--- Tests/dep/Fixtures/101_mattts_dealer/app/Package.swift
+++ Tests/dep/Fixtures/101_mattts_dealer/app/Package.swift
@@ -4,7 +4,5 @@ let package = Package(
     name: "Dealer",
     dependencies: [
         .Package(url: "../DeckOfPlayingCards", versions: Version(1,1,0)..<Version(2,0,0)),
-        .Package(url: "../PlayingCard", versions: Version(1,1,0)..<Version(2,0,0)),
-        .Package(url: "../FisherYates", versions: Version(1,2,0)..<Version(2,0,0))
     ]
 )
