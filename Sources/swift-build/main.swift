2592b20728518c8223a8234541dd9c391d4c8447
Boris Bügling <boris@icculus.org>
2015-12-09 02:19:01 +0100

    Compare `url.basename` instead of `url`
    This is needed to make packages work which are referenced by local paths
    instead of remote URLs.

--- Sources/swift-build/main.swift
+++ Sources/swift-build/main.swift
@@ -52,8 +52,8 @@ do {
 
         for pkg in dependencies {
             let manifest = try Manifest(path: "\(pkg.path)/Package.swift", baseURL: pkg.path)
-            let dependencyUrls = manifest.package.dependencies.map { $0.url }
-            let dependencies = dependencies.filter { dependencyUrls.contains($0.url) }
+            let dependencyUrls = manifest.package.dependencies.map { $0.url.basename }
+            let dependencies = dependencies.filter { dependencyUrls.contains($0.url.basename) }
             try llbuild(srcroot: pkg.path, targets: try pkg.targets(), dependencies: dependencies, prefix: builddir, tmpdir: Path.join(builddir, "\(pkg.name).o"), configuration: configuration)
         }
 
