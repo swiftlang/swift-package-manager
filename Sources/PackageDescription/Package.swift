86f3e32f1d338c97788d271a41e8fdab3dcb4aa2
Kostiantyn Koval <konstantin.koval1@gmail.com>
2015-12-09 12:01:27 +0100

    Add ability to exclude folders in Manifest.
    SR-29 jira tasks.

--- Sources/PackageDescription/Package.swift
+++ Sources/PackageDescription/Package.swift
@@ -49,11 +49,15 @@ public final class Package {
     /// The list of dependencies.
     public var dependencies: [Dependency]
 
+    /// The list of folders to exlude
+    public var exclude: [String]
+
     /// Construct a package.
-    public init(name: String? = nil, targets: [Target] = [], dependencies: [Dependency] = []) {
+    public init(name: String? = nil, targets: [Target] = [], dependencies: [Dependency] = [], exclude: [String] = []) {
         self.name = name
         self.targets = targets
         self.dependencies = dependencies
+        self.exclude = exclude
 
         // Add custom exit handler to cause package to be dumped at exit, if requested.
         //
@@ -80,6 +84,8 @@ public final class Package {
         for target in targets {
             result += target.toTOML("package.targets")
         }
+        result += "\n" + "exclude = \(exclude)"
+
         return result
     }
 }
