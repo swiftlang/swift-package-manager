974591c8113991dd514569eff863ad5037873a6c
Keith Smiley <keithbsmiley@gmail.com>
2016-02-20 20:32:51 -0800

    Add tests for describe function throwing

---
+++ Tests/Build/DescribeTests.swift
@@ -0,0 +1,25 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright 2015 - 2016 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
+import Build
+import XCTest
+
+final class DescribeTests: XCTestCase {
+    func testDescribingNoModulesThrows() {
+        do {
+            let _ = try describe("foo", .Debug, [], [], Xcc: [], Xld: [])
+            XCTFail("This call should throw")
+        } catch Build.Error.NoModules {
+            XCTAssert(true, "This error should be throw")
+        } catch {
+            XCTFail("No other error should be thrown")
+        }
+    }
+}
