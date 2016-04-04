4d9b1b71dbbef3db3e73f00fa0b4ab9a4d525fe4
Max Howell <max.howell@apple.com>
2016-04-04 12:19:48 -0700

    Fix OS X integration tests

--- Sources/swift-build/UserToolchain.swift
+++ Sources/swift-build/UserToolchain.swift
@@ -6,12 +6,18 @@
 
  See http://swift.org/LICENSE.txt for license information
  See http://swift.org/CONTRIBUTORS.txt for Swift project authors
-*/
+ */
 
 import protocol Build.Toolchain
 import struct Utility.Path
 import enum Multitool.Error
 import POSIX
+
+#if os(OSX)
+    private let whichClangArgs = ["xcrun", "--find", "clang"]
+#else
+    private let whichClangArgs = ["which", "clang"]
+#endif
 
 struct UserToolchain: Toolchain {
     let SWIFT_EXEC: String
@@ -19,43 +25,32 @@ struct UserToolchain: Toolchain {
     let sysroot: String?
 
 #if os(OSX)
-    /**
-      On OS X we do not support running in situations where xcrun fails.
-     */
-    init() throws {
-        SWIFT_EXEC = try getenv("SWIFT_EXEC") ?? POSIX.popen(["xcrun", "--find", "swiftc"]).chomp()
-        clang = try getenv("CC") ?? POSIX.popen(["xcrun", "--find", "clang"]).chomp()
-        sysroot = try getenv("SYSROOT") ?? POSIX.popen(["xcrun", "--sdk", "macosx", "--show-sdk-path"]).chomp()
-
-        guard !SWIFT_EXEC.isEmpty && !clang.isEmpty && !sysroot!.isEmpty else {
-            throw Multitool.Error.InvalidToolchain
-        }
-    }
-
     var platformArgs: [String] {
         return ["-target", "x86_64-apple-macosx10.10", "-sdk", sysroot!]
     }
-
 #else
+    let platformArgs: [String] = []
+#endif
 
     init() throws {
         do {
             SWIFT_EXEC = getenv("SWIFT_EXEC")
-                // see if user has put something earlier in the path
-                ?? (try? POSIX.popen(["which", "swiftc"]))?.chomp().abspath()
                 // use the swiftc installed alongside ourselves
                 ?? Path.join(Process.arguments[0], "../swiftc").abspath()
-            clang = try getenv("CC") ?? popen(["which", "clang"]).chomp().abspath()
-            sysroot = nil
+
+            clang = try getenv("CC") ?? POSIX.popen(whichClangArgs).chomp()
+
+            #if os(OSX)
+                sysroot = try getenv("SYSROOT") ?? POSIX.popen(["xcrun", "--sdk", "macosx", "--show-sdk-path"]).chomp()
+            #else
+                sysroot = nil
+            #endif
+
+            guard !SWIFT_EXEC.isEmpty && !clang.isEmpty && (sysroot == nil || !sysroot!.isEmpty) else {
+                throw Multitool.Error.InvalidToolchain
+            }
         } catch POSIX.Error.ExitStatus {
             throw Multitool.Error.InvalidToolchain
         }
-        guard !SWIFT_EXEC.isEmpty && !clang.isEmpty else {
-            throw Multitool.Error.InvalidToolchain
-        }
-
     }
-
-    let platformArgs: [String] = []
-#endif
 }
