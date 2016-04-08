e15bb2cddf3f916b122fc2d2f24d041eb7af0c28
Max Howell <max.howell@apple.com>
2016-04-08 16:37:54 -0700

    de-indent this switch
    Not sure when it became indented, but it is not our code-style.

--- Sources/swift-build/main.swift
+++ Sources/swift-build/main.swift
@@ -61,74 +61,74 @@ do {
     }
 
     switch mode {
-        case .Build(let conf, let toolchain):
-            let dirs = try directories()
-            let (rootPackage, externalPackages) = try fetch(dirs.root)
-            let (modules, externalModules, products) = try transmute(rootPackage, externalPackages: externalPackages)
-            let yaml = try describe(dirs.build, conf, modules, Set(externalModules), products, Xcc: opts.Xcc, Xld: opts.Xld, Xswiftc: opts.Xswiftc, toolchain: toolchain)
-            try build(YAMLPath: yaml, target: "default")
+    case .Build(let conf, let toolchain):
+        let dirs = try directories()
+        let (rootPackage, externalPackages) = try fetch(dirs.root)
+        let (modules, externalModules, products) = try transmute(rootPackage, externalPackages: externalPackages)
+        let yaml = try describe(dirs.build, conf, modules, Set(externalModules), products, Xcc: opts.Xcc, Xld: opts.Xld, Xswiftc: opts.Xswiftc, toolchain: toolchain)
+        try build(YAMLPath: yaml, target: "default")
 
-        case .Init(let initMode):
-            let initPackage = InitPackage(mode: initMode)
-            try initPackage.writePackageStructure()
-                        
-        case .Fetch:
-            try fetch(try directories().root)
+    case .Init(let initMode):
+        let initPackage = InitPackage(mode: initMode)
+        try initPackage.writePackageStructure()
+                    
+    case .Fetch:
+        try fetch(try directories().root)
 
-        case .Usage:
-            usage()
+    case .Usage:
+        usage()
 
-        case .Clean(let mode):
-            let dirs = try directories()
+    case .Clean(let mode):
+        let dirs = try directories()
 
-            switch mode {
-            case .Dist:
-                try rmtree(dirs.Packages)
-                fallthrough
+        switch mode {
+        case .Dist:
+            try rmtree(dirs.Packages)
+            fallthrough
 
-            case .Build:
-                let artifacts = ["debug", "release"].map{ Path.join(dirs.build, $0) }.map{ ($0, "\($0).yaml") }
-                for (dir, yml) in artifacts {
-                    if dir.isDirectory { try rmtree(dir) }
-                    if yml.isFile { try unlink(yml) }
-                }
-
-                let db = Path.join(dirs.build, "build.db")
-                if db.isFile { try unlink(db) }
-
-                try rmdir(dirs.build)
+        case .Build:
+            let artifacts = ["debug", "release"].map{ Path.join(dirs.build, $0) }.map{ ($0, "\($0).yaml") }
+            for (dir, yml) in artifacts {
+                if dir.isDirectory { try rmtree(dir) }
+                if yml.isFile { try unlink(yml) }
             }
 
-        case .Version:
-            print("Apple Swift Package Manager 0.1")
-            
-        case .GenerateXcodeproj(let outpath):
-            let dirs = try directories()
-            let (rootPackage, externalPackages) = try fetch(dirs.root)
-            let (modules, externalModules, products) = try transmute(rootPackage, externalPackages: externalPackages)
-            
-            let xcodeModules = modules.flatMap { $0 as? XcodeModuleProtocol }
-            let externalXcodeModules  = externalModules.flatMap { $0 as? XcodeModuleProtocol }
+            let db = Path.join(dirs.build, "build.db")
+            if db.isFile { try unlink(db) }
 
-            let projectName: String
-            let dstdir: String
-            let packageName = rootPackage.name
+            try rmdir(dirs.build)
+        }
 
-            switch outpath {
-            case let outpath? where outpath.hasSuffix(".xcodeproj"):
-                // if user specified path ending with .xcodeproj, use that
-                projectName = String(outpath.basename.characters.dropLast(10))
-                dstdir = outpath.parentDirectory
-            case let outpath?:
-                dstdir = outpath
-                projectName = packageName
-            case _:
-                dstdir = dirs.root
-                projectName = packageName
-            }
-            let outpath = try Xcodeproj.generate(dstdir: dstdir, projectName: projectName, srcroot: dirs.root, modules: xcodeModules, externalModules: externalXcodeModules, products: products, options: (Xcc: opts.Xcc, Xld: opts.Xld, Xswiftc: opts.Xswiftc))
+    case .Version:
+        print("Apple Swift Package Manager 0.1")
+        
+    case .GenerateXcodeproj(let outpath):
+        let dirs = try directories()
+        let (rootPackage, externalPackages) = try fetch(dirs.root)
+        let (modules, externalModules, products) = try transmute(rootPackage, externalPackages: externalPackages)
+        
+        let xcodeModules = modules.flatMap { $0 as? XcodeModuleProtocol }
+        let externalXcodeModules  = externalModules.flatMap { $0 as? XcodeModuleProtocol }
 
-            print("generated:", outpath.prettied)
+        let projectName: String
+        let dstdir: String
+        let packageName = rootPackage.name
+
+        switch outpath {
+        case let outpath? where outpath.hasSuffix(".xcodeproj"):
+            // if user specified path ending with .xcodeproj, use that
+            projectName = String(outpath.basename.characters.dropLast(10))
+            dstdir = outpath.parentDirectory
+        case let outpath?:
+            dstdir = outpath
+            projectName = packageName
+        case _:
+            dstdir = dirs.root
+            projectName = packageName
+        }
+        let outpath = try Xcodeproj.generate(dstdir: dstdir, projectName: projectName, srcroot: dirs.root, modules: xcodeModules, externalModules: externalXcodeModules, products: products, options: (Xcc: opts.Xcc, Xld: opts.Xld, Xswiftc: opts.Xswiftc))
+
+        print("generated:", outpath.prettied)
     }
 
 } catch {
