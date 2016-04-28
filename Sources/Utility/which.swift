2101765df601cacdfe4215dfd01e1cd081f5f56d
Bouke Haarsma <bouke@haarsma.eu>
2016-04-28 22:36:16 +0200

    [Utility] Memoize `which` to improve test performance
    No memoization has a performance penalty of ~6%.

--- Sources/Utility/which.swift
+++ Sources/Utility/which.swift
@@ -10,10 +10,15 @@
 
 import Foundation
 
+private var memoized = [String: String]()
+
 public func which(_ arg0: String) -> String {
     if arg0.isAbsolute {
         return arg0
+    } else if let fullpath = memoized[arg0] {
+        return fullpath
     } else if let fullpath = try? Utility.popen(["/bin/sh", "-c", "which \(arg0)"]) {
+        memoized[arg0] = fullpath.chomp()
         return fullpath.chomp()
     } else {
         return arg0
