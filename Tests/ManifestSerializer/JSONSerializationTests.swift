c676db3666d32b000dbd448a649f81501400a3b6
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-05-08 22:21:55 +0200

    Added more JSONSerialization tests

--- Tests/ManifestSerializer/JSONSerializationTests.swift
+++ Tests/ManifestSerializer/JSONSerializationTests.swift
@@ -23,7 +23,76 @@ class JSONSerializationTests: XCTestCase {
         assertEqual(package: package, expected: exp)
     }
     
-    //FIXME: more tests to come
+    func testDependencies() {
+        let deps: [Package.Dependency] =
+            [
+                .Package(url: "https://github.com/apple/swift.git", majorVersion: 3),
+                .Package(url: "https://github.com/apple/llvm.git", majorVersion: 2)
+        ]
+        let package = Package(name: "WithDeps", pkgConfig: nil, providers: nil, targets: [], dependencies: deps, testDependencies: [], exclude: [])
+        let exp = NSMutableDictionary.withNew { (dict) in
+            dict["name"] = "WithDeps"
+            let dep1 = [
+                           "url": "https://github.com/apple/swift.git",
+                           "version": [
+                                          "lowerBound":"3.0.0",
+                                          "upperBound":"3.9223372036854775807.9223372036854775807"
+                ]
+            ]
+            let dep2 = [
+                           "url": "https://github.com/apple/llvm.git",
+                           "version": [
+                                          "lowerBound":"2.0.0",
+                                          "upperBound":"2.9223372036854775807.9223372036854775807"
+                ]
+            ]
+            dict["dependencies"] = [dep1, dep2]
+            fillWithEmptyArrays(keyNames: ["testDependencies", "exclude", "package.targets"], dict: dict)
+        }
+        assertEqual(package: package, expected: exp)
+    }
+    
+    func testPkgConfig() {
+        let providers: [SystemPackageProvider] = [
+                            .Brew("BrewPackage"),
+                            .Apt("AptPackage")
+                            ]
+        let package = Package(name: "PkgPackage", pkgConfig: "PkgPackage-1.0", providers: providers)
+        let exp = NSMutableDictionary.withNew { (dict) in
+            dict["name"] = "PkgPackage"
+            dict["pkgConfig"] = "PkgPackage-1.0"
+            dict["package.providers"] = [
+                ["Brew": "BrewPackage"],
+                ["Apt": "AptPackage"]
+            ]
+            fillWithEmptyArrays(keyNames: ["dependencies", "testDependencies", "exclude", "package.targets"], dict: dict)
+        }
+        assertEqual(package: package, expected: exp)
+    }
+    
+    func testExclude() {
+        let package = Package(name: "Exclude", exclude: ["pikachu", "bulbasaur"])
+        let exp = NSMutableDictionary.withNew { (dict) in
+            dict["name"] = "Exclude"
+            dict["exclude"] = ["pikachu", "bulbasaur"]
+            fillWithEmptyArrays(keyNames: ["dependencies", "testDependencies", "package.targets"], dict: dict)
+        }
+        assertEqual(package: package, expected: exp)
+    }
+    
+    func testTargets() {
+        let t1 = Target(name: "One")
+        let t2 = Target(name: "Two", dependencies: [.Target(name: "One")])
+        let package = Package(name: "Targets", targets: [t1, t2])
+        let exp = NSMutableDictionary.withNew { (dict) in
+            dict["name"] = "Targets"
+            let ts1 = ["name": "One", "dependencies": [String]()]
+            let ts2 = ["name": "Two", "dependencies": ["One"]]
+            dict["package.targets"] = [ts1, ts2]
+            fillWithEmptyArrays(keyNames: ["dependencies", "testDependencies", "exclude", ], dict: dict)
+        }
+        assertEqual(package: package, expected: exp)
+    }
 }
 
 extension JSONSerializationTests {
@@ -44,6 +113,10 @@ extension JSONSerializationTests {
     static var allTests : [(String, (JSONSerializationTests) -> () throws -> Void)] {
         return [
             ("testSimple", testSimple),
+            ("testDependencies", testDependencies),
+            ("testPkgConfig", testPkgConfig),
+            ("testExclude", testExclude),
+            ("testTargets", testTargets),
         ]
     }
 }
