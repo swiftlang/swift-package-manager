b0f2aa5af14dd37317d1b8668bea33db434d9cbb
Daniel Dunbar <daniel_dunbar@apple.com>
2016-05-18 09:11:05 -0700

    [ManifestSerializer] Simplify the manifest loading initializer.

--- Sources/ManifestSerializer/Manifest+parse.swift
+++ Sources/ManifestSerializer/Manifest+parse.swift
@@ -18,26 +18,25 @@ import func POSIX.unlink
 import func Utility.fopen
 
 extension Manifest {
-    public init(path pathComponents: String..., baseURL: String, swiftc: String, libdir: String) throws {
-
+    /// Create a manifest by loading from the given path.
+    ///
+    /// - path: The path to the manifest file or directory containing `Package.swift`.
+    public init(path inputPath: String, baseURL: String, swiftc: String, libdir: String) throws {
         guard baseURL.chuzzle() != nil else { fatalError() }  //TODO
 
-        // canonicalize the URL
+        // Canonicalize the URL.
         var baseURL = baseURL
         if URL.scheme(baseURL) == nil {
             baseURL = try realpath(baseURL)
         }
 
-        let joinedPath = Path.join(pathComponents)
-        let path: String
-        if joinedPath.isDirectory {
-            path = Path.join(joinedPath, Manifest.filename)
-        } else {
-            path = joinedPath
-        }
+        // Compute the actual input file path.
+        let path: String = inputPath.isDirectory ? Path.join(inputPath, Manifest.filename) : inputPath
 
+        // Validate that the file exists.
         guard path.isFile else { throw PackageModel.Package.Error.NoManifest(path) }
 
+        // Load the manifest description.
         if let toml = try parse(path: path, swiftc: swiftc, libdir: libdir) {
             let toml = try TOMLItem.parse(toml)
             let package = PackageDescription.Package.fromTOML(toml, baseURL: baseURL)
@@ -46,17 +45,18 @@ extension Manifest {
             self.init(path: path, package: package, products: products)
         } else {
             // As a special case, we accept an empty file as an unnamed package.
+            //
+            // FIXME: We should deprecate this, now that we have the `init` functionality.
             self.init(path: path, package: PackageDescription.Package(), products: [])
         }
     }
 }
 
 private func parse(path manifestPath: String, swiftc: String, libdir: String) throws -> String? {
-
     // For now, we load the manifest by having Swift interpret it directly.
-    // Eventually, we should have two loading processes, one that loads only
-    // the the declarative package specification using the Swift compiler
-    // directly and validates it.
+    // Eventually, we should have two loading processes, one that loads only the
+    // the declarative package specification using the Swift compiler directly
+    // and validates it.
 
     var cmd = [swiftc]
     cmd += ["--driver-mode=swift"]
