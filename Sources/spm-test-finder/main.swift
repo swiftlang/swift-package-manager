01ae625a69183712799a17ab441bff5b9f3ca095
Ankit Aggarwal <ankit.spd@gmail.com>
2016-05-20 20:37:04 +0530

    Use XCTestSuite.default

--- Sources/spm-test-finder/main.swift
+++ Sources/spm-test-finder/main.swift
@@ -22,20 +22,23 @@ do {
         throw Error.unableToLoadBundle(bundlePath)
     }
     
-    let suite = XCTestSuite(forBundlePath: bundlePath)
+    let suite = XCTestSuite.default()
+
     let splitSet: Set<Character> = ["[", " ", "]", ":"]
     for case let testCaseSuite as XCTestSuite in suite.tests {
-        for case let test as XCTestCase in testCaseSuite.tests {
-            let exploded = test.description.characters.split(isSeparator: splitSet.contains).map(String.init)
-            let moduleName = String(String(reflecting: test.dynamicType).characters.split(separator: ".").first!)
-            let className = exploded[1]
-            var methodName = exploded[2]
-            if methodName.hasSuffix("AndReturnError") {
-                methodName = methodName[methodName.startIndex..<methodName.index(methodName.endIndex, offsetBy: -14)]
+        for case let testCaseSuite as XCTestSuite in testCaseSuite.tests {
+            for case let test as XCTestCase in testCaseSuite.tests {
+                let exploded = test.description.characters.split(isSeparator: splitSet.contains).map(String.init)
+                let moduleName = String(String(reflecting: test.dynamicType).characters.split(separator: ".").first!)
+                let className = exploded[1]
+                var methodName = exploded[2]
+                if methodName.hasSuffix("AndReturnError") {
+                    methodName = methodName[methodName.startIndex..<methodName.index(methodName.endIndex, offsetBy: -14)]
+                }
+                print("\(moduleName).\(className)/\(methodName)")
             }
-            print("\(moduleName).\(className)/\(methodName)")
+            print()
         }
-        print()
     }
 } catch Error.invalidUsage {
     print("Usage: spm-test-finder <bundle_path>")
