e14aeca5e2661859ca1ff01c954c4aab98ddb5e1
Ankit Aggarwal <ankit.spd@gmail.com>
2016-06-01 23:33:01 +0530

    [ModuleMapGeneration] Add modulemap link declaration support to modulemap generation.
    This will help in autolinking when using frameworks for Xcode targets.

--- Sources/PackageLoading/ModuleMapGeneration.swift
+++ Sources/PackageLoading/ModuleMapGeneration.swift
@@ -24,6 +24,35 @@ extension CModule {
 
 extension ClangModule {
     
+    /// A link-declaration specifies a library or framework
+    /// against which a program should be linked.
+    /// More info: http://clang.llvm.org/docs/Modules.html#link-declaration
+    /// A `library` modulemap style uses `link` flag for link-declaration where
+    /// as a `framework` uses `link framework` flag and a framework module.
+    public enum ModuleMapStyle {
+        case library
+        case framework
+
+        /// Link declaration flag to be used in modulemap.
+        var linkDeclFlag: String {
+            switch self {
+            case library:
+                return "link"
+            case framework:
+                return "link framework"
+            }
+        }
+
+        var moduleDeclQualifier: String? {
+            switch self {
+            case library:
+                return nil
+            case framework:
+                return "framework"
+            }
+        }
+    }
+
     public enum ModuleMapError: ErrorProtocol {
         case unsupportedIncludeLayoutForModule(String)
     }
@@ -32,7 +61,7 @@ extension ClangModule {
     ///when building swift modules in `XccFlags(prefix: String)`
     ///there shouldn't be need to redo this there but is difficult 
     ///in current architecture
-    public func generateModuleMap(inDir wd: String) throws {
+    public func generateModuleMap(inDir wd: String, modulemapStyle: ModuleMapStyle = .library) throws {
         precondition(wd.isAbsolute)
         
         ///Return if module map is already present
@@ -63,7 +92,7 @@ extension ClangModule {
         let umbrellaHeaderFlat = Path.join(includeDir, "\(c99name).h")
         if umbrellaHeaderFlat.isFile {
             guard dirs.isEmpty else { throw ModuleMapError.unsupportedIncludeLayoutForModule(name) }
-            try createModuleMap(inDir: wd, type: .header(umbrellaHeaderFlat))
+            try createModuleMap(inDir: wd, type: .header(umbrellaHeaderFlat), modulemapStyle: modulemapStyle)
             return
         }
         diagnoseInvalidUmbrellaHeader(includeDir)
@@ -71,12 +100,12 @@ extension ClangModule {
         let umbrellaHeader = Path.join(includeDir, c99name, "\(c99name).h")
         if umbrellaHeader.isFile {
             guard dirs.count == 1 && files.isEmpty else { throw ModuleMapError.unsupportedIncludeLayoutForModule(name) }
-            try createModuleMap(inDir: wd, type: .header(umbrellaHeader))
+            try createModuleMap(inDir: wd, type: .header(umbrellaHeader), modulemapStyle: modulemapStyle)
             return
         }
         diagnoseInvalidUmbrellaHeader(Path.join(includeDir, c99name))
 
-        try createModuleMap(inDir: wd, type: .directory(includeDir))
+        try createModuleMap(inDir: wd, type: .directory(includeDir), modulemapStyle: modulemapStyle)
     }
 
     ///warn user if in case module name and c99name are different and there a `name.h` umbrella header
@@ -93,20 +122,24 @@ extension ClangModule {
         case directory(String)
     }
     
-    private func createModuleMap(inDir wd: String, type: UmbrellaType) throws {
+    private func createModuleMap(inDir wd: String, type: UmbrellaType, modulemapStyle: ModuleMapStyle) throws {
         try Utility.makeDirectories(wd)
         let moduleMapFile = Path.join(wd, self.moduleMap)
         let moduleMap = try fopen(moduleMapFile, mode: .write)
         defer { moduleMap.closeFile() }
         
-        var output = "module \(c99name) {\n"
+        var output = ""
+        if let qualifier = modulemapStyle.moduleDeclQualifier {
+            output += qualifier + " "
+        }
+        output += "module \(c99name) {\n"
         switch type {
         case .header(let header):
             output += "    umbrella header \"\(header)\"\n"
         case .directory(let path):
             output += "    umbrella \"\(path)\"\n"
         }
-        output += "    link \"\(c99name)\"\n"
+        output += "    \(modulemapStyle.linkDeclFlag) \"\(c99name)\"\n"
         output += "    export *\n"
         output += "}\n"
 
