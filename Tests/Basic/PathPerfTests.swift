0724eae422bc8ca7f2e706a326dc56793a995f5a
Anders Bertelrud <anders@apple.com>
2016-06-22 23:34:38 -0700

    Disable a performance test that shouldn't be running in CI yet.

--- Tests/Basic/PathPerfTests.swift
+++ Tests/Basic/PathPerfTests.swift
@@ -12,6 +12,11 @@ import XCTest
 
 import Basic
 
+// FIXME: Performance tests are disabled for the time being because they have
+// too high an impact on overall testing time.
+//
+// See: https://bugs.swift.org/browse/SR-1354
+#if false
 
 class PathPerfTests: XCTestCase {
     
@@ -36,3 +41,5 @@ class PathPerfTests: XCTestCase {
         ("testJoinPerf_X100000",         testJoinPerf_X100000),
     ]
 }
+    
+#endif
