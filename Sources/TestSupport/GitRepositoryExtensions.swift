878d0541182dcafbdd2347d08d11e1e9e2078bbc
Ankit Aggarwal <ankit_aggarwal@apple.com>
2016-09-29 15:59:33 +0530

    [TestSupport] Add commonly used git commands extensions
    These are some of the commonly used git commands in tests which are not
    really needed in GitRepository class right now, so they can be kept as
    extension in test support.

---
+++ Sources/TestSupport/GitRepositoryExtensions.swift
@@ -0,0 +1,50 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright 2016 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
+import Utility
+import SourceControl
+
+/// Extensions useful for unit testing purposes.
+/// Note: These are not thread safe.
+public extension GitRepository {
+
+    /// Returns current branch name. If HEAD is on a detached state, this returns HEAD.
+    func currentBranch() throws -> String {
+        return try popen([Git.tool, "-C", path.asString, "rev-parse", "--abbrev-ref", "HEAD"]).chomp()
+    }
+
+    /// Stage a file.
+    func stage(file: String) throws {
+        try systemQuietly([Git.tool, "-C", path.asString, "add", file])
+    }
+
+    /// Stage multiple files.
+    func stage(files: String...) throws {
+        try systemQuietly([Git.tool, "-C", path.asString, "add"] + files)
+    }
+
+    /// Stage entire unstaged changes.
+    func stageEverything() throws {
+        try systemQuietly([Git.tool, "-C", path.asString, "add", "."])
+    }
+
+    /// Commit the staged changes. If the message is not provided a dummy message will be used for the commit.
+    func commit(message: String? = nil) throws {
+        // FIXME: We don't need to set these everytime but we usually only commit once or twice for a test repo.
+        try systemQuietly([Git.tool, "-C", path.asString, "config", "user.email", "example@example.com"])
+        try systemQuietly([Git.tool, "-C", path.asString, "config", "user.name", "Example Example"])
+        try systemQuietly([Git.tool, "-C", path.asString, "commit", "-m", message ?? "Add some files."])
+    }
+
+    /// Push the changes to specified remote and branch.
+    func push(remote: String, branch: String) throws {
+        try systemQuietly([Git.tool, "-C", path.asString, "push", remote, branch])
+    }
+}
