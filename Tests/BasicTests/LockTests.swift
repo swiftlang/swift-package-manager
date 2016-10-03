3f537a148e6365460abe01c09a48799c3dad843d
Ankit Aggarwal <ankit_aggarwal@apple.com>
2016-10-03 18:48:13 +0530

    [Basic] Add FileLock class
    This class is a wrapper on POSIX's flock() and is part of fix towards:
    - <rdar://problem/28448023> Prevent concurrent package workspace
    manipulations

--- Tests/BasicTests/LockTests.swift
+++ Tests/BasicTests/LockTests.swift
@@ -11,6 +11,7 @@
 import XCTest
 
 import Basic
+import TestSupport
 
 class LockTests: XCTestCase {
     func testBasics() {
@@ -26,7 +27,29 @@ class LockTests: XCTestCase {
         XCTAssertEqual(count, N)
     }
 
+    func testFileLock() throws {
+        // Shared resource file.
+        let sharedResource = try TemporaryFile()
+        // Directory where lock file should be created.
+        let tempDir = try TemporaryDirectory()
+
+        // Run the same executable multiple times and
+        // we can expect the final result to be sum of the
+        // contents we write in the shared file.
+        let N = 10
+        let threads = (1...N).map { idx in
+            return Thread {
+                _ = try! SwiftPMProduct.TestSupportExecutable.execute(["fileLockTest", tempDir.path.asString, sharedResource.path.asString, String(idx)])
+            }
+        }
+        threads.forEach { $0.start() }
+        threads.forEach { $0.join() }
+
+        XCTAssertEqual(try localFileSystem.readFileContents(sharedResource.path).asString, String((N * (N + 1) / 2 )))
+    }
+
     static var allTests = [
         ("testBasics", testBasics),
+        ("testFileLock", testFileLock),
     ]
 }
