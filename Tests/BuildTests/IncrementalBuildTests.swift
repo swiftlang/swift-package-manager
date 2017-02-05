25e65113d87fb2165d9628ced07c248d5489df05
TheOtherDave <davesweeris@mac.com>
2017-02-04 21:13:27 -0800

    deleted some comments

--- Tests/BuildTests/IncrementalBuildTests.swift
+++ Tests/BuildTests/IncrementalBuildTests.swift
@@ -251,17 +251,6 @@ final class IncrementalBuildTests: XCTestCase {
             
         }
     }
-//    import PackageDescription
-//    
-//    let package = Package(
-//        name: "Foo",
-//        targets: [
-//            Target(name: "FooLib"),
-//            Target(name: "Foo2Lib", dependencies: ["FooLib"]),
-//            Target(name: "Foo_macOS", dependencies: ["FooLib"]),
-//            Target(name: "Foo_iOS", dependencies: ["Foo2Lib"]),
-//            ]
-//    )
 
     func testAddRemoveTargetDependencies() {
         fixture(name: "IncrementalBuildTests/TrgtDeps") { prefix in
