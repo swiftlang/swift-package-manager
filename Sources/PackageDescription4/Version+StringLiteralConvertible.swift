215f0f71425f7adec51fc344f5d3784b1bf0db06
Ankit Aggarwal <ankit_aggarwal@apple.com>
2017-02-09 22:50:42 +0530

    Add PackageDescription4 module
    This will be the new PackageDescription API in Swift 4. This is named
    PackageDescription4 because we already have a PackageDescription.
    However this will be installed as PackageDescription in Toolchain so
    clients will actually use `import PackageDescription` when using this
    version of manifest. This will need to be recompiled and installed using
    the bootstrap script as PackageDescription.

---
+++ Sources/PackageDescription4/Version+StringLiteralConvertible.swift
@@ -0,0 +1,80 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
+extension Version: ExpressibleByStringLiteral {
+
+    public init(stringLiteral value: String) {
+        if let version = Version(value.characters) {
+            self.init(version)
+        } else {
+            // If version can't be initialized using the string literal, report the error and initialize with a dummy value.
+            // This is done to fail the invoking tool (like swift build) gracefully rather than just crashing.
+            errors.add("Invalid version string: \(value)")
+            self.init(0, 0, 0)
+        }
+    }
+
+    public init(extendedGraphemeClusterLiteral value: String) {
+        self.init(stringLiteral: value)
+    }
+
+    public init(unicodeScalarLiteral value: String) {
+        self.init(stringLiteral: value)
+    }
+}
+
+extension Version {
+
+    public init(_ version: Version) {
+        major = version.major
+        minor = version.minor
+        patch = version.patch
+        prereleaseIdentifiers = version.prereleaseIdentifiers
+        buildMetadataIdentifier = version.buildMetadataIdentifier
+    }
+
+    public init?(_ versionString: String) {
+        self.init(versionString.characters)
+    }
+
+    public init?(_ characters: String.CharacterView) {
+        let prereleaseStartIndex = characters.index(of: "-")
+        let metadataStartIndex = characters.index(of: "+")
+
+        let requiredEndIndex = prereleaseStartIndex ?? metadataStartIndex ?? characters.endIndex
+        let requiredCharacters = characters.prefix(upTo: requiredEndIndex)
+        let requiredComponents = requiredCharacters.split(separator: ".", maxSplits: 2, omittingEmptySubsequences: false).map{ String($0) }.flatMap{ Int($0) }.filter{ $0 >= 0 }
+
+        guard requiredComponents.count == 3 else {
+            return nil
+        }
+
+        self.major = requiredComponents[0]
+        self.minor = requiredComponents[1]
+        self.patch = requiredComponents[2]
+
+        if let prereleaseStartIndex = prereleaseStartIndex {
+            let prereleaseEndIndex = metadataStartIndex ?? characters.endIndex
+            let prereleaseCharacters = characters[characters.index(after: prereleaseStartIndex)..<prereleaseEndIndex]
+            prereleaseIdentifiers = prereleaseCharacters.split(separator: ".").map{ String($0) }
+        } else {
+            prereleaseIdentifiers = []
+        }
+
+        var buildMetadataIdentifier: String? = nil
+        if let metadataStartIndex = metadataStartIndex {
+            let buildMetadataCharacters = characters.suffix(from: characters.index(after: metadataStartIndex))
+            if !buildMetadataCharacters.isEmpty {
+                buildMetadataIdentifier = String(buildMetadataCharacters)
+            }
+        }
+        self.buildMetadataIdentifier = buildMetadataIdentifier
+    }
+}
---
+++ Sources/PackageDescription4/Version.swift
@@ -0,0 +1,139 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+
+ -------------------------------------------------------------------------
+ [A semantic version](http://semver.org).
+*/
+
+public struct Version {
+    public let (major, minor, patch): (Int, Int, Int)
+    public let prereleaseIdentifiers: [String]
+    public let buildMetadataIdentifier: String?
+    
+    public init(_ major: Int, _ minor: Int, _ patch: Int, prereleaseIdentifiers: [String] = [], buildMetadataIdentifier: String? = nil) {
+        self.major = Swift.max(major, 0)
+        self.minor = Swift.max(minor, 0)
+        self.patch = Swift.max(patch, 0)
+        self.prereleaseIdentifiers = prereleaseIdentifiers
+        self.buildMetadataIdentifier = buildMetadataIdentifier
+    }
+}
+
+// MARK: Equatable
+
+extension Version: Equatable {}
+
+public func ==(v1: Version, v2: Version) -> Bool {
+    guard v1.major == v2.major && v1.minor == v2.minor && v1.patch == v2.patch else {
+        return false
+    }
+    
+    if v1.prereleaseIdentifiers != v2.prereleaseIdentifiers {
+        return false
+    }
+    
+    return v1.buildMetadataIdentifier == v2.buildMetadataIdentifier
+}
+
+// MARK: Hashable
+
+extension Version: Hashable {
+    public var hashValue: Int {
+        // FIXME: We need Swift hashing utilities; this is based on CityHash
+        // inspired code inside the Swift stdlib.
+        let mul: UInt64 = 0x9ddfea08eb382d69
+        var result: UInt64 = 0
+        result = (result &* mul) ^ UInt64(bitPattern: Int64(major.hashValue))
+        result = (result &* mul) ^ UInt64(bitPattern: Int64(minor.hashValue))
+        result = (result &* mul) ^ UInt64(bitPattern: Int64(patch.hashValue))
+        result = prereleaseIdentifiers.reduce(result, { ($0 &* mul) ^ UInt64(bitPattern: Int64($1.hashValue)) })
+        if let build = buildMetadataIdentifier {
+            result = (result &* mul) ^ UInt64(bitPattern: Int64(build.hashValue))
+        }
+        return Int(truncatingBitPattern: result)
+    }
+}
+
+// MARK: Comparable
+
+extension Version: Comparable {}
+
+public func <(lhs: Version, rhs: Version) -> Bool {
+    let lhsComparators = [lhs.major, lhs.minor, lhs.patch]
+    let rhsComparators = [rhs.major, rhs.minor, rhs.patch]
+    
+    if lhsComparators != rhsComparators {
+        return lhsComparators.lexicographicallyPrecedes(rhsComparators)
+    }
+    
+    guard lhs.prereleaseIdentifiers.count > 0 else {
+        return false // Non-prerelease lhs >= potentially prerelease rhs
+    }
+    
+    guard rhs.prereleaseIdentifiers.count > 0 else {
+        return true // Prerelease lhs < non-prerelease rhs 
+    }
+    
+    for (lhsPrereleaseIdentifier, rhsPrereleaseIdentifier) in zip(lhs.prereleaseIdentifiers, rhs.prereleaseIdentifiers) {
+        if lhsPrereleaseIdentifier == rhsPrereleaseIdentifier {
+            continue
+        }
+        
+        let typedLhsIdentifier: Any = Int(lhsPrereleaseIdentifier) ?? lhsPrereleaseIdentifier
+        let typedRhsIdentifier: Any = Int(rhsPrereleaseIdentifier) ?? rhsPrereleaseIdentifier
+        
+        switch (typedLhsIdentifier, typedRhsIdentifier) {
+            case let (int1 as Int, int2 as Int): return int1 < int2
+            case let (string1 as String, string2 as String): return string1 < string2
+            case (is Int, is String): return true // Int prereleases < String prereleases
+            case (is String, is Int): return false
+        default:
+            return false
+        }
+    }
+    
+    return lhs.prereleaseIdentifiers.count < rhs.prereleaseIdentifiers.count
+}
+
+// MARK: BidirectionalIndexType
+
+// FIXME: do we want to keep these APIs now that Version does not conform to
+// BidirectionalCollection?
+extension Version {
+    public func successor() -> Version {
+        return Version(major, minor, patch + 1)
+    }
+
+    public func predecessor() -> Version {
+        if patch == 0 {
+            if minor == 0 {
+                return Version(major - 1, Int.max, Int.max)
+            } else {
+                return Version(major, minor - 1, Int.max)
+            }
+        } else {
+            return Version(major, minor, patch - 1)
+        }
+    }
+}
+
+// MARK: CustomStringConvertible
+
+extension Version: CustomStringConvertible {
+    public var description: String {
+        var base = "\(major).\(minor).\(patch)"
+        if prereleaseIdentifiers.count > 0 {
+            base += "-" + prereleaseIdentifiers.joined(separator: ".")
+        }
+        if let buildMetadataIdentifier = buildMetadataIdentifier {
+            base += "+" + buildMetadataIdentifier
+        }
+        return base
+    }
+}
