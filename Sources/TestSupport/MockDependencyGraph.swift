923700fe29f77579f090955187b0d4eec17d5155
Ankit Aggarwal <ankit_aggarwal@apple.com>
2017-02-09 18:27:12 +0530

    [TestSupport] Fix copyright

--- Sources/TestSupport/MockDependencyGraph.swift
+++ Sources/TestSupport/MockDependencyGraph.swift
@@ -1,10 +1,12 @@
-//
-//  MockGitGraph.swift
-//  SwiftPM
-//
-//  Created by Ankit Aggarwal on 16/12/16.
-//
-//
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
 
 import XCTest
 
