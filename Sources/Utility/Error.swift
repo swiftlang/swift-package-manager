e7ff349d4f2912afe9bbb9bebc2923316fe6ec1c
Ankit Aggarwal <ankit_aggarwal@apple.com>
2017-02-17 23:26:00 +0530

    [Utility] Remove dead code

--- Sources/Utility/Error.swift
+++ Sources/Utility/Error.swift
@@ -9,18 +9,12 @@
 */
 
 public enum Error: Swift.Error {
-    case obsoleteGitVersion
-    case unknownGitError
     case invalidPlatformPath
 }
 
 extension Error: CustomStringConvertible {
     public var description: String {
         switch self {
-        case .obsoleteGitVersion:
-            return "Git 2.0 or higher is required. Please update git and retry"
-        case .unknownGitError:
-            return "Failed to invoke git command. Please try updating git"
         case .invalidPlatformPath: return "Invalid platform path"
         }
     }
