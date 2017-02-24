ca882e52eb9b5d42209c911d048b12a89817a345
Ankit Aggarwal <ankit_aggarwal@apple.com>
2017-02-24 15:36:55 +0530

    Set development boolean to false in versioning

--- Sources/Utility/Versioning.swift
+++ Sources/Utility/Versioning.swift
@@ -81,7 +81,7 @@ public struct Versioning {
     /// The current version of the package manager.
     public static let currentVersion = SwiftVersion(
         version: (3, 1, 0),
-        isDevelopment: true,
+        isDevelopment: false,
         buildIdentifier: getBuildIdentifier())
 
     /// The list of version specific "keys" to search when attempting to load
