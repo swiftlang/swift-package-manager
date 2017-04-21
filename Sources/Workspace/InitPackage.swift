b05821b674bcc0f0d21280711f3f0a81be4f9d13
Ankit Aggarwal <ankit_aggarwal@apple.com>
2017-04-22 02:27:25 +0530

    [Workspace] Fix package init
    workaround for <rdar://problem/31725325> Swift compiler does not infer XCTest related types in -swift-version 4

--- Sources/Workspace/InitPackage.swift
+++ Sources/Workspace/InitPackage.swift
@@ -100,7 +100,7 @@ public final class InitPackage {
                 stream <<< "            dependencies: []),\n"
                 stream <<< "        .testTarget(\n"
                 stream <<< "            name: \"\(pkgname)Tests\",\n"
-                stream <<< "            dependencies: []),\n"
+                stream <<< "            dependencies: [\"\(pkgname)\"]),\n"
                 stream <<< "    ]\n"
             } else if packageType == .executable {
                 stream <<< "    targets: [\n"
@@ -248,7 +248,8 @@ public final class InitPackage {
             stream <<< "    }\n"
             stream <<< "\n"
             stream <<< "\n"
-            stream <<< "    static var allTests = [\n"
+            // FIXME: <rdar://problem/31725325> Swift compiler does not infer XCTest related types in -swift-version 4
+            stream <<< "    static var allTests: [(String, (\(moduleName)Tests) -> () -> Void)] = [\n"
             stream <<< "        (\"testExample\", testExample),\n"
             stream <<< "    ]\n"
             stream <<< "}\n"
