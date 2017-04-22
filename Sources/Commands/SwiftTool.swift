778bf8e8c88ce3ab8ff5d7a3fc65d4cb8af15dcb
Ankit Aggarwal <ankit_aggarwal@apple.com>
2017-04-22 13:48:03 +0530

    [PackageLoading] Allow passing -sdk to compiler when parsing manifest
    Also separate out manifest resource provider from user toolchain.

--- Sources/Commands/SwiftTool.swift
+++ Sources/Commands/SwiftTool.swift
@@ -365,7 +365,7 @@ public class SwiftTool<Options: ToolOptions> {
     private lazy var _manifestLoader: Result<ManifestLoader, AnyError> = {
         return Result(anyError: {
             try ManifestLoader(
-                resources: self.getToolchain(),
+                resources: self.getToolchain().manifestResources,
                 isManifestSandboxEnabled: !self.options.shouldDisableSandbox
             )
         })
