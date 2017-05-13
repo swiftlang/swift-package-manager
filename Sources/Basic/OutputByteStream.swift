2cd520422939c3db351fa87d99cf17b4c06943bc
Ankit Aggarwal <ankit_aggarwal@apple.com>
2017-05-14 00:22:18 +0530

    [Basic] Remove same type constraints
    -- <rdar://problem/31777022> Redundant same-type constraints in swiftpm

--- Sources/Basic/OutputByteStream.swift
+++ Sources/Basic/OutputByteStream.swift
@@ -105,8 +105,7 @@ public class OutputByteStream: TextOutputStream {
     public final func write<C: Collection>(collection bytes: C) where
         C.IndexDistance == Int,
         C.Iterator.Element == UInt8,
-        C.SubSequence: Collection,
-        C.SubSequence.Iterator.Element == UInt8 {
+        C.SubSequence: Collection {
         // This is based on LLVM's raw_ostream.
         let availableBufferSize = self.availableBufferSize
 
@@ -279,8 +278,7 @@ public func <<< (stream: OutputByteStream, value: ArraySlice<UInt8>) -> OutputBy
 public func <<< <C: Collection>(stream: OutputByteStream, value: C) -> OutputByteStream where
     C.Iterator.Element == UInt8,
     C.IndexDistance == Int,
-    C.SubSequence: Collection,
-    C.SubSequence.Iterator.Element == UInt8 {
+    C.SubSequence: Collection {
     stream.write(collection: value)
     return stream
 }
