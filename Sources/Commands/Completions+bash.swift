bcd5db0bb2a66c841c406f2a04def1d3f4cc81d5
Bouke Haarsma <bouke@haarsma.eu>
2017-07-19 21:46:09 +0200

    Remove hard-coded swift compiler flags

--- Sources/Commands/Completions+bash.swift
+++ Sources/Commands/Completions+bash.swift
@@ -1,9 +1,9 @@
 /*
  This source file is part of the Swift.org open source project
- 
+
  Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
- 
+
  See http://swift.org/LICENSE.txt for license information
  See http://swift.org/CONTRIBUTORS.txt for Swift project authors
 */
@@ -17,19 +17,19 @@ import Utility
 /// - Parameter stream: output stream to write the script to.
 func bash_template(on stream: OutputByteStream) {
     stream <<< "#!/bin/bash\n"
-    
+
     stream <<< "_swift() \n"
     stream <<< "{\n"
     stream <<< "    declare -a cur prev\n"
-    
+
     // Setup `cur` and `prev` variables, these will be used in the various
     // functions this script will contain. For example if the user requests
     // completions for `swift pack⇥`, `$cur` is `pack` and `$prev` is `swift`.
     stream <<< "    cur=\"${COMP_WORDS[COMP_CWORD]}\"\n"
     stream <<< "    prev=\"${COMP_WORDS[COMP_CWORD-1]}\"\n"
-    
+
     stream <<< "    COMPREPLY=()\n"
-    
+
     // If we're on the second completion word: `swift #`, then we'll complete
     // the names of the tools and compiler flags.
     stream <<< "    if [[ $COMP_CWORD == 1 ]]; then\n"
@@ -37,11 +37,11 @@ func bash_template(on stream: OutputByteStream) {
     stream <<< "        _swift_compiler\n"
     stream <<< "        return\n"
     stream <<< "    fi\n"
-    
+
     // For subsequent words; we'll look at the second word.  In all other
     // cases, try to complete compiler flags.
     stream <<< "    case ${COMP_WORDS[1]} in\n"
-    
+
     // If it is a tool name, forward completion to the specific tool's completion.
     stream <<< "        (build)\n"
     stream <<< "            _swift_build 2\n"
@@ -55,51 +55,31 @@ func bash_template(on stream: OutputByteStream) {
     stream <<< "        (test)\n"
     stream <<< "            _swift_test 2\n"
     stream <<< "            ;;\n"
-    
+
     // Otherwise; forward completion to the compiler's completion.
     stream <<< "        (*)\n"
     stream <<< "            _swift_compiler\n"
     stream <<< "            ;;\n"
     stream <<< "    esac\n"
     stream <<< "}\n"
-    
+
     SwiftBuildTool(args: []).parser.generateCompletionScript(for: .bash, on: stream)
     SwiftRunTool(args: []).parser.generateCompletionScript(for: .bash, on: stream)
     SwiftPackageTool(args: []).parser.generateCompletionScript(for: .bash, on: stream)
     SwiftTestTool(args: []).parser.generateCompletionScript(for: .bash, on: stream)
-    
-    // Hard-coded compiler flags of Swift 4.0 (beta 2).
-    stream <<< "_swift_compiler()\n"
-    stream <<< "{\n"
-    stream <<< "    case $prev in\n"
-    stream <<< "        (-assert-config)\n"
-    stream <<< "            COMPREPLY=( $(compgen -W \"Debug Release Unchecked DisableReplacement\" -- $cur) )\n"
-    stream <<< "            return\n"
-    stream <<< "            ;;\n"
-    stream <<< "        (-D|-framework|-j|-l|-module-link-name|-module-name|-num-threads|-sdk|-target-cpu|-target|-use-ld)\n"
-    stream <<< "            return\n"
-    stream <<< "            ;;\n"
-    stream <<< "        (-F|-index-store-path|-I|-L|-module-cache-path)\n"
-    stream <<< "            _filedir\n"
-    stream <<< "            ;;\n"
-    stream <<< "    esac\n"
-    stream <<< "    local args\n"
-    stream <<< "    args=\"-assert-config -continue-building-after-errors -D -framework\\\n"
-    stream <<< "         -F -gdwarf-types -gline-tables-only \\\n"
-    stream <<< "         -gnone -g -help -index-store-path -I -j -L -l -module-cache-path \\\n"
-    stream <<< "         -module-link-name -module-name -nostdimport -num-threads -Onone \\\n"
-    stream <<< "         -Ounchecked -O -sdk -static-executable -static-stdlib -suppress-warnings \\\n"
-    stream <<< "         -target-cpu \\\n"
-    stream <<< "         -target -use-ld -version -v -warn-swift3-objc-inference-complete \\\n"
-    stream <<< "         -warn-swift3-objc-inference-minimal\\\n"
-    stream <<< "         -warnings-as-errors -Xcc -Xlinker\"\n"
-    stream <<< "    COMPREPLY+=( $(compgen -W \"$args\" -- $cur))\n"
-    stream <<< "    _filedir\n"
-    stream <<< "}\n"
-    
+
+    // Figure out how to forward to swift compiler's bash completion.
+    stream <<< """
+               _swift_compiler()
+               {
+                   return 0
+               }
+
+
+               """
+
     // Link the `_swift` function to the `swift` command.
     stream <<< "complete -F _swift swift\n"
-    
+
     stream.flush()
 }
-
--- Sources/Commands/Completions+zsh.swift
+++ Sources/Commands/Completions+zsh.swift
@@ -1,9 +1,9 @@
 /*
  This source file is part of the Swift.org open source project
- 
+
  Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
- 
+
  See http://swift.org/LICENSE.txt for license information
  See http://swift.org/CONTRIBUTORS.txt for Swift project authors
 */
@@ -21,16 +21,6 @@ func zsh_template(on stream: OutputByteStream) {
     stream <<< "typeset -A opt_args\n"
     stream <<< "\n"
     stream <<< "_swift() {\n"
-    stream <<< "    declare -a shared_options\n"
-    stream <<< "    shared_options=(\n"
-    stream <<< "        '(-C --chdir)'{-C,--chdir}\"[Change working directory before any other operation]: :_files\"\n"
-    stream <<< "        \"--color[Specify color mode (auto|always|never)]: :{_values \"mode\" auto always never}\"\n"
-    stream <<< "        '(-v --verbose)'{-v,--verbose}'[Increase verbosity of informational output]'\n"
-    stream <<< "        \"-Xcc[Pass flag through to all C compiler invocations]: : \"\n"
-    stream <<< "        \"-Xlinker[Pass flag through to all linker invocations]: : \"\n"
-    stream <<< "        \"-Xswiftc[Pass flag through to all Swift compiler invocations]: : \"\n"
-    stream <<< "    )\n"
-    stream <<< "\n"
     stream <<< "    _arguments -C \\\n"
     stream <<< "        '(- :)--help[prints the synopsis and a list of the most commonly used commands]: :->arg' \\\n"
     stream <<< "        '(-): :->command' \\\n"
@@ -71,57 +61,22 @@ func zsh_template(on stream: OutputByteStream) {
     stream <<< "    esac\n"
     stream <<< "}\n"
     stream <<< "\n"
-    
+
     SwiftBuildTool(args: []).parser.generateCompletionScript(for: .zsh, on: stream)
     SwiftRunTool(args: []).parser.generateCompletionScript(for: .zsh, on: stream)
     SwiftPackageTool(args: []).parser.generateCompletionScript(for: .zsh, on: stream)
     SwiftTestTool(args: []).parser.generateCompletionScript(for: .zsh, on: stream)
-    
-    // Hard-coded compiler flags of Swift 4.0 (beta 2).
-    stream <<< "_swift_compiler() {\n"
-    stream <<< "    declare -a build_options\n"
-    stream <<< "    build_options=(\n"
-    stream <<< "        '-assert-config[Specify the assert_configuration replacement.]: :{_values \"\" Debug Release Unchecked DisableReplacement}'\n"
-    stream <<< "        '-D[Marks a conditional compilation flag as true]: : '\n"
-    stream <<< "        '-framework[Specifies a framework which should be linked against]: : '\n"
-    stream <<< "        '-F[Add directory to framework search path]: :_files'\n"
-    stream <<< "        '-gdwarf-types[Emit full DWARF type info.]'\n"
-    stream <<< "        '-gline-tables-only[Emit minimal debug info for backtraces only]'\n"
-    stream <<< "        \"-gnone[Don't emit debug info]\"\n"
-    stream <<< "        '-g[Emit debug info. This is the preferred setting for debugging with LLDB.]'\n"
-    stream <<< "        '-help[Display available options]'\n"
-    stream <<< "        '-index-store-path[Store indexing data to <path>]: :_files'\n"
-    stream <<< "        '-I[Add directory to the import search path]: :_files'\n"
-    stream <<< "        '-j[Number of commands to execute in parallel]: : '\n"
-    stream <<< "        '-L[Add directory to library link search path]: :_files'\n"
-    stream <<< "        '-l-[Specifies a library which should be linked against]: : '\n"
-    stream <<< "        '-module-cache-path[Specifies the Clang module cache path]: :_files'\n"
-    stream <<< "        '-module-link-name[Library to link against when using this module]: : '\n"
-    stream <<< "        '-module-name[Name of the module to build]: : '\n"
-    stream <<< "        \"-nostdimport[Don't search the standard library import path for modules]\"\n"
-    stream <<< "        '-num-threads[Enable multi-threading and specify number of threads]: : '\n"
-    stream <<< "        '-Onone[Compile without any optimization]'\n"
-    stream <<< "        '-Ounchecked[Compile with optimizations and remove runtime safety checks]'\n"
-    stream <<< "        '-O[Compile with optimizations]'\n"
-    stream <<< "        '-sdk[Compile against <sdk>]: : '\n"
-    stream <<< "        '-static-stdlib[Statically link the Swift standard library]'\n"
-    stream <<< "        '-suppress-warnings[Suppress all warnings]'\n"
-    stream <<< "        '-target-cpu[Generate code for a particular CPU variant]: : '\n"
-    stream <<< "        '-target[Generate code for the given target]: : '\n"
-    stream <<< "        '-use-ld=-[Specifies the linker to be used]'\n"
-    stream <<< "        '-version[Print version information and exit]'\n"
-    stream <<< "        '-v[Show commands to run and use verbose output]'\n"
-    stream <<< "        '-warnings-as-errors[Treat warnings as errors]'\n"
-    stream <<< "        '-Xcc[Pass <arg> to the C/C++/Objective-C compiler]: : '\n"
-    stream <<< "        '-Xlinker[Specifies an option which should be passed to the linker]: : '\n"
-    stream <<< "        '*:inputs:_files'\n"
-    stream <<< "    )\n"
-    stream <<< "    _arguments $build_options\n"
-    stream <<< "}\n"
-    stream <<< "\n"
-    
+
+    // Figure out how to forward to swift compiler's bash completion.
+    stream <<< """
+               _swift_compiler() {
+               }
+
+
+               """
+
     // Run the `_swift` function to register the completions.
     stream <<< "_swift\n"
-    
+
     stream.flush()
 }
