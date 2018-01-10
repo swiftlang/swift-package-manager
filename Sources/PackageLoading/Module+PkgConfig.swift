1d2b094652e7e40ba0127ef420dd55835e355a24
Brett R. Toomey <brettcwx@gmail.com>
2018-01-10 16:46:49 +0100

    Update Module+PkgConfig.swift

--- Sources/PackageLoading/Module+PkgConfig.swift
+++ Sources/PackageLoading/Module+PkgConfig.swift
@@ -153,7 +153,7 @@ func whitelist(pcFile: String, flags: (cFlags: [String], libs: [String])) throws
         }
         return filtered
     }
-    let filtered = filter(flags: flags.cFlags, filters: ["-I", "-F"]) +
+    let filtered = filter(flags: flags.cFlags, filters: ["-I", "-F", "-rdynamic"]) +
         filter(flags: flags.libs, filters: ["-L", "-l", "-F", "-framework"])
     guard filtered.isEmpty else {
         throw PkgConfigError.nonWhitelistedFlags("Non whitelisted flags found: \(filtered) in pc file \(pcFile)")
