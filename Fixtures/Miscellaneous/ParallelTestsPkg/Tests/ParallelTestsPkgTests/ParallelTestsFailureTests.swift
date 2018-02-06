229c8443e8780ef9c6eee09df2e58215e204fa1b
Ankit Aggarwal <ankit_aggarwal@apple.com>
2018-02-06 11:30:11 -0800

    [Commands] Add LinuxMainGenerator
    This adds a new option "--generate-linuxmain" to 'swift test' to
    generate test entries that can be used for XCTest on Linux.  Since only
    macOS supports automatic test discovery, this option will only work on
    macOS. Using it on Linux will result in a "null" generation.
    
    <rdar://problem/37259907> Use macOS's test disovery to generate LinuxMain.swift

--- Fixtures/Miscellaneous/ParallelTestsPkg/Tests/ParallelTestsPkgTests/ParallelTestsFailureTests.swift
+++ Fixtures/Miscellaneous/ParallelTestsPkg/Tests/ParallelTestsPkgTests/ParallelTestsFailureTests.swift
@@ -6,10 +6,4 @@ class ParallelTestsFailureTests: XCTestCase {
     func testSureFailure() {
         XCTFail("Giving up is the only sure way to fail.")
     }
-    
-    static var allTests : [(String, (ParallelTestsFailureTests) -> () throws -> Void)] {
-        return [
-            ("testSureFailure", testSureFailure),
-        ]
-    }
 }
