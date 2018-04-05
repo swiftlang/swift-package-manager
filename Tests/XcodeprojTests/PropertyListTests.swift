c9ff2b90f744998566080c890937eaedbf2aaf3f
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2018-04-06 00:16:38 +0200

    Refactor: Merge Plist and PropertyList into PropertyList

---
+++ Tests/XcodeprojTests/PropertyListTests.swift
@@ -0,0 +1,24 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
+import XCTest
+@testable import Xcodeproj
+
+class PropertyListTests: XCTestCase {
+    func testBasics() {
+        XCTAssertEqual("\"hello \\\" world\"", PropertyList.string("hello \" world").serialize())
+        XCTAssertEqual("(\n   \"hello world\",\n   \"cool\"\n)", PropertyList.array([.string("hello world"), .string("cool")]).serialize())
+        XCTAssertEqual("{\n   polo = (\n      \"hello \\\" world\",\n      \"cool\"\n   );\n   user = \"cool\";\n}", PropertyList.dictionary(["user": .string("cool"), "polo": PropertyList.array([.string("hello \" world"), .string("cool")])]).serialize())
+    }
+
+    static var allTests = [
+        ("testBasics", testBasics),
+    ]
+}
