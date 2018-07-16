161538748c0ce9b71ae300752e8cc015a7876ac1
Ankit Aggarwal <ankit_aggarwal@apple.com>
2018-07-16 16:16:56 -0700

    [Basic] Add API to return FileInfo
    <rdar://problem/39867442> Modify the extension on stat to use a FileInfo struct
    
    We still need to do a bit of cleanup and get rid of most things in
    PathShims.swift

--- Sources/POSIX/stat.swift
+++ Sources/POSIX/stat.swift
@@ -10,32 +10,71 @@
 
 import SPMLibc
 
-/// Extensions to the SPMLibc `stat` structure to interpret the contents in more readable ways.
-extension SPMLibc.stat {
+/// File system information for a particular file.
+public struct FileInfo: Equatable, Codable {
 
-     /// File system entity kind.
-     public enum Kind {
-         case file, directory, symlink, fifo, blockdev, chardev, socket, unknown
+    /// File timestamp wrapper.
+    public struct FileTimestamp: Equatable, Codable {
+        public let seconds: UInt64
+        public let nanoseconds: UInt64
+    }
 
-         fileprivate init(mode: mode_t) {
-             switch mode {
-                 case S_IFREG:  self = .file
-                 case S_IFDIR:  self = .directory
-                 case S_IFLNK:  self = .symlink
-                 case S_IFBLK:  self = .blockdev
-                 case S_IFCHR:  self = .chardev
-                 case S_IFSOCK: self = .socket
-             default:
-                 self = .unknown
-             }
-         }
-     }
+    /// File system entity kind.
+    public enum Kind {
+        case file, directory, symlink, blockdev, chardev, socket, unknown
 
-     /// Kind of file system entity.
-     public var kind: Kind {
-         return Kind(mode: st_mode & S_IFMT)
-     }
- }
+        fileprivate init(mode: mode_t) {
+            switch mode {
+                case S_IFREG:  self = .file
+                case S_IFDIR:  self = .directory
+                case S_IFLNK:  self = .symlink
+                case S_IFBLK:  self = .blockdev
+                case S_IFCHR:  self = .chardev
+                case S_IFSOCK: self = .socket
+            default:
+                self = .unknown
+            }
+        }
+    }
+
+    /// The device number.
+    public let device: UInt64
+
+    /// The inode number.
+    public let inode: UInt64
+
+    /// The mode flags of the file.
+    public let mode: UInt64
+
+    /// The size of the file.
+    public let size: UInt64
+
+    /// The modification time of the file.
+    public let modTime: FileTimestamp
+
+    /// Kind of file system entity.
+    public var kind: Kind {
+        return Kind(mode: mode_t(mode) & S_IFMT)
+    }
+
+    public init(_ buf: SPMLibc.stat) {
+        self.device = UInt64(buf.st_dev)
+        self.inode = UInt64(buf.st_ino)
+        self.mode = UInt64(buf.st_mode)
+        self.size = UInt64(buf.st_size)
+
+      #if os(macOS)
+        let seconds = buf.st_mtimespec.tv_sec
+        let nanoseconds = buf.st_mtimespec.tv_nsec
+      #else
+        let seconds = buf.st_mtim.tv_sec
+        let nanoseconds = buf.st_mtim.tv_nsec
+      #endif
+
+        self.modTime = FileTimestamp(
+            seconds: UInt64(seconds), nanoseconds: UInt64(nanoseconds))
+    }
+}
 
 public func stat(_ path: String) throws -> SPMLibc.stat {
     var sbuf = SPMLibc.stat()
