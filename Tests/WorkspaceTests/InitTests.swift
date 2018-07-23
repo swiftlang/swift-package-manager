19fb0cd71311bb6cb0373c8755721383ebd1c496
jhonnybillm <jhonnybillm@gmail.com>
2018-07-23 18:49:09 -0400

    Add OS check to filter assertion. Little refactor

--- Tests/WorkspaceTests/InitTests.swift
+++ Tests/WorkspaceTests/InitTests.swift
@@ -182,20 +182,19 @@ class InitTests: XCTestCase {
         let tempDir = try TemporaryDirectory(removeTreeOnDeinit: true)
         XCTAssertTrue(localFileSystem.isDirectory(tempDir.path))
         
-        // Create a directory with non c99name.
         let packageRoot = tempDir.path.appending(component: "Foo")
         try localFileSystem.createDirectory(packageRoot)
         XCTAssertTrue(localFileSystem.isDirectory(packageRoot))
         
-        // Create the package
+        // Create package with non c99name.
         let initPackage = try InitPackage(name: "package-name", destinationPath: packageRoot, packageType: InitPackage.PackageType.executable)
-        initPackage.progressReporter = { message in
-        }
         try initPackage.writePackageStructure()
         
-        // Try testing it.
-        XCTAssertSwiftTest(packageRoot)
+        #if os(macOS)
+          XCTAssertSwiftTest(packageRoot)
+        #else
+          XCTAssertBuilds(packageRoot)
+        #endif
         
-//        XCTAssertFileExists(packageRoot.appending(components: ".build", Destination.host.target, "debug", "some_package.swiftmodule"))
     }
 }
