3bd5b62dd0665ed97093089a9014d28ff3d653be
Ankit Aggarwal <ankit_aggarwal@apple.com>
2018-09-30 17:36:34 -0700

    Remove PackageDescription4 from libSwiftPM
    The runtimes are separated from libSwiftPM so there is no need to
    include it in the libSwiftPM product.

--- Package.swift
+++ Package.swift
@@ -30,7 +30,6 @@ let package = Package(
                 "Utility",
                 "SourceControl",
                 "SPMLLBuild",
-                "PackageDescription4",
                 "PackageModel",
                 "PackageLoading",
                 "PackageGraph",
