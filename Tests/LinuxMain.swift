3853cac96012a9b44678ae50a0970bf8db5767aa
Ankit Aggarwal <ankit_aggarwal@apple.com>
2018-11-11 13:40:43 -0800

    [Commands] Fix sorting in LinuxMain.swift generation

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,34 +1,34 @@
 import XCTest
 
-import UtilityTests
 import BasicTests
 import BuildTests
 import CommandsTests
 import FunctionalTests
-import PackageGraphTests
 import POSIXTests
-import XcodeprojTests
-import SourceControlTests
-import WorkspaceTests
 import PackageDescription4Tests
+import PackageGraphTests
 import PackageLoadingTests
 import PackageModelTests
+import SourceControlTests
 import TestSupportTests
+import UtilityTests
+import WorkspaceTests
+import XcodeprojTests
 
 var tests = [XCTestCaseEntry]()
-tests += UtilityTests.__allTests()
 tests += BasicTests.__allTests()
 tests += BuildTests.__allTests()
 tests += CommandsTests.__allTests()
 tests += FunctionalTests.__allTests()
-tests += PackageGraphTests.__allTests()
 tests += POSIXTests.__allTests()
-tests += XcodeprojTests.__allTests()
-tests += SourceControlTests.__allTests()
-tests += WorkspaceTests.__allTests()
 tests += PackageDescription4Tests.__allTests()
+tests += PackageGraphTests.__allTests()
 tests += PackageLoadingTests.__allTests()
 tests += PackageModelTests.__allTests()
+tests += SourceControlTests.__allTests()
 tests += TestSupportTests.__allTests()
+tests += UtilityTests.__allTests()
+tests += WorkspaceTests.__allTests()
+tests += XcodeprojTests.__allTests()
 
 XCTMain(tests)
