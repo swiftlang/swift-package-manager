c509b7a34598ec23d991bcfbb4a3ade38ff32b24
Patrick Metcalfe <git@patrickmetcalfe.com>
2018-12-01 23:00:55 -0800

    Fix Note on FileSystem documentation comment

--- Sources/Basic/FileSystem.swift
+++ Sources/Basic/FileSystem.swift
@@ -95,7 +95,8 @@ public enum FileMode {
     case userUnWritable
     case userWritable
     case executable
-
+    
+    /// File mode as it would be passed to `chmod`
     public var cliArgument: String {
         switch self {
         case .userUnWritable:
@@ -116,7 +117,7 @@ public enum FileMode {
 /// natural filesystem interface, while still allowing clients to transparently
 /// substitute a virtual file system or redirect file system operations.
 ///
-/// NOTE: All of these APIs are synchronous and can block.
+/// - NOTE: All of these APIs are synchronous and can block.
 public protocol FileSystem: class {
     /// Check whether the given path exists and is accessible.
     func exists(_ path: AbsolutePath, followSymlink: Bool) -> Bool
