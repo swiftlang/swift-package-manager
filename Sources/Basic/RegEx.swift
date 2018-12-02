1f6c7a9cf6fcbe31bb3ede337be5295845a9634f
Patrick Metcalfe <git@patrickmetcalfe.com>
2018-12-01 22:17:47 -0800

    Expound on RegEx documentation

--- Sources/Basic/RegEx.swift
+++ Sources/Basic/RegEx.swift
@@ -10,17 +10,37 @@
 
 import Foundation
 
+/// A helpful wrapper around NSRegularExpression
+/// - SeeAlso: NSRegularExpression
 public struct RegEx {
     private let regex: NSRegularExpression
     public typealias Options = NSRegularExpression.Options
     
+    /// Creates a new Regex using `pattern`
+    ///
+    /// - Parameters:
+    ///     - pattern: A valid Regular Expression pattern
+    ///     - options: NSRegularExpression.Options on how the RegEx should be processed.
+    /// - Note: Deliminators must be double escaped. Once for Swift and once for RegEx
+    ///   Example, to math a negative integer: `RegEx(pattern: "-\d")` -> `RegEx(pattern: "-\\d")`
+    /// - SeeAlso: NSRegularExpression
+    /// - Throws: An Error if `pattern` is an invalid Regular Expression
     public init(pattern: String, options: Options = []) throws {
         self.regex = try NSRegularExpression(pattern: pattern, options: options)
     }
     
-    /// Returns match groups for each match. E.g.:
+    /// Returns match groups for every match of the Regex.
     ///
-    /// RegEx(pattern: "([a-z]+)([0-9]+)").matchGroups(in: "foo1 bar2 baz3") -> [["foo", "1"], ["bar", "2"], ["baz", "3"]]
+    /// For every match in the string, it constructs the collection
+    /// of groups matched.
+    /// ```
+    /// RegEx(pattern: "([a-z]+)([0-9]+)").matchGroups(in: "foo1 bar2 baz3")
+    /// ```
+    /// Returns `[["foo", "1"], ["bar", "2"], ["baz", "3"]]`
+    ///
+    /// - Parameters:
+    ///     - in: A string to check for matches to the whole Regex
+    /// - Returns: A collection where each elements is the collection of matched groups.
     public func matchGroups(in string: String) -> [[String]] {
         let nsString = NSString(string: string)
         
