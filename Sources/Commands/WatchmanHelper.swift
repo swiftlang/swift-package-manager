f6fdbd74155493ef950ae280417b2c9bf72b72f9
Matthew Seaman <mattsdeveloper@gmail.com>
2019-04-04 18:02:31 -0400

    Fix typo in comment

--- Sources/Commands/WatchmanHelper.swift
+++ Sources/Commands/WatchmanHelper.swift
@@ -77,7 +77,7 @@ final class WatchmanHelper {
     }
 
     private func run(_ scriptPath: AbsolutePath) throws {
-        // Construct the arugments.
+        // Construct the arguments.
         var args = [String]()
         args += ["--settle", "2"]
         args += ["-p", "Package.swift", "Package.resolved"]
