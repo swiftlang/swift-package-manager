d241925d8650ab84c4dfae67c62fe8907def5aae
Boris Buegling <bbuegling@apple.com>
2019-06-11 14:05:07 -0700

    Init type "manifest" was missing from help

--- Sources/Commands/SwiftPackageTool.swift
+++ Sources/Commands/SwiftPackageTool.swift
@@ -363,7 +363,7 @@ public class SwiftPackageTool: SwiftTool<PackageToolOptions> {
         binder.bind(
             option: initPackageParser.add(
                 option: "--type", kind: InitPackage.PackageType.self,
-                usage: "empty|library|executable|system-module"),
+                usage: "empty|library|executable|system-module|manifest"),
             to: { $0.initMode = $1 })
 
         binder.bind(
