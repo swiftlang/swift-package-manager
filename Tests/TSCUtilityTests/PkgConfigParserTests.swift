5467b531f372f3cedadc0dc5a5a4596c97e3d9d0
aemino <aeminodev@gmail.com>
2019-06-13 19:30:55 -0700

    Add very simple tests to PkgConfigParserTests

--- Tests/TSCUtilityTests/PkgConfigParserTests.swift
+++ Tests/TSCUtilityTests/PkgConfigParserTests.swift
@@ -26,9 +26,11 @@ final class PkgConfigParserTests: XCTestCase {
                 "gtk_binary_version": "3.0.0",
                 "exec_prefix": "/usr/local/Cellar/gtk+3/3.18.9",
                 "targets": "quartz",
-                "pcfiledir": parser.pcFile.parentDirectory.pathString
+                "pcfiledir": parser.pcFile.parentDirectory.pathString,
+                "pc_sysrootdir": Process.env["PKG_CONFIG_PATH"] ?? "/"
             ])
             XCTAssertEqual(parser.dependencies, ["gdk-3.0", "atk", "cairo", "cairo-gobject", "gdk-pixbuf-2.0", "gio-2.0"])
+            XCTAssertEqual(parser.privateDependencies, ["atk", "epoxy", "gio-unix-2.0"])
             XCTAssertEqual(parser.cFlags, ["-I/usr/local/Cellar/gtk+3/3.18.9/include/gtk-3.0"])
             XCTAssertEqual(parser.libs, ["-L/usr/local/Cellar/gtk+3/3.18.9/lib", "-lgtk-3"])
         }
