bbd171d444ef9e688dd842f58e8465675af1d1e3
Saleem Abdulrasool <compnerd@compnerd.org>
2019-08-11 11:32:47 -0700

    Windows: `BOOL` is now WindowsBool
    `BOOL` is now properly bridged into a boolean as `WindowsBool`.
    Unfortunately, as there is no current CI for s-p-m on Windows, this was
    not caught.  Update s-p-m so that we can enable CI.

--- Sources/Basic/Lock.swift
+++ Sources/Basic/Lock.swift
@@ -79,7 +79,8 @@ public final class FileLock {
         overlapped.Offset = 0
         overlapped.OffsetHigh = 0
         overlapped.hEvent = nil
-        if FALSE == LockFileEx(handle, DWORD(LOCKFILE_EXCLUSIVE_LOCK), 0, DWORD(INT_MAX), DWORD(INT_MAX), &overlapped) {
+        if !LockFileEx(handle, DWORD(LOCKFILE_EXCLUSIVE_LOCK), 0,
+                       DWORD(INT_MAX), DWORD(INT_MAX), &overlapped) {
             throw ProcessLockError.unableToAquireLock(errno: Int32(GetLastError()))
         }
       #else
