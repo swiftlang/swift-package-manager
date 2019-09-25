9abcc2260438177cecd7cf5185b144d13e74122b
Ankit Aggarwal <ankit_aggarwal@apple.com>
2019-09-25 13:39:54 -0700

    [Package.swift] Require llbuild using a version-based dependency

--- Package.swift
+++ Package.swift
@@ -246,7 +246,7 @@ import class Foundation.ProcessInfo
 if ProcessInfo.processInfo.environment["SWIFTPM_BOOTSTRAP"] == nil {
     if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
         package.dependencies += [
-            .package(url: "https://github.com/apple/swift-llbuild.git", .branch("master")),
+            .package(url: "https://github.com/apple/swift-llbuild.git", "0.2.0"..<"0.3.0"),
         ]
     } else {
         // In Swift CI, use a local path to llbuild to interoperate with tools
