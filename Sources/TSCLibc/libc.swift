2a1619d8072fd62f57c23607e24665f091396d96
Saleem Abdulrasool <compnerd@compnerd.org>
2019-12-04 20:52:26 -0800

    TSCLibc: updates for building TSCUtility on Windows
    This adds stubs for some of the functionality needed on Windows.  The
    implementation for mkstemps is needed for actually getting s-p-m to run
    on Windows.

--- Sources/TSCLibc/libc.swift
+++ Sources/TSCLibc/libc.swift
@@ -23,8 +23,41 @@
 // char *realpath(const char *path, char *resolved_path);
 public func realpath(
     _ path: String,
-    _ resolvedPath: UnsafeMiutableBufferPointer<CChar>?
-) -> UnsafePointer<CChar>? {
+    _ resolvedPath: UnsafeMutablePointer<CChar>?
+) -> UnsafeMutablePointer<CChar>? {
   fatalError("realpath is unimplemented")
+}
+
+// char *mkdtemp(char *template);
+public func mkdtemp(
+    _ template: UnsafeMutablePointer<CChar>?
+) -> UnsafeMutablePointer<CChar>? {
+  fatalError("mkdtemp is unimplemented")
+}
+
+// int mkstemps(char *template, int suffixlen);
+public func mkstemps(
+    _ template: UnsafeMutablePointer<CChar>?,
+    _ suffixlen: Int32
+) -> Int32 {
+  guard let template = template else { return -EINVAL }
+  return String(cString: template).withCString(encodedAs: UTF16.self) {
+    let capacity: Int = wcslen($0) + 1
+    return $0.withMemoryRebound(to: wchar_t.self, capacity: capacity) {
+      guard _wmktemp_s(UnsafeMutablePointer(mutating: $0), capacity) == 0 else {
+        return -EINVAL
+      }
+
+      var fd: Int32 = -1
+      _wsopen_s(&fd, $0, _O_RDWR | _O_CREAT | _O_BINARY | _O_NOINHERIT,
+                _SH_DENYNO, _S_IREAD | _S_IWRITE)
+
+      String(decodingCString: $0, as: UTF16.self).utf8CString.withUnsafeBytes {
+        template.assign(from: $0.bindMemory(to: CChar.self).baseAddress!,
+                        count: $0.count)
+      }
+      return fd
+    }
+  }
 }
 #endif
