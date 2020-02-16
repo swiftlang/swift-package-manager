5c4a2a3da1acd543f0c5f2f87e974e48e88d966a
Ankit Aggarwal <ankit_aggarwal@apple.com>
2020-02-15 20:35:38 -0800

    Dummy PR to workaround missing preset in XCTest repo

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,1 +1,28 @@
-fatalError("Use `swift test --enable-test-discovery` to run tests")
+import XCTest
+
+import BuildTests
+import CommandsTests
+import FunctionalTests
+import PackageDescription4Tests
+import PackageGraphTests
+import PackageLoadingTests
+import PackageModelTests
+import SourceControlTests
+import WorkspaceTests
+import XCBuildSupportTests
+import XcodeprojTests
+
+var tests = [XCTestCaseEntry]()
+tests += BuildTests.__allTests()
+tests += CommandsTests.__allTests()
+tests += FunctionalTests.__allTests()
+tests += PackageDescription4Tests.__allTests()
+tests += PackageGraphTests.__allTests()
+tests += PackageLoadingTests.__allTests()
+tests += PackageModelTests.__allTests()
+tests += SourceControlTests.__allTests()
+tests += WorkspaceTests.__allTests()
+tests += XCBuildSupportTests.__allTests()
+tests += XcodeprojTests.__allTests()
+
+XCTMain(tests)
