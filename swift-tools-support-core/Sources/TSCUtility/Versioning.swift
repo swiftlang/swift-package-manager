bea8bb3189dde8267c6595d3eceb8c5191029768
Ankit Aggarwal <ankit_aggarwal@apple.com>
2020-04-13 10:01:44 -0700

    [5.3] Set development = false in versioning

--- swift-tools-support-core/Sources/TSCUtility/Versioning.swift
+++ swift-tools-support-core/Sources/TSCUtility/Versioning.swift
@@ -75,7 +75,7 @@ public struct Versioning {
     /// The current version of the package manager.
     public static let currentVersion = SwiftVersion(
         version: (5, 3, 0),
-        isDevelopment: true,
+        isDevelopment: false,
         buildIdentifier: getBuildIdentifier())
 
     /// The list of version specific "keys" to search when attempting to load
