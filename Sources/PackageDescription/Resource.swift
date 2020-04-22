ddddb2837ade926c7562b728367aebebaa4df2e5
Ben Scholtysik <bscholtysik@apple.com>
2020-04-22 13:45:34 -0700

    Update source docs for Resource and Target

--- Sources/PackageDescription/Resource.swift
+++ Sources/PackageDescription/Resource.swift
@@ -8,12 +8,29 @@
  See http://swift.org/CONTRIBUTORS.txt for Swift project authors
 */
 
+/// A resource to bundle with the Swift package.
+///
+/// If a Swift package declares a Swift tools version of 5.3 or later, it can
+/// include resource files. Similar to source code, the Swift package manager
+/// scopes resources to a target. As a result, you need to put your resources
+/// into the subfolder for the target they belong to. For example, any resources
+/// for the `MyLibrary` target need to reside in `Sources/MyLibrary`. Use
+/// subdirectories to organize your resource files in a way that makes it easy
+/// to identify and manage them.
+///
+/// Per default, the Swift package manager handles common resources types for
+/// Apple platforms automatically. You don’t need to declare Interface Builder
+/// files, for example, XIB and storyboards, CoreData file types, and asset
+/// catalogs as resources in your package manifest. However, you must declare
+/// other file types as resources, for example, image files, explicitly using
+/// the `copy(_:)` or `process(_:localization:)`` rule. Alternatively, you can
+/// exclude resource files from a target by using `exclude(_:)``.
 public struct Resource: Encodable {
 
     /// Defines the explicit type of localization for resources.
     public enum Localization: String, Encodable {
 
-        /// The Package's default localization.
+        /// The package's default localization.
         case `default`
 
         /// Localization for base internationalization.
@@ -35,25 +52,32 @@ public struct Resource: Encodable {
         self.localization = localization
     }
 
-    /// Apply the platform-specific rule to the given path.
+    /// Apply the platform-specific rule to a resource at the given path.
     ///
-    /// Matching paths will be processed according to the platform for which this
-    /// target is being built. For example, image files might get optimized when
-    /// building for platforms that support such optimizations.
+    /// Use the process rule to process resources at the given path
+    /// according to the platform it builds the target for. For example, the
+    /// Swift package manager may optimize image files for platforms that
+    /// support such optimizations. If no optimization is available for a file
+    /// type, the Swift package manager copies the file.
     ///
-    /// By default, a file will be copied if there is no specialized processing
-    /// for its file type.
+    /// If the given path represents a directory, the Swift package manager
+    /// applies the process rule recursively to each file in the directory.
     ///
-    /// If path is a directory, the rule is applied recursively to each file in the
-    /// directory.
+    /// - Parameters:
+    ///     - path: The path for a resource.
+    ///     - localization: The explicit type for the resource.
     public static func process(_ path: String, localization: Localization? = nil) -> Resource {
         return Resource(rule: "process", path: path, localization: localization)
     }
 
-    /// Apply the copy rule to the given path.
+    /// Apply the copy rule to a resource at the given path.
     ///
-    /// Matching paths will be copied as-is and will be at the top-level
-    /// in the bundle. The structure is retained if path is a directory.
+    /// Use the copy rule to copy resources at the given path as is to the
+    /// top-level in the package’s resource bundle. If the path represents a
+    /// directory, Xcode preserves its structure.
+    ///
+    /// - Parameters:
+    ///     - path: The path for a resource.
     public static func copy(_ path: String) -> Resource {
         return Resource(rule: "copy", path: path, localization: nil)
     }
