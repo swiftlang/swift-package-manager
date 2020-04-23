6ad7d9a078c52c6cfd1a0030f010be0facf5d452
Anders Bertelrud <anders@apple.com>
2020-04-23 16:26:07 -0700

    [5.2] [SR-12667] Fix a SwiftPM 5.2 regression that causes non-source files under declared source directories to be treated as source files
    When the source scanning logic was reworked in preparation for support of resources, there was an unintentional change to the semantics of how files with unrecognized filename suffixes under declared source directories were treated.  This restores the previous semantics of only treating files with recognized source file suffixes as sources.
    
    (cherry picked from commit 83380d162bc6beacafb94ec78f4aee05f45650eb)

--- Tests/PackageLoadingTests/PackageBuilderTests.swift
+++ Tests/PackageLoadingTests/PackageBuilderTests.swift
@@ -1585,6 +1585,30 @@ class PackageBuilderTests: XCTestCase {
         }
     }
 
+    func testUnknownSourceFilesUnderDeclaredSourcesIgnoredInV5_2Manifest() throws {
+        // Files with unknown suffixes under declared sources are not considered valid sources in 5.2 manifest.
+        let fs = InMemoryFileSystem(emptyFiles:
+            "/Sources/lib/movie.mkv",
+            "/Sources/lib/lib.c",
+            "/Sources/lib/include/lib.h"
+        )
+
+        let manifest = Manifest.createManifest(
+            name: "pkg",
+            v: .v5_2,
+            targets: [
+                TargetDescription(name: "lib", dependencies: [], path: "./Sources/lib", sources: ["."]),
+            ]
+        )
+
+        PackageBuilderTester(manifest, in: fs) { package in
+            package.checkModule("lib") { module in
+                module.checkSources(root: "/Sources/lib", paths: "lib.c")
+                module.check(includeDir: "/Sources/lib/include")
+            }
+        }
+    }
+
     func testBuildSettings() {
         let fs = InMemoryFileSystem(emptyFiles:
             "/Sources/exe/main.swift",
