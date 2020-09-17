b753bd04d3730d2a6306026d6985ba2cb466760d
Mattt <mattt@me.com>
2020-09-17 01:38:46 -0700

    Extract PackageRequirement into separate file

--- Sources/PackageGraph/DependencyResolver.swift
+++ Sources/PackageGraph/DependencyResolver.swift
@@ -32,36 +32,6 @@ public enum DependencyResolverError: Error, Equatable, CustomStringConvertible {
     }
 }
 
-/// A requirement that a package must satisfy.
-public enum PackageRequirement: Hashable {
-
-    /// The requirement is specified by the version set.
-    case versionSet(VersionSetSpecifier)
-
-    /// The requirement is specified by the revision.
-    ///
-    /// The revision string (identifier) should be valid and present in the
-    /// container. Only one revision requirement per container is possible
-    /// i.e. two revision requirements for same container will lead to
-    /// unsatisfiable resolution. The revision requirement can either come
-    /// from initial set of constraints or from dependencies of a revision
-    /// requirement.
-    case revision(String)
-
-    /// Un-versioned requirement i.e. a version should not resolved.
-    case unversioned
-}
-
-extension PackageRequirement: CustomStringConvertible {
-    public var description: String {
-        switch self {
-        case .versionSet(let versionSet): return versionSet.description
-        case .revision(let revision): return revision
-        case .unversioned: return "unversioned"
-        }
-    }
-}
-
 /// A container of packages.
 ///
 /// This is the top-level unit of package resolution, i.e. the unit at which
