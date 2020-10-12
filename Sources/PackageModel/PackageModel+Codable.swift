35255eecc16b20903b17432409b6b5d7e360e9d0
Mattt <mattt@me.com>
2020-10-12 10:47:31 -0700

    Reorganize ProductType declaration
    Move ProductType Codable conformance closer to declaration

--- Sources/PackageModel/PackageModel+Codable.swift
+++ Sources/PackageModel/PackageModel+Codable.swift
@@ -11,42 +11,6 @@
 import Foundation
 import TSCBasic
 
-extension ProductType: Codable {
-    private enum CodingKeys: String, CodingKey {
-        case library, executable, test
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        switch self {
-        case let .library(a1):
-            var unkeyedContainer = container.nestedUnkeyedContainer(forKey: .library)
-            try unkeyedContainer.encode(a1)
-        case .executable:
-            try container.encodeNil(forKey: .executable)
-        case .test:
-            try container.encodeNil(forKey: .test)
-        }
-    }
-
-    public init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        guard let key = values.allKeys.first(where: values.contains) else {
-            throw DecodingError.dataCorrupted(.init(codingPath: decoder.codingPath, debugDescription: "Did not find a matching key"))
-        }
-        switch key {
-        case .library:
-            var unkeyedValues = try values.nestedUnkeyedContainer(forKey: key)
-            let a1 = try unkeyedValues.decode(ProductType.LibraryType.self)
-            self = .library(a1)
-        case .test:
-            self = .test
-        case .executable:
-            self = .executable
-        }
-    }
-}
-
 extension SystemPackageProviderDescription: Codable {
     private enum CodingKeys: String, CodingKey {
         case brew, apt, yum
