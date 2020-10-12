6cefd0dabe0f090ef25743c0a80af4f212e3bdfd
Mattt <mattt@me.com>
2020-10-12 10:47:31 -0700

    Reorganize ProductType declaration
    Move ProductType Codable conformance closer to declaration

--- Sources/PackageModel/Product.swift
+++ Sources/PackageModel/Product.swift
@@ -11,50 +11,6 @@
 import TSCBasic
 import TSCUtility
 
-/// The type of product.
-public enum ProductType: CustomStringConvertible, Equatable {
-
-    /// The type of library.
-    public enum LibraryType: String, Codable {
-
-        /// Static library.
-        case `static`
-
-        /// Dynamic library.
-        case `dynamic`
-
-        /// The type of library is unspecified and should be decided by package manager.
-        case automatic
-    }
-
-    /// A library product.
-    case library(LibraryType)
-
-    /// An executable product.
-    case executable
-
-    /// A test product.
-    case test
-    
-    public var description: String {
-        switch self {
-        case .executable:
-            return "executable"
-        case .test:
-            return "test"
-        case .library(let type):
-            switch type {
-            case .automatic:
-                return "automatic"
-            case .dynamic:
-                return "dynamic"
-            case .static:
-                return "static"
-            }
-        }
-    }
-}
-
 public class Product: Codable {
 
     /// The name of the product.
@@ -92,8 +48,94 @@ public class Product: Codable {
     }
 }
 
+/// The type of product.
+public enum ProductType: Equatable {
+
+    /// The type of library.
+    public enum LibraryType: String, Codable {
+
+        /// Static library.
+        case `static`
+
+        /// Dynamic library.
+        case `dynamic`
+
+        /// The type of library is unspecified and should be decided by package manager.
+        case automatic
+    }
+
+    /// A library product.
+    case library(LibraryType)
+
+    /// An executable product.
+    case executable
+
+    /// A test product.
+    case test
+}
+
+// MARK: - CustomStringConvertible
+
 extension Product: CustomStringConvertible {
     public var description: String {
         return "<Product: \(name)>"
+    }
+}
+
+extension ProductType: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .executable:
+            return "executable"
+        case .test:
+            return "test"
+        case .library(let type):
+            switch type {
+            case .automatic:
+                return "automatic"
+            case .dynamic:
+                return "dynamic"
+            case .static:
+                return "static"
+            }
+        }
+    }
+}
+
+// MARK: - Codable
+
+extension ProductType: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case library, executable, test
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case let .library(a1):
+            var unkeyedContainer = container.nestedUnkeyedContainer(forKey: .library)
+            try unkeyedContainer.encode(a1)
+        case .executable:
+            try container.encodeNil(forKey: .executable)
+        case .test:
+            try container.encodeNil(forKey: .test)
+        }
+    }
+
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        guard let key = values.allKeys.first(where: values.contains) else {
+            throw DecodingError.dataCorrupted(.init(codingPath: decoder.codingPath, debugDescription: "Did not find a matching key"))
+        }
+        switch key {
+        case .library:
+            var unkeyedValues = try values.nestedUnkeyedContainer(forKey: key)
+            let a1 = try unkeyedValues.decode(ProductType.LibraryType.self)
+            self = .library(a1)
+        case .test:
+            self = .test
+        case .executable:
+            self = .executable
+        }
     }
 }
