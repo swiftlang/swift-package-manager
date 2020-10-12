3e307e8b3e45e2ca4bcccfe2f4e4c780a9044e45
Mattt <mattt@me.com>
2020-10-12 12:31:31 -0700

    Extract Manifest description types into separate files

---
+++ Sources/PackageModel/Manifest/PlatformDescription.swift
@@ -0,0 +1,21 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright (c) 2014 - 2020 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
+public struct PlatformDescription: Codable, Equatable {
+    public let platformName: String
+    public let version: String
+    public let options: [String]
+
+    public init(name: String, version: String, options: [String] = []) {
+        self.platformName = name
+        self.version = version
+        self.options = options
+    }
+}
