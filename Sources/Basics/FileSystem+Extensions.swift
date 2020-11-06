36206ad57c7dce8fa9cf0ff3cd93f98bfe6bcd79
tomer doron <tomer@apple.com>
2020-11-05 22:00:40 -0800

    add Basics module to place common utilities such as FileSystem extensions (#3025)
    motivation: with the introduction of TSC there is no more room for common SwiftPM utilities such as FS extensions
    
    changes:
    * add Basics module that dependends on TSC and make everything depend on it
    * add FS extensions for getting ~/.swiftm and SwiftPM's cache directory

---
+++ Sources/Basics/FileSystem+Extensions.swift
@@ -0,0 +1,29 @@
+/*
+ This source file is part of the Swift.org open source project
+
+ Copyright (c) 2020 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+ */
+
+import TSCBasic
+
+extension FileSystem {
+    /// SwiftPM directory under user's home directory (~/.swiftpm)
+    public var dotSwiftPM: AbsolutePath {
+        return self.homeDirectory.appending(component: ".swiftpm")
+    }
+}
+
+extension FileSystem {
+    /// SwiftPM cache directory under usre's caches directory (if exists)
+    public var swiftPMCacheDirectory: AbsolutePath {
+        if let cachesDirectory = self.cachesDirectory {
+            return cachesDirectory.appending(component: "org.swift.swiftpm")
+        } else {
+            return self.dotSwiftPM.appending(component: "cache")
+        }
+    }
+}
