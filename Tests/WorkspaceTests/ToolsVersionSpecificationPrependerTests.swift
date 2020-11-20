305401815e29db0fd5891a7eb626c141f217fcbc
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-11-19 22:41:42 -0500

    add test cases for new function `prependToolsVersionSpecification(toManifestAt:specifying:fileSystem:)`
    Some quality-of-life refactors for existing test cases are thrown in as well.

--- Tests/WorkspaceTests/ToolsVersionSpecificationPrependerTests.swift
+++ Tests/WorkspaceTests/ToolsVersionSpecificationPrependerTests.swift
@@ -1,12 +1,18 @@
-/*
- This source file is part of the Swift.org open source project
-
- Copyright (c) 2014 - 2020 Apple Inc. and the Swift project authors
- Licensed under Apache License v2.0 with Runtime Library Exception
-
- See http://swift.org/LICENSE.txt for license information
- See http://swift.org/CONTRIBUTORS.txt for Swift project authors
-*/
+// WorkspaceTests/ToolsVersionSpecificationPrependerTests.swift
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2020 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+// -----------------------------------------------------------------------------
+///
+/// This file tests global functions `prependToolsVersionSpecification(toDefaultManifestIn:specifying:fileSystem:)` and `prependToolsVersionSpecification(toManifestAt:specifying:fileSystem:)`.
+///
+// -----------------------------------------------------------------------------
 
 import XCTest
 
@@ -14,15 +20,16 @@ import TSCBasic
 import PackageModel
 import Workspace
 
-// FIXME: Add tests for version-specific manifests.
+/// Test cases for `prependToolsVersionSpecification(toDefaultManifestIn:specifying:fileSystem:)` and `prependToolsVersionSpecification(toManifestAt:specifying:fileSystem:)`.
 class ToolsVersionSpecificationPrependerTests: XCTestCase {
-
+    
+    /// Tests `prependToolsVersionSpecification(toDefaultManifestIn:specifying:fileSystem:)`.
     func testNonVersionSpecificManifests() throws {
         // Empty file.
         var stream = BufferedOutputByteStream()
         stream <<< ""
 
-        prependToolsVersionSpecificationCover(stream) { result in
+        prependToolsVersionSpecificationToDefaultManifest(stream: stream) { result in
             XCTAssertEqual(result, "// swift-tools-version:4.1.2\n")
         }
 
@@ -30,24 +37,24 @@ class ToolsVersionSpecificationPrependerTests: XCTestCase {
         stream = BufferedOutputByteStream()
         stream <<< "\n"
 
-        prependToolsVersionSpecificationCover(stream) { result in
+        prependToolsVersionSpecificationToDefaultManifest(stream: stream) { result in
             XCTAssertEqual(result, "// swift-tools-version:4.1.2\n\n")
         }
 
         // File with some contents.
         stream = BufferedOutputByteStream()
-        stream <<< "let package = ... " <<< "\n"
+        stream <<< "let package = ... \n"
 
-        prependToolsVersionSpecificationCover(stream) { result in
+        prependToolsVersionSpecificationToDefaultManifest(stream: stream) { result in
             XCTAssertEqual(result, "// swift-tools-version:4.1.2\nlet package = ... \n")
         }
 
-        // File already having a valid version.
+        // File already having a valid version specifier.
         stream = BufferedOutputByteStream()
         stream <<< "// swift-tools-version:3.1.2\n"
         stream <<< "..."
 
-        prependToolsVersionSpecificationCover(stream) { result in
+        prependToolsVersionSpecificationToDefaultManifest(stream: stream) { result in
             XCTAssertEqual(result, "// swift-tools-version:4.1.2\n...")
         }
 
@@ -56,42 +63,149 @@ class ToolsVersionSpecificationPrependerTests: XCTestCase {
         stream <<< "// swift-tools-version:3.1.2\n"
         stream <<< "..."
 
-        prependToolsVersionSpecificationCover(stream, version: ToolsVersion(version: "2.1.0")) { result in
+        prependToolsVersionSpecificationToDefaultManifest(stream: stream, version: ToolsVersion(version: "2.1.0")) { result in
             XCTAssertEqual(result, "// swift-tools-version:2.1\n...")
         }
 
-        // Contents with invalid specifier line.
+        // Contents with invalid tools version specification (ignoring the validity of the version specifier).
         stream = BufferedOutputByteStream()
         stream <<< "// swift-tool-version:3.1.2\n"
         stream <<< "..."
 
-        prependToolsVersionSpecificationCover(stream) { result in
+        prependToolsVersionSpecificationToDefaultManifest(stream: stream) { result in
             XCTAssertEqual(result, "// swift-tools-version:4.1.2\n// swift-tool-version:3.1.2\n...")
         }
 
-        // Contents with invalid specifier string.
+        // Contents with invalid version specifier.
         stream = BufferedOutputByteStream()
         stream <<< "// swift-tools-version:-3.1.2\n"
         stream <<< "..."
 
-        prependToolsVersionSpecificationCover(stream) { result in
+        prependToolsVersionSpecificationToDefaultManifest(stream: stream) { result in
             XCTAssertEqual(result, "// swift-tools-version:4.1.2\n...")
         }
 
-        // Contents with invalid specifier string and some meta data.
+        // Contents with invalid version specifier and some meta data.
         stream = BufferedOutputByteStream()
         stream <<< "// swift-tools-version:-3.1.2;hello\n"
         stream <<< "..."
 
-        prependToolsVersionSpecificationCover(stream) { result in
+        prependToolsVersionSpecificationToDefaultManifest(stream: stream) { result in
             // Note: Right now we lose the metadata but if we ever start using it, we should preserve it.
             XCTAssertEqual(result, "// swift-tools-version:4.1.2\n...")
         }
 
         // Try to write a version with prerelease and build meta data.
         let toolsVersion = ToolsVersion(version: "4.1.2-alpha.beta+sha.1234")
-        prependToolsVersionSpecificationCover(stream, version: toolsVersion) { result in
+        
+        stream = BufferedOutputByteStream()
+        stream <<< "let package = ... \n"
+        
+        prependToolsVersionSpecificationToDefaultManifest(
+            stream: stream,
+            version: toolsVersion
+        ) { result in
+            XCTAssertEqual(result, "// swift-tools-version:4.1.2\nlet package = ... \n")
+        }
+    }
+    
+    /// Tests `prependToolsVersionSpecification(toManifestAt:specifying:fileSystem:)` with test cases not in `testNonVersionSpecificManifests()`.
+    func testVersionSpecificManifests() throws {
+        // Empty file.
+        var stream = BufferedOutputByteStream()
+        stream <<< ""
+        
+        prependToolsVersionSpecificationToVersionSpecificManifest(stream: stream) { result in
+            XCTAssertEqual(result, "// swift-tools-version:4.1.2\n")
+        }
+        
+        // File with just a new line.
+        stream = BufferedOutputByteStream()
+        stream <<< "\n"
+        
+        prependToolsVersionSpecificationToVersionSpecificManifest(stream: stream) { result in
+            XCTAssertEqual(result, "// swift-tools-version:4.1.2\n\n")
+        }
+        
+        // File with some contents.
+        stream = BufferedOutputByteStream()
+        stream <<< "let package = ... \n"
+        
+        prependToolsVersionSpecificationToVersionSpecificManifest(stream: stream) { result in
+            XCTAssertEqual(result, "// swift-tools-version:4.1.2\nlet package = ... \n")
+        }
+        
+        // File already having a valid version specifier.
+        stream = BufferedOutputByteStream()
+        stream <<< "// swift-tools-version:3.1.2\n"
+        stream <<< "..."
+        
+        prependToolsVersionSpecificationToVersionSpecificManifest(stream: stream) { result in
             XCTAssertEqual(result, "// swift-tools-version:4.1.2\n...")
+        }
+        
+        // Write a version with zero in patch number.
+        stream = BufferedOutputByteStream()
+        stream <<< "// swift-tools-version:3.1.2\n"
+        stream <<< "..."
+        
+        prependToolsVersionSpecificationToVersionSpecificManifest(stream: stream, version: ToolsVersion(version: "2.1.0")) { result in
+            XCTAssertEqual(result, "// swift-tools-version:2.1\n...")
+        }
+        
+        // Contents with invalid tools version specification (ignoring the validity of the version specifier).
+        stream = BufferedOutputByteStream()
+        stream <<< "// swift-tool-version:3.1.2\n"
+        stream <<< "..."
+        
+        prependToolsVersionSpecificationToVersionSpecificManifest(stream: stream) { result in
+            XCTAssertEqual(result, "// swift-tools-version:4.1.2\n// swift-tool-version:3.1.2\n...")
+        }
+        
+        // Contents with invalid version specifier.
+        stream = BufferedOutputByteStream()
+        stream <<< "// swift-tools-version:-3.1.2\n"
+        stream <<< "..."
+        
+        prependToolsVersionSpecificationToVersionSpecificManifest(stream: stream) { result in
+            XCTAssertEqual(result, "// swift-tools-version:4.1.2\n...")
+        }
+        
+        // Contents with invalid version specifier and some meta data.
+        stream = BufferedOutputByteStream()
+        stream <<< "// swift-tools-version:-3.1.2;hello\n"
+        stream <<< "..."
+        
+        prependToolsVersionSpecificationToVersionSpecificManifest(stream: stream) { result in
+            // Note: Right now we lose the metadata but if we ever start using it, we should preserve it.
+            XCTAssertEqual(result, "// swift-tools-version:4.1.2\n...")
+        }
+        
+        // Try to write a version with prerelease and build meta data.
+        let toolsVersionWithMetadata = ToolsVersion(version: "4.1.2-alpha.beta+sha.1234")
+        
+        stream = BufferedOutputByteStream()
+        stream <<< "let package = ... \n"
+        
+        prependToolsVersionSpecificationToVersionSpecificManifest(
+            stream: stream,
+            version: toolsVersionWithMetadata
+        ) { result in
+            XCTAssertEqual(result, "// swift-tools-version:4.1.2\nlet package = ... \n")
+        }
+        
+        // Try to write a version higher than that the manifest is for.
+        let toolsVersionXL = ToolsVersion.v5_3
+        
+        stream = BufferedOutputByteStream()
+        stream <<< "let package = ... \n"
+        
+        prependToolsVersionSpecificationToVersionSpecificManifest(
+            stream: stream,
+            manifestFilePath: AbsolutePath("/pkg/Package@swift-5.2.0.swift"),
+            version: toolsVersionXL
+        ) { result in
+            XCTAssertEqual(result, "// swift-tools-version:5.3\nlet package = ... \n")
         }
     }
 
@@ -100,26 +214,57 @@ class ToolsVersionSpecificationPrependerTests: XCTestCase {
         XCTAssertEqual(ToolsVersion(version: "4.2.0").zeroedPatch.description, "4.2.0")
         XCTAssertEqual(ToolsVersion(version: "6.0.129").zeroedPatch.description, "6.0.0")
     }
-
-    func prependToolsVersionSpecificationCover(
-        _ stream: BufferedOutputByteStream,
+    
+    /// Does the boilerplate filesystem preparations, then calls `prependToolsVersionSpecification(toDefaultManifestIn:specifying:fileSystem:)`, for `testNonVersionSpecificManifests()`.
+    /// - Parameters:
+    ///   - stream: The stream to read from and write to the filesystem.
+    ///   - version: The Swift tools version to specify.
+    ///   - resultHandler: The result handler.
+    func prependToolsVersionSpecificationToDefaultManifest(
+        stream: BufferedOutputByteStream,
         version: ToolsVersion = ToolsVersion(version: "4.1.2"),
-        _ result: (ByteString) -> Void
+        resultHandler: (ByteString) -> Void
     ) {
         do {
-            let fs: FileSystem = InMemoryFileSystem()
+            let inMemoryFileSystem: FileSystem = InMemoryFileSystem()
 
-            let file = AbsolutePath("/pkg/Package.swift")
+            let manifestFilePath = AbsolutePath("/pkg/Package.swift")
 
-            try fs.createDirectory(file.parentDirectory, recursive: true)
-            try fs.writeFileContents(file, bytes: stream.bytes)
+            try inMemoryFileSystem.createDirectory(manifestFilePath.parentDirectory, recursive: true)
+            try inMemoryFileSystem.writeFileContents(manifestFilePath, bytes: stream.bytes)
 
-			try prependToolsVersionSpecification(
-				toDefaultManifestIn: file.parentDirectory, specifying: version, fileSystem: fs)
+            try prependToolsVersionSpecification(
+                toDefaultManifestIn: manifestFilePath.parentDirectory, specifying: version, fileSystem: inMemoryFileSystem)
 
-            result(try fs.readFileContents(file))
+            resultHandler(try inMemoryFileSystem.readFileContents(manifestFilePath))
         } catch {
             XCTFail("Failed with error \(error)")
         }
     }
+    
+    /// Does the boilerplate filesystem preparations, then calls `prependToolsVersionSpecification(toManifestAt:specifying:fileSystem:)` for `testVersionSpecificManifests`.
+    /// - Parameters:
+    ///   - stream: The stream to read from and write to the filesystem.
+    ///   - manifestFilePath: The path to the manifest file to prepend the Swift tools version specification to.
+    ///   - version: The Swift tools version to specify.
+    ///   - resultHandler: The result handler.
+    func prependToolsVersionSpecificationToVersionSpecificManifest(
+        stream: BufferedOutputByteStream,
+        manifestFilePath: AbsolutePath = .init("/pkg/Package@swift-5.2.0.swift"),
+        version: ToolsVersion = ToolsVersion(version: "4.1.2"),
+        resultHandler: (ByteString) -> Void
+    ) {
+        do {
+            let inMemoryFileSystem: FileSystem = InMemoryFileSystem()
+            try inMemoryFileSystem.createDirectory(manifestFilePath.parentDirectory, recursive: true)
+            try inMemoryFileSystem.writeFileContents(manifestFilePath, bytes: stream.bytes)
+            
+            try prependToolsVersionSpecification(toManifestAt: manifestFilePath, specifying: version, fileSystem: inMemoryFileSystem)
+            
+            resultHandler(try inMemoryFileSystem.readFileContents(manifestFilePath))
+        } catch {
+            XCTFail("Failed with error \(error)")
+        }
+    }
+    
 }
