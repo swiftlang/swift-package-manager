f9d20403b0b822d813ffa9a57b4562c20a0616e6
Owen Voorhees <owen@squibner.com>
2020-11-21 13:51:28 -0800

    [PackageEditor] Add an end-to-end test of adding package deps

--- Tests/SPMPackageEditorTests/PackageEditorTests.swift
+++ Tests/SPMPackageEditorTests/PackageEditorTests.swift
@@ -16,6 +16,93 @@ import SourceControl
 @testable import SPMPackageEditor
 
 final class PackageEditorTests: XCTestCase {
+
+    func testAddDependency() throws {
+        let manifest = """
+            // swift-tools-version:5.2
+            import PackageDescription
+
+            let package = Package(
+                name: "exec",
+                dependencies: [
+                    .package(url: "https://github.com/foo/goo", from: "1.0.1"),
+                ],
+                targets: [
+                    .target(
+                        name: "foo",
+                        dependencies: []),
+                    .target(
+                        name: "bar",
+                        dependencies: []),
+                    .testTarget(
+                        name: "fooTests",
+                        dependencies: ["foo", "bar"]),
+                ]
+            )
+            """
+
+        let fs = InMemoryFileSystem(emptyFiles:
+            "/pkg/Package.swift",
+            "/pkg/Sources/foo/source.swift",
+            "/pkg/Sources/bar/source.swift",
+            "/pkg/Tests/fooTests/source.swift",
+            "end")
+
+        let manifestPath = AbsolutePath("/pkg/Package.swift")
+        try fs.writeFileContents(manifestPath) { $0 <<< manifest }
+        try fs.createDirectory(.init("/pkg/repositories"), recursive: false)
+        try fs.createDirectory(.init("/pkg/repo"), recursive: false)
+
+
+        let provider = InMemoryGitRepositoryProvider()
+        let repo = InMemoryGitRepository(path: .init("/pkg/repo"), fs: fs)
+        try repo.writeFileContents(.init("/Package.swift"), bytes: .init(encodingAsUTF8: """
+        // swift-tools-version:5.2
+        import PackageDescription
+
+        let package = Package(name: "repo")
+        """))
+        repo.commit()
+        try repo.tag(name: "1.1.1")
+        provider.add(specifier: .init(url: "http://www.githost.com/repo"), repository: repo)
+        
+        let context = try PackageEditorContext(
+            manifestPath: AbsolutePath("/pkg/Package.swift"),
+            repositoryManager: RepositoryManager(path: .init("/pkg/repositories"), provider: provider, fileSystem: fs),
+            toolchain: Resources.default.toolchain,
+            fs: fs
+        )
+        let editor = PackageEditor(context: context)
+
+        try editor.addPackageDependency(url: "http://www.githost.com/repo.git", requirement: .exact("1.1.1"))
+
+        let newManifest = try fs.readFileContents(manifestPath).cString
+        XCTAssertEqual(newManifest, """
+            // swift-tools-version:5.2
+            import PackageDescription
+
+            let package = Package(
+                name: "exec",
+                dependencies: [
+                    .package(url: "https://github.com/foo/goo", from: "1.0.1"),
+                    .package(name: "repo", url: "http://www.githost.com/repo.git", .exact("1.1.1")),
+                ],
+                targets: [
+                    .target(
+                        name: "foo",
+                        dependencies: []),
+                    .target(
+                        name: "bar",
+                        dependencies: []),
+                    .testTarget(
+                        name: "fooTests",
+                        dependencies: ["foo", "bar"]),
+                ]
+            )
+            """)
+    }
+
+
     func testAddTarget() throws {
         let manifest = """
             // swift-tools-version:5.2
