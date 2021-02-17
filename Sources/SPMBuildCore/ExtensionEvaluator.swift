162dc7f11b157b1cbdf607a277077e1e60d55971
Anders Bertelrud <anders@apple.com>
2021-02-17 13:39:37 -0800

    Pass through the tools version of the package that declares the extension, which will control availability annotations in the PackageExtension module.

--- Sources/SPMBuildCore/ExtensionEvaluator.swift
+++ Sources/SPMBuildCore/ExtensionEvaluator.swift
@@ -101,6 +101,7 @@ extension PackageGraph {
                 let (extensionOutput, emittedText) = try runExtension(
                     sources: extTarget.sources,
                     input: extensionInput,
+                    toolsVersion: package.manifest.toolsVersion,
                     extensionRunner: extensionRunner,
                     diagnostics: diagnostics,
                     fileSystem: fileSystem
@@ -161,13 +162,13 @@ extension PackageGraph {
     
     /// Private helper function that serializes an ExtensionEvaluationInput as input JSON, calls the extension runner to invoke the extension, and finally deserializes the output JSON it emits to a ExtensionEvaluationOutput.  Adds any errors or warnings to `diagnostics`, and throws an error if there was a failure.
     /// FIXME: This should be asynchronous, taking a queue and a completion closure.
-    fileprivate func runExtension(sources: Sources, input: ExtensionEvaluationInput, extensionRunner: ExtensionRunner, diagnostics: DiagnosticsEngine, fileSystem: FileSystem) throws -> (output: ExtensionEvaluationOutput, stdoutText: Data) {
+    fileprivate func runExtension(sources: Sources, input: ExtensionEvaluationInput, toolsVersion: ToolsVersion, extensionRunner: ExtensionRunner, diagnostics: DiagnosticsEngine, fileSystem: FileSystem) throws -> (output: ExtensionEvaluationOutput, stdoutText: Data) {
         // Serialize the ExtensionEvaluationInput to JSON.
         let encoder = JSONEncoder()
         let inputJSON = try encoder.encode(input)
         
         // Call the extension runner.
-        let (outputJSON, stdoutText) = try extensionRunner.runExtension(sources: sources, inputJSON: inputJSON, diagnostics: diagnostics, fileSystem: fileSystem)
+        let (outputJSON, stdoutText) = try extensionRunner.runExtension(sources: sources, inputJSON: inputJSON, toolsVersion: toolsVersion, diagnostics: diagnostics, fileSystem: fileSystem)
 
         // Deserialize the JSON to an ExtensionEvaluationOutput.
         let output: ExtensionEvaluationOutput
@@ -278,6 +279,7 @@ public protocol ExtensionRunner {
     func runExtension(
         sources: Sources,
         inputJSON: Data,
+        toolsVersion: ToolsVersion,
         diagnostics: DiagnosticsEngine,
         fileSystem: FileSystem
     ) throws -> (outputJSON: Data, stdoutText: Data)
