03d0bf59278b95a835d17ce5ea419f0606d3dce8
Anders Bertelrud <anders@apple.com>
2021-02-18 09:13:30 -0800

    Add Radar number to comment about temporary workaround.

--- Sources/PackageLoading/ManifestLoader.swift
+++ Sources/PackageLoading/ManifestLoader.swift
@@ -513,7 +513,7 @@ public final class ManifestLoader: ManifestLoaderProtocol {
         // We might have some non-fatal output (warnings/notes) from the compiler even when
         // we were able to parse the manifest successfully.
         if let compilerOutput = result.compilerOutput {
-            // Temporary workaround to filter out debug output from integrated Swift driver.
+            // FIXME: Temporary workaround to filter out debug output from integrated Swift driver. [rdar://73710910]
             if !(compilerOutput.hasPrefix("<unknown>:0: remark: new Swift driver at") && compilerOutput.hasSuffix("will be used")) {
                 diagnostics?.emit(.warning(ManifestLoadingDiagnostic(output: compilerOutput, diagnosticFile: result.diagnosticFile)))
             }
