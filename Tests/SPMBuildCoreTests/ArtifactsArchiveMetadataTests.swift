1f2eda34deec3c92b97d116f192fd734657c4613
Anders Bertelrud <anders@apple.com>
2021-03-11 10:04:38 -0800

    Move down the parsing of binary artifacts such as XCFrameworks and ArtifactArchives to SPMBuildCore (which makes sense anyway, but in particular so that plugins can have access to it). (#3349)

--- Tests/SPMBuildCoreTests/ArtifactsArchiveMetadataTests.swift
+++ Tests/SPMBuildCoreTests/ArtifactsArchiveMetadataTests.swift
@@ -1,14 +1,14 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2014 - 2019 Apple Inc. and the Swift project authors
+ Copyright (c) 2014 - 2021 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See http://swift.org/LICENSE.txt for license information
  See http://swift.org/CONTRIBUTORS.txt for Swift project authors
  */
 
-import Build
+import SPMBuildCore
 import TSCBasic
 import TSCUtility
 import XCTest
