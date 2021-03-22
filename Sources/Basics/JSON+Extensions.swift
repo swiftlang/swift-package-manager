bc44943430ae43582536fad14c3929c9fd8de18a
Yim Lee <yim_lee@apple.com>
2021-03-22 11:32:06 -0700

    Add another JSONEncoder.makeWithDefaults variation (#3366)
    Make another JSONEncoder.makeWithDefaults variation
    
    It would be nice if we can have sorted keys without pretty printing.

--- Sources/Basics/JSON+Extensions.swift
+++ Sources/Basics/JSON+Extensions.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2020 Apple Inc. and the Swift project authors
+ Copyright (c) 2020-2021 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See http://swift.org/LICENSE.txt for license information
@@ -78,24 +78,36 @@ extension JSONDecoder {
 
 extension JSONEncoder {
     public static func makeWithDefaults(prettified: Bool = true, dateEncodingStrategy: DateEncodingStrategy = .safeISO8601) -> JSONEncoder {
+        Self.makeWithDefaults(sortKeys: prettified, prettyPrint: prettified, escapeSlashes: !prettified, dateEncodingStrategy: dateEncodingStrategy)
+    }
+
+    public static func makeWithDefaults(sortKeys: Bool, prettyPrint: Bool, escapeSlashes: Bool, dateEncodingStrategy: DateEncodingStrategy = .safeISO8601) -> JSONEncoder {
         let encoder = JSONEncoder()
-        if prettified {
-            // output formatting
+        var outputFormatting: JSONEncoder.OutputFormatting = []
+
+        if sortKeys {
             #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-            if #available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *) {
-                encoder.outputFormatting = [.sortedKeys, .prettyPrinted, .withoutEscapingSlashes]
-            } else if #available(iOS 11.0, watchOS 4.0, tvOS 11.0, *) {
-                encoder.outputFormatting = [.sortedKeys, .prettyPrinted]
-            } else {
-                encoder.outputFormatting = [.prettyPrinted]
+            if #available(macOS 10.15, iOS 11.0, watchOS 4.0, tvOS 11.0, *) {
+                outputFormatting.insert(.sortedKeys)
             }
-            #elseif compiler(>=5.3)
-            encoder.outputFormatting = [.sortedKeys, .prettyPrinted, .withoutEscapingSlashes]
             #else
-            encoder.outputFormatting = [.sortedKeys, .prettyPrinted]
+            outputFormatting.insert(.sortedKeys)
             #endif
         }
-        // date
+        if prettyPrint {
+            outputFormatting.insert(.prettyPrinted)
+        }
+        if !escapeSlashes {
+            #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+            if #available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *) {
+                outputFormatting.insert(.withoutEscapingSlashes)
+            }
+            #elseif compiler(>=5.3)
+            outputFormatting.insert(.withoutEscapingSlashes)
+            #endif
+        }
+
+        encoder.outputFormatting = outputFormatting
         encoder.dateEncodingStrategy = dateEncodingStrategy
         return encoder
     }
