9b4d0cfcc0557823a1fd1cb679d3e8044b5c24bf
Anders Bertelrud <anders@apple.com>
2021-03-28 00:31:58 -0700

    Move down the helper function to check if the compiler supports `-entry-point-function-name` from the unit tests down to SPMTestSupport

--- Sources/SPMTestSupport/Resources.swift
+++ Sources/SPMTestSupport/Resources.swift
@@ -65,4 +65,12 @@ public class Resources: ManifestResourceProvider {
     public static var havePD4Runtime: Bool {
         return Resources.default.binDir == nil
     }
+    
+    public var swiftCompilerSupportsRenamingMainSymbol: Bool {
+        return (try? withTemporaryDirectory { tmpDir in
+            FileManager.default.createFile(atPath: "\(tmpDir)/foo.swift", contents: Data())
+            let result = try Process.popen(args: self.swiftCompiler.pathString, "-c", "-Xfrontend", "-entry-point-function-name", "-Xfrontend", "foo", "\(tmpDir)/foo.swift", "-o", "\(tmpDir)/foo.o")
+            return try !result.utf8stderrOutput().contains("unknown argument: '-entry-point-function-name'")
+        }) ?? false
+    }
 }
