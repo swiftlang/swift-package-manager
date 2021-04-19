d7563b2f4463b0bc7191c386389d76b673c77a95
Anders Bertelrud <anders@apple.com>
2021-04-19 11:53:21 -0700

    Generalize MiscellaneousTests.testOverridingSwiftcArguments() to be architecture-neutral (#3414)
    This unit test was already specific to macOS, and this fix doesn't change that; what it does is to make the architecture the same as the host.  The old hardcoding of x64_64 seems to work on the desktop even on Apple Silicon, but does not appear to work in CI.  This also fixes the use of 10.20 as a hypothetical future macOS release.  It also renames the test to reflect the apparent intent of the test.
    
    rdar://76770888
    (cherry picked from commit 6eae39c591fb9937bbb5890f4564c070b8c4b78b)

---
+++ Fixtures/Miscellaneous/DistantFutureDeploymentTarget/Package.swift
@@ -0,0 +1,9 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "DistantFutureDeploymentTarget",
+    targets: [
+        .target(name: "DistantFutureDeploymentTarget", path: "Sources"),
+    ]
+)
