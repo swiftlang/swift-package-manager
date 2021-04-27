bdd22ae4387e07ef30477e58b5d87c7bbdeb0749
tomer doron <tomer@apple.com>
2021-04-27 15:28:35 -0700

    improve location of config directory (#3430)
    motivation: use more idiomatic location for SwiftPM config
    
    changes:
    * on darwin platforms, use ~/Library/org.swift.swiftpm to store SwiftPM configs, which is more idiomatic
    * if idiomatic config directory is different from ~/.swift/config, create a symlink ~/.swift/config -> idiomatic
    * add temporary transition code for beta users of 5.5 that may already have ~/.swift/config in place
    * refactor and centralize the computation of config directory
    * refactor and centralize the computation of cache directory (no functional changes)

--- Sources/Basics/FileSystem+Extensions.swift
+++ Sources/Basics/FileSystem+Extensions.swift
@@ -9,6 +9,9 @@
  */
 
 import TSCBasic
+import class Foundation.FileManager
+
+// MARK: - user level
 
 extension FileSystem {
     /// SwiftPM directory under user's home directory (~/.swiftpm)
@@ -17,13 +20,93 @@ extension FileSystem {
     }
 }
 
+
+// MARK: - cache
+
 extension FileSystem {
+    private var idiomaticUserCacheDirectory: AbsolutePath? {
+        // in TSC: FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)
+        self.cachesDirectory
+    }
+
     /// SwiftPM cache directory under user's caches directory (if exists)
     public var swiftPMCacheDirectory: AbsolutePath {
-        if let cachesDirectory = self.cachesDirectory {
-            return cachesDirectory.appending(component: "org.swift.swiftpm")
+        if let path = self.idiomaticUserCacheDirectory {
+            return path.appending(component: "org.swift.swiftpm")
         } else {
-            return self.dotSwiftPM.appending(component: "cache")
+            return self.dotSwiftPMCachesDirectory
         }
+    }
+
+    fileprivate var dotSwiftPMCachesDirectory: AbsolutePath {
+        return self.dotSwiftPM.appending(component: "cache")
+    }
+}
+
+extension FileSystem {
+    public func getOrCreateSwiftPMCacheDirectory() throws -> AbsolutePath {
+        let idiomaticCacheDirectory = self.swiftPMCacheDirectory
+        // Create idiomatic if necessary
+        if !self.exists(idiomaticCacheDirectory) {
+            try self.createDirectory(idiomaticCacheDirectory, recursive: true)
+        }
+        // Create ~/.swiftpm if necessary
+        if !self.exists(self.dotSwiftPM) {
+            try self.createDirectory(self.dotSwiftPM, recursive: true)
+        }
+        // Create ~/.swiftpm/cache symlink if necessary
+        if !self.exists(self.dotSwiftPMCachesDirectory, followSymlink: false) {
+            try self.createSymbolicLink(dotSwiftPMCachesDirectory, pointingAt: idiomaticCacheDirectory, relative: false)
+        }
+        return idiomaticCacheDirectory
+    }
+}
+
+// MARK: - config
+
+extension FileSystem {
+    private var idiomaticUserConfigDirectory: AbsolutePath? {
+        return FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).first.flatMap { AbsolutePath($0.path) }
+    }
+
+    /// SwiftPM config directory under user's config directory (if exists)
+    public var swiftPMConfigDirectory: AbsolutePath {
+        if let path = self.idiomaticUserConfigDirectory {
+            return path.appending(component: "org.swift.swiftpm")
+        } else {
+            return self.dotSwiftPMConfigDirectory
+        }
+    }
+
+    fileprivate var dotSwiftPMConfigDirectory: AbsolutePath {
+        return self.dotSwiftPM.appending(component: "config")
+    }
+}
+
+extension FileSystem {
+    public func getOrCreateSwiftPMConfigDirectory() throws -> AbsolutePath {
+        let idiomaticConfigDirectory = self.swiftPMConfigDirectory
+
+        // temporary 5.5, remove on next version: transition from ~/.swiftpm/config to idiomatic location + symbolic link
+        if idiomaticConfigDirectory != self.dotSwiftPMConfigDirectory &&
+            self.exists(self.dotSwiftPMConfigDirectory) && self.isDirectory(self.dotSwiftPMConfigDirectory) &&
+            !self.exists(idiomaticConfigDirectory) {
+            print("transitioning \(self.dotSwiftPMConfigDirectory) to \(idiomaticConfigDirectory)")
+            try self.move(from: self.dotSwiftPMConfigDirectory, to: idiomaticConfigDirectory)
+        }
+
+        // Create idiomatic if necessary
+        if !self.exists(idiomaticConfigDirectory) {
+            try self.createDirectory(idiomaticConfigDirectory, recursive: true)
+        }
+        // Create ~/.swiftpm if necessary
+        if !self.exists(self.dotSwiftPM) {
+            try self.createDirectory(self.dotSwiftPM, recursive: true)
+        }
+        // Create ~/.swiftpm/config symlink if necessary
+        if !self.exists(self.dotSwiftPMConfigDirectory, followSymlink: false) {
+            try self.createSymbolicLink(dotSwiftPMConfigDirectory, pointingAt: idiomaticConfigDirectory, relative: false)
+        }
+        return idiomaticConfigDirectory
     }
 }
