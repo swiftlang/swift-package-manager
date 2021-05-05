581b2406a705b4127fa2ccecf6f3afa97e1ef679
Anders Bertelrud <anders@apple.com>
2021-05-04 18:53:02 -0700

    Adjust to SE-0303: Add `path`, `exclude`, and `sources` parameters to plugin targets.
    (cherry picked from commit caab591e5f39f5f5c2eb77df380a18bba48b7663)
    
    # Conflicts:
    #	Tests/PackageLoadingTests/PD5_5LoadingTests.swift

--- Sources/PackageDescription/Target.swift
+++ Sources/PackageDescription/Target.swift
@@ -268,8 +268,6 @@ public final class Target {
         case .plugin:
             precondition(
                 url == nil &&
-                exclude.isEmpty &&
-                sources == nil &&
                 resources == nil &&
                 publicHeadersPath == nil &&
                 pkgConfig == nil &&
@@ -901,14 +899,17 @@ public final class Target {
     public static func plugin(
         name: String,
         capability: PluginCapability,
-        dependencies: [Dependency] = []
+        dependencies: [Dependency] = [],
+        path: String? = nil,
+        exclude: [String] = [],
+        sources: [String]? = nil
     ) -> Target {
       return Target(
           name: name,
           dependencies: dependencies,
-          path: nil,
-          exclude: [],
-          sources: nil,
+          path: path,
+          exclude: exclude,
+          sources: sources,
           publicHeadersPath: nil,
           type: .plugin,
           pluginCapability: capability)
