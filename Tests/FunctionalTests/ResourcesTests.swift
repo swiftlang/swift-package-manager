51b2ffda7e5a4932e451a2c13915fb65e2474720
Florian Friedrich <ffried@me.com>
2021-05-08 18:32:34 +0200

    Add functional test for binary with moved resources (#3472)
    * Add functional tests for binary with moved resources
    
    * Remove .gitignore for resources fixtures
    
    * Use local file system for file operations
    
    * Build each product separately

--- Tests/FunctionalTests/ResourcesTests.swift
+++ Tests/FunctionalTests/ResourcesTests.swift
@@ -45,4 +45,44 @@ class ResourcesTests: XCTestCase {
                 """)
         }
     }
+
+    func testMovedBinaryResources() {
+        fixture(name: "Resources/Moved") { prefix in
+            var executables = ["SwiftyResource"]
+
+            // Objective-C module requires macOS
+            #if os(macOS)
+            executables.append("SeaResource")
+            #endif
+
+            let binPath = try AbsolutePath(
+                executeSwiftBuild(prefix, configuration: .Release, extraArgs: ["--show-bin-path"]).stdout
+                    .trimmingCharacters(in: .whitespacesAndNewlines)
+            )
+
+            for execName in executables {
+                _ = try executeSwiftBuild(prefix, configuration: .Release, extraArgs: ["--product", execName])
+
+                try withTemporaryDirectory(prefix: execName) { tmpDirPath in
+                    defer {
+                        // Unblock and remove the tmp dir on deinit.
+                        try? localFileSystem.chmod(.userWritable, path: tmpDirPath, options: [.recursive])
+                        try? localFileSystem.removeFileTree(tmpDirPath)
+                    }
+
+                    let destBinPath = tmpDirPath.appending(component: execName)
+                    // Move the binary
+                    try localFileSystem.move(from: binPath.appending(component: execName), to: destBinPath)
+                    // Move the resources
+                    try localFileSystem
+                        .getDirectoryContents(binPath)
+                        .filter { $0.contains(execName) && $0.hasSuffix(".bundle") || $0.hasSuffix(".resources") }
+                        .forEach { try localFileSystem.move(from: binPath.appending(component: $0), to: tmpDirPath.appending(component: $0)) }
+                    // Run the binary
+                    let output = try Process.checkNonZeroExit(args: destBinPath.pathString)
+                    XCTAssertTrue(output.contains("foo"))
+                }
+            }
+        }
+    }
 }
