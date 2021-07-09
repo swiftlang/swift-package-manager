a0415d4ab3e8ca41d2fb9aed40b3925d52148bcf
Aris Sarris <ar.sarris@gmail.com>
2021-07-09 10:49:40 +0300

    Make Package.swift not found error more descriptive SR-14829

--- Sources/Commands/Error.swift
+++ Sources/Commands/Error.swift
@@ -30,7 +30,7 @@ extension Error: CustomStringConvertible {
         case .invalidToolchain(let problem):
             return problem
         case .rootManifestFileNotFound:
-            return "root manifest not found"
+            return "Could not find \(Manifest.filename) in this folder or any of it's parent folders."
         }
     }
 }
