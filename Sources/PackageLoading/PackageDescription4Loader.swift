deb278090d8ef53ace5263e65bd871393073d76b
Boris Bügling <bbuegling@apple.com>
2021-07-20 22:07:14 -0700

    Error on relative file URLs
    It turns out that relative file URLs actually do not exist, see [here](https://github.com/apple/swift-package-manager/pull/3604#issuecomment-881715869) for more discussion. Instead we will now error and guide users to using a local package reference.
    
    rdar://80751251
    
    (cherry picked from commit e6a7323a34af55d66ffc2f53fa46491b553fafc7)

--- Sources/PackageLoading/PackageDescription4Loader.swift
+++ Sources/PackageLoading/PackageDescription4Loader.swift
@@ -345,7 +345,11 @@ extension PackageDependencyDescription {
             } else if dependencyLocation.hasPrefix(filePrefix) {
                 // FIXME: SwiftPM can't handle file locations with file:// scheme so we need to
                 // strip that. We need to design a Location data structure for SwiftPM.
-                return AbsolutePath(String(dependencyLocation.dropFirst(filePrefix.count)), relativeTo: AbsolutePath(packageLocation)).pathString
+                let location = String(dependencyLocation.dropFirst(filePrefix.count))
+                if location.first != "/" {
+                    throw ManifestParseError.invalidManifestFormat("file:// URLs cannot be relative, did you mean to use `.package(path:)`?", diagnosticFile: nil)
+                }
+                return AbsolutePath(location).pathString
             } else if URL.scheme(dependencyLocation) == nil {
                 // If the dependency URL is not remote, try to "fix" it.
                 // If the URL has no scheme, we treat it as a path (either absolute or relative to the base URL).
