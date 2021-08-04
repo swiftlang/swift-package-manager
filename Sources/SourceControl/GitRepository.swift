22e68468a2f09687161da2b96f6b839acad7fb3d
Mattt <mattt@me.com>
2021-08-04 06:30:11 -0700

    Add --prefix argument to archive-source command

--- Sources/SourceControl/GitRepository.swift
+++ Sources/SourceControl/GitRepository.swift
@@ -482,6 +482,7 @@ public final class GitRepository: Repository, WorkingCheckout {
         try self.lock.withLock {
             try callGit("archive",
                         "--format", "zip",
+                        "--prefix", path.basenameWithoutExt,
                         "--output", path.pathString,
                         "HEAD",
                         failureMessage: "Couldn’t create an archive")
