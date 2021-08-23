28dfd8bf1fa8fdbe888e2669db7e2e23c195d0f1
Boris Buegling <bbuegling@apple.com>
2021-08-23 13:50:48 -0700

    Fix integration test for Xcode 13
    This fixes `Found multiple targets named 'FooLib'` which is happening in this test when using the XCBuild from Xcode 13. We should figure out separately what to do about that case, but for now it seems most practical to change the test in order to not block other work in SwiftPM.

--- IntegrationTests/Fixtures/XCBuild/Libraries/Foo/Package.swift
+++ IntegrationTests/Fixtures/XCBuild/Libraries/Foo/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Foo",
     products: [
-        .library(name: "FooLib", type: .static, targets: ["FooLib"]),
+        .library(name: "FooLibProduct", type: .static, targets: ["FooLib"]),
     ],
     dependencies: [
         .package(path: "../Bar")
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .target(name: "FooLib", dependencies: ["CFooLib"]),
         .target(name: "CFooLib", dependencies: [
-            .product(name: "BarLib", package: "Bar"),
+            .product(name: "BarLibProduct", package: "Bar"),
         ]),
     ],
     swiftLanguageVersions: [.v4_2, .v5]
