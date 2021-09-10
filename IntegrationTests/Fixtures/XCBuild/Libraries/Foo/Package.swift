9180312d387074658fb1ee453e918803fce41525
Boris Bügling <bbuegling@apple.com>
2021-09-10 09:15:06 -0700

    Disambiguate PIF target names (#3716)
    * Revert "Fix integration test for Xcode 13 (#3686)"
    
    This reverts commit 376fd156d6b3fd366eb284fb918d3e692ffa0131.
    
    * Disambiguate PIF target names
    
    A product can have the same name as a target in SwiftPM, but since we create a PIF target for each, we need to come up with a unique name for products.
    
    rdar://82744792

--- IntegrationTests/Fixtures/XCBuild/Libraries/Foo/Package.swift
+++ IntegrationTests/Fixtures/XCBuild/Libraries/Foo/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Foo",
     products: [
-        .library(name: "FooLibProduct", type: .static, targets: ["FooLib"]),
+        .library(name: "FooLib", type: .static, targets: ["FooLib"]),
     ],
     dependencies: [
         .package(path: "../Bar")
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .target(name: "FooLib", dependencies: ["CFooLib"]),
         .target(name: "CFooLib", dependencies: [
-            .product(name: "BarLibProduct", package: "Bar"),
+            .product(name: "BarLib", package: "Bar"),
         ]),
     ],
     swiftLanguageVersions: [.v4_2, .v5]
