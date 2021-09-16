swiftlang/swift-package-manager

commit 3d225980bb27b43940f02553b8c43d835746271f

improve manifest loading diagnostics handling (#3720)
motivation: manifest loading uses its own diagniostics engine to be able and communicate errors through the delegate, but the code did not actually use the right dignostics engine

changes: more clearly name the variable and make sure the right diagnotics engine is used while loading the manifest

Sources/Workspace/Workspace.swift
@@ -1619,9 +1619,8 @@ extension Workspace {
         diagnostics: DiagnosticsEngine,
         completion: @escaping (Result<Manifest, Error>) -> Void
     ) {
-        // Load the manifest, bracketed by the calls to the delegate callbacks. The delegate callback is only passed any diagnostics emited during the parsing of the manifest, but they are also forwarded up to the caller.
+        // Load the manifest, bracketed by the calls to the delegate callbacks.
         delegate?.willLoadManifest(packagePath: packagePath, url: packageLocation, version: version, packageKind: packageKind)
-        let manifestDiagnostics = DiagnosticsEngine(handlers: [{diagnostics.emit($0)}])
         diagnostics.with(location: PackageLocation.Local(packagePath: packagePath)) { diagnostics in
             do {
                 // Load the tools version for the package.
@@ -1631,6 +1630,8 @@ extension Workspace {
                 try toolsVersion.validateToolsVersion(currentToolsVersion, packagePath: packagePath.pathString)
 
                 // Load the manifest.
+                // The delegate callback is only passed any diagnostics emitted during the parsing of the manifest, but they are also forwarded up to the caller.
+                let manifestLoadingDiagnostics = DiagnosticsEngine(handlers: [{diagnostics.emit($0)}])
                 manifestLoader.load(at: packagePath,
                                     packageIdentity: packageIdentity,
                                     packageKind: packageKind,
@@ -1640,20 +1641,17 @@ extension Workspace {
                                     toolsVersion: toolsVersion,
                                     identityResolver: self.identityResolver,
                                     fileSystem: localFileSystem,
-                                    diagnostics: diagnostics,
+                                    diagnostics: manifestLoadingDiagnostics,
                                     on: .sharedConcurrent) { result in
 
                     switch result {
+                    // Diagnostics.fatalError indicates that a more specific diagnostic has already been added.
+                    case .failure(Diagnostics.fatalError):
+                        break
                     case .failure(let error):
-                        // Diagnostics.fatalError indicates that a more specific diagnostic has already been added.
-                        switch error {
-                        case Diagnostics.fatalError:
-                            break
-                        default:
-                            diagnostics.emit(error)
-                        }
+                        diagnostics.emit(error)
                     case .success(let manifest):
-                        self.delegate?.didLoadManifest(packagePath: packagePath, url: packageLocation, version: version, packageKind: packageKind, manifest: manifest, diagnostics: manifestDiagnostics.diagnostics)
+                        self.delegate?.didLoadManifest(packagePath: packagePath, url: packageLocation, version: version, packageKind: packageKind, manifest: manifest, diagnostics: manifestLoadingDiagnostics.diagnostics)
                     }
                     completion(result)
                 }

commit 8340e818cfac73efe8176d32ac23ff8abd8ea809

Fix the default location for diagnostics resulting from package manifest loading.
This broke in 3d22598.

Sources/Workspace/Workspace.swift
@@ -1638,7 +1638,7 @@ extension Workspace {
 
                 // Load the manifest.
                 // The delegate callback is only passed any diagnostics emitted during the parsing of the manifest, but they are also forwarded up to the caller.
-                let manifestLoadingDiagnostics = DiagnosticsEngine(handlers: [{diagnostics.emit($0)}])
+                let manifestLoadingDiagnostics = DiagnosticsEngine(handlers: [{ diagnostics.emit($0) }], defaultLocation: diagnostics.defaultLocation)
                 manifestLoader.load(at: packagePath,
                                     packageIdentity: packageIdentity,
                                     packageKind: packageKind,

Tests/WorkspaceTests/WorkspaceTests.swift
@@ -174,6 +174,39 @@ final class WorkspaceTests: XCTestCase {
         }
     }
 
+    func testManifestParseError() throws {
+        try testWithTemporaryDirectory { path in
+            let pkgDir = path.appending(component: "MyPkg")
+            try localFileSystem.writeFileContents(pkgDir.appending(component: "Package.swift")) {
+                $0 <<<
+                    """
+                    // swift-tools-version:4.0
+                    import PackageDescription
+                    #error("An error in MyPkg")
+                    let package = Package(
+                        name: "MyPkg"
+                    )
+                    """
+            }
+            let workspace = try Workspace(
+                fileSystem: localFileSystem,
+                location: .init(forRootPackage: pkgDir, fileSystem: localFileSystem),
+                customManifestLoader: ManifestLoader(toolchain: ToolchainConfiguration.default),
+                delegate: MockWorkspaceDelegate()
+            )
+            let diagnostics = DiagnosticsEngine()
+            let rootInput = PackageGraphRootInput(packages: [pkgDir], dependencies: [])
+            let rootManifests = try tsc_await { workspace.loadRootManifests(packages: rootInput.packages, diagnostics: diagnostics, completion: $0) }
+            XCTAssert(rootManifests.count == 0, "\(rootManifests)")
+            XCTAssert(diagnostics.diagnostics.count == 1, "\(diagnostics.diagnostics)")
+            let diagnostic = try XCTUnwrap(diagnostics.diagnostics.first)
+            XCTAssert(diagnostic.message.behavior == .error, "\(diagnostic.message.behavior)")
+            XCTAssert(diagnostic.message.text.contains("An error in MyPkg"), "\(diagnostic.message.text)")
+            XCTAssert(diagnostic.location is PackageLocation.Local, "\(diagnostic.location)")
+            XCTAssertEqual((diagnostic.location as? PackageLocation.Local)?.packagePath, pkgDir)
+        }
+    }
+
     func testMultipleRootPackages() throws {
         let sandbox = AbsolutePath("/tmp/ws/")
         let fs = InMemoryFileSystem()