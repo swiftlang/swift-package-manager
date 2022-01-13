bdddc36e50247e25aae1fb4a214dc50d72e9d581
Anders Bertelrud <anders@apple.com>
2022-01-13 00:22:54 -0800

    Mark the SwiftPM 5.6 branch as a release branch
    As we do with every release, we need to turn off the development bit in SwiftPM so that it disallows use of 999.0 package manifest tools versions and also (somewhat less importantly) shows the version without a `-dev` suffix in response to `swift package --version`.
    
    Specific changes:
    - change `development` from true to false in SwiftVersion.swift
    
    rdar://87535197

--- Sources/Basics/SwiftVersion.swift
+++ Sources/Basics/SwiftVersion.swift
@@ -1,7 +1,7 @@
 /*
  This source file is part of the Swift.org open source project
 
- Copyright (c) 2021 Apple Inc. and the Swift project authors
+ Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
  Licensed under Apache License v2.0 with Runtime Library Exception
 
  See http://swift.org/LICENSE.txt for license information
@@ -64,7 +64,7 @@ extension SwiftVersion {
     /// The current version of the package manager.
     public static let currentVersion = SwiftVersion(
         version: (5, 6, 0),
-        isDevelopment: true,
+        isDevelopment: false,
         buildIdentifier: getBuildIdentifier()
     )
 }
