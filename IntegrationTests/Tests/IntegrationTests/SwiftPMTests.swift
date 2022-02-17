7b472857095a2a1aefd30ffbe24176da77b02377
tomer doron <tomer@apple.com>
2022-02-16 23:23:39 -0800

    improve example created by package init --type executable
    This reverts commit e58e1c2bab00b69182d63763e43da56d0976d13f.
    
    brings back #4122

--- IntegrationTests/Tests/IntegrationTests/SwiftPMTests.swift
+++ IntegrationTests/Tests/IntegrationTests/SwiftPMTests.swift
@@ -1,12 +1,12 @@
 /*
-This source file is part of the Swift.org open source project
+ This source file is part of the Swift.org open source project
 
-Copyright (c) 2014 - 2020 Apple Inc. and the Swift project authors
-Licensed under Apache License v2.0 with Runtime Library Exception
+ Copyright (c) 2014 - 2020 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
 
-See http://swift.org/LICENSE.txt for license information
-See http://swift.org/CONTRIBUTORS.txt for Swift project authors
-*/
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+ */
 
 import XCTest
 import TSCBasic
@@ -16,13 +16,13 @@ final class SwiftPMTests: XCTestCase {
     func testBinaryTargets() throws {
         try XCTSkip("FIXME: ld: warning: dylib (/../BinaryTargets.6YVYK4/TestBinary/.build/x86_64-apple-macosx/debug/SwiftFramework.framework/SwiftFramework) was built for newer macOS version (10.15) than being linked (10.10)")
 
-        #if !os(macOS)
-            try XCTSkip("Test requires macOS")
-        #endif
+#if !os(macOS)
+        try XCTSkip("Test requires macOS")
+#endif
 
-        try binaryTargetsFixture { prefix in
+        try binaryTargetsFixture { fixturePath in
             do {
-                let (stdout, stderr) = try sh(swiftRun, "--package-path", prefix, "exe")
+                let (stdout, stderr) = try sh(swiftRun, "--package-path", fixturePath, "exe")
                 XCTAssertNoMatch(stderr, .contains("warning: "))
                 XCTAssertEqual(stdout, """
                     SwiftFramework()
@@ -32,18 +32,18 @@ final class SwiftPMTests: XCTestCase {
             }
 
             do {
-                let (stdout, stderr) = try sh(swiftRun, "--package-path", prefix, "cexe")
+                let (stdout, stderr) = try sh(swiftRun, "--package-path", fixturePath, "cexe")
                 XCTAssertNoMatch(stderr, .contains("warning: "))
                 XCTAssertMatch(stdout, .contains("<CLibrary: "))
             }
 
             do {
-                let invalidPath = prefix.appending(component: "SwiftFramework.xcframework")
-                let (_, stderr) = try shFails(swiftPackage, "--package-path", prefix, "compute-checksum", invalidPath)
+                let invalidPath = fixturePath.appending(component: "SwiftFramework.xcframework")
+                let (_, stderr) = try shFails(swiftPackage, "--package-path", fixturePath, "compute-checksum", invalidPath)
                 XCTAssertMatch(stderr, .contains("error: unexpected file type; supported extensions are: zip"))
 
-                let validPath = prefix.appending(component: "SwiftFramework.zip")
-                let (stdout, _) = try sh(swiftPackage, "--package-path", prefix, "compute-checksum", validPath)
+                let validPath = fixturePath.appending(component: "SwiftFramework.zip")
+                let (stdout, _) = try sh(swiftPackage, "--package-path", fixturePath, "compute-checksum", validPath)
                 XCTAssertEqual(stdout.spm_chomp(), "d1f202b1bfe04dea30b2bc4038f8059dcd75a5a176f1d81fcaedb6d3597d1158")
             }
         }
@@ -51,33 +51,35 @@ final class SwiftPMTests: XCTestCase {
 
     func testArchCustomization() throws {
         #if !os(macOS)
-            try XCTSkip("Test requires macOS")
+        try XCTSkip("Test requires macOS")
         #endif
 
         try withTemporaryDirectory { tmpDir in
-            let foo = tmpDir.appending(component: "foo")
-            try localFileSystem.createDirectory(foo)
-            try sh(swiftPackage, "--package-path", foo, "init", "--type", "executable")
-
-            try localFileSystem.removeFileTree(foo.appending(RelativePath("Sources/foo/main.swift")))
-            try localFileSystem.writeFileContents(foo.appending(RelativePath("Sources/foo/main.m"))) {
+            let packagePath = tmpDir.appending(component: "foo")
+            try localFileSystem.createDirectory(packagePath)
+            try sh(swiftPackage, "--package-path", packagePath, "init", "--type", "executable")
+            // delete any files generated
+            for entry in try localFileSystem.getDirectoryContents(packagePath.appending(components: "Sources", "foo")) {
+                try localFileSystem.removeFileTree(packagePath.appending(components: "Sources", "foo", entry))
+            }
+            try localFileSystem.writeFileContents(packagePath.appending(RelativePath("Sources/foo/main.m"))) {
                 $0 <<< "int main() {}"
             }
             let archs = ["x86_64", "arm64"]
 
             for arch in archs {
-                try sh(swiftBuild, "--package-path", foo, "--arch", arch)
-                let fooPath = foo.appending(RelativePath(".build/\(arch)-apple-macosx/debug/foo"))
+                try sh(swiftBuild, "--package-path", packagePath, "--arch", arch)
+                let fooPath = packagePath.appending(RelativePath(".build/\(arch)-apple-macosx/debug/foo"))
                 XCTAssertFileExists(fooPath)
             }
 
-            let args = [swiftBuild.pathString, "--package-path", foo.pathString] + archs.flatMap{ ["--arch", $0] }
+            let args = [swiftBuild.pathString, "--package-path", packagePath.pathString] + archs.flatMap{ ["--arch", $0] }
             try _sh(args)
 
-            let fooPath = foo.appending(RelativePath(".build/apple/Products/Debug/foo"))
+            let fooPath = packagePath.appending(RelativePath(".build/apple/Products/Debug/foo"))
             XCTAssertFileExists(fooPath)
 
-            let objectsDir = foo.appending(RelativePath(".build/apple/Intermediates.noindex/foo.build/Debug/foo.build/Objects-normal"))
+            let objectsDir = packagePath.appending(RelativePath(".build/apple/Intermediates.noindex/foo.build/Debug/foo.build/Objects-normal"))
             for arch in archs {
                 XCTAssertDirectoryExists(objectsDir.appending(component: arch))
             }
