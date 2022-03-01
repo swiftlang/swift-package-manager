0787cae2240fb8e7b17fce56281fd6ef7e72a0f8
tom doron <tomer@apple.com>
2022-02-28 17:52:00 -0800

    fixup

--- Tests/BuildTests/BuildPlanTests.swift
+++ Tests/BuildTests/BuildPlanTests.swift
@@ -469,7 +469,7 @@ final class BuildPlanTests: XCTestCase {
       #else
         XCTAssertEqual(try result.buildProduct(for: "exe").linkArguments(), [
             "/fake/path/to/swiftc", "-g", "-L", "/path/to/build/release",
-            "-o", "/path/to/build/release/exe", "-module-name", "exe", "-emit-executable",
+            "-o", "/path/to/build/release/exe", "-module-name", "exe", "-static-stdlib", "-emit-executable",
             "-Xlinker", "--gc-sections", "-Xlinker", "-rpath=$ORIGIN",
             "@/path/to/build/release/exe.product/Objects.LinkFileList",
             "-target", defaultTargetTriple,
