f936c296a3ecaa401380612f9163b1ec5b26393a
Saleem Abdulrasool <compnerd@compnerd.org>
2022-03-03 09:49:33 -0800

    Build: adjust the BuildSystemDelegate hander
    llbuild does not make guarantees that the output from subcommands are
    emitted in complete buffers.  This shows up particularly well on
    Windows.  clang will emit diagnostics which get read in different
    chunks.  By emitting these eagerly we would print partial messages which
    would leave the terminal in the improper state due to the message not
    being complete.  With multiple parallel threads writing the messages
    partially, we would get splits in the message that would yield
    unreadable messages.
    
    Switch to a buffering approach, buffering the entire content until the
    command exits.  At that time we can emit the complete message in a
    "single" write (which is locked).  This ensures that the output is no
    longer partially emitted and corrects the rendering with the animated
    emission on Windows.

--- Sources/Build/BuildOperationBuildSystemDelegateHandler.swift
+++ Sources/Build/BuildOperationBuildSystemDelegateHandler.swift
@@ -410,6 +410,8 @@ final class BuildOperationBuildSystemDelegateHandler: LLBuildBuildSystemDelegate
     /// Swift parsers keyed by llbuild command name.
     private var swiftParsers: [String: SwiftCompilerOutputParser] = [:]
 
+    private var nonSwiftMessageBuffers: [String:[UInt8]] = [:]
+
     /// The build execution context.
     private let buildExecutionContext: BuildExecutionContext
 
@@ -565,11 +567,7 @@ final class BuildOperationBuildSystemDelegateHandler: LLBuildBuildSystemDelegate
         if let swiftParser = swiftParsers[command.name] {
             swiftParser.parse(bytes: data)
         } else {
-            queue.async {
-                self.progressAnimation.clear()
-                self.outputStream <<< data
-                self.outputStream.flush()
-            }
+            self.nonSwiftMessageBuffers[command.name, default: []] += data
         }
     }
 
@@ -578,6 +576,14 @@ final class BuildOperationBuildSystemDelegateHandler: LLBuildBuildSystemDelegate
         process: ProcessHandle,
         result: CommandExtendedResult
     ) {
+        if let buffer = self.nonSwiftMessageBuffers[command.name] {
+            queue.async {
+                self.progressAnimation.clear()
+                self.outputStream <<< buffer
+                self.outputStream.flush()
+                self.nonSwiftMessageBuffers[command.name] = nil
+            }
+        }
         if result.result == .failed {
             // The command failed, so we queue up an asynchronous task to see if we have any error messages from the target to provide advice about.
             queue.async {
