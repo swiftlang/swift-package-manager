19b9461f22c05c5df699489035c1eb97c5a0924d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-03-25 15:11:30 -0600

    Added rudimentary documentation.

--- Sources/SPMClientUtils/ClientUtils.swift
+++ Sources/SPMClientUtils/ClientUtils.swift
@@ -22,6 +22,24 @@
 
 extension Workspace {
 
+    /// Loads a manifest as the root manifest of the workspace.
+    ///
+    /// This method is most easily used with `tsc_await` from the `TSCBasic` module in the `swift-tools-support-core` package:
+    ///
+    /// ```swift
+    /// let manifest = try tsc_await { completion in
+    ///     someWorkspace.loadRootManifest(
+    ///         at: somePath,
+    ///         diagnosticObserver: someObserver,
+    ///         completion: completion
+    ///     )
+    /// }
+    /// ```
+    ///
+    /// - Parameters:
+    ///     - path: The path to the package’s root directory.
+    ///     - diagnosticObserver: An observer to handle diagnostics.
+    ///     - completion: A closure that will be executed when the loading is complete (or has failed).
     public func loadRootManifest(
         at path: AbsolutePath,
         diagnosticObserver: DiagnosticObserver,
@@ -34,6 +52,24 @@ extension Workspace {
         )
     }
 
+    /// Loads a package as the root package of the workspace.
+    ///
+    /// This method is most easily used with `tsc_await` from the `TSCBasic` module in the `swift-tools-support-core` package:
+    ///
+    /// ```swift
+    /// let package = try tsc_await { completion in
+    ///     someWorkspace.loadRootPackage(
+    ///         at: somePath,
+    ///         diagnosticObserver: someObserver,
+    ///         completion: completion
+    ///     )
+    /// }
+    /// ```
+    ///
+    /// - Parameters:
+    ///     - path: The path to the package’s root directory.
+    ///     - diagnosticObserver: An observer to handle diagnostics.
+    ///     - completion: A closure that will be executed when the loading is complete (or has failed).
     public func loadRootPackage(
         at path: AbsolutePath,
         diagnosticObserver: DiagnosticObserver,
@@ -46,6 +82,12 @@ extension Workspace {
         )
     }
 
+    /// Loads the dependency graph of a package as the root package of the workspace.
+    ///
+    /// - Parameters:
+    ///     - rootPath: The path to the package’s root directory.
+    ///     - explicitProduct: Optional; a string may be passed in order to simulate an explicit product name being supplied to the command line (which may affect the resolution of immediate dependencies by causing the inclusion of an otherwise unused product).
+    ///     - diagnosticObserver: An observer to handle diagnostics.
     public func loadPackageGraph(
         rootPath: AbsolutePath,
         explicitProduct: String? = nil,
@@ -59,10 +101,17 @@ extension Workspace {
     }
 }
 
+/// An observer that handles diagnostics emitted while a workspace is loading its components.
 public struct DiagnosticObserver {
 
     fileprivate let observabilitySystem: ObservabilitySystem
 
+    /// Creates a diagnostic observer.
+    ///
+    /// - Parameters:
+    ///     - handler: A closure that will be executed each time a diagnostic is emitted.
+    ///     - scope: The label of the scope where the diagnostic was emmitted.
+    ///     - diagnostic: The diagnostic message.
     public init(_ handler: @escaping (_ scope: String, _ diagnostic: String) -> Void) {
         observabilitySystem = ObservabilitySystem(
             { (scope: ObservabilityScope, diagnostic: Basics.Diagnostic) -> Void in
