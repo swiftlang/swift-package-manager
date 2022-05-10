ccb04d03c444bae602bba4ff7d6689789105763e
Saleem Abdulrasool <compnerd@compnerd.org>
2022-05-10 14:50:59 -0700

    Add `nuget` package provider
    Introduce the `nuget` package provider, which is the only option on
    Windows, but is portable to other systems as well. This is required
    for enabling the test suite on Windows.
    
    Refs: #4369

--- Sources/PackageLoading/Target+PkgConfig.swift
+++ Sources/PackageLoading/Target+PkgConfig.swift
@@ -126,6 +126,8 @@ extension SystemPackageProviderDescription {
             return "    apt-get install \(packages.joined(separator: " "))\n"
         case .yum(let packages):
             return "    yum install \(packages.joined(separator: " "))\n"
+        case .nuget(let packages):
+            return "    nuget install \(packages.joined(separator: " "))\n"
         }
     }
 
@@ -147,6 +149,13 @@ extension SystemPackageProviderDescription {
         case .yum:
             if case .linux(.fedora) = platform {
                 return true
+            }
+        case .nuget:
+            switch platform {
+            case .darwin, .windows, .linux:
+                return true
+            case .android:
+                return false
             }
         }
         return false
@@ -177,6 +186,8 @@ extension SystemPackageProviderDescription {
         case .apt:
             return []
         case .yum:
+            return []
+        case .nuget:
             return []
         }
     }
