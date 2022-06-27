dbf5bde5cf4efb4b7ba25fac8d0bd2ddbe2cdb4e
Ellie Shin <ellie_shin@apple.com>
2022-06-27 14:29:12 -0700

    update

--- Tests/FunctionalTests/ModuleAliasingFixtureTests.swift
+++ Tests/FunctionalTests/ModuleAliasingFixtureTests.swift
@@ -77,23 +77,4 @@ class ModuleAliasingFixtureTests: XCTestCase {
             XCTAssertEqual(result.exitStatus, .terminated(code: 0), "output: \(output)")
         }
     }
-
-    func testModuleNestedDeps2() throws {
-        #if swift(<5.7)
-        try XCTSkipIf(true, "Module aliasing is only supported on swift 5.7+")
-        #endif
-
-        try fixture(name: "ModuleAliasing/NestedDeps2") { fixturePath in
-            let pkgPath = fixturePath.appending(components: "AppPkg")
-            let buildPath = pkgPath.appending(components: ".build", UserToolchain.default.triple.platformBuildPathComponent(), "debug")
-            XCTAssertBuilds(pkgPath, extraArgs: ["--vv"])
-            XCTAssertFileExists(buildPath.appending(components: "App"))
-            XCTAssertFileExists(buildPath.appending(components: "BUtils.swiftmodule"))
-            XCTAssertFileExists(buildPath.appending(components: "CUtils.swiftmodule"))
-            XCTAssertFileExists(buildPath.appending(components: "XUtils.swiftmodule"))
-            let result = try SwiftPMProduct.SwiftBuild.executeProcess([], packagePath: pkgPath)
-            let output = try result.utf8Output() + result.utf8stderrOutput()
-            XCTAssertEqual(result.exitStatus, .terminated(code: 0), "output: \(output)")
-        }
-    }
 }
