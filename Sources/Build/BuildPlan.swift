eca39ccd9ebe586741c5903aeb670a6cee41d380
tomer doron <tomer@apple.com>
2022-07-21 23:09:13 -0700

    Revert "improve heuristics for @main support (#5678)"
    This reverts commit ea2498143dd7a3670a1305e0ad8859563a0698a8.

--- Sources/Build/BuildPlan.swift
+++ Sources/Build/BuildPlan.swift
@@ -626,26 +626,21 @@ public final class SwiftTargetBuildDescription {
     public let isTestDiscoveryTarget: Bool
 
     /// True if this module needs to be parsed as a library based on the target type and the configuration
-    /// of the source code
+    /// of the source code (for example because it has a single source file whose name isn't "main.swift").
+    /// This deactivates heuristics in the Swift compiler that treats single-file modules and source files
+    /// named "main.swift" specially w.r.t. whether they can have an entry point.
+    ///
+    /// See https://bugs.swift.org/browse/SR-14488 for discussion about improvements so that SwiftPM can
+    /// convey the intent to build an executable module to the compiler regardless of the number of files
+    /// in the module or their names.
     var needsToBeParsedAsLibrary: Bool {
-        switch self.target.type {
+        switch target.type {
         case .library, .test:
             return true
         case .executable:
-            // This deactivates heuristics in the Swift compiler that treats single-file modules and source files
-            // named "main.swift" specially w.r.t. whether they can have an entry point.
-            //
-            // See https://bugs.swift.org/browse/SR-14488 for discussion about improvements so that SwiftPM can
-            // convey the intent to build an executable module to the compiler regardless of the number of files
-            // in the module or their names.
-            if self.toolsVersion < .v5_5 || self.sources.count != 1 {
-                return false
-            }
-            // looking into the file content to see if it is using the @main annotation which requires parse-as-library
-            // this is not bullet-proof since theoretically the file can contain the @main string for other reasons
-            // but it is the closest to accurate we can do at this point
-            let content: String = self.sources.first.flatMap({ try? self.fileSystem.readFileContents($0) }) ?? ""
-            return content.contains("@main")
+            guard toolsVersion >= .v5_5 else { return false }
+            let sources = self.sources
+            return sources.count == 1 && sources.first?.basename != "main.swift"
         default:
             return false
         }
