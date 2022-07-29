cf25d8c55773096e36cccf242c4c098bb112cd46
Liu Liu <i@liuliu.me>
2022-07-29 10:39:32 -0700

    Workaround combinations of `-std=c++17` and `-fmodules` caused crash (#5584)
    * Workaround combinations of `-std=c++17` and `-fmodules` caused crash
    
    On Linux (Ubuntu 20.04), SwiftPM will error out with https://github.com/liuliu/swift-mujoco
    
    The crash reported to llvm-project: https://github.com/llvm/llvm-project/issues/55980
    
    This leads to me investigate how and why these flags added. It turns out
    it is already disabled on Apple platform with C++.
    
    This PR disabled `-fmodules` for C++ at all because these ongoing
    issues. I am not sure if Apple internally benefits with performance
    improvements of `-fmodules` because it is dsiabled on their respective
    platforms.
    
    Alternatively, I can disable this for c++17 only. Let me know what do
    you prefer. Personally I would prefer smaller combinations otherwise you
    will have increasing larger test metrics.
    
    * Disable modules for C++ on all platforms only for Swift 5.7 and above.
    
    * Add test case for c++17 + fmodules crash.
    
    * Update to move this change to vNext.

---
+++ Fixtures/Miscellaneous/CXX17CompilerCrash/v5_7/Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "CXX17WithFModules",
+    targets: [
+        .target(
+            name: "lodepng",
+            path: "lodepng",
+            sources: ["lodepng.cpp"],
+            publicHeadersPath: "include"),
+        .target(name: "CXX17WithFModules", dependencies: ["lodepng"], path: "./", sources: ["src/user_objects.cc"], publicHeadersPath: "include"),
+    ],
+    cxxLanguageStandard: .cxx17
+)
---
+++ Fixtures/Miscellaneous/CXX17CompilerCrash/v5_7/src/user_objects.cc
@@ -0,0 +1,1 @@
+#include "user_objects.h"
