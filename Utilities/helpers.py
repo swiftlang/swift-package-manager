7b898e6cad75a3c096ad947508eb948ad5f614d4
Mishal Shah <shahmishal@users.noreply.github.com>
2022-08-23 20:46:22 -0700

    Migrate to python3 (#5549) (#5743)
    Main swift project moved to python3, moving swiftpm build tools to
    python3 to match.
    
    (cherry picked from commit c0d5daa8fb43d87a6043a79c3cd132987eb40623)
    
    Co-authored-by: Evan Wilde <etceterawilde@gmail.com>
    (cherry picked from commit 61372ba420220b359069e858753a0dfb4f69ac0f)

--- Utilities/helpers.py
+++ Utilities/helpers.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 ##===----------------------------------------------------------------------===##
 ##
 ## This source file is part of the Swift open source project
