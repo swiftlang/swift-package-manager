e00045be31fcab9056656254b14d5d0947ec224e
stevapple <stevapple@icloud.com>
2022-09-08 22:13:52 +0800

    Remove `rdar://73710910` workaround

--- Sources/PackageLoading/ManifestLoader.swift
+++ Sources/PackageLoading/ManifestLoader.swift
@@ -290,8 +290,7 @@ public final class ManifestLoader: ManifestLoaderProtocol {
         // We might have some non-fatal output (warnings/notes) from the compiler even when
         // we were able to parse the manifest successfully.
         if let compilerOutput = result.compilerOutput {
-            // FIXME: Temporary workaround to filter out debug output from integrated Swift driver. [rdar://73710910]
-            if !(compilerOutput.hasPrefix("<unknown>:0: remark: new Swift driver at") && compilerOutput.hasSuffix("will be used")) {
+            if !compilerOutput.isEmpty {
                 let metadata = result.diagnosticFile.map { diagnosticFile -> ObservabilityMetadata in
                     var metadata = ObservabilityMetadata()
                     metadata.manifestLoadingDiagnosticFile = diagnosticFile
