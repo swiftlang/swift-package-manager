77f58f12117e3aa0cf1ee2127e371132a7854f04
Mishal Shah <shahmishal@users.noreply.github.com>
2022-09-20 10:14:22 -0700

    Update the version to 5.7.1 to match Swift compiler

--- Sources/Basics/SwiftVersion.swift
+++ Sources/Basics/SwiftVersion.swift
@@ -54,7 +54,7 @@ public struct SwiftVersion {
 extension SwiftVersion {
     /// The current version of the package manager.
     public static let current = SwiftVersion(
-        version: (5, 7, 0),
+        version: (5, 7, 1),
         isDevelopment: false,
         buildIdentifier: getBuildIdentifier()
     )
