0de0bc86b8b963a88eeed526ed63839f1e52399e
Boris Bügling <bbuegling@apple.com>
2022-09-27 17:17:45 -0700

    Improve collections configuration warning (#5783)
    If we just copied the config to the new location or if the configs at old and new location are identical, do not warn about the presence of both configs. The warning's purpose is making people aware of the fact that different versions of SwiftPM are using different configs, but if they are identical, there's no need for them to really know about it.
    
    rdar://99721607

--- Sources/Workspace/Workspace.swift
+++ Sources/Workspace/Workspace.swift
@@ -3660,7 +3660,7 @@ extension FileSystem {
 extension Workspace.Location {
     func validatingSharedLocations(
         fileSystem: FileSystem,
-        warningHandler: (String) -> Void
+        warningHandler: @escaping (String) -> Void
     ) throws -> Self {
         var location = self
 
