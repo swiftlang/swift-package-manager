256c68237e3ed62867cd163b78eed3640e211710
Boris Bügling <bbuegling@apple.com>
2022-10-31 15:37:29 -0700

    Fix `disableSandboxForPluginCommands` (#5855)

--- Sources/Build/BuildOperation.swift
+++ Sources/Build/BuildOperation.swift
@@ -102,7 +102,7 @@ public final class BuildOperation: PackageStructureDelegate, SPMBuildCore.BuildS
         additionalFileRules: [FileRuleDescription],
         pluginScriptRunner: PluginScriptRunner,
         pluginWorkDirectory: AbsolutePath,
-        disableSandboxForPluginCommands: Bool = false,
+        disableSandboxForPluginCommands: Bool,
         outputStream: OutputByteStream,
         logLevel: Basics.Diagnostic.Severity,
         fileSystem: TSCBasic.FileSystem,
