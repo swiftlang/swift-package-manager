773bd474a34c51fb0994bde5604cc67b230e022c
Nick Cooke <nickcooke@google.com>
2022-12-24 18:11:28 -0500

    Remove outdated comment

--- Sources/Build/BuildPlan.swift
+++ Sources/Build/BuildPlan.swift
@@ -1517,8 +1517,6 @@ public final class MixedTargetBuildDescription {
             self.moduleMap = productModuleMapPath
         }
 
-        // ------- FINAL END ------- //
-
         // MARK: Generate intermediate artifacts used to build the target.
         // Building a mixed target uses intermediate module maps to expose
         // private headers to the Swift part of the module.
