ecc929199b96425ffd61dcc4cc652ea30fff039b
Nick Cooke <nickcooke@google.com>
2022-12-26 15:02:00 -0500

    Remove newline

--- Sources/Build/BuildPlan.swift
+++ Sources/Build/BuildPlan.swift
@@ -437,7 +437,6 @@ public final class ClangTargetBuildDescription {
         }
 
         args += ["-I", clangTarget.includeDir.pathString]
-
         args += additionalFlags
         if enableModules {
             args += try moduleCacheArgs
