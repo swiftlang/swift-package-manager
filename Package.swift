f8e2987fb85da1de5971f4b37946eab42d5cacdf
Max Desiatov <m_desiatov@apple.com>
2023-01-05 20:01:24 +0000

    5.8: set `relatedDependenciesBranch` to 5.8 in `Package.swift` (#5993)
    This makes sure that dependencies of SwiftPM built with SwiftPM and coming from the Swift project use `release/5.8` branch.

--- Package.swift
+++ Package.swift
@@ -651,7 +651,7 @@ let package = Package(
 // this right now.
 
 /// When not using local dependencies, the branch to use for llbuild and TSC repositories.
-let relatedDependenciesBranch = "main"
+let relatedDependenciesBranch = "release/5.8"
 
 if ProcessInfo.processInfo.environment["SWIFTPM_LLBUILD_FWK"] == nil {
     if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
