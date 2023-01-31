707f4821491d87b04ea763d0f2e8a124e6bcb1d5
Max Desiatov <m_desiatov@apple.com>
2023-01-31 11:36:33 +0000

    Fix bad merge

--- Sources/Basics/FileSystem/AsyncFileSystem.swift
+++ Sources/Basics/FileSystem/AsyncFileSystem.swift
@@ -231,8 +231,6 @@ extension AsyncFileSystem {
     }
 }
 
-    func withLock<T>(on path: AbsolutePath, type: FileLock.LockType, _ body: () throws -> T) throws -> T {
-        throw FileSystemError(.unsupported, path)
 extension AsyncFileSystem {
     public func forceCreateDirectory(at path: AbsolutePath) throws {
         try self.createDirectory(path.parentDirectory, recursive: true)
