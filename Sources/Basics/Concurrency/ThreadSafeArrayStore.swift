cc69a984280d272ba4dcd4a734990315ed16b482
Max Desiatov <m_desiatov@apple.com>
2023-02-08 23:37:57 +0000

    Fix `MockHashAlgorithm` conformance to `Sendable` (#6133)
    This `Sendable` requirement on `HashAlgorithm` appeared in recent commits of TSC.

--- Sources/Basics/Concurrency/ThreadSafeArrayStore.swift
+++ Sources/Basics/Concurrency/ThreadSafeArrayStore.swift
@@ -82,3 +82,9 @@ public final class ThreadSafeArrayStore<Value> {
         }
     }
 }
+
+#if swift(<5.7)
+extension ThreadSafeArrayStore: UnsafeSendable where Value: Sendable {}
+#else
+extension ThreadSafeArrayStore: @unchecked Sendable where Value: Sendable {}
+#endif
