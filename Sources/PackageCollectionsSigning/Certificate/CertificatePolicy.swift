36d0a8af200e2147406616089584998552f222cf
Boris Bügling <bbuegling@apple.com>
2023-03-03 21:08:06 -0800

    Integrate swift-certificates library (#6216)
    * Integrate swift-certificates library
    
    This integrates the swift-certificates library into the build.
    
    * Add swift-crypto back to bootstrap
    
    Co-authored-by: Yim Lee <yim_lee@apple.com>
    
    * Fix CMake build
    
    * Fix up docs and swift-crypto
    
    ---------
    
    Co-authored-by: Yim Lee <yim_lee@apple.com>

--- Sources/PackageCollectionsSigning/Certificate/CertificatePolicy.swift
+++ Sources/PackageCollectionsSigning/Certificate/CertificatePolicy.swift
@@ -102,11 +102,7 @@ extension CertificatePolicy {
     }
 
     #elseif os(Linux) || os(Windows) || os(Android)
-    #if CRYPTO_v2
     typealias BoringSSLVerifyCallback = @convention(c) (CInt, OpaquePointer?) -> CInt
-    #else
-    typealias BoringSSLVerifyCallback = @convention(c) (CInt, UnsafeMutablePointer<X509_STORE_CTX>?) -> CInt
-    #endif
 
     /// Verifies a certificate chain.
     ///
