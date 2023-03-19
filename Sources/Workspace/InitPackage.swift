9e11016f3551cea116ae1e0ac84db086ba8bbc31
Doug Gregor <dgregor@apple.com>
2023-03-19 11:55:41 -0700

    [Macro package init] Use triple-slash documentation comments (#6306)
    In the generated `stringify` macro declaration, use triple-slash
    documentation comments rather than double-slash comments, so we get
    better documentation.

--- Sources/Workspace/InitPackage.swift
+++ Sources/Workspace/InitPackage.swift
@@ -415,12 +415,12 @@ public final class InitPackage {
             // The Swift Programming Language
             // https://docs.swift.org/swift-book
 
-            // A macro that produces both a value and a string containing the
-            // source code that generated the value. For example,
-            //
-            //     #stringify(x + y)
-            //
-            // produces a tuple `(x + y, "x + y")`.
+            /// A macro that produces both a value and a string containing the
+            /// source code that generated the value. For example,
+            ///
+            ///     #stringify(x + y)
+            ///
+            /// produces a tuple `(x + y, "x + y")`.
             @freestanding(expression)
             public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "\(moduleName)Macros", type: "StringifyMacro")
             """
