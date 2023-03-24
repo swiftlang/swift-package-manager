54761c750a21759da26c87cdc0128ef623754903
Boris Buegling <bbuegling@apple.com>
2023-03-24 11:36:53 -0700

    Turn development mode back on
    It seems like we were a little too quick with this. We do want users to be able to try out macros with the 5.9 toolchain, but they are still a pre-release feature.

--- Sources/Basics/SwiftVersion.swift
+++ Sources/Basics/SwiftVersion.swift
@@ -55,7 +55,7 @@ extension SwiftVersion {
     /// The current version of the package manager.
     public static let current = SwiftVersion(
         version: (5, 9, 0),
-        isDevelopment: false,
+        isDevelopment: true,
         buildIdentifier: getBuildIdentifier()
     )
 }
