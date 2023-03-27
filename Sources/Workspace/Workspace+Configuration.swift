d52001bb7fad11295beda3dfeccfeeeca6d872bb
tomer doron <tomer@apple.com>
2023-03-27 14:13:11 -0700

    improve diagnostics when registry configuration is broken (#6331)
    motivation: better user experience
    
    changes: catch the JSON error and wrap it with a more detailed diagnostics
    
    Co-authored-by: Max Desiatov <m_desiatov@apple.com>

--- Sources/Workspace/Workspace+Configuration.swift
+++ Sources/Workspace/Workspace+Configuration.swift
@@ -684,9 +684,12 @@ extension Workspace.Configuration {
                 return RegistryConfiguration()
             }
 
-            let data: Data = try fileSystem.readFileContents(self.path)
-            let decoder = JSONDecoder.makeWithDefaults()
-            return try decoder.decode(RegistryConfiguration.self, from: data)
+            do {
+                let decoder = JSONDecoder.makeWithDefaults()
+                return try decoder.decode(path: self.path, fileSystem: self.fileSystem, as: RegistryConfiguration.self)
+            } catch {
+                throw StringError("Failed loading registries configuration from '\(self.path)': \(error)")
+            }
         }
 
         public func save(_ configuration: RegistryConfiguration) throws {
