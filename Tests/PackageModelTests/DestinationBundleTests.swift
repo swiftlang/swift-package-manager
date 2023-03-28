ddcaeee78e239178fa9ff3dcea8d09603b4be4c0
Max Desiatov <m_desiatov@apple.com>
2023-03-28 18:42:32 +0100

    Destinations: throw error on duplicate artifact ID (#6343)
    `swift experimental-destination install` command doesn't check if a newly installed bundle contains same IDs as already installed bundles.
    
    Moved installation code from `InstallDestination.swift` to separate static functions on `DestinationBundle` to make it testable. Also added tests that exercise these code paths and verify that installation is successful or throws correct errors when needed.
    
    rdar://106090132

---
+++ Tests/PackageModelTests/DestinationBundleTests.swift
@@ -0,0 +1,125 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Basics
+import PackageModel
+import XCTest
+
+import struct TSCBasic.AbsolutePath
+import struct TSCBasic.ByteString
+import class TSCBasic.InMemoryFileSystem
+
+private let testArtifactID = "test-artifact"
+
+private let infoJSON = ByteString(stringLiteral: """
+{
+  "artifacts" : {
+    "\(testArtifactID)" : {
+      "type" : "crossCompilationDestination",
+      "version" : "0.0.1",
+      "variants" : [
+        {
+          "path" : "\(testArtifactID)/aarch64-unknown-linux",
+          "supportedTriples" : [
+            "arm64-apple-macosx13.0"
+          ]
+        }
+      ]
+    }
+  },
+  "schemaVersion" : "1.0"
+}
+""")
+
+final class DestinationBundleTests: XCTestCase {
+    func testInstallDestination() throws {
+        let system = ObservabilitySystem.makeForTesting()
+
+        let bundleName1 = "test1.artifactbundle"
+        let bundleName2 = "test2.artifactbundle"
+        let bundlePath1 = "/\(bundleName1)"
+        let bundlePath2 = "/\(bundleName2)"
+        let destinationsDirectory = try AbsolutePath(validating: "/destinations")
+        let fileSystem = InMemoryFileSystem(files: [
+            "\(bundlePath1)/info.json": infoJSON,
+            "\(bundlePath2)/info.json": infoJSON,
+        ])
+        try fileSystem.createDirectory(destinationsDirectory)
+
+        try DestinationBundle.install(
+            bundlePathOrURL: bundlePath1,
+            destinationsDirectory: destinationsDirectory,
+            fileSystem,
+            system.topScope
+        )
+
+        let invalidPath = "foobar"
+        XCTAssertThrowsError(try DestinationBundle.install(
+            bundlePathOrURL: "foobar",
+            destinationsDirectory: destinationsDirectory,
+            fileSystem,
+            system.topScope
+        )) {
+            guard let error = $0 as? DestinationError else {
+                XCTFail("Unexpected error type")
+                return
+            }
+
+            switch error {
+            case .pathIsNotDirectory(let path):
+                XCTAssertEqual(path.pathString, "/\(invalidPath)")
+            default:
+                XCTFail("Unexpected error value")
+            }
+        }
+
+        XCTAssertThrowsError(try DestinationBundle.install(
+            bundlePathOrURL: bundlePath1,
+            destinationsDirectory: destinationsDirectory,
+            fileSystem,
+            system.topScope
+        )) {
+            guard let error = $0 as? DestinationError else {
+                XCTFail("Unexpected error type")
+                return
+            }
+
+            switch error {
+            case .destinationBundleAlreadyInstalled(let installedBundleName):
+                XCTAssertEqual(bundleName1, installedBundleName)
+            default:
+                XCTFail("Unexpected error value")
+            }
+        }
+
+        XCTAssertThrowsError(try DestinationBundle.install(
+            bundlePathOrURL: bundlePath2,
+            destinationsDirectory: destinationsDirectory,
+            fileSystem,
+            system.topScope
+        )) {
+            guard let error = $0 as? DestinationError else {
+                XCTFail("Unexpected error type")
+                return
+            }
+
+            switch error {
+            case .destinationArtifactAlreadyInstalled(let installedBundleName, let newBundleName, let artifactID):
+                XCTAssertEqual(bundleName1, installedBundleName)
+                XCTAssertEqual(bundleName2, newBundleName)
+                XCTAssertEqual(artifactID, testArtifactID)
+            default:
+                XCTFail("Unexpected error value")
+            }
+        }
+    }
+}
