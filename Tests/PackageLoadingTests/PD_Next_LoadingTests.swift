swiftlang/swift-package-manager

commit aaa47e9798271d818cad88179b2b95ac87523b8b

Support Target Grouping (#6356)
Support Target Grouping
- Allow opting out of package boundary
- Default set to package boundary

Resolves rdar://107454728

Fixtures/Miscellaneous/TargetGrouping/libPkg/Package.swift
@@ -1,0 +1,19 @@
+// swift-tools-version:999.0
+import PackageDescription
+
+let package = Package(
+    name: "libPkg",
+    products: [
+        .executable(name: "ExampleApp", targets: ["ExampleApp"]),
+        .library(name: "MainLib", targets: ["MainLib"]),
+    ],
+    targets: [
+        .executableTarget(name: "ExampleApp", group: .excluded, dependencies: ["MainLib"]),
+        .target(name: "MainLib", group: .package, dependencies: ["Core"]),
+        .target(name: "Core", dependencies: ["DataManager"]),
+        .target(name: "DataManager", group: .package, dependencies: ["DataModel"]),
+        .target(name: "DataModel"),
+        .testTarget(name: "MainLibTests", group: .package, dependencies: ["MainLib"]),
+        .testTarget(name: "BlackBoxTests", group: .excluded, dependencies: ["MainLib"])
+    ]
+)

Fixtures/Miscellaneous/TargetGrouping/libPkg/Sources/Core/Core.swift
@@ -1,0 +1,11 @@
+import DataManager
+
+public struct PublicCore {
+    public let publicVar: Int
+    public init(publicVar: Int) {
+        self.publicVar = publicVar
+    }
+    public func publicCoreFunc() {
+        managePublicFunc()
+    }
+}

Fixtures/Miscellaneous/TargetGrouping/libPkg/Sources/DataManager/File.swift
@@ -1,0 +1,5 @@
+import DataModel
+
+public func managePublicFunc() -> PublicData? {
+    return nil
+}

Fixtures/Miscellaneous/TargetGrouping/libPkg/Sources/DataModel/File.swift
@@ -1,0 +1,1 @@
+public class PublicData {}

Fixtures/Miscellaneous/TargetGrouping/libPkg/Sources/ExampleApp/main.swift
@@ -1,0 +1,3 @@
+import MainLib
+
+publicFunc()

Fixtures/Miscellaneous/TargetGrouping/libPkg/Sources/MainLib/Lib.swift
@@ -1,0 +1,6 @@
+import Core
+
+public func publicFunc() -> Int {
+    print("public decl")
+    return PublicCore(publicVar: 10).publicVar
+}

Fixtures/Miscellaneous/TargetGrouping/libPkg/Tests/BlackBoxTests/BlackBoxTests.swift
@@ -1,0 +1,9 @@
+import MainLib
+import XCTest
+
+class BlackBoxTests: XCTestCase {
+    func testBlackBox() {
+        let x = publicFunc()
+        XCTAssertTrue(x > 0)
+    }
+}

Fixtures/Miscellaneous/TargetGrouping/libPkg/Tests/MainLibTests/MainLibTests.swift
@@ -1,0 +1,9 @@
+import MainLib
+import XCTest
+
+class TestMainLib: XCTestCase {
+    func testMainLib() {
+        let x = publicFunc()
+        XCTAssertTrue(x > 0)
+    }
+}

commit a3c19181f44c6e0ba8620dacac7eea3c6fe965ea

Add a minimal test for the PD macro API
This should avoid regressions like the one introduced by #6356

Tests/PackageLoadingTests/PD_Next_LoadingTests.swift
@@ -39,4 +39,21 @@ class PackageDescriptionNextLoadingTests: PackageDescriptionLoadingTests {
             }
         }
     }
+
+    func testMacroTargets() throws {
+        let content = """
+            import CompilerPluginSupport
+            import PackageDescription
+
+            let package = Package(name: "MyPackage",
+                targets: [
+                    .macro(name: "MyMacro"),
+                ]
+            )
+            """
+
+        let observability = ObservabilitySystem.makeForTesting()
+        let (_, diagnostics) = try loadAndValidateManifest(content, observabilityScope: observability.topScope)
+        XCTAssertEqual(diagnostics.count, 0, "unexpected diagnostics: \(diagnostics)")
+    }
 }
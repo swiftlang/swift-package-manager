a3c19181f44c6e0ba8620dacac7eea3c6fe965ea
Boris Buegling <bbuegling@apple.com>
2023-04-06 09:54:10 -0700

    Add a minimal test for the PD macro API
    This should avoid regressions like the one introduced by #6356

--- Tests/PackageLoadingTests/PD_Next_LoadingTests.swift
+++ Tests/PackageLoadingTests/PD_Next_LoadingTests.swift
@@ -39,4 +39,21 @@ class PackageDescriptionNextLoadingTests: PackageDescriptionLoadingTests {
             }
         }
     }
+
+    func testMacroTargets() throws {
+        let content = """
+            import CompilerPluginSupport
+            import PackageDescription
+
+            let package = Package(name: "MyPackage",
+                targets: [
+                    .macro(name: "MyMacro"),
+                ]
+            )
+            """
+
+        let observability = ObservabilitySystem.makeForTesting()
+        let (_, diagnostics) = try loadAndValidateManifest(content, observabilityScope: observability.topScope)
+        XCTAssertEqual(diagnostics.count, 0, "unexpected diagnostics: \(diagnostics)")
+    }
 }
