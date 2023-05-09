78cceb7b6bd32b334999cb0a4837e970bdce990a
Boris Bügling <bbuegling@apple.com>
2023-05-09 15:13:23 -0700

    Fix plugin templates (#6517)
    We are using `pkgname` everywhere as target names and also as dependency references, but plugin targets were using `typeName` which leads to newly created plugin packages being broken if the difference was meaningful (e.g. if the name includes dashes).

--- Sources/Workspace/InitPackage.swift
+++ Sources/Workspace/InitPackage.swift
@@ -294,7 +294,7 @@ public final class InitPackage {
                 } else if packageType == .buildToolPlugin {
                     param += """
                             .plugin(
-                                name: "\(typeName)",
+                                name: "\(pkgname)",
                                 capability: .buildTool()
                             ),
                         ]
@@ -302,7 +302,7 @@ public final class InitPackage {
                 } else if packageType == .commandPlugin {
                     param += """
                             .plugin(
-                                name: "\(typeName)",
+                                name: "\(pkgname)",
                                 capability: .command(intent: .custom(
                                     verb: "\(typeName)",
                                     description: "prints hello world"
