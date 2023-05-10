a2436ede5b7f088e76d5b8566ec0c30d04d785c5
Ellie Shin <ellie_shin@apple.com>
2023-05-10 07:29:49 -0700

    Enable package visibility feature in tools version 5.9
    The package visibility proposal has officially been accepted:
    https://forums.swift.org/t/accepted-se-0386-package-access-modifier/64904
    Resolves rdar://109152010

--- Sources/PackageModel/PackageModel.swift
+++ Sources/PackageModel/PackageModel.swift
@@ -126,7 +126,7 @@ extension Manifest {
         return self.toolsVersion >= .v5_8
     }
     public var usePackageNameFlag: Bool {
-        return self.toolsVersion >= .vNext
+        return self.toolsVersion >= .v5_9
     }
 }
 
