b1ef178f2abc36a4abdc1641d21b612fd4539b61
Max Desiatov <m_desiatov@apple.com>
2023-05-10 21:05:10 +0100

    Flip the quarantine check condition for tests to pass

--- Sources/PackageModel/SwiftSDKBundle.swift
+++ Sources/PackageModel/SwiftSDKBundle.swift
@@ -248,7 +248,7 @@ public struct SwiftSDKBundle {
     ) throws {
         #if os(macOS)
         // Check the quarantine attribute on bundles downloaded manually in the browser.
-        guard fileSystem.hasAttribute(.quarantine, bundlePath) else {
+        guard !fileSystem.hasAttribute(.quarantine, bundlePath) else {
             throw DestinationError.quarantineAttributePresent(bundlePath: bundlePath)
         }
         #endif
