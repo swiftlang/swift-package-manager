2c57ad1d4ff1c40f988c6acfc0aa9fe58c68f25b
Boris Bügling <bbuegling@apple.com>
2023-05-12 16:19:29 -0700

    Add comma to executable template (#6551)
    We do this in all the other templates and it generally makes it easier to add new targets to the template after the fact.

--- Sources/Workspace/InitPackage.swift
+++ Sources/Workspace/InitPackage.swift
@@ -278,7 +278,7 @@ public final class InitPackage {
                 if packageType == .executable {
                     param += """
                             .executableTarget(
-                                name: "\(pkgname)")
+                                name: "\(pkgname)"),
                         ]
                     """
                 } else if packageType == .tool {
