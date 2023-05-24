ca77856f1f9396d791f3544c1e565d86a8f8533f
Max Desiatov <m_desiatov@apple.com>
2023-05-24 21:28:34 +0100

    Use `forTimeInterval:` argument name for `Thread.sleep`

--- Fixtures/Miscellaneous/HangingTest/Tests/HangingTestTests/HangingTestTests.swift
+++ Fixtures/Miscellaneous/HangingTest/Tests/HangingTestTests/HangingTestTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class aaaaTests: XCTestCase {
     func testExample() throws {
         while true {
-            Thread.sleep(for: 1)
+            Thread.sleep(forTimeInterval: 1)
         }
     }
 }
