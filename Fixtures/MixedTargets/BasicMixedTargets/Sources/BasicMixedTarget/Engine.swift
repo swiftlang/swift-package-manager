b01c677869395b257c3921c426a63eb6e659cae3
Nick Cooke <nickcooke@google.com>
2023-07-30 11:24:03 -0400

    Update test target Swift code to not reference non-public types

--- Fixtures/MixedTargets/BasicMixedTargets/Sources/BasicMixedTarget/Engine.swift
+++ Fixtures/MixedTargets/BasicMixedTargets/Sources/BasicMixedTarget/Engine.swift
@@ -1,4 +1,7 @@
 import Foundation
 
 // This type is Objective-C compatible and used in `OldCar`.
-@objc public class Engine: CarPart {}
+// FIXME(ncooke3): When Swift compiler's header generation logic is updated,
+// subclass type from Objective-C.
+// @objc public class Engine: CarPart {}
+@objc public class Engine: NDObject {}
--- Fixtures/MixedTargets/BasicMixedTargets/Sources/BasicMixedTarget/NewCar.swift
+++ Fixtures/MixedTargets/BasicMixedTargets/Sources/BasicMixedTarget/NewCar.swift
@@ -5,10 +5,11 @@ public class NewCar {
     var engine: Engine? = nil
     // The following types are defined in Objective-C.
     var driver: Driver? = nil
-    var transmission: Transmission? = nil
-    var hasStickShift: Bool {
-        return transmission != nil && transmission!.transmissionKind == .manual 
-    }
+    // TODO(ncooke3): Update when non-public headers are exposed to Swift.
+    // var transmission: Transmission? = nil
+    // var hasStickShift: Bool {
+    //     return transmission != nil && transmission!.transmissionKind == .manual
+    // }
 
     public init() {}
 }
