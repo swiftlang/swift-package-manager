63e73626d1db54441ea9e4ee67ca506d92602ec9
Boris Buegling <bbuegling@apple.com>
2023-07-31 13:30:12 -0700

    Fix another XCBuild compatibility issue
    Similar to #6752, there was another case where we needed the new setting.
    
    rdar://113157040

--- Sources/XCBuildSupport/PIFBuilder.swift
+++ Sources/XCBuildSupport/PIFBuilder.swift
@@ -677,6 +677,7 @@ final class PackagePIFProjectBuilder: PIFProjectBuilder {
 
         if let resourceBundle = addResourceBundle(for: target, in: pifTarget) {
             settings[.PACKAGE_RESOURCE_BUNDLE_NAME] = resourceBundle
+            settings[.GENERATE_RESOURCE_ACCESSORS] = "YES"
             impartedSettings[.EMBED_PACKAGE_RESOURCE_BUNDLE_NAMES, default: ["$(inherited)"]].append(resourceBundle)
         }
 
