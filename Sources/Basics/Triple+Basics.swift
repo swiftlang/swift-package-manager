79be31adc42e09ed8a9f976986a4e40787bb0ef3
Max Desiatov <m_desiatov@apple.com>
2023-08-16 15:53:22 +0100

    Fix backward compatibility in host triple checks
    It's useful to be able to set host triple in Swift SDKs to `arm64-apple-macosx13.0` to allow cross-compiling from an older version of macOS. This triple is not recognized as directly matching `arm64-apple-macosx14.0` on a newer version of macOS.
    
    We should support backward compatibility with Swift SDKs that were built for older version of macOS.
    
    (cherry picked from commit 82b1e83728092ff82a1941248410832086b6c88a)
    
    # Conflicts:
    #	Sources/PackageModel/SwiftSDKBundle.swift
    #	Tests/PackageModelTests/SwiftSDKBundleTests.swift

--- Sources/Basics/Triple+Basics.swift
+++ Sources/Basics/Triple+Basics.swift
@@ -117,6 +117,14 @@ extension Triple {
             )
         }
     }
+
+    public func matches(_ triple: Triple) -> Bool {
+        guard self.isMacOSX, let version = self._macOSVersion, let comparedVersion = triple._macOSVersion else {
+            return self.tripleString == triple.tripleString
+        }
+
+        return version >= comparedVersion
+    }
 }
 
 extension Triple {
