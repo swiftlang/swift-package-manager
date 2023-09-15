de8e6e8e416f0ddf66ded9bf9c9abafe9517f3c3
Boris Bügling <bbuegling@apple.com>
2023-09-15 09:33:27 -0700

    Compute path to `swift-plugin-server` on demand (#6899)
    This takes a significant amount of time when the selected Xcode doesn't have a `swift-plugin-server` executable:
    
    ```
    /tmp/exec
    ❯ time /usr/bin/xcrun --find swift-plugin-server
    xcrun: error: sh -c '/Users/neonacho/Downloads/Xcode.app/Contents/Developer/usr/bin/xcodebuild -sdk /Users/neonacho/Downloads/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.3.sdk -find swift-plugin-server 2> /dev/null' failed with exit code 17664: (null) (errno=No such file or directory)
    xcrun: error: unable to find utility "swift-plugin-server", not a developer tool or in PATH
    /usr/bin/xcrun --find swift-plugin-server  0.34s user 0.17s system 32% cpu 1.579 total
    ```
    
    Doing this on demand is at least gated on `self.buildParameters.toolchain.isSwiftDevelopmentToolchain` so it shouldn't happen for the primary usage scenario on macOS.

--- Sources/PackageModel/UserToolchain.swift
+++ Sources/PackageModel/UserToolchain.swift
@@ -601,13 +601,10 @@ public final class UserToolchain: Toolchain {
             environment: environment
         )
 
-        let swiftPluginServerPath: AbsolutePath?
         let xctestPath: AbsolutePath?
         if case .custom(_, let useXcrun) = searchStrategy, !useXcrun {
-            swiftPluginServerPath = nil
             xctestPath = nil
         } else {
-            swiftPluginServerPath = try Self.derivePluginServerPath(triple: triple)
             xctestPath = try Self.deriveXCTestPath(
                 swiftSDK: self.swiftSDK,
                 triple: triple,
@@ -622,8 +619,7 @@ public final class UserToolchain: Toolchain {
             swiftCompilerEnvironment: environment,
             swiftPMLibrariesLocation: swiftPMLibrariesLocation,
             sdkRootPath: self.swiftSDK.pathsConfiguration.sdkRootPath,
-            xctestPath: xctestPath,
-            swiftPluginServerPath: swiftPluginServerPath
+            xctestPath: xctestPath
         )
     }
 
@@ -697,8 +693,8 @@ public final class UserToolchain: Toolchain {
 
     private static func derivePluginServerPath(triple: Triple) throws -> AbsolutePath? {
         if triple.isDarwin() {
-            let xctestFindArgs = ["/usr/bin/xcrun", "--find", "swift-plugin-server"]
-            if let path = try? TSCBasic.Process.checkNonZeroExit(arguments: xctestFindArgs, environment: [:])
+            let pluginServerPathFindArgs = ["/usr/bin/xcrun", "--find", "swift-plugin-server"]
+            if let path = try? TSCBasic.Process.checkNonZeroExit(arguments: pluginServerPathFindArgs, environment: [:])
                 .spm_chomp() {
                 return try AbsolutePath(validating: path)
             }
@@ -829,7 +825,13 @@ public final class UserToolchain: Toolchain {
         configuration.xctestPath
     }
 
+    private let _swiftPluginServerPath = ThreadSafeBox<AbsolutePath?>()
+
     public var swiftPluginServerPath: AbsolutePath? {
-        configuration.swiftPluginServerPath
+        get throws {
+            try _swiftPluginServerPath.memoize {
+                return try Self.derivePluginServerPath(triple: self.targetTriple)
+            }
+        }
     }
 }
