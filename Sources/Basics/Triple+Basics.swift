5bcb8b89e271032bea7e6deefa7f7b2b8a610943
Max Desiatov <m_desiatov@apple.com>
2023-09-20 19:21:09 +0100

    Fix test failure after rebasing on top of latest `main`

--- Sources/Basics/Triple+Basics.swift
+++ Sources/Basics/Triple+Basics.swift
@@ -186,13 +186,21 @@ extension Triple {
 
     /// Returns `true` if code compiled for `triple` can run on `self` value of ``Triple``.
     public func isRuntimeCompatible(with triple: Triple) -> Bool {
+        guard self != triple else {
+            return true
+        }
+
         if
             self.arch == triple.arch &&
             self.vendor == triple.vendor &&
             self.os == triple.os &&
-            self.environment == triple.environment
+            self.environment == triple.environment,
+            // If either of the triples have no `osVersion` specified, we can't determine compatibility and will
+            // have to return `false`.
+            let selfOSVersion = self.osVersion,
+            let osVersion = triple.osVersion
         {
-            return self.osVersion >= triple.osVersion
+            return selfOSVersion >= osVersion
         } else {
             return false
         }
