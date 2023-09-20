6ba5b96ab587c204bd7a799ccaf2ae24be208853
Boris Bügling <bbuegling@apple.com>
2023-09-19 19:38:55 -0700

    Skip tests which need `-suppress-warnings` to work if it doesn't work (#6930)

--- Sources/SPMTestSupport/Toolchain.swift
+++ Sources/SPMTestSupport/Toolchain.swift
@@ -111,6 +111,29 @@ extension UserToolchain {
         }
     }
 
+    public func supportsSuppressWarnings() -> Bool {
+        do {
+            try testWithTemporaryDirectory { tmpPath in
+                let inputPath = tmpPath.appending("best.swift")
+                try localFileSystem.writeFileContents(inputPath, string: "let foo: String? = \"bar\"\nprint(foo)\n")
+                let outputPath = tmpPath.appending("foo")
+                let serializedDiagnosticsPath = tmpPath.appending("out.dia")
+                let toolchainPath = self.swiftCompilerPath.parentDirectory.parentDirectory
+                try Process.checkNonZeroExit(arguments: ["/usr/bin/xcrun", "--toolchain", toolchainPath.pathString, "swiftc", inputPath.pathString, "-Xfrontend", "-serialize-diagnostics-path", "-Xfrontend", serializedDiagnosticsPath.pathString, "-o", outputPath.pathString, "-suppress-warnings"])
+
+                let diaFileContents = try localFileSystem.readFileContents(serializedDiagnosticsPath)
+                let diagnosticsSet = try SerializedDiagnostics(bytes: diaFileContents)
+
+                if diagnosticsSet.diagnostics.contains(where: { $0.text.contains("warning") }) {
+                    throw StringError("does not support suppressing warnings")
+                }
+            }
+            return true
+        } catch {
+            return false
+        }
+    }
+
     /// Helper function to determine whether we should run SDK-dependent tests.
     public func supportsSDKDependentTests() -> Bool {
         return ProcessInfo.processInfo.environment["SWIFTCI_DISABLE_SDK_DEPENDENT_TESTS"] == nil
