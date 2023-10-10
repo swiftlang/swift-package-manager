18ea3ea46722cf9671c98344b2814aa10ffabc5c
Max Desiatov <m_desiatov@apple.com>
2023-10-10 20:11:00 +0100

    [NFC] Build/PackageModel: split `Target.swift` and `BuildPlan.swift` (#6972)
    ### Motivation:
    
    The `Build` module is not very well documented and the overall flow of
    build planning is not obvious when trying to read its code.
    
    ### Modifications:
    
    `Target.swift` and `BuildPlan.swift` are split into separate files that
    contain either separate types or extensions with corresponding
    functionality. Now that both files are split, their extensions mostly
    correspond to types of targets that SwiftPM supports:
    
    * Clang
    * Swift
    * Plugin
    * System Library
    
    This separation is also closely reflected in `BuildPlan` extensions:
    
    * Clang
    * Product
    * Swift
    * Test
    
    Also moved product-related build manifest code to
    `LLBuildManifestBuilder+Product.swift`.
    
    ### Result:
    
    Now it's much easier to navigate across the `Build` module codebase, and
    each step of build planning has a separate directory that describes the
    overall flow:
    
    1. Build description is formed (the existing `BuildDescription`
    directory);
    2. Build planning assigns necessary flags, options, and arguments (the
    new `BuildPlan` directory);
    3. Build manifest is written out for llbuild (the existing
    `BuildManifest` directory).
    
    As a bonus, these files can now be built in parallel, but I don't think
    it's noticeable when building. Another benefit is that imports for each
    new file are explicit and don't pollute the corresponding namespace with
    wildcard imports of whole modules.

---
+++ Sources/Build/BuildPlan/BuildPlan+Clang.swift
@@ -0,0 +1,58 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2014-2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import class PackageModel.BinaryTarget
+import class PackageModel.ClangTarget
+import class PackageModel.SwiftTarget
+import class PackageModel.SystemLibraryTarget
+
+extension BuildPlan {
+    /// Plan a Clang target.
+    func plan(clangTarget: ClangTargetBuildDescription) throws {
+        for case .target(let dependency, _) in try clangTarget.target.recursiveDependencies(satisfying: buildEnvironment) {
+            switch dependency.underlyingTarget {
+            case is SwiftTarget:
+                if case let .swift(dependencyTargetDescription)? = targetMap[dependency] {
+                    if let moduleMap = dependencyTargetDescription.moduleMap {
+                        clangTarget.additionalFlags += ["-fmodule-map-file=\(moduleMap.pathString)"]
+                    }
+                }
+
+            case let target as ClangTarget where target.type == .library:
+                // Setup search paths for C dependencies:
+                clangTarget.additionalFlags += ["-I", target.includeDir.pathString]
+
+                // Add the modulemap of the dependency if it has one.
+                if case let .clang(dependencyTargetDescription)? = targetMap[dependency] {
+                    if let moduleMap = dependencyTargetDescription.moduleMap {
+                        clangTarget.additionalFlags += ["-fmodule-map-file=\(moduleMap.pathString)"]
+                    }
+                }
+            case let target as SystemLibraryTarget:
+                clangTarget.additionalFlags += ["-fmodule-map-file=\(target.moduleMapPath.pathString)"]
+                clangTarget.additionalFlags += try pkgConfig(for: target).cFlags
+            case let target as BinaryTarget:
+                if case .xcframework = target.kind {
+                    let libraries = try self.parseXCFramework(for: target)
+                    for library in libraries {
+                        library.headersPaths.forEach {
+                            clangTarget.additionalFlags += ["-I", $0.pathString]
+                        }
+                        clangTarget.libraryBinaryPaths.insert(library.libraryPath)
+                    }
+                }
+            default: continue
+            }
+        }
+    }
+
+}
