3473fd3740fbbf58b1f2c2a2fc32f72e36c0daa5
Max Desiatov <m_desiatov@apple.com>
2023-10-17 16:13:19 +0100

    Avoid use of `temp_await` in `PackageRegistryToolTests.swift`
    Follow up to https://github.com/apple/swift-package-manager/pull/7009, which provides the detailed motivation: we should avoid `temp_await` as it can lead to deadlocks when combined with Swift Concurrency.

--- Sources/Basics/Archiver/Archiver.swift
+++ Sources/Basics/Archiver/Archiver.swift
@@ -53,12 +53,43 @@ public protocol Archiver {
 }
 
 extension Archiver {
+    /// Asynchronously extracts the contents of an archive to a destination folder.
+    ///
+    /// - Parameters:
+    ///   - archivePath: The `AbsolutePath` to the archive to extract.
+    ///   - destinationPath: The `AbsolutePath` to the directory to extract to.
     public func extract(
         from archivePath: AbsolutePath,
         to destinationPath: AbsolutePath
     ) async throws {
         try await withCheckedThrowingContinuation {
             self.extract(from: archivePath, to: destinationPath, completion: $0.resume(with:))
+        }
+    }    
+
+    /// Asynchronously compress the contents of a directory to a destination archive.
+    ///
+    /// - Parameters:
+    ///   - directory: The `AbsolutePath` to the archive to extract.
+    ///   - destinationPath: The `AbsolutePath` to the directory to extract to.
+    public func compress(
+        directory: AbsolutePath,
+        to destinationPath: AbsolutePath
+    ) async throws {
+        try await withCheckedThrowingContinuation {
+            self.compress(directory: directory, to: destinationPath, completion: $0.resume(with:))
+        }
+    }
+
+    /// Asynchronously validates if a file is an archive.
+    ///
+    /// - Parameters:
+    ///   - path: The `AbsolutePath` to the archive to validate.
+    public func validate(
+        path: AbsolutePath
+    ) async throws -> Bool {
+        try await withCheckedThrowingContinuation {
+            self.validate(path: path, completion: $0.resume(with:))
         }
     }
 }
