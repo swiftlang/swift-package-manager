swiftlang/swift-package-manager

commit cf5facdf9bb2c4c8ec8fab8bdd98a65eb82e22ab

Read SwiftSyntax version for macro template from a configuration file (#6774)
In 5.9 we're using a version that's hard-coded in the template, but that is annoying to maintain. It also seems like ideally we would be using a version that's aligned with the toolchain that's being used. This adds a new configuration file that we can ship as 'usr/share/pm/config.json' in the toolchain to configure this. There's a fallback to the 5.9 version if we can't find the config and also a way to customize it when instantiating a `UserToolchain` in case a client needs to do that.

rdar://113287350

Co-authored-by: Alex Hoppen <ahoppen@apple.com>

Sources/PackageModel/InstalledSwiftPMConfiguration.swift
@@ -1,0 +1,38 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+public struct InstalledSwiftPMConfiguration: Codable {
+    public struct Version: Codable, CustomStringConvertible {
+        let major: Int
+        let minor: Int
+        let patch: Int
+        let prereleaseIdentifier: String?
+
+        public init(major: Int, minor: Int, patch: Int, prereleaseIdentifier: String? = nil) {
+            self.major = major
+            self.minor = minor
+            self.patch = patch
+            self.prereleaseIdentifier = prereleaseIdentifier
+        }
+
+        public var description: String {
+            return "\(major).\(minor).\(patch).\(prereleaseIdentifier.map { "-\($0)" } ?? "")"
+        }
+    }
+
+    let version: Int
+    public let swiftSyntaxVersionForMacroTemplate: Version
+
+    public static var `default`: InstalledSwiftPMConfiguration {
+        return .init(version: 0, swiftSyntaxVersionForMacroTemplate: .init(major: 509, minor: 0, patch: 0))
+    }
+}

Utilities/InstalledSwiftPMConfiguration/Sources/InstalledSwiftPMConfiguration.swift
@@ -1,0 +1,1 @@
+../../../Sources/PackageModel/InstalledSwiftPMConfiguration.swift

.gitignore
@@ -17,3 +17,4 @@ Package.resolved
 *.pyc
 .docc-build
 .vscode
+Utilities/InstalledSwiftPMConfiguration/config.json

Sources/Commands/PackageTools/Init.swift
@@ -51,6 +51,7 @@ extension SwiftPackageTool {
                 name: packageName,
                 packageType: initMode,
                 destinationPath: cwd,
+                installedSwiftPMConfiguration: swiftTool.getHostToolchain().installedSwiftPMConfiguration,
                 fileSystem: swiftTool.fileSystem
             )
             initPackage.progressReporter = { message in

Sources/PackageModel/CMakeLists.txt
@@ -15,6 +15,7 @@ add_library(PackageModel
   DependencyMapper.swift
   Diagnostics.swift
   IdentityResolver.swift
+  InstalledSwiftPMConfiguration.swift
   Manifest/Manifest.swift
   Manifest/PackageConditionDescription.swift
   Manifest/PackageDependencyDescription.swift

Sources/PackageModel/Toolchain.swift
@@ -40,6 +40,9 @@ public protocol Toolchain {
     /// An array of paths to search for libraries at link time.
     var librarySearchPaths: [AbsolutePath] { get }
 
+    /// Configuration from the used toolchain.
+    var installedSwiftPMConfiguration: InstalledSwiftPMConfiguration { get }
+
     /// Path of the `clang` compiler.
     func getClangCompiler() throws -> AbsolutePath
 

Sources/PackageModel/UserToolchain.swift
@@ -86,6 +86,8 @@ public final class UserToolchain: Toolchain {
 
     public let isSwiftDevelopmentToolchain: Bool
 
+    public let installedSwiftPMConfiguration: InstalledSwiftPMConfiguration
+
     /// Returns the runtime library for the given sanitizer.
     public func runtimeLibrary(for sanitizer: Sanitizer) throws -> AbsolutePath {
         // FIXME: This is only for SwiftPM development time support. It is OK
@@ -475,7 +477,8 @@ public final class UserToolchain: Toolchain {
         swiftSDK: SwiftSDK,
         environment: EnvironmentVariables = .process(),
         searchStrategy: SearchStrategy = .default,
-        customLibrariesLocation: ToolchainConfiguration.SwiftPMLibrariesLocation? = nil
+        customLibrariesLocation: ToolchainConfiguration.SwiftPMLibrariesLocation? = nil,
+        customInstalledSwiftPMConfiguration: InstalledSwiftPMConfiguration? = nil
     ) throws {
         self.swiftSDK = swiftSDK
         self.environment = environment
@@ -517,6 +520,18 @@ public final class UserToolchain: Toolchain {
         #else
         self.isSwiftDevelopmentToolchain = false
         #endif
+
+        if let customInstalledSwiftPMConfiguration {
+            self.installedSwiftPMConfiguration = customInstalledSwiftPMConfiguration
+        } else {
+            let path = self.swiftCompilerPath.parentDirectory.parentDirectory.appending(components: ["share", "pm", "config.json"])
+            if localFileSystem.exists(path) {
+                self.installedSwiftPMConfiguration = try JSONDecoder.makeWithDefaults().decode(path: path, fileSystem: localFileSystem, as: InstalledSwiftPMConfiguration.self)
+            } else {
+                // We *could* eventually make this an error, but not for a few releases.
+                self.installedSwiftPMConfiguration = InstalledSwiftPMConfiguration.default
+            }
+        }
 
         // Use the triple from Swift SDK or compute the host triple using swiftc.
         var triple = try swiftSDK.targetTriple ?? Triple.getHostTriple(usingSwiftCompiler: swiftCompilers.compile)

Sources/SPMTestSupport/misc.swift
@@ -369,3 +369,20 @@ extension RelativePath: ExpressibleByStringInterpolation {
         try! self.init(validating: value)
     }
 }
+
+extension InitPackage {
+    public convenience init(
+        name: String,
+        packageType: PackageType,
+        destinationPath: AbsolutePath,
+        fileSystem: FileSystem
+    ) throws {
+        try self.init(
+            name: name,
+            options: InitPackageOptions(packageType: packageType),
+            destinationPath: destinationPath,
+            installedSwiftPMConfiguration: .default,
+            fileSystem: fileSystem
+        )
+    }
+}

commit 870fb958b0e7d92c32ef30bc4cabf3f7fe8a2044

SwiftSyntax version in new macro template has trailing period in dependency version, causing error reading manifest
This fixes a regression I believe was introduced in #6774 where the new macro package template has its dependency on swift-syntax expressed as `from: "509.0.0."`, including an unncessary trailing period character. This causes an error reading the manifest.

I have not tested this, but I experienced the bug and from source inspection believe this is likely the issue.

Resolves rdar://117132800

Sources/PackageModel/InstalledSwiftPMConfiguration.swift
@@ -25,7 +25,7 @@ public struct InstalledSwiftPMConfiguration: Codable {
         }
 
         public var description: String {
-            return "\(major).\(minor).\(patch).\(prereleaseIdentifier.map { "-\($0)" } ?? "")"
+            return "\(major).\(minor).\(patch)\(prereleaseIdentifier.map { "-\($0)" } ?? "")"
         }
     }
 