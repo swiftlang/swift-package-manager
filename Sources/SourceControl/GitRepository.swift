3b1dfc1ddbd338d3bca6f8aae5442b772038bbf4
tomer doron <tomer@apple.com>
2023-12-01 13:58:56 -0800

    adjust git call supported by older git clients (#7155)
    motivation: new git call added does not work with older git clients (eg Centos7)
    
    changes: use "git config --get remote.origin.url" instead of "git remote
    get-url origin"

--- Sources/SourceControl/GitRepository.swift
+++ Sources/SourceControl/GitRepository.swift
@@ -211,7 +211,7 @@ public struct GitRepositoryProvider: RepositoryProvider, Cancellable {
     }
 
     public func isValidDirectory(_ directory: Basics.AbsolutePath, for repository: RepositorySpecifier) throws -> Bool {
-        let remoteURL = try self.git.run(["-C", directory.pathString, "remote", "get-url", "origin"])
+        let remoteURL = try self.git.run(["-C", directory.pathString, "config", "--get", "remote.origin.url"])
         return remoteURL == repository.url
     }
 
