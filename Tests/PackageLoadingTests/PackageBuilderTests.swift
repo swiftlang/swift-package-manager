4bd4bb47e7082493d41c297ed4e3365e8ff1e08d
Max Desiatov <m_desiatov@apple.com>
2023-12-02 16:20:13 +0000

    Update comments and reduce the diff

--- Tests/PackageLoadingTests/PackageBuilderTests.swift
+++ Tests/PackageLoadingTests/PackageBuilderTests.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift open source project
 //
-// Copyright (c) 2014-2021 Apple Inc. and the Swift project authors
+// Copyright (c) 2014-2023 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See http://swift.org/LICENSE.txt for license information
@@ -19,8 +19,7 @@ import XCTest
 import class TSCBasic.InMemoryFileSystem
 
 /// Tests for the handling of source layout conventions.
-class PackageBuilderTests: XCTestCase {
-
+final class PackageBuilderTests: XCTestCase {
     func testDotFilesAreIgnored() throws {
         let fs = InMemoryFileSystem(emptyFiles:
             "/Sources/foo/.Bar.swift",
