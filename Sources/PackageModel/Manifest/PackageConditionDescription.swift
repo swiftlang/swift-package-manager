e9fa373059501061643eb0975e6df8a754462077
Max Desiatov <m_desiatov@apple.com>
2023-12-05 16:59:35 +0000

    [NFC] Replace `PackageConditionProtocol` with `PackageCondition` (#7117)
    Unblocks `PackageGraph` value types refactoring in
    https://github.com/apple/swift-package-manager/pull/7160 and macros
    cross-compilation fix in
    https://github.com/apple/swift-package-manager/pull/7118.
    
    Multiple places in our codebase pass around and store `[any
    PackageConditionProtocol]` arrays of existentials with `FIXME` notes
    that those should be sets and not arrays.
    
    We also can't convert types containing these arrays to value types with
    auto-generated `Hashable` conformance, since neither elements of these
    arrays nor arrays themselves are `Hashable`. Adding `Hashable`
    requirement to `PackageConditionProtocol` doesn't fix the issue, since
    existentials of this protocol still wouldn't conform to `Hashable`.
    
    A simple alternative is to create `enum PackageCondition` with cases for
    each condition type. We only have two of those types and they're always
    declared in the same module. There's no use case for externally defined
    types for this protocol. That allows us to convert uses of `[any
    PackageConditionProtocol]` to `[PackageCondition]`. Existing protocol is
    kept around until clients of SwiftPM can migrate to the new
    `PackageCondition` enum.
    
    This also allows us to remove a custom conformance to `Hashable` on
    `ResolvedTarget`, unblocking a conversion of `ResolvedTarget` to a value
    type and making it `Sendable` in the future.

--- Sources/PackageModel/Manifest/PackageConditionDescription.swift
+++ Sources/PackageModel/Manifest/PackageConditionDescription.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift open source project
 //
-// Copyright (c) 2014-2020 Apple Inc. and the Swift project authors
+// Copyright (c) 2014-2023 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See http://swift.org/LICENSE.txt for license information
@@ -27,11 +27,12 @@ public protocol PackageConditionProtocol: Codable {
     func satisfies(_ environment: BuildEnvironment) -> Bool
 }
 
-/// Wrapper for package condition so it can be conformed to Codable.
+/// Wrapper for package condition so it can conform to Codable.
 struct PackageConditionWrapper: Codable, Equatable, Hashable {
     var platform: PlatformsCondition?
     var config: ConfigurationCondition?
 
+    @available(*, deprecated, renamed: "underlying")
     var condition: PackageConditionProtocol {
         if let platform {
             return platform
@@ -42,6 +43,17 @@ struct PackageConditionWrapper: Codable, Equatable, Hashable {
         }
     }
 
+    var underlying: PackageCondition {
+        if let platform {
+            return .platforms(platform)
+        } else if let config {
+            return .configuration(config)
+        } else {
+            fatalError("unreachable")
+        }
+    }
+
+    @available(*, deprecated, message: "Use .init(_ condition: PackageCondition) instead")
     init(_ condition: PackageConditionProtocol) {
         switch condition {
         case let platform as PlatformsCondition:
@@ -51,6 +63,55 @@ struct PackageConditionWrapper: Codable, Equatable, Hashable {
         default:
             fatalError("unknown condition \(condition)")
         }
+    }
+
+    init(_ condition: PackageCondition) {
+        switch condition {
+        case let .platforms(platforms):
+            self.platform = platforms
+        case let .configuration(configuration):
+            self.config = configuration
+        }
+    }
+}
+
+/// One of possible conditions used in package manifests to restrict targets from being built for certain platforms or
+/// build configurations.
+public enum PackageCondition: Hashable {
+    case platforms(PlatformsCondition)
+    case configuration(ConfigurationCondition)
+
+    public func satisfies(_ environment: BuildEnvironment) -> Bool {
+        switch self {
+        case .configuration(let configuration):
+            return configuration.satisfies(environment)
+        case .platforms(let platforms):
+            return platforms.satisfies(environment)
+        }
+    }
+
+    public var platformsCondition: PlatformsCondition? {
+        guard case let .platforms(platformsCondition) = self else {
+            return nil
+        }
+
+        return platformsCondition
+    }
+
+    public var configurationCondition: ConfigurationCondition? {
+        guard case let .configuration(configurationCondition) = self else {
+            return nil
+        }
+
+        return configurationCondition
+    }
+
+    public init(platforms: [Platform]) {
+        self = .platforms(.init(platforms: platforms))
+    }
+
+    public init(configuration: BuildConfiguration) {
+        self = .configuration(.init(configuration: configuration))
     }
 }
 
