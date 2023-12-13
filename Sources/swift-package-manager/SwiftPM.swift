a3b9e5541360dc55e71a6fd2c19ea6bdf2c794cb
Max Desiatov <m_desiatov@apple.com>
2023-12-13 16:20:56 +0000

    Revert "Avoid using `temp_await` in `SwiftTestTool.swift`" (#7196)
    Reverts apple/swift-package-manager#7016 to fix macOS CI regression:
    
    ```
    duplicate symbol '_async_MainTu' in:
        /Users/ec2-user/jenkins/workspace/swift-package-manager-with-xcode-self-hosted-PR-osx/branch-main/swiftpm/.build/x86_64-apple-macosx/debug/swift_package.build/Entrypoint.swift.o
        /Users/ec2-user/jenkins/workspace/swift-package-manager-with-xcode-self-hosted-PR-osx/branch-main/swiftpm/.build/x86_64-apple-macosx/debug/swift_test.build/Entrypoint.swift.o
    duplicate symbol '_async_Main' in:
        /Users/ec2-user/jenkins/workspace/swift-package-manager-with-xcode-self-hosted-PR-osx/branch-main/swiftpm/.build/x86_64-apple-macosx/debug/swift_package.build/Entrypoint.swift.o
        /Users/ec2-user/jenkins/workspace/swift-package-manager-with-xcode-self-hosted-PR-osx/branch-main/swiftpm/.build/x86_64-apple-macosx/debug/swift_test.build/Entrypoint.swift.o
    ld: 2 duplicate symbols for architecture x86_64
    ```
    
    This error was fixed in Swift 5.10, which is not available on CI yet.

--- Sources/swift-package-manager/SwiftPM.swift
+++ Sources/swift-package-manager/SwiftPM.swift
@@ -31,7 +31,7 @@ struct SwiftPM {
         case "swift-experimental-sdk":
             await SwiftSDKTool.main()
         case "swift-test":
-            await SwiftTestTool.main()
+            SwiftTestTool.main()
         case "swift-run":
             SwiftRunTool.main()
         case "swift-package-collection":
