427bc40d4a887ce917271e62c29026370725be06
ordo-ci <104988168+ordo-ci@users.noreply.github.com>
2024-01-10 12:37:53 +0300

    Optional xcframework for Linux

--- Sources/SPMBuildCore/BinaryTarget+Extensions.swift
+++ Sources/SPMBuildCore/BinaryTarget+Extensions.swift
@@ -96,7 +96,7 @@ extension Triple.OS {
     /// Returns a representation of the receiver that can be compared with platform strings declared in an XCFramework.
     fileprivate var asXCFrameworkPlatformString: String? {
         switch self {
-        case .darwin, .linux, .wasi, .win32, .openbsd, .noneOS:
+        case .darwin, .wasi, .win32, .openbsd, .noneOS:
             return nil // XCFrameworks do not support any of these platforms today.
         case .macosx:
             return "macos"
@@ -106,6 +106,8 @@ extension Triple.OS {
             return "tvos"
         case .watchos:
             return "watchos"
+        case .linux:
+            return ProcessInfo.processInfo.environment["_SWIFTPM_EXPERIMENTAL_LINUX_XCFRAMEWORK"] == "1" ? "linux" : nil
         default:
             return nil // XCFrameworks do not support any of these platforms today.
         }
