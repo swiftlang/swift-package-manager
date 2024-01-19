6bbf6e239b18fd18a691fb8a2e83dcf0e0eab500
Max Desiatov <m_desiatov@apple.com>
2024-01-19 14:44:45 +0000

    Fix use of `package func`

--- Sources/SPMBuildCore/ResolvedPackage+Extensions.swift
+++ Sources/SPMBuildCore/ResolvedPackage+Extensions.swift
@@ -14,7 +14,8 @@ import struct PackageGraph.ResolvedPackage
 import struct PackageGraph.ResolvedTarget
 
 extension ResolvedPackage {
-    package func packageNameArgument(target: ResolvedTarget, isPackageNameSupported: Bool) -> [String] {
+    @_spi(SwiftPMInternal)
+    public func packageNameArgument(target: ResolvedTarget, isPackageNameSupported: Bool) -> [String] {
         if self.manifest.usePackageNameFlag, target.packageAccess {
             ["-package-name", self.identity.description.spm_mangledToC99ExtendedIdentifier()]
         } else {
