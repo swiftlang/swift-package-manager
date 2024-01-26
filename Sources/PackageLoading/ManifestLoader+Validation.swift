3e8328e3de533dd1ed570aee83a90d3b20fdb5fe
Boris Bügling <bbuegling@apple.com>
2024-01-26 08:47:02 -0800

    Clean up product dependency API (#7298)
    As part of
    https://github.com/apple/swift-evolution/blob/main/proposals/0339-module-aliasing-for-disambiguation.md,
    a new API with an optional `package` parameter was accidentally added.
    We can safely remove this API since #4203 added validation that
    prohibited the use of product declarations without a package anyway. As
    part of this, I also cleaned up that diagnostic since "unknown package
    'unknown package name'" looks quite ugly.
    
    rdar://114303935
    fixes #7158

--- Sources/PackageLoading/ManifestLoader+Validation.swift
+++ Sources/PackageLoading/ManifestLoader+Validation.swift
@@ -191,7 +191,7 @@ public struct ManifestValidator {
                 case .product(_, let packageName, _, _):
                     if self.manifest.packageDependency(referencedBy: targetDependency) == nil {
                         diagnostics.append(.unknownTargetPackageDependency(
-                            packageName: packageName ?? "unknown package name",
+                            packageName: packageName,
                             targetName: target.name,
                             validPackages: self.manifest.dependencies
                         ))
@@ -262,8 +262,14 @@ extension Basics.Diagnostic {
         .error("unknown dependency '\(dependency)' in target '\(targetName)'; valid dependencies are: \(validDependencies.map{ "\($0.descriptionForValidation)" }.joined(separator: ", "))")
     }
 
-    static func unknownTargetPackageDependency(packageName: String, targetName: String, validPackages: [PackageDependency]) -> Self {
-        .error("unknown package '\(packageName)' in dependencies of target '\(targetName)'; valid packages are: \(validPackages.map{ "\($0.descriptionForValidation)" }.joined(separator: ", "))")
+    static func unknownTargetPackageDependency(packageName: String?, targetName: String, validPackages: [PackageDependency]) -> Self {
+        let messagePrefix: String
+        if let packageName {
+            messagePrefix = "unknown package '\(packageName)'"
+        } else {
+            messagePrefix = "undeclared package"
+        }
+        return .error("\(messagePrefix) in dependencies of target '\(targetName)'; valid packages are: \(validPackages.map{ "\($0.descriptionForValidation)" }.joined(separator: ", "))")
     }
 
     static func invalidBinaryLocation(targetName: String) -> Self {
