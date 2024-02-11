d13af21679617141cf1d558eaeae667bb7e94a21
Rauhul Varma <rauhul@apple.com>
2024-02-11 11:45:51 -0800

    Add doc comment to ProgressAnimation.dynamic

--- Sources/Basics/ProgressAnimation/ProgressAnimationProtocol.swift
+++ Sources/Basics/ProgressAnimation/ProgressAnimationProtocol.swift
@@ -21,6 +21,23 @@ public typealias ProgressAnimationProtocol = TSCUtility.ProgressAnimationProtoco
 /// Namespace to nest public progress animations under.
 @_spi(SwiftPMInternal)
 public enum ProgressAnimation {
+    /// Dynamically create a progress animation based on the current stream
+    /// capabilities and desired verbosity.
+    ///
+    /// - Parameters:
+    ///   - stream: A stream to write animations into.
+    ///   - verbose: The verbosity level of other output in the system.
+    ///   - ttyTerminalAnimationFactory: A progress animation to use when the
+    ///     output stream is connected to a terminal with support for special
+    ///     escape sequences.
+    ///   - dumbTerminalAnimationFactory: A progress animation to use when the
+    ///     output stream is connected to a terminal without support for special
+    ///     escape sequences for clearing lines or controlling cursor positions.
+    ///   - defaultAnimationFactory: A progress animation to use when the
+    ///     desired output is verbose or the output stream verbose or is not
+    ///     connected to a terminal, e.g. a pipe or file.
+    /// - Returns: A progress animation instance matching the stream
+    ///   capabilities and desired verbosity.
     static func dynamic(
         stream: WritableByteStream,
         verbose: Bool,
