79c72ae1cbeb6be04c824352ae6c67df0a7af154
Alex Hoppen <ahoppen@apple.com>
2024-03-19 13:22:35 +0100

    Change `relatedDependenciesBranch` to `release/6.0`

--- Package.swift
+++ Package.swift
@@ -736,7 +736,7 @@ if ProcessInfo.processInfo.environment["SWIFTCI_DISABLE_SDK_DEPENDENT_TESTS"] ==
 // this right now.
 
 /// When not using local dependencies, the branch to use for llbuild and TSC repositories.
-let relatedDependenciesBranch = "main"
+let relatedDependenciesBranch = "release/6.0"
 
 if ProcessInfo.processInfo.environment["SWIFTPM_LLBUILD_FWK"] == nil {
     if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
