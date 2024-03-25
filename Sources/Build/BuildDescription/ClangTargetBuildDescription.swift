f17f58478be13384679b2ae2be32e3bd1be797e1
Pavel Yaskevich <xedin@apache.org>
2024-03-25 12:55:40 -0700

    Suppress warnings for remote Clang targets
    This mirrors behavior of remote swift targets which
    suppress warnings as well.

--- Sources/Build/BuildDescription/ClangTargetBuildDescription.swift
+++ Sources/Build/BuildDescription/ClangTargetBuildDescription.swift
@@ -323,6 +323,15 @@ package final class ClangTargetBuildDescription {
             args += ["-I", toolchainResourcesPath.pathString]
         }
 
+        // suppress warnings if the package is remote
+        if self.package.isRemote {
+            args += ["-w"]
+            // `-w` (suppress warnings) and `-Werror` (warnings as errors) flags are mutually exclusive
+            if let index = args.firstIndex(of: "-Werror") {
+                args.remove(at: index)
+            }
+        }
+
         return args
     }
 
