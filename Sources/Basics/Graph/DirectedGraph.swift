d811fafe6290d710976cb5e6f999a004cbb9dd1c
Max Desiatov <m_desiatov@apple.com>
2024-04-16 22:02:36 +0100

    Add `UndirectedGraph` and `DirectedGraph` data structures (#7460)
    `DirectedGraph` can be used to represent the existing modules graph in the `PackageGraph` module hopefully with better performance due to reduced number of allocations. `UndirectedGraph` could represent a new linkage graph concept, which would help with breaking dependency cycles for certain packages with products that don't have a linkage cycle.
    
    I started with an adjacency list in a directed graph implementation, but then proceeded to an undirected graph where adjacency matrix seemed obviously more performant when adding edges: just flipping two bits in the matrix instead of adding potential array (re-)allocations with the list.
    
    I'm unsure though if either is a clear win in all cases, adjacency matrix may be fast at adding edges, but not the fastest for different kinds of graphs analysis and lookups. Keeping both implementations for future reference.

---
+++ Sources/Basics/Graph/DirectedGraph.swift
@@ -0,0 +1,54 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2024 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import struct DequeModule.Deque
+
+/// Directed graph that stores edges in [adjacency lists](https://en.wikipedia.org/wiki/Adjacency_list).
+struct DirectedGraph<Node> {
+    init(nodes: [Node]) {
+        self.nodes = nodes
+        self.edges = .init(repeating: [], count: nodes.count)
+    }
+
+    private var nodes: [Node]
+    private var edges: [[Int]]
+
+    mutating func addEdge(source: Int, destination: Int) {
+        self.edges[source].append(destination)
+    }
+    
+    /// Checks whether a path via previously created edges between two given nodes exists.
+    /// - Parameters:
+    ///   - source: `Index` of a node to start traversing edges from.
+    ///   - destination: `Index` of a node to which a path could exist via edges from `source`.
+    /// - Returns: `true` if a path from `source` to `destination` exists, `false` otherwise.
+    func areNodesConnected(source: Int, destination: Int) -> Bool {
+        var todo = Deque<Int>([source])
+        var done = Set<Int>()
+
+        while !todo.isEmpty {
+            let nodeIndex = todo.removeFirst()
+
+            for reachableIndex in self.edges[nodeIndex] {
+                if reachableIndex == destination {
+                    return true
+                } else if !done.contains(reachableIndex) {
+                    todo.append(reachableIndex)
+                }
+            }
+
+            done.insert(nodeIndex)
+        }
+
+        return false
+    }
+}
