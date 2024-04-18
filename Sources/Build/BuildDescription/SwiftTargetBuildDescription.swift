213844c3f0326778cc9699e1a7ac4535d86695c5
Ben Barham <ben_barham@apple.com>
2024-04-17 21:11:32 -0700

    Update -swift-version to default to 6 for swift-tools-version 6
    We missed updating this when adding 6.0. The change itself is extremely
    simple, with the bulk being updates to our tests so they don't fail on
    old compiler versions (where language version "6" is unknown).
    
    Also removes disabling the concurrency and string processing imports
    from manifest loading, as this was causing the `package` global to not
    be main actor isolated by default and thus error in Swift 6. They seemed
    to have been disabled to avoid erroring in SDKs that didn't have them,
    but that is definitely not an issue any longer.

--- Sources/Build/BuildDescription/SwiftTargetBuildDescription.swift
+++ Sources/Build/BuildDescription/SwiftTargetBuildDescription.swift
@@ -405,6 +405,11 @@ package final class SwiftTargetBuildDescription {
             """
             import Foundation
 
+            #if compiler(>=6.0)
+            extension Foundation.Bundle: @unchecked @retroactive Sendable {}
+            #else
+            extension Foundation.Bundle: @unchecked Sendable {}
+            #endif
             extension Foundation.Bundle {
                 static let module: Bundle = {
                     let mainPath = \(mainPathSubstitution)
