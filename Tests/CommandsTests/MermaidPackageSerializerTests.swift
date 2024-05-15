ee0db4bced6c6694f363226cc37f9c9d310f257e
Pavel Yaskevich <xedin@apache.org>
2024-05-15 13:21:16 -0700

    [Tests] NFC: Fix `loadPackageGraph` -> `loadModulesGraph` replaced by reverts

--- Tests/CommandsTests/MermaidPackageSerializerTests.swift
+++ Tests/CommandsTests/MermaidPackageSerializerTests.swift
@@ -34,7 +34,7 @@ final class MermaidPackageSerializerTests: XCTestCase {
             "/A/Sources/ATarget/main.swift",
             "/A/Tests/ATargetTests/TestCases.swift"
         )
-        let graph = try loadPackageGraph(
+        let graph = try loadModulesGraph(
             fileSystem: fileSystem,
             manifests: [
                 Manifest.createRootManifest(
@@ -79,7 +79,7 @@ final class MermaidPackageSerializerTests: XCTestCase {
         )
 
         let observability = ObservabilitySystem.makeForTesting()
-        let graph = try loadPackageGraph(
+        let graph = try loadModulesGraph(
             fileSystem: fileSystem,
             manifests: [
                 Manifest.createRootManifest(
@@ -138,7 +138,7 @@ final class MermaidPackageSerializerTests: XCTestCase {
         )
 
         let observability = ObservabilitySystem.makeForTesting()
-        let graph = try loadPackageGraph(
+        let graph = try loadModulesGraph(
             fileSystem: fileSystem,
             manifests: [
                 Manifest.createRootManifest(
