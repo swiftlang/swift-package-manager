141aa0805cd491c3cdbe15a8cee5dd21d77358e0
Boris Bügling <bbuegling@apple.com>
2024-05-17 17:36:14 -0700

    [6.0] Update diagnostics for duplicated product/target names (#7575)
    - Explanation:
    
    Improves diagnostic messages for duplicate product/target to include
    involved packages and other useful information.
    
    - Scope: Package manifests/graphs with duplicate product/target names.
    
    - Main Branch PRs:
    https://github.com/apple/swift-package-manager/pull/7572
    
    - Resolves: rdar://127623219
    
    - Risk: Very Low
    
    - Reviewed By: @xedin 
    
    - Testing: New tests were added to the suite and existing tests updated.
    
    
    (cherry picked from commit e5123e483c18bff7afdc3b2029f9e1924779bbc8)

--- Tests/BuildTests/ModuleAliasingBuildTests.swift
+++ Tests/BuildTests/ModuleAliasingBuildTests.swift
@@ -262,7 +262,7 @@ final class ModuleAliasingBuildTests: XCTestCase {
             ],
             observabilityScope: observability.topScope
         )) { error in
-            XCTAssertEqual((error as? PackageGraphError)?.description, "multiple products named 'Logging' in: 'barpkg' (at '\(barPkg)'), 'foopkg' (at '\(fooPkg)')")
+            XCTAssertEqual((error as? PackageGraphError)?.description, "multiple packages (\'barpkg\' (at '\(barPkg)'), \'foopkg\' (at '\(fooPkg)')) declare products with a conflicting name: \'Logging’; product names need to be unique across the package graph")
         }
     }
 
@@ -324,7 +324,7 @@ final class ModuleAliasingBuildTests: XCTestCase {
             ],
             observabilityScope: observability.topScope
         )) { error in
-            XCTAssertEqual((error as? PackageGraphError)?.description, "multiple products named 'Logging' in: 'barpkg' (at '\(barPkg)'), 'foopkg' (at '\(fooPkg)')")
+            XCTAssertEqual((error as? PackageGraphError)?.description, "multiple packages (\'barpkg\' (at '\(barPkg)'), \'foopkg\' (at '\(fooPkg)')) declare products with a conflicting name: \'Logging’; product names need to be unique across the package graph")
         }
     }
 
@@ -4692,7 +4692,7 @@ final class ModuleAliasingBuildTests: XCTestCase {
 
             XCTFail("unexpectedly resolved the package graph successfully")
         } catch {
-            XCTAssertEqual(error.interpolationDescription, "multiple products named 'SomeProduct' in: 'other' (at '\(AbsolutePath("/Other"))'), 'some' (at '\(AbsolutePath("/Some"))')")
+            XCTAssertEqual(error.interpolationDescription, "multiple packages ('other' (at '\(AbsolutePath("/Other"))'), 'some' (at '\(AbsolutePath("/Some"))')) declare products with a conflicting name: 'SomeProduct’; product names need to be unique across the package graph")
         }
         XCTAssertEqual(observability.diagnostics.map { $0.description }.sorted(), ["[warning]: product aliasing requires tools-version 5.2 or later, so it is not supported by \'other\'", "[warning]: product aliasing requires tools-version 5.2 or later, so it is not supported by \'some\'"])
     }
