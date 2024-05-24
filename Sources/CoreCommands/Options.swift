70549a28244567febac48ce427ae33141623443e
Jonathan Grynspan <jgrynspan@apple.com>
2024-05-24 17:47:45 -0400

    Enable swift-testing by default.
    This PR sets the default state of `--{enable,disable}-experimental-swift-testing` to
    enabled.
    
    This PR is speculative.

--- Sources/CoreCommands/Options.swift
+++ Sources/CoreCommands/Options.swift
@@ -593,42 +593,8 @@ public struct TestLibraryOptions: ParsableArguments {
             return callerSuppliedValue
         }
 
-        // If the active package has a dependency on swift-testing, automatically enable support for it so that extra steps are not needed.
-        let workspace = try swiftCommandState.getActiveWorkspace()
-        let root = try swiftCommandState.getWorkspaceRoot()
-        let rootManifests = try temp_await {
-            workspace.loadRootManifests(
-                packages: root.packages,
-                observabilityScope: swiftCommandState.observabilityScope,
-                completion: $0
-            )
-        }
-
-        // Is swift-testing among the dependencies of the package being built?
-        // If so, enable support.
-        let isEnabledByDependency = rootManifests.values.lazy
-            .flatMap(\.dependencies)
-            .map(\.identity)
-            .map(String.init(describing:))
-            .contains("swift-testing")
-        if isEnabledByDependency {
-            swiftCommandState.observabilityScope.emit(debug: "Enabling swift-testing support due to its presence as a package dependency.")
-            return true
-        }
-
-        // Is swift-testing the package being built itself (unlikely)? If so,
-        // enable support.
-        let isEnabledByName = root.packages.lazy
-            .map(PackageIdentity.init(path:))
-            .map(String.init(describing:))
-            .contains("swift-testing")
-        if isEnabledByName {
-            swiftCommandState.observabilityScope.emit(debug: "Enabling swift-testing support because it is a root package.")
-            return true
-        }
-
-        // Default to disabled since swift-testing is experimental (opt-in.)
-        return false
+        // Default to enabled.
+        return true
     }
 
     /// Get the set of enabled testing libraries.
