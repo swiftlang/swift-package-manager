353538ecc9fc4629c6bc2f3883bf6bd80acd9f44
domciiiiix <tynieumieszmowic@gmail.com>
2024-05-27 12:47:25 +0100

    Update Package.swift
    /d

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See http://swift.org/LICENSE.txt for license information
-// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors 
 //
 //===----------------------------------------------------------------------===//
 
