377bb696c09ebd5e58c1c859b19707ccf0ff0daa
Doug Schaefer <dschaefer2@apple.com>
2024-05-29 16:11:30 -0400

    Fix header on PrepareForIndexTests

--- Tests/BuildTests/PrepareForIndexTests.swift
+++ Tests/BuildTests/PrepareForIndexTests.swift
@@ -1,11 +1,14 @@
-// Copyright (C) 2024 Apple Inc. All rights reserved.
+//===----------------------------------------------------------------------===//
 //
-// This document is the property of Apple Inc.
-// It is considered confidential and proprietary.
+// This source file is part of the Swift open source project
 //
-// This document may not be reproduced or transmitted in any form,
-// in whole or in part, without the express written permission of
-// Apple Inc.
+// Copyright (c) 2015-2023 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
 
 import Build
 import Foundation
@@ -32,31 +35,32 @@ class PrepareForIndexTests: XCTestCase {
 
         // Make sure we're building the swift modules
         let outputs = manifest.commands.flatMap(\.value.tool.outputs).map(\.name)
-        XCTAssertTrue(outputs.contains(where: { $0.hasSuffix(".swiftmodule")}))
+        XCTAssertTrue(outputs.contains(where: { $0.hasSuffix(".swiftmodule") }))
 
         // Ensure swiftmodules built with correct arguments
-        let coreCommands = manifest.commands.values.filter({
+        let coreCommands = manifest.commands.values.filter {
             $0.tool.outputs.contains(where: {
                 $0.name.hasSuffix("debug/Core.build/Core.swiftmodule")
             })
-        })
+        }
         XCTAssertEqual(coreCommands.count, 1)
         let coreSwiftc = try XCTUnwrap(coreCommands.first?.tool as? SwiftCompilerTool)
         XCTAssertTrue(coreSwiftc.otherArguments.contains("-experimental-skip-all-function-bodies"))
 
         // Ensure tools are built normally
-        let toolCommands = manifest.commands.values.filter({
+        let toolCommands = manifest.commands.values.filter {
             $0.tool.outputs.contains(where: {
                 $0.name.hasSuffix("debug/Modules-tool/SwiftSyntax.swiftmodule")
             })
-        })
+        }
         XCTAssertEqual(toolCommands.count, 1)
         let toolSwiftc = try XCTUnwrap(toolCommands.first?.tool as? SwiftCompilerTool)
         XCTAssertFalse(toolSwiftc.otherArguments.contains("-experimental-skip-all-function-bodies"))
 
         // Make sure only object files for tools are built
-        XCTAssertTrue(outputs.filter({ $0.hasSuffix(".o") }).allSatisfy({ $0.contains("-tool.build/")}),
-                      "outputs:\n\t\(outputs.filter({ $0.hasSuffix(".o") }).joined(separator: "\n\t"))"
+        XCTAssertTrue(
+            outputs.filter { $0.hasSuffix(".o") }.allSatisfy { $0.contains("-tool.build/") },
+            "outputs:\n\t\(outputs.filter { $0.hasSuffix(".o") }.joined(separator: "\n\t"))"
         )
     }
 }
