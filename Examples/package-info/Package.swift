7a2f512e1f6d31ad18ac171821f71d2ad11bf9f4
Christos Koninis <c.koninis@afse.eu>
2024-06-14 15:34:25 +0300

    Update swift-tools-version and platform version for Example package-info project (#7665)

--- Examples/package-info/Package.swift
+++ Examples/package-info/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 
 import PackageDescription
 
 let package = Package(
     name: "package-info",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v13),
         .iOS(.v13)
     ],
     dependencies: [
