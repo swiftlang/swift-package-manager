37969ce23ed27ea152c435bfe91a959f895a54e7
Pavel Yaskevich <xedin@apache.org>
2024-06-18 15:28:21 -0700

    [Build] Introduce `LLBuildSystemConfiguration` to carry all configuration

--- Sources/Build/BuildPlan/BuildPlan.swift
+++ Sources/Build/BuildPlan/BuildPlan.swift
@@ -192,9 +192,6 @@ public class BuildPlan: SPMBuildCore.BuildPlan {
     /// Build parameters used for tools.
     public let toolsBuildParameters: BuildParameters
 
-    package let llbuildManifestPath: AbsolutePath
-    package let buildDescriptionPath: AbsolutePath
-
     /// The package graph.
     public let graph: ModulesGraph
 
@@ -268,8 +265,6 @@ public class BuildPlan: SPMBuildCore.BuildPlan {
     public init(
         destinationBuildParameters: BuildParameters,
         toolsBuildParameters: BuildParameters,
-        customLLBuildManifestPath: AbsolutePath? = nil,
-        customBuildDescriptionPath: AbsolutePath? = nil,
         graph: ModulesGraph,
         additionalFileRules: [FileRuleDescription] = [],
         buildToolPluginInvocationResults: [ResolvedModule.ID: [BuildToolPluginInvocationResult]] = [:],
@@ -280,8 +275,6 @@ public class BuildPlan: SPMBuildCore.BuildPlan {
     ) throws {
         self.destinationBuildParameters = destinationBuildParameters
         self.toolsBuildParameters = toolsBuildParameters
-        self.llbuildManifestPath = customLLBuildManifestPath ?? destinationBuildParameters.llbuildManifest
-        self.buildDescriptionPath = customBuildDescriptionPath ?? destinationBuildParameters.buildDescriptionPath
         self.graph = graph
         self.buildToolPluginInvocationResults = buildToolPluginInvocationResults
         self.prebuildCommandResults = prebuildCommandResults
