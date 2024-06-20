9e04d3c111f9ab53672361d61fd7de7255d44550
Max Desiatov <m_desiatov@apple.com>
2024-06-20 22:46:30 +0100

    Don’t reload package manifest when a header is added
    Currently, any new header file causes a package reload. This means that while background indexing is writing header files into the index build directory, the package will get reloaded, which is unnecessary.
    
    (cherry picked from commit 1d0e412ed7f3f53133e143fa2eccc13b089765a4)

---
+++ Tests/WorkspaceTests/WorkspaceReloadTests.swift
@@ -0,0 +1,54 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2014-2024 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Basics
+import PackageFingerprint
+@testable import PackageGraph
+import PackageLoading
+import PackageModel
+import PackageRegistry
+import PackageSigning
+import SourceControl
+import SPMBuildCore
+import SPMTestSupport
+@testable import Workspace
+import XCTest
+
+final class WorkspaceReloadTests: XCTestCase {
+    func testHeaderReloading() throws {
+        let sandbox = AbsolutePath("/tmp/ws/")
+        let fs = InMemoryFileSystem()
+
+        let workspace = try MockWorkspace(
+            sandbox: sandbox,
+            fileSystem: fs,
+            roots: [
+                MockPackage(
+                    name: "Foo",
+                    targets: [
+                        MockTarget(name: "Foo", dependencies: []),
+                    ]
+                ),
+            ]
+        )
+
+        try workspace.checkPackageGraph(roots: ["Foo"], deps: []) { graph, diagnostics in
+            try XCTAssertFalse(
+                workspace.getOrCreateWorkspace().fileAffectsSwiftOrClangBuildSettings(
+                    filePath: "/tmp/ws/.build/header.h",
+                    packageGraph: graph
+                )
+            )
+            XCTAssertNoDiagnostics(diagnostics)
+        }
+    }
+}
