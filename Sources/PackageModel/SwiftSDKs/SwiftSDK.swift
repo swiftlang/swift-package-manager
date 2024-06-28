ac04c375279042d6719d4de21ee7666cdb328b0b
Max Desiatov <m_desiatov@apple.com>
2024-06-28 13:01:38 +0100

    Update Sources/PackageModel/SwiftSDKs/SwiftSDK.swift
    Co-authored-by: Alastair Houghton <ahoughton@apple.com>

--- Sources/PackageModel/SwiftSDKs/SwiftSDK.swift
+++ Sources/PackageModel/SwiftSDKs/SwiftSDK.swift
@@ -78,7 +78,7 @@ extension SwiftSDKError: CustomStringConvertible {
         case .checksumNotProvided(let url):
             return """
             Bundles installed from remote URLs (such as \(url)) require their checksum passed via `--checksum` option.
-            The distributor of the bundle must compute it with `swift package compute-checksum` \
+            The distributor of the bundle must compute it with the `swift package compute-checksum` \
             command and provide it with their Swift SDK installation instructions.
             """
         case .invalidBundleArchive(let archivePath):
