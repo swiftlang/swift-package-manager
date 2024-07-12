c3a66cf8b2a83057afefd74634073032abad70ab
Andrew Hoos <ahoos@apple.com>
2024-07-12 15:00:39 -0700

    Remove temp_await from PackageRegistry(Tests)
    TODO: refactor package registry from callbacks to async/await

--- Sources/PackageRegistry/ChecksumTOFU.swift
+++ Sources/PackageRegistry/ChecksumTOFU.swift
@@ -22,13 +22,13 @@ import struct TSCUtility.Version
 struct PackageVersionChecksumTOFU {
     private let fingerprintStorage: PackageFingerprintStorage?
     private let fingerprintCheckingMode: FingerprintCheckingMode
-    private let versionMetadataProvider: (PackageIdentity.RegistryIdentity, Version) throws -> RegistryClient
+    private let versionMetadataProvider: (PackageIdentity.RegistryIdentity, Version) async throws -> RegistryClient
         .PackageVersionMetadata
 
     init(
         fingerprintStorage: PackageFingerprintStorage?,
         fingerprintCheckingMode: FingerprintCheckingMode,
-        versionMetadataProvider: @escaping (PackageIdentity.RegistryIdentity, Version) throws -> RegistryClient
+        versionMetadataProvider: @escaping (PackageIdentity.RegistryIdentity, Version) async throws -> RegistryClient
             .PackageVersionMetadata
     ) {
         self.fingerprintStorage = fingerprintStorage
@@ -122,37 +122,39 @@ struct PackageVersionChecksumTOFU {
                 //   - No storage available
                 //   - Checksum not found in storage
                 //   - Reading from storage resulted in error
-                do {
-                    let versionMetadata = try self.versionMetadataProvider(package, version)
-                    guard let sourceArchiveResource = versionMetadata.sourceArchive else {
-                        throw RegistryError.missingSourceArchive
-                    }
-                    guard let checksum = sourceArchiveResource.checksum else {
-                        throw RegistryError.sourceArchiveMissingChecksum(
+                Task {
+                    do {
+                        let versionMetadata = try await self.versionMetadataProvider(package, version)
+                        guard let sourceArchiveResource = versionMetadata.sourceArchive else {
+                            throw RegistryError.missingSourceArchive
+                        }
+                        guard let checksum = sourceArchiveResource.checksum else {
+                            throw RegistryError.sourceArchiveMissingChecksum(
+                                registry: registry,
+                                package: package.underlying,
+                                version: version
+                            )
+                        }
+
+                        self.writeToStorage(
+                            registry: registry,
+                            package: package,
+                            version: version,
+                            checksum: checksum,
+                            contentType: .sourceCode,
+                            observabilityScope: observabilityScope,
+                            callbackQueue: callbackQueue
+                        ) { writeResult in
+                            completion(writeResult.tryMap { _ in checksum })
+                        }
+                    } catch {
+                        completion(.failure(RegistryError.failedRetrievingReleaseChecksum(
                             registry: registry,
                             package: package.underlying,
-                            version: version
-                        )
+                            version: version,
+                            error: error
+                        )))
                     }
-
-                    self.writeToStorage(
-                        registry: registry,
-                        package: package,
-                        version: version,
-                        checksum: checksum,
-                        contentType: .sourceCode,
-                        observabilityScope: observabilityScope,
-                        callbackQueue: callbackQueue
-                    ) { writeResult in
-                        completion(writeResult.tryMap { _ in checksum })
-                    }
-                } catch {
-                    completion(.failure(RegistryError.failedRetrievingReleaseChecksum(
-                        registry: registry,
-                        package: package.underlying,
-                        version: version,
-                        error: error
-                    )))
                 }
             }
         }
