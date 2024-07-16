696ea0680ef0335dc66fdbf7c4a72b957a592c49
Max Desiatov <m_desiatov@apple.com>
2024-07-16 19:21:27 +0100

    Fix build error

--- Sources/PackageLoading/TargetSourcesBuilder.swift
+++ Sources/PackageLoading/TargetSourcesBuilder.swift
@@ -563,7 +563,7 @@ public struct TargetSourcesBuilder {
 }
 
 /// Describes a rule for including a source or resource file in a target.
-public struct   : Sendable {
+public struct FileRuleDescription: Sendable {
     /// A rule semantically describes a file/directory in a target.
     ///
     /// It is up to the build system to translate a rule into a build command.
