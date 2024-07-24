83c96a147b31d5d5bfafd5078a10a04e16163f22
James Dempsey <dempsey@tapas-software.net>
2024-07-23 22:04:08 -0300

    Use @_disfavoredOverload instead of second init method to resolve ambiguity

--- Sources/PackageDescription/PackageDescription.swift
+++ Sources/PackageDescription/PackageDescription.swift
@@ -260,6 +260,7 @@ public final class Package {
     ///   - swiftLanguageVersions: The list of Swift versions with which this package is compatible.
     ///   - cLanguageStandard: The C language standard to use for all C targets in this package.
     ///   - cxxLanguageStandard: The C++ language standard to use for all C++ targets in this package.
+    @_disfavoredOverload
     @available(_PackageDescription, introduced: 5.3)
     @available(_PackageDescription, deprecated: 6, renamed:"init(name:defaultLocalization:platforms:pkgConfig:providers:products:dependencies:targets:swiftLanguageModes:cLanguageStandard:cxxLanguageStandard:)")
     public init(
@@ -271,7 +272,7 @@ public final class Package {
         products: [Product] = [],
         dependencies: [Dependency] = [],
         targets: [Target] = [],
-        swiftLanguageVersions: [SwiftVersion]?,
+        swiftLanguageVersions: [SwiftVersion]? = nil,
         cLanguageStandard: CLanguageStandard? = nil,
         cxxLanguageStandard: CXXLanguageStandard? = nil
     ) {
@@ -289,50 +290,6 @@ public final class Package {
         self.cxxLanguageStandard = cxxLanguageStandard
         registerExitHandler()
     }
-    
-    /// Initializes a Swift package with configuration options you provide.
-    ///
-    /// - Parameters:
-    ///   - name: The name of the Swift package, or `nil` to use the package's Git URL to deduce the name.
-    ///   - defaultLocalization: The default localization for resources.
-    ///   - platforms: The list of supported platforms with a custom deployment target.
-    ///   - pkgConfig: The name to use for C modules. If present, Swift Package Manager searches for a
-    ///   `<name>.pc` file to get the additional flags required for a system target.
-    ///   - providers: The package providers for a system target.
-    ///   - products: The list of products that this package makes available for clients to use.
-    ///   - dependencies: The list of package dependencies.
-    ///   - targets: The list of targets that are part of this package.
-    ///   - cLanguageStandard: The C language standard to use for all C targets in this package.
-    ///   - cxxLanguageStandard: The C++ language standard to use for all C++ targets in this package.
-    @available(_PackageDescription, introduced: 5.3)
-    @available(_PackageDescription, deprecated: 6, renamed:"init(name:defaultLocalization:platforms:pkgConfig:providers:products:dependencies:targets:swiftLanguageModes:cLanguageStandard:cxxLanguageStandard:)")
-    public init(
-        name: String,
-        defaultLocalization: LanguageTag? = nil,
-        platforms: [SupportedPlatform]? = nil,
-        pkgConfig: String? = nil,
-        providers: [SystemPackageProvider]? = nil,
-        products: [Product] = [],
-        dependencies: [Dependency] = [],
-        targets: [Target] = [],
-        cLanguageStandard: CLanguageStandard? = nil,
-        cxxLanguageStandard: CXXLanguageStandard? = nil
-    ) {
-        self.name = name
-        self.defaultLocalization = defaultLocalization
-        self.platforms = platforms
-        self.pkgConfig = pkgConfig
-        self.providers = providers
-        self.products = products
-        self.dependencies = dependencies
-        self.targets = targets
-        self.traits = []
-        self.swiftLanguageModes = []
-        self.cLanguageStandard = cLanguageStandard
-        self.cxxLanguageStandard = cxxLanguageStandard
-        registerExitHandler()
-    }
-
     
     /// Initializes a Swift package with configuration options you provide.
     ///
