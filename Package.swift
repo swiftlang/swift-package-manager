559a13a37f32344a1787b750511688db7f5709fd
Max Desiatov <m_desiatov@apple.com>
2024-07-30 11:10:43 +0100

    NFC: enable `InferSendableFromCaptures` on almost all modules
    `SourceControl` is excluded for now as it triggers concurrency errors when this feature is enabled.

--- Package.swift
+++ Package.swift
@@ -845,3 +845,19 @@ if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
         .package(path: "../swift-certificates"),
     ]
 }
+
+// FIXME: `SourceControl` module needs to be updated to support `InferSendableFromCaptures`.
+for target in package.targets where target.type == .regular && target.name != "SourceControl" {
+    let sendableFromCaptures = SwiftSetting.enableUpcomingFeature("InferSendableFromCaptures")
+    let updatedSwiftSettings: [SwiftSetting]
+
+    if var swiftSettings = target.swiftSettings {
+        swiftSettings.append(sendableFromCaptures)
+        updatedSwiftSettings = swiftSettings
+    } else {
+        updatedSwiftSettings = [sendableFromCaptures]
+
+    }
+
+    target.swiftSettings = updatedSwiftSettings
+}
