a94e7cc97d009cc1cd33c36663af82752e559831
Max Desiatov <m_desiatov@apple.com>
2024-08-07 15:46:03 +0100

    Add `async`-friendly `WritableStream`, create `_AsyncFileSystem` module (#7831)
    Existing `WritableByteStream` protocol used by `AsyncProcess` is not
    thread-safe. It's new `WritableStream` replacement is, with an added
    conformance to the existing `OpenWritableFile`. That one is built on top
    `AsyncFS`, which has been moved to the `Basics` module. That allows us
    to adopt it in `AsyncProcess` in the future.
    
    This is NFC, new API is adopted solely by `QueryEngine`, which is
    exercised only in tests.

--- Sources/Basics/Concurrency/ConcurrencyHelpers.swift
+++ Sources/Basics/Concurrency/ConcurrencyHelpers.swift
@@ -52,3 +52,17 @@ extension DispatchQueue {
 // As of Swift 5.7 and 5.8 swift-corelibs-foundation doesn't have `Sendable` annotations yet.
 extension URL: @unchecked Sendable {}
 #endif
+
+extension DispatchQueue {
+    package func scheduleOnQueue<T>(work: @escaping @Sendable () throws -> T) async throws -> T {
+        try await withCheckedThrowingContinuation { continuation in
+            self.async {
+                do {
+                    continuation.resume(returning: try work())
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
