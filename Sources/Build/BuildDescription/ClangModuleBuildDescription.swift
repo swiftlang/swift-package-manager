512faa77c9c4020c33eb549be440c92106e5e0c5
Ryan Mansfield <ryan_mansfield@apple.com>
2024-09-12 15:27:28 -0400

    Guard resource access header with __OBJC__ to avoid including Foundation.h in languages that do not support it.
    Add check the file extension for assembly files to not include
    Foundation.h there as well.
    
    Fixes: #7949

--- Sources/Build/BuildDescription/ClangModuleBuildDescription.swift
+++ Sources/Build/BuildDescription/ClangModuleBuildDescription.swift
@@ -253,7 +253,8 @@ public final class ClangModuleBuildDescription {
     /// default value (possibly based on the filename suffix).
     public func basicArguments(
         isCXX isCXXOverride: Bool? = .none,
-        isC: Bool = false
+        isC: Bool = false,
+        isAsm: Bool = false
     ) throws -> [String] {
         // For now fall back on the hold semantics if the C++ nature isn't specified. This is temporary until clients
         // have been updated.
@@ -307,7 +308,7 @@ public final class ClangModuleBuildDescription {
         // Include the path to the resource header unless the arguments are
         // being evaluated for a C file. A C file cannot depend on the resource
         // accessor header due to it exporting a Foundation type (`NSBundle`).
-        if let resourceAccessorHeaderFile, !isC {
+        if let resourceAccessorHeaderFile, !isC && !isAsm {
             args += ["-include", resourceAccessorHeaderFile.pathString]
         }
 
@@ -387,8 +388,9 @@ public final class ClangModuleBuildDescription {
 
         let isCXX = path.source.extension.map { SupportedLanguageExtension.cppExtensions.contains($0) } ?? false
         let isC = path.source.extension.map { $0 == SupportedLanguageExtension.c.rawValue } ?? false
+        let isAsm = path.source.extension.map { SupportedLanguageExtension.assemblyExtensions.contains($0) } ?? false
 
-        var args = try basicArguments(isCXX: isCXX, isC: isC)
+        var args = try basicArguments(isCXX: isCXX, isC: isC, isAsm: isAsm)
 
         args += ["-MD", "-MT", "dependencies", "-MF", path.deps.pathString]
 
@@ -512,6 +514,7 @@ public final class ClangModuleBuildDescription {
 
         let headerContent =
             """
+            #if __OBJC__
             #import <Foundation/Foundation.h>
 
             #if __cplusplus
@@ -524,6 +527,7 @@ public final class ClangModuleBuildDescription {
 
             #if __cplusplus
             }
+            #endif
             #endif
             """
 
