642ec156a42001ed5ee8f56e356e2d3b7ec9b5a1
Kyle <kyle201817146@gmail.com>
2024-09-30 00:52:33 +0800

    Fix CI build issue for Swift 6 language mode

--- Fixtures/Miscellaneous/Plugins/DocCForBinaryDependency/Package.swift
+++ Fixtures/Miscellaneous/Plugins/DocCForBinaryDependency/Package.swift
@@ -1,6 +1,4 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
