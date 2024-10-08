0e0fea1b20ab879b1358dd759543b4a6fb326a17
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-10-08 18:30:42 +0330

    feat: prefer the current implementation over the old one

--- Fixtures/Miscellaneous/CheckTestLibraryEnvironmentVariable/Tests/CheckTestLibraryEnvironmentVariableTests/CheckTestLibraryEnvironmentVariableTests.swift
+++ Fixtures/Miscellaneous/CheckTestLibraryEnvironmentVariable/Tests/CheckTestLibraryEnvironmentVariableTests/CheckTestLibraryEnvironmentVariableTests.swift
@@ -2,7 +2,7 @@ import XCTest
 
 final class CheckTestLibraryEnvironmentVariableTests: XCTestCase {
     func testEnvironmentVariable() throws {
-        let envvar = ProcessInfo.processInfo.environment["SWIFT_PM_TEST_LIBRARY"]
-        XCTAssertEqual(envvar, "XCTest")
+        let envvar = ProcessInfo.processInfo.environment["SWIFT_TESTING_ENABLED"]
+        XCTAssertEqual(envvar, "0")
     }
 }
