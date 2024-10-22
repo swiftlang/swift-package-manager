e2ea0587164f9dc509765fb3f5707f738f0fb919
furby™ <devs@wabi.foundation>
2024-10-22 10:06:56 -0600

    Remove unused target in XCFrameworkExternal fixture.
    Signed-off-by: furby™ <devs@wabi.foundation>

--- Fixtures/DependencyResolution/External/XCFrameworkExternal/Package.swift
+++ Fixtures/DependencyResolution/External/XCFrameworkExternal/Package.swift
@@ -5,13 +5,12 @@ import PackageDescription
 let package = Package(
     name: "Foo",
     products: [
-        .library(name: "Foo", targets: ["Foo", "Baz"]),
+        .library(name: "Baz", targets: ["Baz"]),
     ],
     dependencies: [
         .package(path: "./Inner")
     ],
     targets: [
-        .target(name: "Foo", path: "./Foo"),
         .target(name: "Baz", dependencies: [.product(name: "InnerBar", package: "Inner")], path: "./Baz")
     ]
 )
