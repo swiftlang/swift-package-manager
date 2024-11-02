5e953fe6a0412f0286299bc9fcdcf1acb415cea0
Yuta Saito <kateinoigakukun@gmail.com>
2024-11-02 14:17:20 +0900

    Fix initial manifest caching with nested scratch dir and local cache (#8088)
    ### Motivation:
    
    When `--scratch-path` is set to a directory whose parent directory does
    not exist and `--manifest-cache local` is set, the initial manifest
    cache creation fails with the following warning:
    
    ```
    warning: 'tmp.vulugm1mqv': failed loading cached manifest for 'tmp.vulugm1mqv': Error Domain=NSCocoaErrorDomain Code=4 "The file “check” doesn’t exist." UserInfo={NSFilePath=/private/var/folders/k2/q6yzck31253_q1j4kxkzq5jh0000gn/T/tmp.vUlUgM1Mqv/.build/check, NSUnderlyingError=0x600001072ac0 {Error Domain=NSPOSIXErrorDomain Code=2 "No such file or directory"}}
    warning: 'tmp.vulugm1mqv': failed storing manifest for 'tmp.vulugm1mqv' in cache: Error Domain=NSCocoaErrorDomain Code=4 "The file “check” doesn’t exist." UserInfo={NSFilePath=/private/var/folders/k2/q6yzck31253_q1j4kxkzq5jh0000gn/T/tmp.vUlUgM1Mqv/.build/check, NSUnderlyingError=0x600001078060 {Error Domain=NSPOSIXErrorDomain Code=2 "No such file or directory"}}
    warning: 'tmp.vulugm1mqv': failed closing manifest db cache: Error Domain=NSCocoaErrorDomain Code=4 "The file “check” doesn’t exist." UserInfo={NSFilePath=/private/var/folders/k2/q6yzck31253_q1j4kxkzq5jh0000gn/T/tmp.vUlUgM1Mqv/.build/check, NSUnderlyingError=0x600001078360 {Error Domain=NSPOSIXErrorDomain Code=2 "No such file or directory"}}
    ```
    
    This is because directory creation in the manifest.db file creation
    process is not recursive.
    
    ### Modifications:
    
    This patch fixes the issue by making the directory creation recursive.
    
    ### Result:
    
    The following command sequence should work without warnings.
    
    ```console
    $ swift package init
    $ swift package dump-package --manifest-cache local  --scratch-path .build/check
    ```

--- Sources/Basics/SQLiteBackedCache.swift
+++ Sources/Basics/SQLiteBackedCache.swift
@@ -238,7 +238,7 @@ package final class SQLiteBackedCache<Value: Codable>: Closable {
         switch self.location {
         case .path(let path):
             if !self.fileSystem.exists(path.parentDirectory) {
-                try self.fileSystem.createDirectory(path.parentDirectory)
+                try self.fileSystem.createDirectory(path.parentDirectory, recursive: true)
             }
             return try self.fileSystem.withLock(on: path, type: .exclusive, body)
         case .memory, .temporary:
