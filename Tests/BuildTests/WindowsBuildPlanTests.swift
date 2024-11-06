51bd364cedfc55e6042652a1980d4fecb02129eb
Doug Schaefer <dschaefer2@apple.com>
2024-11-06 10:10:48 -0500

    Add tests to ensure dynamics are picked up on Windows only.

--- Tests/BuildTests/WindowsBuildPlanTests.swift
+++ Tests/BuildTests/WindowsBuildPlanTests.swift
@@ -14,6 +14,7 @@ import XCTest
 
 import Basics
 @testable import Build
+import LLBuildManifest
 import _InternalTestSupport
 
 @_spi(DontAdoptOutsideOfSwiftPMExposedForBenchmarksAndTestsOnly)
@@ -23,7 +24,8 @@ final class WindowsBuildPlanTests: XCTestCase {
     // Tests that our build plan is build correctly to handle separation
     // of object files that export symbols and ones that don't and to ensure
     // DLL products pick up the right ones.
-    func testDynamicSymbolHandling() async throws {
+
+    func doTest(triple: Triple) async throws {
         let fs = InMemoryFileSystem(emptyFiles: [
             "/libPkg/Sources/coreLib/coreLib.swift",
             "/libPkg/Sources/dllLib/dllLib.swift",
@@ -68,14 +70,71 @@ final class WindowsBuildPlanTests: XCTestCase {
             observabilityScope: observability.topScope
         )
 
-        let plan = try await mockBuildPlan(
+        let label: String
+        let dylibPrefix: String
+        let dylibExtension: String
+        let dynamic: String
+        switch triple {
+        case Triple.x86_64Windows:
+            label = "x86_64-unknown-windows-msvc"
+            dylibPrefix = ""
+            dylibExtension = "dll"
+            dynamic = "/dynamic"
+        case Triple.x86_64MacOS:
+            label = "x86_64-apple-macosx"
+            dylibPrefix = "lib"
+            dylibExtension = "dylib"
+            dynamic = ""
+        case Triple.x86_64Linux:
+            label = "x86_64-unknown-linux-gnu"
+            dylibPrefix = "lib"
+            dylibExtension = "so"
+            dynamic = ""
+        default:
+            label = "fixme"
+            dylibPrefix = ""
+            dylibExtension = ""
+            dynamic = ""
+        }
+
+        let tools: [String: [String]] = [
+            "C.exe-\(label)-debug.exe": [
+                "/path/to/build/\(label)/debug/coreLib.build/coreLib.swift.o",
+                "/path/to/build/\(label)/debug/exe.build/main.swift.o",
+                "/path/to/build/\(label)/debug/objectLib.build/objectLib.swift.o",
+                "/path/to/build/\(label)/debug/staticLib.build/staticLib.swift.o",
+                "/path/to/build/\(label)/debug/\(dylibPrefix)DLLProduct.\(dylibExtension)",
+                "/path/to/build/\(label)/debug/exe.product/Objects.LinkFileList",
+            ] + (triple.isMacOSX ? [] : [
+                // modulewrap
+                "/path/to/build/\(label)/debug/coreLib.build/coreLib.swiftmodule.o",
+                "/path/to/build/\(label)/debug/exe.build/exe.swiftmodule.o",
+                "/path/to/build/\(label)/debug/objectLib.build/objectLib.swiftmodule.o",
+                "/path/to/build/\(label)/debug/staticLib.build/staticLib.swiftmodule.o",
+            ]),
+            "C.DLLProduct-\(label)-debug.dylib": [
+                "/path/to/build/\(label)/debug/coreLib.build/coreLib.swift.o",
+                "/path/to/build/\(label)/debug/dllLib.build\(dynamic)/dllLib.swift.o",
+                "/path/to/build/\(label)/debug/DLLProduct.product/Objects.LinkFileList",
+            ] + (triple.isMacOSX ? [] : [
+                "/path/to/build/\(label)/debug/coreLib.build/coreLib.swiftmodule.o",
+                "/path/to/build/\(label)/debug/dllLib.build/dllLib.swiftmodule.o",
+            ])
+        ]
+
+        let plan = try await BuildPlan(
+            destinationBuildParameters: mockBuildParameters(
+                destination: .target,
+                triple: triple
+            ),
+            toolsBuildParameters: mockBuildParameters(
+                destination: .host,
+                triple: triple
+            ),
             graph: graph,
             fileSystem: fs,
             observabilityScope: observability.topScope
         )
-
-        let result = try BuildPlanResult(plan: plan)
-        let exe = try result.buildProduct(for: "exe")
 
         let llbuild = LLBuildManifestBuilder(
             plan,
@@ -84,8 +143,23 @@ final class WindowsBuildPlanTests: XCTestCase {
         )
         try llbuild.generateManifest(at: "/manifest")
 
-        for command in llbuild.manifest.commands {
-            print(command.key)
+        for (name, inputNames) in tools {
+            let command = try XCTUnwrap(llbuild.manifest.commands[name])
+            XCTAssertEqual(Set(command.tool.inputs), Set(inputNames.map({ Node.file(.init($0)) })))
         }
+    }
+
+    func testWindows() async throws {
+        try await doTest(triple: .x86_64Windows)
+    }
+
+    // Make sure we didn't mess up macOS
+    func testMacOS() async throws {
+        try await doTest(triple: .x86_64MacOS)
+    }
+
+    // Make sure we didn't mess up linux
+    func testLinux() async throws {
+        try await doTest(triple: .x86_64Linux)
     }
 }
