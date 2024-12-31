524c4eb2fb6989b4a64fd30bb51387a09fd700b0
aydinomer00 <109145643+aydinomer00@users.noreply.github.com>
2024-12-31 11:08:16 +0300

    Fix: Make diagnose-api-breaking-changes handle git submodules
    This commit improves the diagnose-api-breaking-changes command to properly handle packages with git submodules:
    
    - Add proper submodule initialization and management
    - Implement comprehensive APIDigester for analyzing API changes
    - Add support for recursive analysis of submodules
    - Add detailed change reporting with categorization (breaking/non-breaking/deprecated)
    - Improve error handling and reporting
    - Add proper documentation
    
    Resolves #XXX

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
+
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2014-2024 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
 import PackageDescription
+import class Foundation.ProcessInfo
 
 let package = Package(
     name: "swift-package-manager",
