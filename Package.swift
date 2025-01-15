swiftlang/swift-package-manager

commit d457fa46b396248e46361776faacb9e0020b92d1

Public API for getting information about build targets (#6763)
Until now, SourceKit-LSP has been using a couple of internal data
structures of the build plan, which were incidentally public, for this.
This changes exposes a new public API for accessing information about
build targets, including those for plugins.
    
Note that this API is part of a new module which could be the start of a
"proper" public API for SwiftPM. That said, right now it depends on
several types of existing modules in its public interface and that isn't
likely to be resolved as part of this particular change.

rdar://112120976

Package.swift
@@ -42,6 +42,7 @@ let swiftPMDataModelProduct = (
         "PackageMetadata",
         "PackageModel",
         "SourceControl",
+        "SourceKitLSPAPI",
         "Workspace",
     ]
 )
@@ -146,6 +147,14 @@ let package = Package(
                 .unsafeFlags(["-enable-library-evolution"]),
             ],
             linkerSettings: packageLibraryLinkSettings
+        ),
+
+        .target(
+            name: "SourceKitLSPAPI",
+            dependencies: [
+                "Build",
+                "SPMBuildCore"
+            ]
         ),
 
         // MARK: SwiftPM specific support libraries
@@ -518,6 +527,7 @@ let package = Package(
             name: "SPMTestSupport",
             dependencies: [
                 "Basics",
+                "Build",
                 "PackageFingerprint",
                 "PackageGraph",
                 "PackageLoading",
@@ -536,6 +546,14 @@ let package = Package(
             dependencies: []),
 
         // MARK: SwiftPM tests
+
+        .testTarget(
+            name: "SourceKitLSPAPITests",
+            dependencies: [
+                "SourceKitLSPAPI",
+                "SPMTestSupport",
+            ]
+        ),
 
         .testTarget(
             name: "BasicsTests",

Sources/SPMTestSupport/MockBuildTestHelper.swift
@@ -10,63 +10,66 @@
 //
 //===----------------------------------------------------------------------===//
 
-@testable import PackageModel
-@testable import TSCUtility
-@testable import Build
 import Basics
+import Build
+import PackageModel
 import SPMBuildCore
+import TSCUtility
 import XCTest
 
-struct MockToolchain: PackageModel.Toolchain {
+public struct MockToolchain: PackageModel.Toolchain {
 #if os(Windows)
-    let librarianPath = AbsolutePath("/fake/path/to/link.exe")
+    public let librarianPath = AbsolutePath("/fake/path/to/link.exe")
 #elseif os(iOS) || os(macOS) || os(tvOS) || os(watchOS)
-    let librarianPath = AbsolutePath("/fake/path/to/libtool")
+    public let librarianPath = AbsolutePath("/fake/path/to/libtool")
 #else
-    let librarianPath = AbsolutePath("/fake/path/to/llvm-ar")
+    public let librarianPath = AbsolutePath("/fake/path/to/llvm-ar")
 #endif
-    let swiftCompilerPath = AbsolutePath("/fake/path/to/swiftc")
-    let includeSearchPaths = [AbsolutePath]()
-    let librarySearchPaths = [AbsolutePath]()
-    let swiftResourcesPath: AbsolutePath? = nil
-    let swiftStaticResourcesPath: AbsolutePath? = nil
-    let isSwiftDevelopmentToolchain = false
-    let sdkRootPath: AbsolutePath? = nil
-    let swiftPluginServerPath: AbsolutePath? = nil
-    let extraFlags = PackageModel.BuildFlags()
-    let installedSwiftPMConfiguration = InstalledSwiftPMConfiguration.default
+    public let swiftCompilerPath = AbsolutePath("/fake/path/to/swiftc")
+    public let includeSearchPaths = [AbsolutePath]()
+    public let librarySearchPaths = [AbsolutePath]()
+    public let swiftResourcesPath: AbsolutePath? = nil
+    public let swiftStaticResourcesPath: AbsolutePath? = nil
+    public let isSwiftDevelopmentToolchain = false
+    public let sdkRootPath: AbsolutePath? = nil
+    public let swiftPluginServerPath: AbsolutePath? = nil
+    public let extraFlags = PackageModel.BuildFlags()
+    public let installedSwiftPMConfiguration = InstalledSwiftPMConfiguration.default
 
-    func getClangCompiler() throws -> AbsolutePath {
+    public func getClangCompiler() throws -> AbsolutePath {
         return "/fake/path/to/clang"
     }
 
-    func _isClangCompilerVendorApple() throws -> Bool? {
+    public func _isClangCompilerVendorApple() throws -> Bool? {
       #if os(macOS)
         return true
       #else
         return false
       #endif
     }
+
+    public init() {
+    }
 }
 
 
 extension Basics.Triple {
-    static let x86_64MacOS = try! Self("x86_64-apple-macosx")
-    static let x86_64Linux = try! Self("x86_64-unknown-linux-gnu")
-    static let arm64Linux = try! Self("aarch64-unknown-linux-gnu")
-    static let arm64Android = try! Self("aarch64-unknown-linux-android")
-    static let windows = try! Self("x86_64-unknown-windows-msvc")
-    static let wasi = try! Self("wasm32-unknown-wasi")
+    public static let x86_64MacOS = try! Self("x86_64-apple-macosx")
+    public static let x86_64Linux = try! Self("x86_64-unknown-linux-gnu")
+    public static let arm64Linux = try! Self("aarch64-unknown-linux-gnu")
+    public static let arm64Android = try! Self("aarch64-unknown-linux-android")
+    public static let windows = try! Self("x86_64-unknown-windows-msvc")
+    public static let wasi = try! Self("wasm32-unknown-wasi")
 }
 
-let hostTriple = try! UserToolchain.default.targetTriple
+public let hostTriple = try! UserToolchain.default.targetTriple
 #if os(macOS)
-    let defaultTargetTriple: String = hostTriple.tripleString(forPlatformVersion: "10.13")
+public let defaultTargetTriple: String = hostTriple.tripleString(forPlatformVersion: "10.13")
 #else
-    let defaultTargetTriple: String = hostTriple.tripleString
+public let defaultTargetTriple: String = hostTriple.tripleString
 #endif
 
-func mockBuildParameters(
+public func mockBuildParameters(
     buildPath: AbsolutePath = "/path/to/build",
     config: BuildConfiguration = .debug,
     toolchain: PackageModel.Toolchain = MockToolchain(),
@@ -108,7 +111,7 @@ func mockBuildParameters(
     )
 }
 
-func mockBuildParameters(environment: BuildEnvironment) -> BuildParameters {
+public func mockBuildParameters(environment: BuildEnvironment) -> BuildParameters {
     let triple: Basics.Triple
     switch environment.platform {
     case .macOS:
@@ -130,34 +133,34 @@ enum BuildError: Swift.Error {
     case error(String)
 }
 
-struct BuildPlanResult {
+public struct BuildPlanResult {
 
-    let plan: Build.BuildPlan
-    let targetMap: [String: TargetBuildDescription]
-    let productMap: [String: Build.ProductBuildDescription]
+    public let plan: Build.BuildPlan
+    public let targetMap: [String: TargetBuildDescription]
+    public let productMap: [String: Build.ProductBuildDescription]
 
-    init(plan: Build.BuildPlan) throws {
+    public init(plan: Build.BuildPlan) throws {
         self.plan = plan
         self.productMap = try Dictionary(throwingUniqueKeysWithValues: plan.buildProducts.compactMap { $0 as? Build.ProductBuildDescription }.map{ ($0.product.name, $0) })
         self.targetMap = try Dictionary(throwingUniqueKeysWithValues: plan.targetMap.map{ ($0.0.name, $0.1) })
     }
 
-    func checkTargetsCount(_ count: Int, file: StaticString = #file, line: UInt = #line) {
+    public func checkTargetsCount(_ count: Int, file: StaticString = #file, line: UInt = #line) {
         XCTAssertEqual(plan.targetMap.count, count, file: file, line: line)
     }
 
-    func checkProductsCount(_ count: Int, file: StaticString = #file, line: UInt = #line) {
+    public func checkProductsCount(_ count: Int, file: StaticString = #file, line: UInt = #line) {
         XCTAssertEqual(plan.productMap.count, count, file: file, line: line)
     }
 
-    func target(for name: String) throws -> TargetBuildDescription {
+    public func target(for name: String) throws -> TargetBuildDescription {
         guard let target = targetMap[name] else {
             throw BuildError.error("Target \(name) not found.")
         }
         return target
     }
 
-    func buildProduct(for name: String) throws -> Build.ProductBuildDescription {
+    public func buildProduct(for name: String) throws -> Build.ProductBuildDescription {
         guard let product = productMap[name] else {
             // <rdar://problem/30162871> Display the thrown error on macOS
             throw BuildError.error("Product \(name) not found.")
@@ -167,7 +170,7 @@ struct BuildPlanResult {
 }
 
 extension TargetBuildDescription {
-    func swiftTarget() throws -> SwiftTargetBuildDescription {
+    public func swiftTarget() throws -> SwiftTargetBuildDescription {
         switch self {
         case .swift(let target):
             return target
@@ -176,7 +179,7 @@ extension TargetBuildDescription {
         }
     }
 
-    func clangTarget() throws -> ClangTargetBuildDescription {
+    public func clangTarget() throws -> ClangTargetBuildDescription {
         switch self {
         case .clang(let target):
             return target

Sources/Build/BuildDescription/ClangTargetBuildDescription.swift
@@ -321,6 +321,42 @@ public final class ClangTargetBuildDescription {
         return args
     }
 
+    public func emitCommandLine(for filePath: AbsolutePath) throws -> [String] {
+        let standards = [
+            (clangTarget.cxxLanguageStandard, SupportedLanguageExtension.cppExtensions),
+            (clangTarget.cLanguageStandard, SupportedLanguageExtension.cExtensions),
+        ]
+
+        guard let path = try self.compilePaths().first(where: { $0.source == filePath }) else {
+            throw BuildDescriptionError.requestedFileNotPartOfTarget(
+                targetName: self.target.name,
+                requestedFilePath: filePath
+            )
+        }
+
+        let isCXX = path.source.extension.map { SupportedLanguageExtension.cppExtensions.contains($0) } ?? false
+        let isC = path.source.extension.map { $0 == SupportedLanguageExtension.c.rawValue } ?? false
+
+        var args = try basicArguments(isCXX: isCXX, isC: isC)
+
+        args += ["-MD", "-MT", "dependencies", "-MF", path.deps.pathString]
+
+        // Add language standard flag if needed.
+        if let ext = path.source.extension {
+            for (standard, validExtensions) in standards {
+                if let standard, validExtensions.contains(ext) {
+                    args += ["-std=\(standard)"]
+                }
+            }
+        }
+
+        args += ["-c", path.source.pathString, "-o", path.object.pathString]
+
+        let clangCompiler = try buildParameters.toolchain.getClangCompiler().pathString
+        args.insert(clangCompiler, at: 0)
+        return args
+    }
+
     /// Returns the build flags from the declared build settings.
     private func buildSettingsFlags() throws -> [String] {
         let scope = buildParameters.createScope(for: target)

commit d8197b43b251338069f4f2097a3db80d905587b3

`_InternalTestSupport` should not depend on `Build`
Looks like something I caused a while ago in d457fa46b3 which was later further entrenched in `PluginInvocationTests`, but we should keep the "data model" and "build" parts of SwiftPM separate, so this PR cleans that up again.

Package.swift
@@ -682,12 +682,23 @@ let package = Package(
 
         // MARK: Additional Test Dependencies
 
+            .target(
+                /** SwiftPM internal build test suite support library */
+                name: "_InternalBuildTestSupport",
+                dependencies: [
+                    "Build",
+                    "_InternalTestSupport"
+                ],
+                swiftSettings: [
+                    .unsafeFlags(["-static"]),
+                ]
+            ),
+
         .target(
             /** SwiftPM internal test suite support library */
             name: "_InternalTestSupport",
             dependencies: [
                 "Basics",
-                "Build",
                 "PackageFingerprint",
                 "PackageGraph",
                 "PackageLoading",
@@ -748,7 +759,7 @@ let package = Package(
         ),
         .testTarget(
             name: "BuildTests",
-            dependencies: ["Build", "PackageModel", "Commands", "_InternalTestSupport"]
+            dependencies: ["Build", "PackageModel", "Commands", "_InternalTestSupport", "_InternalBuildTestSupport"]
         ),
         .testTarget(
             name: "LLBuildManifestTests",

Sources/_InternalBuildTestSupport/MockBuildTestHelper.swift
@@ -1,0 +1,229 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Basics
+
+@_spi(SwiftPMInternal)
+import Build
+
+import _InternalTestSupport
+import struct PackageGraph.ModulesGraph
+import struct PackageGraph.ResolvedModule
+import struct PackageGraph.ResolvedProduct
+import PackageModel
+import SPMBuildCore
+import TSCUtility
+import XCTest
+
+public func mockBuildPlan(
+    buildPath: AbsolutePath? = nil,
+    environment: BuildEnvironment,
+    toolchain: PackageModel.Toolchain = MockToolchain(),
+    graph: ModulesGraph,
+    commonFlags: PackageModel.BuildFlags = .init(),
+    indexStoreMode: BuildParameters.IndexStoreMode = .off,
+    omitFramePointers: Bool? = nil,
+    driverParameters: BuildParameters.Driver = .init(),
+    linkingParameters: BuildParameters.Linking = .init(),
+    targetSanitizers: EnabledSanitizers = .init(),
+    fileSystem fs: any FileSystem,
+    observabilityScope: ObservabilityScope
+) async throws -> Build.BuildPlan {
+    try await mockBuildPlan(
+        buildPath: buildPath,
+        config: environment.configuration ?? .debug,
+        platform: environment.platform,
+        toolchain: toolchain,
+        graph: graph,
+        commonFlags: commonFlags,
+        indexStoreMode: indexStoreMode,
+        omitFramePointers: omitFramePointers,
+        driverParameters: driverParameters,
+        linkingParameters: linkingParameters,
+        targetSanitizers: targetSanitizers,
+        fileSystem: fs,
+        observabilityScope: observabilityScope
+    )
+}
+
+public func mockBuildPlan(
+    buildPath: AbsolutePath? = nil,
+    config: BuildConfiguration = .debug,
+    triple: Basics.Triple? = nil,
+    platform: PackageModel.Platform? = nil,
+    toolchain: PackageModel.Toolchain = MockToolchain(),
+    graph: ModulesGraph,
+    commonFlags: PackageModel.BuildFlags = .init(),
+    indexStoreMode: BuildParameters.IndexStoreMode = .off,
+    omitFramePointers: Bool? = nil,
+    driverParameters: BuildParameters.Driver = .init(),
+    linkingParameters: BuildParameters.Linking = .init(),
+    targetSanitizers: EnabledSanitizers = .init(),
+    fileSystem fs: any FileSystem,
+    observabilityScope: ObservabilityScope
+) async throws -> Build.BuildPlan {
+    let inferredTriple: Basics.Triple
+    if let platform {
+        precondition(triple == nil)
+
+        inferredTriple = switch platform {
+        case .macOS:
+            Triple.x86_64MacOS
+        case .linux:
+            Triple.arm64Linux
+        case .android:
+            Triple.arm64Android
+        case .windows:
+            Triple.windows
+        default:
+            fatalError("unsupported platform in tests")
+        }
+    } else {
+        inferredTriple = triple ?? hostTriple
+    }
+
+    let commonDebuggingParameters = BuildParameters.Debugging(
+        triple: inferredTriple,
+        shouldEnableDebuggingEntitlement: config == .debug,
+        omitFramePointers: omitFramePointers
+    )
+
+    var destinationParameters = mockBuildParameters(
+        destination: .target,
+        buildPath: buildPath,
+        config: config,
+        toolchain: toolchain,
+        flags: commonFlags,
+        triple: inferredTriple,
+        indexStoreMode: indexStoreMode
+    )
+    destinationParameters.debuggingParameters = commonDebuggingParameters
+    destinationParameters.driverParameters = driverParameters
+    destinationParameters.linkingParameters = linkingParameters
+    destinationParameters.sanitizers = targetSanitizers
+
+    var hostParameters = mockBuildParameters(
+        destination: .host,
+        buildPath: buildPath,
+        config: config,
+        toolchain: toolchain,
+        flags: commonFlags,
+        triple: inferredTriple,
+        indexStoreMode: indexStoreMode
+    )
+    hostParameters.debuggingParameters = commonDebuggingParameters
+    hostParameters.driverParameters = driverParameters
+    hostParameters.linkingParameters = linkingParameters
+
+    return try await BuildPlan(
+        destinationBuildParameters: destinationParameters,
+        toolsBuildParameters: hostParameters,
+        graph: graph,
+        fileSystem: fs,
+        observabilityScope: observabilityScope
+    )
+}
+
+package func mockPluginTools(
+    plugins: IdentifiableSet<ResolvedModule>,
+    fileSystem: any FileSystem,
+    buildParameters: BuildParameters,
+    hostTriple: Basics.Triple
+) async throws -> [ResolvedModule.ID: [String: PluginTool]] {
+    var accessibleToolsPerPlugin: [ResolvedModule.ID: [String: PluginTool]] = [:]
+    for plugin in plugins where accessibleToolsPerPlugin[plugin.id] == nil {
+        let accessibleTools = try await plugin.preparePluginTools(
+            fileSystem: fileSystem,
+            environment: buildParameters.buildEnvironment,
+            for: hostTriple
+        ) { name, path in
+            buildParameters.buildPath.appending(path)
+        }
+
+        accessibleToolsPerPlugin[plugin.id] = accessibleTools
+    }
+
+    return accessibleToolsPerPlugin
+}
+
+enum BuildError: Swift.Error {
+    case error(String)
+}
+
+public struct BuildPlanResult {
+    public let plan: Build.BuildPlan
+
+    public var productMap: IdentifiableSet<Build.ProductBuildDescription> {
+        self.plan.productMap
+    }
+
+    public var targetMap: IdentifiableSet<Build.ModuleBuildDescription> {
+        self.plan.targetMap
+    }
+
+    public init(plan: Build.BuildPlan) throws {
+        self.plan = plan
+    }
+
+    public func checkTargetsCount(_ count: Int, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertEqual(self.targetMap.count, count, file: file, line: line)
+    }
+
+    public func checkProductsCount(_ count: Int, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertEqual(self.productMap.count, count, file: file, line: line)
+    }
+
+    public func moduleBuildDescription(for name: String) throws -> Build.ModuleBuildDescription {
+        let matches = self.targetMap.filter({ $0.module.name == name })
+        guard matches.count == 1 else {
+            if matches.isEmpty {
+                throw BuildError.error("Target \(name) not found.")
+            } else {
+                throw BuildError.error("More than one target \(name) found.")
+            }
+        }
+        return matches.first!
+    }
+
+    public func buildProduct(for name: String) throws -> Build.ProductBuildDescription {
+        let matches = self.productMap.filter({ $0.product.name == name })
+        guard matches.count == 1 else {
+            if matches.isEmpty {
+                // <rdar://problem/30162871> Display the thrown error on macOS
+                throw BuildError.error("Product \(name) not found.")
+            } else {
+                throw BuildError.error("More than one target \(name) found.")
+            }
+        }
+        return matches.first!
+    }
+}
+
+extension Build.ModuleBuildDescription {
+    public func swift() throws -> SwiftModuleBuildDescription {
+        switch self {
+        case .swift(let description):
+            return description
+        default:
+            throw BuildError.error("Unexpected \(self) type found")
+        }
+    }
+
+    public func clang() throws -> ClangModuleBuildDescription {
+        switch self {
+        case .clang(let description):
+            return description
+        default:
+            throw BuildError.error("Unexpected \(self) type")
+        }
+    }
+}

Sources/_InternalTestSupport/MockBuildTestHelper.swift
@@ -12,9 +12,6 @@
 
 import Basics
 
-@_spi(SwiftPMInternal)
-import Build
-
 import struct PackageGraph.ModulesGraph
 import struct PackageGraph.ResolvedModule
 import struct PackageGraph.ResolvedProduct
@@ -148,208 +145,4 @@ public func mockBuildParameters(
         config: environment.configuration ?? .debug,
         triple: triple
     )
-}
-
-public func mockBuildPlan(
-    buildPath: AbsolutePath? = nil,
-    environment: BuildEnvironment,
-    toolchain: PackageModel.Toolchain = MockToolchain(),
-    graph: ModulesGraph,
-    commonFlags: PackageModel.BuildFlags = .init(),
-    indexStoreMode: BuildParameters.IndexStoreMode = .off,
-    omitFramePointers: Bool? = nil,
-    driverParameters: BuildParameters.Driver = .init(),
-    linkingParameters: BuildParameters.Linking = .init(),
-    targetSanitizers: EnabledSanitizers = .init(),
-    fileSystem fs: any FileSystem,
-    observabilityScope: ObservabilityScope
-) async throws -> Build.BuildPlan {
-    try await mockBuildPlan(
-        buildPath: buildPath,
-        config: environment.configuration ?? .debug,
-        platform: environment.platform,
-        toolchain: toolchain,
-        graph: graph,
-        commonFlags: commonFlags,
-        indexStoreMode: indexStoreMode,
-        omitFramePointers: omitFramePointers,
-        driverParameters: driverParameters,
-        linkingParameters: linkingParameters,
-        targetSanitizers: targetSanitizers,
-        fileSystem: fs,
-        observabilityScope: observabilityScope
-    )
-}
-
-public func mockBuildPlan(
-    buildPath: AbsolutePath? = nil,
-    config: BuildConfiguration = .debug,
-    triple: Basics.Triple? = nil,
-    platform: PackageModel.Platform? = nil,
-    toolchain: PackageModel.Toolchain = MockToolchain(),
-    graph: ModulesGraph,
-    commonFlags: PackageModel.BuildFlags = .init(),
-    indexStoreMode: BuildParameters.IndexStoreMode = .off,
-    omitFramePointers: Bool? = nil,
-    driverParameters: BuildParameters.Driver = .init(),
-    linkingParameters: BuildParameters.Linking = .init(),
-    targetSanitizers: EnabledSanitizers = .init(),
-    fileSystem fs: any FileSystem,
-    observabilityScope: ObservabilityScope
-) async throws -> Build.BuildPlan {
-    let inferredTriple: Basics.Triple
-    if let platform {
-        precondition(triple == nil)
-
-        inferredTriple = switch platform {
-        case .macOS:
-            Triple.x86_64MacOS
-        case .linux:
-            Triple.arm64Linux
-        case .android:
-            Triple.arm64Android
-        case .windows:
-            Triple.windows
-        default:
-            fatalError("unsupported platform in tests")
-        }
-    } else {
-        inferredTriple = triple ?? hostTriple
-    }
-
-    let commonDebuggingParameters = BuildParameters.Debugging(
-        triple: inferredTriple,
-        shouldEnableDebuggingEntitlement: config == .debug,
-        omitFramePointers: omitFramePointers
-    )
-
-    var destinationParameters = mockBuildParameters(
-        destination: .target,
-        buildPath: buildPath,
-        config: config,
-        toolchain: toolchain,
-        flags: commonFlags,
-        triple: inferredTriple,
-        indexStoreMode: indexStoreMode
-    )
-    destinationParameters.debuggingParameters = commonDebuggingParameters
-    destinationParameters.driverParameters = driverParameters
-    destinationParameters.linkingParameters = linkingParameters
-    destinationParameters.sanitizers = targetSanitizers
-
-    var hostParameters = mockBuildParameters(
-        destination: .host,
-        buildPath: buildPath,
-        config: config,
-        toolchain: toolchain,
-        flags: commonFlags,
-        triple: inferredTriple,
-        indexStoreMode: indexStoreMode
-    )
-    hostParameters.debuggingParameters = commonDebuggingParameters
-    hostParameters.driverParameters = driverParameters
-    hostParameters.linkingParameters = linkingParameters
-
-    return try await BuildPlan(
-        destinationBuildParameters: destinationParameters,
-        toolsBuildParameters: hostParameters,
-        graph: graph,
-        fileSystem: fs,
-        observabilityScope: observabilityScope
-    )
-}
-
-package func mockPluginTools(
-    plugins: IdentifiableSet<ResolvedModule>,
-    fileSystem: any FileSystem,
-    buildParameters: BuildParameters,
-    hostTriple: Basics.Triple
-) async throws -> [ResolvedModule.ID: [String: PluginTool]] {
-    var accessibleToolsPerPlugin: [ResolvedModule.ID: [String: PluginTool]] = [:]
-    for plugin in plugins where accessibleToolsPerPlugin[plugin.id] == nil {
-        let accessibleTools = try await plugin.preparePluginTools(
-            fileSystem: fileSystem,
-            environment: buildParameters.buildEnvironment,
-            for: hostTriple
-        ) { name, path in
-            buildParameters.buildPath.appending(path)
-        }
-
-        accessibleToolsPerPlugin[plugin.id] = accessibleTools
-    }
-
-    return accessibleToolsPerPlugin
-}
-
-enum BuildError: Swift.Error {
-    case error(String)
-}
-
-public struct BuildPlanResult {
-    public let plan: Build.BuildPlan
-
-    public var productMap: IdentifiableSet<Build.ProductBuildDescription> {
-        self.plan.productMap
-    }
-
-    public var targetMap: IdentifiableSet<Build.ModuleBuildDescription> {
-        self.plan.targetMap
-    }
-
-    public init(plan: Build.BuildPlan) throws {
-        self.plan = plan
-    }
-
-    public func checkTargetsCount(_ count: Int, file: StaticString = #file, line: UInt = #line) {
-        XCTAssertEqual(self.targetMap.count, count, file: file, line: line)
-    }
-
-    public func checkProductsCount(_ count: Int, file: StaticString = #file, line: UInt = #line) {
-        XCTAssertEqual(self.productMap.count, count, file: file, line: line)
-    }
-
-    public func moduleBuildDescription(for name: String) throws -> Build.ModuleBuildDescription {
-        let matches = self.targetMap.filter({ $0.module.name == name })
-        guard matches.count == 1 else {
-            if matches.isEmpty {
-                throw BuildError.error("Target \(name) not found.")
-            } else {
-                throw BuildError.error("More than one target \(name) found.")
-            }
-        }
-        return matches.first!
-    }
-
-    public func buildProduct(for name: String) throws -> Build.ProductBuildDescription {
-        let matches = self.productMap.filter({ $0.product.name == name })
-        guard matches.count == 1 else {
-            if matches.isEmpty {
-                // <rdar://problem/30162871> Display the thrown error on macOS
-                throw BuildError.error("Product \(name) not found.")
-            } else {
-                throw BuildError.error("More than one target \(name) found.")
-            }
-        }
-        return matches.first!
-    }
-}
-
-extension Build.ModuleBuildDescription {
-    public func swift() throws -> SwiftModuleBuildDescription {
-        switch self {
-        case .swift(let description):
-            return description
-        default:
-            throw BuildError.error("Unexpected \(self) type found")
-        }
-    }
-
-    public func clang() throws -> ClangModuleBuildDescription {
-        switch self {
-        case .clang(let description):
-            return description
-        default:
-            throw BuildError.error("Unexpected \(self) type")
-        }
-    }
 }

Tests/BuildTests/BuildPlanTests.swift
@@ -26,6 +26,7 @@ import PackageLoading
 @testable import PackageModel
 
 import SPMBuildCore
+import _InternalBuildTestSupport
 import _InternalTestSupport
 import SwiftDriver
 import Workspace

Tests/BuildTests/CrossCompilationBuildPlanTests.swift
@@ -28,11 +28,11 @@ import func _InternalTestSupport.embeddedCxxInteropPackageGraph
 import func _InternalTestSupport.macrosPackageGraph
 import func _InternalTestSupport.macrosTestsPackageGraph
 import func _InternalTestSupport.mockBuildParameters
-import func _InternalTestSupport.mockBuildPlan
+import func _InternalBuildTestSupport.mockBuildPlan
 import func _InternalTestSupport.toolsExplicitLibrariesGraph
 import func _InternalTestSupport.trivialPackageGraph
 
-import struct _InternalTestSupport.BuildPlanResult
+import struct _InternalBuildTestSupport.BuildPlanResult
 import func _InternalTestSupport.XCTAssertMatch
 import func _InternalTestSupport.XCTAssertNoDiagnostics
 

Tests/BuildTests/LLBuildManifestBuilderTests.swift
@@ -20,6 +20,7 @@ import PackageGraph
 import PackageModel
 import struct SPMBuildCore.BuildParameters
 
+import _InternalBuildTestSupport
 @_spi(SwiftPMInternal)
 import _InternalTestSupport
 

Tests/BuildTests/ModuleAliasingBuildTests.swift
@@ -19,6 +19,7 @@ import Basics
 import PackageLoading
 @testable import PackageModel
 import SPMBuildCore
+import _InternalBuildTestSupport
 import _InternalTestSupport
 import SwiftDriver
 import Workspace

Tests/BuildTests/PluginInvocationTests.swift
@@ -22,6 +22,7 @@ import PackageLoading
 import PackageModel
 
 @testable import SPMBuildCore
+import _InternalBuildTestSupport
 import _InternalTestSupport
 import Workspace
 import XCTest