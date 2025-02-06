954701169a2801e279fa73e2e403c601ce1f2301
Sam Khouri <bkhouri@apple.com>
2025-02-05 22:38:22 -0500

    Attempt solution at a custom XFail for XCTest..

--- Sources/_InternalTestSupport/Commands.swift
+++ Sources/_InternalTestSupport/Commands.swift
@@ -1,8 +1,38 @@
 import SPMBuildCore
 import XCTest
 
+public struct XFailCaseName {
+    let testName: String
+    let reason: String
+
+    public init(_ testName: String, because reason: String) {
+        self.testName = testName
+        self.reason = reason
+    }
+}
 open class BuildSystemProviderTestCase: XCTestCase {
     open var buildSystemProvider: BuildSystemProvider.Kind {
         fatalError("\(self) does not implement \(#function)")
+    }
+
+    open var xFailTestCaseNames: [XFailCaseName] {
+        return []
+    }
+
+    override open func recordFailure(withDescription description: String, inFile filePath: String, atLine lineNumber: Int, expected: Bool) {
+        // Get current test name:
+        print("--->> In recordFailure: Test name is >>>\(self.name)<<<")
+
+        if self.xFailTestCaseNames.map({ item in item.testName }).contains(self.name) {
+            // do nothing
+            print("--->> In recordFailure: Test name is >>>\(self.name)<<< is expected to fail, so mark as passed!!")
+        } else {
+            super.recordFailure(
+                withDescription: description,
+                inFile: filePath,
+                atLine: lineNumber,
+                expected: expected
+            )
+        }
     }
 }
