e0c6ff29404fac4aeb11259648d71a3ec32668e6
Ben Barham <ben_barham@apple.com>
2025-02-07 16:50:48 -0800

    Add a new `BuildDescription.load` API for use in SourceKit-LSP
    Build tool plugins need to be built and run as part of the build plan
    generation. Rather than relying on clients to do so, add a new API that
    does both. This does unfortunately expose much more of the SwiftPM API
    than previously, but given it is all required to a generate a
    `BuildPlan`, this was really already the case.
    
    Long term this module should be replaced by a long-running BSP server
    that can prepare and provide build settings in the one process.
    
    Resolves rdar://102242345.
    
    (cherry picked from commit 4fa0769ef5d4fa0d2bdfbdb0a80f8a6ffe065b2d)

--- Package.swift
+++ Package.swift
@@ -169,7 +169,11 @@ let package = Package(
         .target(
             name: "SourceKitLSPAPI",
             dependencies: [
+                "Basics",
                 "Build",
+                "PackageGraph",
+                "PackageLoading",
+                "PackageModel",
                 "SPMBuildCore",
             ],
             exclude: ["CMakeLists.txt"],
