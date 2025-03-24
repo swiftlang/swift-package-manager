3dad0aa3b87cd3d2102e38b75a0515123e1a541e
Sam Khouri <bkhouri@apple.com>
2025-03-24 13:39:54 -0400

    Test: Merge IntegrationTests into Tests
    Consolidate all the tests into a single folders.  This change only moves
    the IntegrationTests/Sources and IntegrationTests/Tests to their
    respective directory and update tests to make use of the existing
    executeSwift* functions.
    
    A subsequent change will find better homes for the tests under
    Tests/IntegrationTests.

--- Package.swift
+++ Package.swift
@@ -775,7 +775,6 @@ let package = Package(
                 .unsafeFlags(["-static"]),
             ]
         ),
-
         .target(
             /** Test for thread-sanitizer. */
             name: "tsan_utils",
@@ -826,6 +825,13 @@ let package = Package(
         .testTarget(
             name: "LLBuildManifestTests",
             dependencies: ["Basics", "LLBuildManifest", "_InternalTestSupport"]
+        ),
+        .testTarget(
+            name: "IntegrationTests",
+            dependencies: [
+                "_InternalTestSupport",
+                "SPMBuildCore",
+            ]
         ),
         .testTarget(
             name: "WorkspaceTests",
@@ -983,6 +989,7 @@ if ProcessInfo.processInfo.environment["SWIFTCI_DISABLE_SDK_DEPENDENT_TESTS"] ==
                 "dummy-swiftc",
             ]
         ),
+
     ])
 }
 #endif
