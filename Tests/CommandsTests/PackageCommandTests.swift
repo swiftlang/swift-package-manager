516649a1017b52080e2565efd23e064cc97ca3cc
Paul LeMarquand <plemarquand@apple.com>
2025-03-25 13:42:29 -0400

    Fixup another PackageCommandTest

--- Tests/CommandsTests/PackageCommandTests.swift
+++ Tests/CommandsTests/PackageCommandTests.swift
@@ -871,7 +871,7 @@ class PackageCommandTestCase: CommandsBuildProviderTestCase {
             XCTAssertMatch(contents, .prefix("// swift-tools-version:\(version < .v5_4 ? "" : " ")\(versionSpecifier)\n"))
 
             XCTAssertFileExists(manifest)
-            XCTAssertEqual(try fs.getDirectoryContents(path.appending("Sources")), ["CustomName.swift"])
+            XCTAssertEqual(try fs.getDirectoryContents(path.appending("Sources").appending("CustomName")), ["CustomName.swift"])
         }
     }
 
