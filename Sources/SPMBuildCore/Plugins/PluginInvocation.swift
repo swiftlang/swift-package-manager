d35d848b1f9efb9117a7160be44e9a74d488db70
Doug Schaefer <dschaefer2@apple.com>
2025-04-01 17:08:23 -0400

    Tighten up how the executable is checked.
    We really want to make sure the tool isn't a built tool since it
    won't always be available whent the prebuild command runs.
    Instead of checking file exists, save away the source of the
    plugin tool, built or vended, and pass in the paths to the built
    tools to be checked in the delegate.

--- Sources/SPMBuildCore/Plugins/PluginInvocation.swift
+++ Sources/SPMBuildCore/Plugins/PluginInvocation.swift
@@ -43,10 +43,19 @@ public enum PluginAction {
 public struct PluginTool {
     public let path: AbsolutePath
     public let triples: [String]?
+    public let source: Source
 
-    public init(path: AbsolutePath, triples: [String]? = nil) {
+    public enum Source {
+        // Built from an executable target
+        case built
+        // Brought in from a binary target
+        case vended
+    }
+
+    public init(path: AbsolutePath, triples: [String]? = nil, source: Source) {
         self.path = path
         self.triples = triples
+        self.source = source
     }
 }
 
@@ -507,11 +516,14 @@ extension PluginModule {
         // Determine additional input dependencies for any plugin commands,
         // based on any executables the plugin target depends on.
         let toolPaths = accessibleTools.values.map(\.path).sorted()
+        
+        let builtToolPaths = accessibleTools.values.filter({ $0.source == .built }).map((\.path)).sorted()
 
         let delegate = DefaultPluginInvocationDelegate(
             fileSystem: fileSystem,
             delegateQueue: delegateQueue,
-            toolPaths: toolPaths
+            toolPaths: toolPaths,
+            builtToolPaths: builtToolPaths
         )
 
         let startTime = DispatchTime.now()
@@ -707,7 +719,7 @@ public extension ResolvedModule {
             switch tool {
             case .builtTool(let name, let path):
                 if let path = try await builtToolHandler(name, path) {
-                    tools[name] = PluginTool(path: path)
+                    tools[name] = PluginTool(path: path, source: .built)
                 }
             case .vendedTool(let name, let path, let triples):
                 // Avoid having the path of an unsupported tool overwrite a supported one.
@@ -715,7 +727,7 @@ public extension ResolvedModule {
                     continue
                 }
                 let priorTriples = tools[name]?.triples ?? []
-                tools[name] = PluginTool(path: path, triples: priorTriples + triples)
+                tools[name] = PluginTool(path: path, triples: priorTriples + triples, source: .vended)
             }
         }
 
@@ -839,6 +851,7 @@ final class DefaultPluginInvocationDelegate: PluginInvocationDelegate {
     let fileSystem: FileSystem
     let delegateQueue: DispatchQueue
     let toolPaths: [AbsolutePath]
+    let builtToolPaths: [AbsolutePath]
     var outputData = Data()
     var diagnostics = [Basics.Diagnostic]()
     var buildCommands = [BuildToolPluginInvocationResult.BuildCommand]()
@@ -847,11 +860,13 @@ final class DefaultPluginInvocationDelegate: PluginInvocationDelegate {
     package init(
         fileSystem: FileSystem,
         delegateQueue: DispatchQueue,
-        toolPaths: [AbsolutePath]
+        toolPaths: [AbsolutePath],
+        builtToolPaths: [AbsolutePath]
     ) {
         self.fileSystem = fileSystem
         self.delegateQueue = delegateQueue
         self.toolPaths = toolPaths
+        self.builtToolPaths = builtToolPaths
     }
 
     func pluginCompilationStarted(commandLine: [String], environment: [String: String]) {}
@@ -905,7 +920,7 @@ final class DefaultPluginInvocationDelegate: PluginInvocationDelegate {
     ) -> Bool {
         dispatchPrecondition(condition: .onQueue(self.delegateQueue))
         // executable must exist before running prebuild command
-        if !fileSystem.exists(executable) {
+        if builtToolPaths.contains(executable) {
             self.diagnostics
                 .append(
                     .error(
