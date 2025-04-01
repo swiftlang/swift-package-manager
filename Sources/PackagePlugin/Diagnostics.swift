b7082febb4ea27956a528e1545232f63671b69e4
Graham Lee <glee23@apple.com>
2025-04-01 11:48:11 +0100

    Clean up documentation in Diagnostics.

--- Sources/PackagePlugin/Diagnostics.swift
+++ Sources/PackagePlugin/Diagnostics.swift
@@ -10,17 +10,29 @@
 //
 //===----------------------------------------------------------------------===//
 
-/// Emits errors, warnings, and remarks to be shown as a result of running the
-/// plugin. After emitting one or more errors, the plugin should return a
-/// non-zero exit code.
+/// Emits messages the system shows as a result of running the plugin.
+///
+/// > Note: If the plugin emits one or more errors, it should return a
+/// > non-zero exit code.
 public struct Diagnostics {
 
-    /// Severity of the diagnostic.
+    /// Severity of the diagnostic message.
     public enum Severity: String, Encodable {
-        case error, warning, remark
+        /// The diagnostic message is an error.
+        case error
+        /// The diagnostic message is a warning.
+        case warning
+        /// The diagnostic message is a remark.
+        case remark
     }
     
-    /// Emits an error with a specified severity and message, and optional file path and line number.
+    /// Emits a message with a specified severity, and optional file path and line number.
+    ///
+    /// - Parameters:
+    ///   - severity: The severity of the message.
+    ///   - description: The message to display.
+    ///   - file: The source file to which the message relates.
+    ///   - line: The line number in the source file to which the message relates.
     public static func emit(_ severity: Severity, _ description: String, file: String? = #file, line: Int? = #line) {
         let message: PluginToHostMessage
         switch severity {
@@ -36,21 +48,38 @@ public struct Diagnostics {
     }
 
     /// Emits an error with the specified message, and optional file path and line number.
+    ///
+    /// - Parameters:
+    ///   - message: The text of the error.
+    ///   - file: The source file to which the error relates.
+    ///   - line: The line number in the source file to which the error relates.
     public static func error(_ message: String, file: String? = #file, line: Int? = #line) {
         self.emit(.error, message, file: file, line: line)
     }
 
     /// Emits a warning with the specified message, and optional file path and line number.
+    ///
+    /// - Parameters:
+    ///   - message: The text of the warning.
+    ///   - file: The source file to which the warning relates.
+    ///   - line: The line number in the source file to which the warning relates.
     public static func warning(_ message: String, file: String? = #file, line: Int? = #line) {
         self.emit(.warning, message, file: file, line: line)
     }
 
     /// Emits a remark with the specified message, and optional file path and line number.
+    ///
+    /// - Parameters:
+    ///   - message: The text of the remark.
+    ///   - file: The source file to which the remark relates.
+    ///   - line: The line number in the source file to which the remark relates.
     public static func remark(_ message: String, file: String? = #file, line: Int? = #line) {
         self.emit(.remark, message, file: file, line: line)
     }
 
-    /// Emits a progress message
+    /// Emits a progress message.
+    ///
+    /// - Parameter message: The text of the progress update.
     public static func progress(_ message: String) {
         try? pluginHostConnection.sendMessage(.emitProgress(message: message))
     }
