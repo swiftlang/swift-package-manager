9b6cd6851d1a606c6ae5f77b6bd48f9e46aaee9e
Paul LeMarquand <plemarquand@apple.com>
2025-04-12 20:53:22 -0400

    Adopt MemberImportVisiblity - SourceKitLSPAPI

--- Package.swift
+++ Package.swift
@@ -203,6 +203,7 @@ let package = Package(
             ],
             exclude: ["CMakeLists.txt"],
             swiftSettings: [
+                .enableExperimentalFeature("MemberImportVisibility"),
                 .enableExperimentalFeature("AccessLevelOnImport"),
                 .unsafeFlags(["-static"]),
             ]
