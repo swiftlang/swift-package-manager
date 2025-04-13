28bfbc62fbb314eabe033b79a51f788e42eafe70
Paul LeMarquand <plemarquand@apple.com>
2025-04-12 21:09:40 -0400

    Adopt MemberImportVisiblity - PackageRegistry

--- Package.swift
+++ Package.swift
@@ -271,6 +271,7 @@ let package = Package(
             ],
             exclude: ["CMakeLists.txt"],
             swiftSettings: [
+                .enableExperimentalFeature("MemberImportVisibility"),
                 .unsafeFlags(["-static"]),
             ]
         ),
