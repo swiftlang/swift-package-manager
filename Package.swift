7cae693d9e3b442f3b80ca08e0ef0c1cc1715efc
Paul LeMarquand <plemarquand@apple.com>
2025-04-14 22:01:42 -0400

    Adopt MemberImportVisiblity - DriverSupport

--- Package.swift
+++ Package.swift
@@ -479,6 +479,7 @@ let package = Package(
             ],
             exclude: ["CMakeLists.txt"],
             swiftSettings: [
+                .enableExperimentalFeature("MemberImportVisibility"),
                 .unsafeFlags(["-static"]),
             ]
         ),
