swiftlang/swift-package-manager

commit 393a8f32da9f49203932400be4e4b04eefc7b1e8

Adopt new PIF builder in SwiftBuildSupport (#8454)
### Motivation:

Switch from the legacy PIF builder in `SwiftBuildSupport` to 
the new one, introduced by PR #8405.

The new PIF builder (i.e., `SwiftBuildSupport/PackagePIFBuilder*.swift`)
is the exact same we use in Xcode.

### Modifications:

Replaces the old PIF builder (i.e., `SwiftBuildSupport/PIF.swift` 
and `SwiftBuildSupport/PIFBuilder.swift` ) with the new one 
(i.e., `SwiftBuildSupport/PackagePIFBuilder*.swift`).

### Result:

The new PIF builder now fully replaces the legacy PIF builder.

In particular, all these Swift Build tests are passing (i.e., same as before this PR):

* BuildPlanSwiftBuildTests
* APIDiffSwiftBuildTests
* BuildCommandSwiftBuildTests
* PackageCommandSwiftBuildTests
* RunCommandSwiftBuildTests
* TestCommandSwiftBuildTests

I also improved the PIF logging too. Try the `--very-verbose` option to see what I mean :-)

Tracked by rdar://147527170.

IntegrationTests/Tests/IntegrationTests/SwiftPMTests.swift
@@ -75,10 +75,15 @@ private struct SwiftPMTests {
     }
 
     @Test(
+        .requireHostOS(.windows, when: false),
         .requireThreadSafeWorkingDirectory,
         .bug(
             "https://github.com/swiftlang/swift-package-manager/issues/8416",
-            "swift run using --build-system swiftbuild fails to run executable"
+            "[Linux] swift run using --build-system swiftbuild fails to run executable"
+        ),
+        .bug(
+            "https://github.com/swiftlang/swift-package-manager/issues/8514",
+            "[Windows] Integration test SwiftPMTests.packageInitExecutable with --build-system swiftbuild is skipped"
         ),
         arguments: BuildSystemProvider.allCases
     )
@@ -119,12 +124,12 @@ private struct SwiftPMTests {
             try localFileSystem.createDirectory(packagePath)
             try sh(swiftPackage, "--package-path", packagePath, "init", "--type", "library")
             try withKnownIssue(
-                    """
-                    Linux: /lib/x86_64-linux-gnu/Scrt1.o:function _start: error: undefined reference to 'main'
-                    Windows: lld-link: error: subsystem must be defined
-                    MacOS: Could not find or use auto-linked library 'Testing': library 'Testing' not found
-                    """,
-                    isIntermittent: true
+                """
+                Linux: /lib/x86_64-linux-gnu/Scrt1.o:function _start: error: undefined reference to 'main'
+                Windows: lld-link: error: subsystem must be defined
+                MacOS: Could not find or use auto-linked library 'Testing': library 'Testing' not found
+                """,
+                isIntermittent: true
             ) {
                 try sh(swiftBuild, "--package-path", packagePath, "--build-system", buildSystemProvider.rawValue, "--vv")
                 let (stdout, stderr) = try sh(

Sources/SwiftBuildSupport/BuildSystem.swift
@@ -14,7 +14,7 @@ extension BuildSubset {
     var pifTargetName: String {
         switch self {
         case .product(let name, _):
-            _PackagePIFProjectBuilder.targetName(for: name)
+            targetName(forProductName: name)
         case .target(let name, _):
             name
         case .allExcludingTests:

Sources/SwiftBuildSupport/PackagePIFBuilder+Helpers.swift
@@ -55,17 +55,27 @@ import struct PackageGraph.ResolvedProduct
 
 import func PackageLoading.pkgConfigArgs
 
+// TODO: Move this back to `PackagePIFBuilder` once we get rid of `#if canImport(SwiftBuild)`.
+func targetName(forProductName name: String, suffix: String? = nil) -> String {
+    let suffix = suffix ?? ""
+    return "\(name)\(suffix)-product"
+}
+
 #if canImport(SwiftBuild)
 
 import enum SwiftBuild.ProjectModel
 
 // MARK: - PIF GUID Helpers
 
-enum TargetGUIDSuffix: String {
+enum TargetSuffix: String {
     case testable, dynamic
+    
+    func hasSuffix(id: GUID) -> Bool {
+        id.value.hasSuffix("-\(self.rawValue)")
+    }
 }
 
-extension TargetGUIDSuffix? {
+extension TargetSuffix? {
     func description(forName name: String) -> String {
         switch self {
         case .some(let suffix):
@@ -76,34 +86,45 @@ extension TargetGUIDSuffix? {
     }
 }
 
+extension GUID {
+    func hasSuffix(_ suffix: TargetSuffix) -> Bool {
+        self.value.hasSuffix("-\(suffix.rawValue)")
+    }
+}
+
 extension PackageModel.Module {
-    func pifTargetGUID(suffix: TargetGUIDSuffix? = nil) -> GUID {
+    var pifTargetGUID: GUID { pifTargetGUID(suffix: nil) }
+
+    func pifTargetGUID(suffix: TargetSuffix?) -> GUID {
         PackagePIFBuilder.targetGUID(forModuleName: self.name, suffix: suffix)
     }
 }
 
 extension PackageGraph.ResolvedModule {
-    func pifTargetGUID(suffix: TargetGUIDSuffix? = nil) -> GUID {
+    var pifTargetGUID: GUID { pifTargetGUID(suffix: nil) }
+
+    func pifTargetGUID(suffix: TargetSuffix?) -> GUID {
         self.underlying.pifTargetGUID(suffix: suffix)
     }
 }
 
 extension PackageModel.Product {
-    func pifTargetGUID(suffix: TargetGUIDSuffix? = nil) -> GUID {
+    var pifTargetGUID: GUID { pifTargetGUID(suffix: nil) }
+
+    func pifTargetGUID(suffix: TargetSuffix?) -> GUID {
         PackagePIFBuilder.targetGUID(forProductName: self.name, suffix: suffix)
     }
 }
 
 extension PackageGraph.ResolvedProduct {
-    func pifTargetGUID(suffix: TargetGUIDSuffix? = nil) -> GUID {
+    var pifTargetGUID: GUID { pifTargetGUID(suffix: nil) }
+
+    func pifTargetGUID(suffix: TargetSuffix?) -> GUID {
         self.underlying.pifTargetGUID(suffix: suffix)
     }
 
-    /// Helper function to consistently generate a target name string for a product in a package.
-    /// This format helps make sure that targets and products with the same name (as they often have) have different
-    /// target names in the PIF.
-    func targetNameForProduct(suffix: String = "") -> String {
-        "\(name)\(suffix) product"
+    func targetName(suffix: TargetSuffix? = nil) -> String {
+        PackagePIFBuilder.targetName(forProductName: self.name, suffix: suffix)
     }
 }
 
@@ -112,7 +133,7 @@ extension PackagePIFBuilder {
     ///
     /// This format helps make sure that there is no collision with any other PIF targets,
     /// and in particular that a PIF target and a PIF product can have the same name (as they often do).
-    static func targetGUID(forModuleName name: String, suffix: TargetGUIDSuffix? = nil) -> GUID {
+    static func targetGUID(forModuleName name: String, suffix: TargetSuffix? = nil) -> GUID {
         let suffixDescription = suffix.description(forName: name)
         return "PACKAGE-TARGET:\(name)\(suffixDescription)"
     }
@@ -121,9 +142,16 @@ extension PackagePIFBuilder {
     ///
     /// This format helps make sure that there is no collision with any other PIF targets,
     /// and in particular that a PIF target and a PIF product can have the same name (as they often do).
-    static func targetGUID(forProductName name: String, suffix: TargetGUIDSuffix? = nil) -> GUID {
+    static func targetGUID(forProductName name: String, suffix: TargetSuffix? = nil) -> GUID {
         let suffixDescription = suffix.description(forName: name)
         return "PACKAGE-PRODUCT:\(name)\(suffixDescription)"
+    }
+    
+    /// Helper function to consistently generate a target name string for a product in a package.
+    /// This format helps make sure that targets and products with the same name (as they often have) have different
+    /// target names in the PIF.
+    static func targetName(forProductName name: String, suffix: TargetSuffix? = nil) -> String {
+        return SwiftBuildSupport.targetName(forProductName: name, suffix: suffix?.rawValue)
     }
 }
 
@@ -796,7 +824,14 @@ extension TSCUtility.Version {
     }
 }
 
-// MARK: - Swift Build PIF Helpers
+// MARK: - Swift Build ProjectModel Helpers
+
+/// Helpful for logging.
+extension ProjectModel.GUID: @retroactive CustomStringConvertible  {
+    public var description: String {
+        value
+    }
+}
 
 extension ProjectModel.BuildSettings {
     subscript(_ setting: MultipleValueSetting, default defaultValue: [String]) -> [String] {
@@ -1010,6 +1045,55 @@ extension ProjectModel.BuildSettings.Declaration {
         default:
             preconditionFailure("Unexpected BuildSettings.Declaration: \(declaration.name)")
         }
+    }
+}
+
+// MARK: - ObservabilityScope Helpers
+
+extension ObservabilityScope {
+    /// Logs an informational PIF message (intended for developers, not end users).
+    func logPIF(
+        _ severity: Diagnostic.Severity = .debug,
+        indent: UInt = 0,
+        _ message: String,
+        sourceFile: StaticString = #fileID,
+        sourceLine: UInt = #line
+    ) {
+        var metadata = ObservabilityMetadata()
+        metadata.sourceLocation = SourceLocation(sourceFile, sourceLine)
+
+        let indentation = String(repeating: "  ", count: Int(indent))
+        let message = "PIF: \(indentation)\(message)"
+        
+        let diagnostic = Diagnostic(severity: severity, message: message, metadata: metadata)
+        self.emit(diagnostic)
+    }
+}
+
+extension ObservabilityMetadata {
+    public var sourceLocation: SourceLocation? {
+        get {
+            self[SourceLocationKey.self]
+        }
+        set {
+            self[SourceLocationKey.self] = newValue
+        }
+    }
+
+    private enum SourceLocationKey: Key {
+        typealias Value = SourceLocation
+    }
+}
+
+public struct SourceLocation: Sendable {
+    public let file: StaticString
+    public let line: UInt
+
+    public init(_ file: StaticString, _ line: UInt) {
+        precondition(file.description.hasContent)
+        
+        self.file = file
+        self.line = line
     }
 }
 

Sources/SwiftBuildSupport/PackagePIFBuilder.swift
@@ -59,18 +59,14 @@ public final class PackagePIFBuilder {
     /// Scope for logging informational debug messages (intended for developers, not end users).
     let observabilityScope: ObservabilityScope
 
-    /// Logs an informational debug message (intended for developers, not end users).
+    /// Logs an informational message (intended for developers, not end users).
     func log(
         _ severity: Diagnostic.Severity,
         _ message: String,
         sourceFile: StaticString = #fileID,
         sourceLine: UInt = #line
     ) {
-        var metadata = ObservabilityMetadata()
-        metadata.sourceLocation = SourceLocation(sourceFile, sourceLine)
-
-        let diagnostic = Diagnostic(severity: severity, message: message, metadata: metadata)
-        self.observabilityScope.emit(diagnostic)
+        self.observabilityScope.logPIF(severity, message, sourceFile: sourceFile, sourceLine: sourceLine)
     }
 
     unowned let delegate: BuildDelegate
@@ -84,15 +80,15 @@ public final class PackagePIFBuilder {
         /// If a pure Swift package is open in the workspace.
         var hostsOnlyPackages: Bool { get }
 
-        /// Returns `true` if the package is managed by the user (i.e., the user is allowed to modify its sources,
-        /// package structure, etc).
+        /// Returns `true` if the package is managed by the user
+        /// (i.e., the user is allowed to modify its sources, package structure, etc).
         var isUserManaged: Bool { get }
 
         /// Whether or not this package is required by *branch* or *revision*.
         var isBranchOrRevisionBased: Bool { get }
 
-        /// For executables — only executables for now — we check to see if there is a custom package product type
-        /// provider that can provide this information.
+        /// For executables — only executables for now — we check to see if there is a
+        /// custom package product type provider that can provide this information.
         func customProductType(forExecutable product: PackageModel.Product) -> ProjectModel.Target.ProductType?
 
         /// Returns all *device family* IDs for all SDK variants.
@@ -373,25 +369,24 @@ public final class PackagePIFBuilder {
     /// Build the PIF.
     @discardableResult
     public func build() throws -> [ModuleOrProduct] {
-        self.log(.info, "Building PIF for package \(self.package.identity)")
+        self.log(
+            .info,
+            "Building PIF project for package '\(self.package.identity)' " +
+            "(\(package.products.count) products, \(package.modules.count) modules)"
+        )
 
-        var builder = PackagePIFProjectBuilder(createForPackage: package, builder: self)
-        self.addProjectBuildSettings(&builder)
-
-        self._pifProject = builder.project
+        var projectBuilder = PackagePIFProjectBuilder(createForPackage: package, builder: self)
+        self.addProjectBuildSettings(&projectBuilder)
 
         //
-        // Construct PIF *targets* (for modules, products, and test bundles) based on the contents of the parsed
-        // package.
-        // These PIF targets will be sent down to Swift Build.
+        // Construct PIF *targets* (for modules, products, and test bundles) based on the contents
+        // of the parsed package. These PIF targets will be sent down to Swift Build.
         //
         // We also track all constructed objects as `ModuleOrProduct` value for easy introspection by clients.
         // In SwiftPM a product is a codeless entity with a reference to the modules(s) that contains the
-        // implementation.
-        // In order to avoid creating two ModuleOrProducts for each product in the package, the logic below creates a
-        // single
-        // unified ModuleOrProduct from the combination of a product and the single target that contains its
-        // implementation.
+        // implementation. In order to avoid creating two ModuleOrProducts for each product in the package,
+        // the logic below creates a single unified ModuleOrProduct from the combination of a product
+        // and the single target that contains its implementation.
         //
         // Products. SwiftPM considers unit tests to be products, so in this discussion, the term *product*
         // refers to an *executable*, a *library*, or an *unit test*.
@@ -402,54 +397,58 @@ public final class PackagePIFBuilder {
         // the structure of the client(s).
         //
 
+        self.log(.debug, "Processing \(package.products.count) products:")
+        
         // For each of the **products** in the package we create a corresponding `PIFTarget` of the appropriate type.
         for product in self.package.products {
             switch product.type {
             case .library(.static):
                 let libraryType = self.delegate.customLibraryType(product: product.underlying) ?? .static
-                try builder.makeLibraryProduct(product, type: libraryType)
+                try projectBuilder.makeLibraryProduct(product, type: libraryType)
 
             case .library(.dynamic):
                 let libraryType = self.delegate.customLibraryType(product: product.underlying) ?? .dynamic
-                try builder.makeLibraryProduct(product, type: libraryType)
+                try projectBuilder.makeLibraryProduct(product, type: libraryType)
 
             case .library(.automatic):
                 // Check if this is a system library product.
                 if product.isSystemLibraryProduct {
-                    try builder.makeSystemLibraryProduct(product)
+                    try projectBuilder.makeSystemLibraryProduct(product)
                 } else {
                     // Otherwise, it is a regular library product.
                     let libraryType = self.delegate.customLibraryType(product: product.underlying) ?? .automatic
-                    try builder.makeLibraryProduct(product, type: libraryType)
+                    try projectBuilder.makeLibraryProduct(product, type: libraryType)
                 }
 
             case .executable, .test:
-                try builder.makeMainModuleProduct(product)
+                try projectBuilder.makeMainModuleProduct(product)
 
             case .plugin:
-                try builder.makePluginProduct(product)
+                try projectBuilder.makePluginProduct(product)
 
             case .snippet, .macro:
                 break // TODO: Double-check what's going on here as we skip snippet modules too (rdar://147705448)
             }
         }
 
+        self.log(.debug, "Processing \(package.modules.count) modules:")
+
         // For each of the **modules** in the package other than those that are the *main* module of a product
         // —— which we've already dealt with above —— we create a corresponding `PIFTarget` of the appropriate type.
         for module in self.package.modules {
             switch module.type {
             case .executable:
-                try builder.makeTestableExecutableSourceModule(module)
+                try projectBuilder.makeTestableExecutableSourceModule(module)
 
             case .snippet:
                 // Already handled as a product. Note that snippets don't need testable modules.
                 break
 
             case .library:
-                try builder.makeLibraryModule(module)
+                try projectBuilder.makeLibraryModule(module)
 
             case .systemModule:
-                try builder.makeSystemLibraryModule(module)
+                try projectBuilder.makeSystemLibraryModule(module)
 
             case .test:
                 // Skip test module targets.
@@ -461,17 +460,18 @@ public final class PackagePIFBuilder {
                 break
 
             case .plugin:
-                try builder.makePluginModule(module)
+                try projectBuilder.makePluginModule(module)
 
             case .macro:
-                try builder.makeMacroModule(module)
+                try projectBuilder.makeMacroModule(module)
             }
         }
 
-        let customModulesAndProducts = try delegate.addCustomTargets(pifProject: builder.project)
-        builder.builtModulesAndProducts.append(contentsOf: customModulesAndProducts)
+        let customModulesAndProducts = try delegate.addCustomTargets(pifProject: projectBuilder.project)
+        projectBuilder.builtModulesAndProducts.append(contentsOf: customModulesAndProducts)
 
-        return builder.builtModulesAndProducts
+        self._pifProject = projectBuilder.project
+        return projectBuilder.builtModulesAndProducts
     }
 
     /// Configure the project-wide build settings.
@@ -658,31 +658,6 @@ extension PackagePIFBuilder.LinkedPackageBinary {
         case .module(let moduleDependency, _):
             self.init(module: moduleDependency, package: package)
         }
-    }
-}
-
-extension ObservabilityMetadata {
-    public var sourceLocation: SourceLocation? {
-        get {
-            self[SourceLocationKey.self]
-        }
-        set {
-            self[SourceLocationKey.self] = newValue
-        }
-    }
-
-    private enum SourceLocationKey: Key {
-        typealias Value = SourceLocation
-    }
-}
-
-public struct SourceLocation: Sendable {
-    public let file: StaticString
-    public let line: UInt
-
-    public init(_ file: StaticString, _ line: UInt) {
-        self.file = file
-        self.line = line
     }
 }
 

commit ffdc0c026c0541f5af8b391489705e24fba38788

Tests: Run packageInitExecutable test on all platforms for native build system (#8527)
### Motivation:

Increase code coverage for our integration tests.

This is an issue I introduced in #8454 while ignoring 
`--build-system swiftbuild` issues on Windows (i.e., actually this 
specific issue #8514). I accidentally skipped the packageInitExecutable
test for `--build-system native` as well (which should be green).
This PR fixes that.

### Modifications:

Swift Testing currently doesn't support combining *conditional traits*
(e.g.,`skipHostOS(.windows)`) with *parametric tests*. So the 2nd 
best approach is to split the tests as I just did.

IntegrationTests/Tests/IntegrationTests/SwiftPMTests.swift
@@ -75,7 +75,15 @@ private struct SwiftPMTests {
     }
 
     @Test(
-        .requireHostOS(.windows, when: false),
+        .requireThreadSafeWorkingDirectory,
+        arguments: [BuildSystemProvider.native]
+    )
+    func packageInitExecutable(_ buildSystemProvider: BuildSystemProvider) throws {
+        try _packageInitExecutable(buildSystemProvider)
+    }
+
+    @Test(
+        .skipHostOS(.windows),
         .requireThreadSafeWorkingDirectory,
         .bug(
             "https://github.com/swiftlang/swift-package-manager/issues/8416",
@@ -85,30 +93,31 @@ private struct SwiftPMTests {
             "https://github.com/swiftlang/swift-package-manager/issues/8514",
             "[Windows] Integration test SwiftPMTests.packageInitExecutable with --build-system swiftbuild is skipped"
         ),
-        arguments: BuildSystemProvider.allCases
+        arguments: [BuildSystemProvider.swiftbuild]
     )
-    func packageInitExecutable(_ buildSystemProvider: BuildSystemProvider) throws {
-        // Executable
-        do {
-            try withTemporaryDirectory { tmpDir in
-                let packagePath = tmpDir.appending(component: "foo")
-                try localFileSystem.createDirectory(packagePath)
-                try sh(swiftPackage, "--package-path", packagePath, "init", "--type", "executable")
-                try sh(swiftBuild, "--package-path", packagePath, "--build-system", buildSystemProvider.rawValue)
+    func packageInitExecutablSkipWindows(_ buildSystemProvider: BuildSystemProvider) throws {
+        try _packageInitExecutable(buildSystemProvider)
+    }
 
-                try withKnownIssue("Error while loading shared libraries: libswiftCore.so: cannot open shared object file: No such file or directory") {
-                    // The 'native' build system uses 'swiftc' as the linker driver, which adds an RUNPATH to the swift runtime libraries in the SDK.
-                    // 'swiftbuild' directly calls clang, which does not add the extra RUNPATH, so runtime libraries cannot be found.
-                    let (stdout, stderr) = try sh(
-                        swiftRun, "--package-path", packagePath, "--build-system", buildSystemProvider.rawValue
-                    )
-                    #expect(!stderr.contains("error:"))
-                    #expect(stdout.contains("Hello, world!"))
-                } when: {
-                    buildSystemProvider == .swiftbuild && ProcessInfo.hostOperatingSystem == .linux
-                }
+    private func _packageInitExecutable(_ buildSystemProvider: BuildSystemProvider) throws {
+        try withTemporaryDirectory { tmpDir in
+            let packagePath = tmpDir.appending(component: "foo")
+            try localFileSystem.createDirectory(packagePath)
+            try sh(swiftPackage, "--package-path", packagePath, "init", "--type", "executable")
+            try sh(swiftBuild, "--package-path", packagePath, "--build-system", buildSystemProvider.rawValue)
+
+            try withKnownIssue("Error while loading shared libraries: libswiftCore.so: cannot open shared object file: No such file or directory") {
+                // The 'native' build system uses 'swiftc' as the linker driver, which adds an RUNPATH to the swift runtime libraries in the SDK.
+                // 'swiftbuild' directly calls clang, which does not add the extra RUNPATH, so runtime libraries cannot be found.
+                let (stdout, stderr) = try sh(
+                    swiftRun, "--package-path", packagePath, "--build-system", buildSystemProvider.rawValue
+                )
+                #expect(!stderr.contains("error:"))
+                #expect(stdout.contains("Hello, world!"))
+            } when: {
+                buildSystemProvider == .swiftbuild && ProcessInfo.hostOperatingSystem == .linux
             }
-        }
+            }
     }
 
     @Test(