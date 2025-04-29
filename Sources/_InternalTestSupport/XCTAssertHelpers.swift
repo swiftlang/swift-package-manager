ed31e6e9559377f95066c64f16fe8cf6d8141dbf
Bassam (Sam) Khouri <bkhouri@users.noreply.github.com>
2025-04-29 16:35:06 -0400

    Tests: Enable more WorkspaceTests on Windows (#8457)
    Many WorkspaceTests instantiate a MockWorkspace, which takes a path as
    an input. The tests were defining a POSIX-like path as the sandbox
    directory, however, some assertions were incorrect as, on these *nix
    like filesystem, the canonical path name and the path were identical.
    
    Update the WorkspaceTests expectation accordingly, and add some
    automated tests to cover the InMemoryFileSystem (which is commented out
    as a crash occurs in the Linux Platform CI build with Swift Testing
    tests).
    
    Relates: #8433
    rdar://148248105

--- Sources/_InternalTestSupport/XCTAssertHelpers.swift
+++ Sources/_InternalTestSupport/XCTAssertHelpers.swift
@@ -46,7 +46,7 @@ public func XCTAssertEqual<T:Equatable, U:Equatable> (_ lhs:(T,U), _ rhs:(T,U), 
 
 public func XCTSkipIfCI(file: StaticString = #filePath, line: UInt = #line) throws {
     // TODO: is this actually the right variable now?
-    if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] != nil {
+    if isInCiEnvironment {
         throw XCTSkip("Skipping because the test is being run on CI", file: file, line: line)
     }
 }
