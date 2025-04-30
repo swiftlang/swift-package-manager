swiftlang/swift-package-manager

commit c8d439a2ccc679477165d6dc992ef4a44b23b77c

Switch to ProjectModel API in the new SwiftBuildSupport PIF builder (#8441)
### Motivation:

The goal is to adopt the new `SwiftBuild.ProjectModel` API. This new API
provides a *typesafer* and *modern* way of building PIFs
programmatically.

This continues the work I started in PR #8405, introducing now our new
PIF builder for packages.

### Modifications:

Replaces all `SwiftBuild.PIF` (aka, `SWBProjectModel.PIF`) API usage, in
`PackagePIFBuilder`, with the new `SwiftBuild.ProjectModel` API instead.

### Result:

`PackagePIFBuilder` is now modernized... but still not actually used.
This will come in my next pull request.

Tracked by rdar://147526957.

Sources/SwiftBuildSupport/PIFBuilder.swift
@@ -1641,7 +1641,7 @@ private struct PIFBuildSettingAssignment {
     let platforms: [PIF.BuildSettings.Platform]?
 }
 
-extension BuildSettings.AssignmentTable {
+extension PackageModel.BuildSettings.AssignmentTable {
     fileprivate var pifAssignments: [PIF.BuildSettings.MultipleValueSetting: [PIFBuildSettingAssignment]] {
         var pifAssignments: [PIF.BuildSettings.MultipleValueSetting: [PIFBuildSettingAssignment]] = [:]
 
@@ -1679,7 +1679,7 @@ extension BuildSettings.AssignmentTable {
     }
 }
 
-extension BuildSettings.Assignment {
+extension PackageModel.BuildSettings.Assignment {
     fileprivate var configurations: [BuildConfiguration] {
         if let configurationCondition = conditions.lazy.compactMap(\.configurationCondition).first {
             [configurationCondition.configuration]
@@ -1866,7 +1866,7 @@ extension PIF.BuildSettings {
                 .filter { isSupportedVersion($0) }.map(\.description)
         }
 
-        func computeEffectiveTargetVersion(for assignment: BuildSettings.Assignment) throws -> String {
+        func computeEffectiveTargetVersion(for assignment: PackageModel.BuildSettings.Assignment) throws -> String {
             let versions = assignment.values.compactMap { SwiftLanguageVersion(string: $0) }
             if let effectiveVersion = computeEffectiveSwiftVersions(for: versions).last {
                 return effectiveVersion

Sources/SwiftBuildSupport/PackagePIFBuilder+Plugins.swift
@@ -18,9 +18,10 @@ import enum Basics.Sandbox
 import struct Basics.SourceControlURL
 
 #if canImport(SwiftBuild)
-import enum SwiftBuild.PIF
 
-extension PIFPackageBuilder {
+import enum SwiftBuild.ProjectModel
+
+extension PackagePIFBuilder {
     /// Contains all of the information resulting from applying a build tool plugin to a package target thats affect how
     /// a target is built.
     ///

commit e84c0baa156ba29abf94715e8f61220985cf417e

Some PIF delegate fixes for Swift Build  (#8573)
### Motivation:

Fixes a few issues introduced by #8441, where the PIF model transitioned from 
reference types to value types (i.e., the new SwiftBuild.ProjectModel API.)

### Modifications:

Just a couple of changes in protocol PackagePIFBuilder.BuildDelegate,
ensuring we can actually mutate the passed-in ProjectModel.Project value.

Sources/SwiftBuildSupport/PIFBuilder.swift
@@ -279,7 +279,7 @@ fileprivate final class PackagePIFBuilderDelegate: PackagePIFBuilder.BuildDelega
         []
     }
     
-    func addCustomTargets(pifProject: SwiftBuild.ProjectModel.Project) throws -> [PackagePIFBuilder.ModuleOrProduct] {
+    func addCustomTargets(pifProject: inout SwiftBuild.ProjectModel.Project) throws -> [PackagePIFBuilder.ModuleOrProduct] {
         return []
     }
     
@@ -293,6 +293,7 @@ fileprivate final class PackagePIFBuilderDelegate: PackagePIFBuilder.BuildDelega
     
     func configureLibraryProduct(
         product: PackageModel.Product,
+        project: inout ProjectModel.Project,
         target: WritableKeyPath<ProjectModel.Project, ProjectModel.Target>,
         additionalFiles: WritableKeyPath<ProjectModel.Group, ProjectModel.Group>
     ) {

Sources/SwiftBuildSupport/PackagePIFBuilder.swift
@@ -124,7 +124,7 @@ public final class PackagePIFBuilder {
         func customSDKOptions(forPlatform: PackageModel.Platform) -> [String]
 
         /// Create additional custom PIF targets after all targets have been built.
-        func addCustomTargets(pifProject: ProjectModel.Project) throws -> [PackagePIFBuilder.ModuleOrProduct]
+        func addCustomTargets(pifProject: inout ProjectModel.Project) throws -> [PackagePIFBuilder.ModuleOrProduct]
 
         /// Should we suppresses the specific product dependency, updating the provided build settings if necessary?
         /// The specified product may be in the same package or a different one.
@@ -141,6 +141,7 @@ public final class PackagePIFBuilder {
         /// Provides additional configuration and files for the specified library product.
         func configureLibraryProduct(
             product: PackageModel.Product,
+            project: inout ProjectModel.Project,
             target: WritableKeyPath<ProjectModel.Project, ProjectModel.Target>,
             additionalFiles: WritableKeyPath<ProjectModel.Group, ProjectModel.Group>
         )
@@ -474,7 +475,7 @@ public final class PackagePIFBuilder {
             }
         }
 
-        let customModulesAndProducts = try delegate.addCustomTargets(pifProject: projectBuilder.project)
+        let customModulesAndProducts = try delegate.addCustomTargets(pifProject: &projectBuilder.project)
         projectBuilder.builtModulesAndProducts.append(contentsOf: customModulesAndProducts)
 
         self._pifProject = projectBuilder.project

Sources/SwiftBuildSupport/PackagePIFProjectBuilder+Products.swift
@@ -703,6 +703,7 @@ extension PackagePIFProjectBuilder {
         // Additional configuration and files for this library product.
         pifBuilder.delegate.configureLibraryProduct(
             product: product.underlying,
+            project: &self.project,
             target: librayUmbrellaTargetKeyPath,
             additionalFiles: additionalFilesGroupKeyPath
         )