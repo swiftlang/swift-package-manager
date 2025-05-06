f3fc979a1b1a144598534d1f6667eafdaa2069be
Sam Khouri <bkhouri@apple.com>
2025-05-06 09:48:48 -0400

    Convert Environment/Graph and other suites to ST
    Convert BasicsTests/Environemnt/*.swift,
    BasicsTests/Graph/* and a couple other in BasicsTests to Swift Testing

--- Tests/BasicsTests/Graph/DirectedGraphTests.swift
+++ Tests/BasicsTests/Graph/DirectedGraphTests.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift open source project
 //
-// Copyright (c) 2024 Apple Inc. and the Swift project authors
+// Copyright (c) 2024-2025 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See http://swift.org/LICENSE.txt for license information
@@ -13,21 +13,22 @@
 @_spi(DontAdoptOutsideOfSwiftPMExposedForBenchmarksAndTestsOnly)
 import Basics
 
-import XCTest
+import Testing
 
-final class DirectedGraphTests: XCTestCase {
-    func testNodesConnection() {
+struct DirectedGraphTests {
+    @Test
+    func nodesConnection() {
         var graph = DirectedGraph(nodes: ["app1", "lib1", "lib2", "app2", "lib3"])
         graph.addEdge(source: 0, destination: 1)
         graph.addEdge(source: 1, destination: 2)
-        XCTAssertTrue(graph.areNodesConnected(source: 0, destination: 2))
-        XCTAssertFalse(graph.areNodesConnected(source: 2, destination: 0))
+        #expect(graph.areNodesConnected(source: 0, destination: 2))
+        #expect(!graph.areNodesConnected(source: 2, destination: 0))
 
         graph.addEdge(source: 0, destination: 4)
         graph.addEdge(source: 3, destination: 4)
-        XCTAssertTrue(graph.areNodesConnected(source: 3, destination: 4))
-        XCTAssertTrue(graph.areNodesConnected(source: 0, destination: 4))
-        XCTAssertFalse(graph.areNodesConnected(source: 1, destination: 4))
-        XCTAssertTrue(graph.areNodesConnected(source: 0, destination: 4))
+        #expect(graph.areNodesConnected(source: 3, destination: 4))
+        #expect(graph.areNodesConnected(source: 0, destination: 4))
+        #expect(!graph.areNodesConnected(source: 1, destination: 4))
+        #expect(graph.areNodesConnected(source: 0, destination: 4))
     }
 }
