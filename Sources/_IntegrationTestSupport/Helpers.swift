6898ac6b50a977e4503b048802e675f6819dc47b
Sam Khouri <bkhouri@apple.com>
2025-05-16 19:01:43 -0400

    remove commented code

--- Sources/_IntegrationTestSupport/Helpers.swift
+++ Sources/_IntegrationTestSupport/Helpers.swift
@@ -10,7 +10,6 @@
 
 import Foundation
 import Testing
-// import TSCBasic
 import _InternalTestSupport
 import TSCTestSupport
 import enum TSCUtility.Git
@@ -109,26 +108,6 @@ public let lldb: AbsolutePath = {
     #endif
 }()
 
-// public let swiftpmBinaryDirectory: AbsolutePath = {
-//     if let environmentPath = ProcessInfo.processInfo.environment["SWIFTPM_CUSTOM_BIN_DIR"] {
-//         return try! AbsolutePath(validating: environmentPath)
-//     }
-
-//     return swift.parentDirectory
-// }()
-
-// public let swiftBuild: AbsolutePath = swiftpmBinaryDirectory.appending(component: "swift-build")
-
-// public let swiftPackage: AbsolutePath = swiftpmBinaryDirectory.appending(component: "swift-package")
-
-// public let swiftTest: AbsolutePath = swiftpmBinaryDirectory.appending(component: "swift-test")
-
-// public let swiftRun: AbsolutePath = swiftpmBinaryDirectory.appending(component: "swift-run")
-
-// public let isSelfHosted: Bool = {
-//     ProcessInfo.processInfo.environment["SWIFTCI_IS_SELF_HOSTED"] != nil
-// }()
-
 @discardableResult
 package func sh(
     _ arguments: CustomStringConvertible...,
@@ -152,29 +131,6 @@ package func sh(
     return (stdout, stderr, result.exitStatus)
 }
 
-// @discardableResult
-// package func shFails(
-//     _ arguments: CustomStringConvertible...,
-//     env: [String: String] = [:],
-//     file: StaticString = #file,
-//     line: UInt = #line
-// ) throws -> ShReturnType {
-//     let result = try _sh(arguments, env: env, file: file, line: line)
-//     let stdout = try result.utf8Output()
-//     let stderr = try result.utf8stderrOutput()
-
-//     if result.exitStatus == .terminated(code: 0) {
-//         Issue
-//             .record(
-//                 Comment(
-//                     "Command unexpectedly succeeded with exit code: \(result.exitStatus) - \(result.integrationTests_debugDescription)"
-//                 )
-//             )
-//     }
-
-//     return (stdout, stderr, result.exitStatus)
-// }
-
 @discardableResult
 package func _sh(
     _ arguments: [CustomStringConvertible],
@@ -197,125 +153,6 @@ package func _sh(
     )
     return result
 }
-
-// /// Test-helper function that runs a block of code on a copy of a test fixture
-// /// package.  The copy is made into a temporary directory, and the block is
-// /// given a path to that directory.  The block is permitted to modify the copy.
-// /// The temporary copy is deleted after the block returns.  The fixture name may
-// /// contain `/` characters, which are treated as path separators, exactly as if
-// /// the name were a relative path.
-// public func fixture(
-//     name: String,
-//     file: StaticString = #file,
-//     line: UInt = #line,
-//     body: (AbsolutePath) throws -> Void
-// ) {
-//     do {
-//         // Make a suitable test directory name from the fixture subpath.
-//         let fixtureSubpath = try RelativePath(validating: name)
-//         let copyName = fixtureSubpath.components.joined(separator: "_")
-
-//         // Create a temporary directory for the duration of the block.
-//         try withTemporaryDirectory(prefix: copyName) { tmpDirPath in
-
-//             defer {
-//                 // Unblock and remove the tmp dir on deinit.
-//                 try? localFileSystem.chmod(.userWritable, path: tmpDirPath, options: [.recursive])
-//                 try? localFileSystem.removeFileTree(tmpDirPath)
-//             }
-
-//             // Construct the expected path of the fixture.
-//             // FIXME: This seems quite hacky; we should provide some control over where fixtures are found.
-//             let fixtureDir = try AbsolutePath(
-//                 validating: "../../../Fixtures/\(name)",
-//                 relativeTo: AbsolutePath(validating: #file)
-//             )
-
-//             // Check that the fixture is really there.
-//             guard localFileSystem.isDirectory(fixtureDir) else {
-//                 Issue.record(Comment("No such fixture: \(fixtureDir)"))
-//                 return
-//             }
-
-//             // The fixture contains either a checkout or just a Git directory.
-//             if localFileSystem.isFile(fixtureDir.appending(component: "Package.swift")) {
-//                 // It's a single package, so copy the whole directory as-is.
-//                 let dstDir = tmpDirPath.appending(component: copyName)
-//                 #if os(Windows)
-//                 try localFileSystem.copy(from: fixtureDir, to: dstDir)
-//                 #else
-//                 try systemQuietly("cp", "-R", "-H", fixtureDir.pathString, dstDir.pathString)
-//                 #endif
-
-//                 // Invoke the block, passing it the path of the copied fixture.
-//                 try body(dstDir)
-//             } else {
-//                 // Copy each of the package directories and construct a git repo in it.
-//                 for fileName in try! localFileSystem.getDirectoryContents(fixtureDir).sorted() {
-//                     let srcDir = fixtureDir.appending(component: fileName)
-//                     guard localFileSystem.isDirectory(srcDir) else { continue }
-//                     let dstDir = tmpDirPath.appending(component: fileName)
-//                     #if os(Windows)
-//                     try localFileSystem.copy(from: srcDir, to: dstDir)
-//                     #else
-//                     try systemQuietly("cp", "-R", "-H", srcDir.pathString, dstDir.pathString)
-//                     #endif
-//                     initGitRepo(dstDir, tag: "1.2.3", addFile: false)
-//                 }
-
-//                 // Invoke the block, passing it the path of the copied fixture.
-//                 try body(tmpDirPath)
-//             }
-//         }
-//     } catch {
-//         Issue.record(error)
-//     }
-// }
-
-// /// Test-helper function that creates a new Git repository in a directory.  The new repository will contain
-// /// exactly one empty file unless `addFile` is `false`, and if a tag name is provided, a tag with that name will be
-// /// created.
-// public func initGitRepo(
-//     _ dir: AbsolutePath,
-//     tag: String? = nil,
-//     addFile: Bool = true,
-//     file: StaticString = #file,
-//     line: UInt = #line
-// ) {
-//     initGitRepo(dir, tags: tag.flatMap { [$0] } ?? [], addFile: addFile, file: file, line: line)
-// }
-
-// public func initGitRepo(
-//     _ dir: AbsolutePath,
-//     tags: [String],
-//     addFile: Bool = true,
-//     file: StaticString = #file,
-//     line: UInt = #line
-// ) {
-//     do {
-//         if addFile {
-//             let file = dir.appending(component: "file.swift")
-//             try localFileSystem.writeFileContents(file, bytes: "")
-//         }
-
-//         try systemQuietly([Git.tool, "-C", dir.pathString, "init"])
-//         try systemQuietly([
-//             Git.tool, "-C", dir.pathString, "config", "user.email", "example@example.com",
-//         ])
-//         try systemQuietly([
-//             Git.tool, "-C", dir.pathString, "config", "user.name", "Example Example",
-//         ])
-//         try systemQuietly([Git.tool, "-C", dir.pathString, "config", "commit.gpgsign", "false"])
-//         try systemQuietly([Git.tool, "-C", dir.pathString, "add", "."])
-//         try systemQuietly([Git.tool, "-C", dir.pathString, "commit", "-m", "Add some files."])
-
-//         for tag in tags {
-//             try systemQuietly([Git.tool, "-C", dir.pathString, "tag", tag])
-//         }
-//     } catch {
-//         Issue.record(error)
-//     }
-// }
 
 public func binaryTargetsFixture(_ closure: (AbsolutePath) async throws -> Void) async throws {
     try await fixture(name: "BinaryTargets") { fixturePath in
