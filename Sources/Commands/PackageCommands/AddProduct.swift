5a815c7486c0b1d75c3e0d147adc3a1c5ddeed5a
John Bute <johnbute@Johns-MacBook-Pro-2.local>
2025-06-04 16:10:41 -0400

    fulfilling cases for addproduct, will need to revisit to make sure adding product of type template works

--- Sources/Commands/PackageCommands/AddProduct.swift
+++ Sources/Commands/PackageCommands/AddProduct.swift
@@ -32,6 +32,7 @@ extension SwiftPackageCommand {
             case staticLibrary = "static-library"
             case dynamicLibrary = "dynamic-library"
             case plugin
+            case template
         }
 
         package static let configuration = CommandConfiguration(
@@ -85,6 +86,7 @@ extension SwiftPackageCommand {
             case .dynamicLibrary: .library(.dynamic)
             case .staticLibrary: .library(.static)
             case .plugin: .plugin
+            case .template: .template
             }
 
             let product = ProductDescription(
