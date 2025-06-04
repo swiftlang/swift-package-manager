b50a4fc730aed4a23875be98a8dac5661a7f68c4
John Bute <johnbute@Johns-MacBook-Pro-2.local>
2025-06-04 16:02:02 -0400

    fixtures + cleanup

---
+++ Fixtures/Miscellaneous/ShowTemplates/app/Plugins/GenerateStuffPlugin/plugin.swift
@@ -0,0 +1,29 @@
+//
+//  plugin.swift
+//  app
+//
+//  Created by John Bute on 2025-06-03.
+//
+
+import Foundation
+
+import PackagePlugin
+
+/// plugin that will kickstart the template executable=≠≠
+@main
+
+struct TemplatePlugin: CommandPlugin {
+    func performCommand(
+        context: PluginContext,
+        arguments: [String]
+    ) async throws {
+        let tool = try context.tool(named: "GenerateStuff")
+        let process = Process()
+
+        process.executableURL = URL(filePath: tool.url.path())
+        process.arguments = arguments.filter { $0 != "--" }
+
+        try process.run()
+        process.waitUntilExit()
+    }
+}
---
+++ Fixtures/Miscellaneous/ShowTemplates/app/Templates/GenerateStuff/Template.swift
@@ -0,0 +1,53 @@
+import ArgumentParser
+import Foundation
+import SystemPackage
+
+extension FilePath {
+    static func / (left: FilePath, right: String) -> FilePath {
+        left.appending(right)
+    }
+}
+
+extension String {
+    func write(toFile: FilePath) throws {
+        try self.write(toFile: toFile.string, atomically: true, encoding: .utf8)
+    }
+}
+
+//basic structure of a template that uses string interpolation
+@main
+struct HelloTemplateTool: ParsableCommand {
+
+    //swift argument parser needed to expose arguments to template generator
+    @Option(help: "The name of your app")
+    var name: String
+
+    @Flag(help: "Include a README?")
+    var includeReadme: Bool = false
+
+    //entrypoint of the template executable, that generates just a main.swift and a readme.md
+    func run() throws {
+        let fs = FileManager.default
+
+        let rootDir = FilePath(fs.currentDirectoryPath)
+
+        let mainFile = rootDir / "Soures" / name / "main.swift"
+
+        try fs.createDirectory(atPath: mainFile.removingLastComponent().string, withIntermediateDirectories: true)
+
+        try """
+            // This is the entry point to your command-line app
+            print("Hello, \(name)!")
+
+            """.write(toFile: mainFile)
+
+        if includeReadme {
+            try """
+                # \(name)
+                This is a new Swift app!
+                """.write(toFile: rootDir / "README.md")
+        }
+
+        print("Project generated at \(rootDir)")
+    }
+}
