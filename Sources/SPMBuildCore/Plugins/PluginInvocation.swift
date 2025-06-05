9d54614bed7dbdc25fab1a8566a4eda79c61ae94
John Bute <johnbute@Johns-MacBook-Pro-2.local>
2025-06-05 09:34:51 -0400

    plugins support for executing templates

--- Sources/SPMBuildCore/Plugins/PluginInvocation.swift
+++ Sources/SPMBuildCore/Plugins/PluginInvocation.swift
@@ -696,7 +696,7 @@ fileprivate func collectAccessibleTools(
             }
         }
         // For an executable target we create a `builtTool`.
-        else if executableOrBinaryModule.type == .executable {
+        else if executableOrBinaryModule.type == .executable || executableOrBinaryModule.type == .template { //john-to-revisit
             return try [.builtTool(name: builtToolName, path: RelativePath(validating: executableOrBinaryModule.name))]
         }
         else {
