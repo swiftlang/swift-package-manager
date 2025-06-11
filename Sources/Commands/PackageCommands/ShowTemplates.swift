0d10aa928a21be6e750b3990f306789c8c8bf9a6
John Bute <johnbute@Johns-MacBook-Pro-2.local>
2025-06-11 10:32:01 -0400

    added git functionality to show-templates

--- Sources/Commands/PackageCommands/ShowTemplates.swift
+++ Sources/Commands/PackageCommands/ShowTemplates.swift
@@ -17,6 +17,9 @@ import Foundation
 import PackageModel
 import PackageGraph
 import Workspace
+import TSCUtility
+import TSCBasic
+import SourceControl
 
 struct ShowTemplates: AsyncSwiftCommand {
     static let configuration = CommandConfiguration(
@@ -25,11 +28,60 @@ struct ShowTemplates: AsyncSwiftCommand {
     @OptionGroup(visibility: .hidden)
     var globalOptions: GlobalOptions
 
+    @Option(name: .customLong("template-url"), help: "The git URL of the template.")
+    var templateURL: String?
+
+    // Git-specific options
+    @Option(help: "The exact package version to depend on.")
+    var exact: Version?
+
+    @Option(help: "The specific package revision to depend on.")
+    var revision: String?
+
+    @Option(help: "The branch of the package to depend on.")
+    var branch: String?
+
+    @Option(help: "The package version to depend on (up to the next major version).")
+    var from: Version?
+
+    @Option(help: "The package version to depend on (up to the next minor version).")
+    var upToNextMinorFrom: Version?
+
+    @Option(help: "Specify upper bound on the package version range (exclusive).")
+    var to: Version?
+
     @Option(help: "Set the output format.")
     var format: ShowTemplatesMode = .flatlist
 
     func run(_ swiftCommandState: SwiftCommandState) async throws {
-        let packageGraph = try await swiftCommandState.loadPackageGraph()
+
+        let packagePath: Basics.AbsolutePath
+            var deleteAfter = false
+
+            // Use local current directory or fetch Git package
+            if let templateURL = self.templateURL {
+                let requirement = try checkRequirements()
+                packagePath = try await getPackageFromGit(destination: templateURL, requirement: requirement)
+                deleteAfter = true
+            } else {
+                guard let cwd = swiftCommandState.fileSystem.currentWorkingDirectory else {
+                    throw InternalError("No template URL provided and no current directory")
+                }
+                packagePath = cwd
+            }
+
+            defer {
+                if deleteAfter {
+                    try? FileManager.default.removeItem(atPath: packagePath.pathString)
+                }
+            }
+
+
+        let packageGraph = try await swiftCommandState.withTemporaryWorkspace(switchingTo: packagePath) { workspace, root in
+            return try await swiftCommandState.loadPackageGraph()
+
+        }
+
         let rootPackages = packageGraph.rootPackages.map { $0.identity }
 
         let templates = packageGraph.allModules.filter({
@@ -87,4 +139,118 @@ struct ShowTemplates: AsyncSwiftCommand {
             }
         }
     }
+
+    func getPackageFromGit(
+        destination: String,
+        requirement: PackageDependency.SourceControl.Requirement
+    ) async throws -> Basics.AbsolutePath {
+        let repositoryProvider = GitRepositoryProvider()
+
+        let fetchStandalonePackageByURL = { () async throws -> Basics.AbsolutePath in
+            try withTemporaryDirectory(removeTreeOnDeinit: false) { (tempDir: Basics.AbsolutePath) in
+                let url = SourceControlURL(destination)
+                let repositorySpecifier = RepositorySpecifier(url: url)
+
+                // This is the working clone destination
+                let bareCopyPath = tempDir.appending(component: "bare-copy")
+
+                let workingCopyPath = tempDir.appending(component: "working-copy")
+
+                try repositoryProvider.fetch(repository: repositorySpecifier, to: bareCopyPath)
+
+                try FileManager.default.createDirectory(atPath: workingCopyPath.pathString, withIntermediateDirectories: true)
+
+                        // Validate directory (now should exist)
+                        guard try repositoryProvider.isValidDirectory(bareCopyPath) else {
+                            throw InternalError("Invalid directory at \(workingCopyPath)")
+                        }
+
+
+
+                let repository = try repositoryProvider.createWorkingCopyFromBare(repository: repositorySpecifier, sourcePath: bareCopyPath, at: workingCopyPath, editable: true)
+
+
+                try FileManager.default.removeItem(at: bareCopyPath.asURL)
+
+                switch requirement {
+                case .range(let versionRange):
+                    let tags = try repository.getTags()
+                    let versions = tags.compactMap { Version($0) }
+                    let filteredVersions = versions.filter { versionRange.contains($0) }
+                    guard let latestVersion = filteredVersions.max() else {
+                        throw InternalError("No tags found within the specified version range \(versionRange)")
+                    }
+                    try repository.checkout(tag: latestVersion.description)
+
+                case .exact(let exactVersion):
+                    try repository.checkout(tag: exactVersion.description)
+
+                case .branch(let branchName):
+                    try repository.checkout(branch: branchName)
+
+                case .revision(let revision):
+                    try repository.checkout(revision: .init(identifier: revision))
+                }
+
+                return workingCopyPath
+            }
+        }
+
+        return try await fetchStandalonePackageByURL()
+    }
+
+
+    func checkRequirements() throws -> PackageDependency.SourceControl.Requirement {
+        var requirements : [PackageDependency.SourceControl.Requirement] = []
+
+        if let exact {
+            requirements.append(.exact(exact))
+        }
+
+        if let branch {
+            requirements.append(.branch(branch))
+        }
+
+        if let revision {
+            requirements.append(.revision(revision))
+        }
+
+        if let from {
+            requirements.append(.range(.upToNextMajor(from: from)))
+        }
+
+        if let upToNextMinorFrom {
+            requirements.append(.range(.upToNextMinor(from: upToNextMinorFrom)))
+        }
+
+        if requirements.count > 1 {
+            throw StringError(
+                "must specify at most one of --exact, --branch, --revision, --from, or --up-to-next-minor-from"
+            )
+        }
+
+        guard let firstRequirement = requirements.first else {
+            throw StringError(
+                "must specify one of --exact, --branch, --revision, --from, or --up-to-next-minor-from"
+            )
+        }
+
+        let requirement: PackageDependency.SourceControl.Requirement
+        if case .range(let range) = firstRequirement {
+            if let to {
+                requirement = .range(range.lowerBound ..< to)
+            } else {
+                requirement = .range(range)
+            }
+        } else {
+            requirement = firstRequirement
+
+            if self.to != nil {
+                throw StringError("--to can only be specified with --from or --up-to-next-minor-from")
+            }
+        }
+        return requirement
+
+    }
+
 }
