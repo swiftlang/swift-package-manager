6d28520418d79d1b399cb1739f15aa63c7f7f28f
Saleem Abdulrasool <compnerd@compnerd.org>
2025-06-13 12:00:17 -0700

    BasicsTests: avoid `@testable` import
    Use the newly extended APIs for testing and use a non-testable import of
    the package, avoiding a recompilation.

--- Tests/BasicsTests/ConcurrencyHelpersTests.swift
+++ Tests/BasicsTests/ConcurrencyHelpersTests.swift
@@ -11,7 +11,7 @@
 //===----------------------------------------------------------------------===//
 import Foundation
 
-@testable import Basics
+import Basics
 import TSCTestSupport
 import Testing
 
