61019f68500fce654f16285a1920de2b4f89ed94
Saleem Abdulrasool <compnerd@compnerd.org>
2025-06-13 12:53:00 -0700

    Tests: remove `@testable import PackageCollectionsModel`
    Use the newly extended APIs for testing and use a non-testable import of
    the package, avoiding a recompilation.

--- Tests/PackageCollectionsTests/PackageCollectionValidationTests.swift
+++ Tests/PackageCollectionsTests/PackageCollectionValidationTests.swift
@@ -14,7 +14,7 @@ import Foundation
 import XCTest
 
 import PackageCollections
-@testable import PackageCollectionsModel
+import PackageCollectionsModel
 import PackageModel
 
 class PackageCollectionValidationTests: XCTestCase {
