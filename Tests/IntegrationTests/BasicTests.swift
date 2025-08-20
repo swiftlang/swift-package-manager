d2f95561e4fc603ef3eafca25b63b9bb924758d0
Chris Miles <chrismiles@apple.com>
2025-08-20 10:12:48 -0700

    Fixed API change in swift play basic integration tests.

--- Tests/IntegrationTests/BasicTests.swift
+++ Tests/IntegrationTests/BasicTests.swift
@@ -465,7 +465,8 @@ private struct BasicTests {
             try localFileSystem.createDirectory(packagePath)
             try await executeSwiftPackage(
                 packagePath,
-                extraArgs: ["init", "--type", "library", "--name", packageName]
+                extraArgs: ["init", "--type", "library", "--name", packageName],
+                buildSystem: .native
             )
             try localFileSystem.writeFileContents(
                 packagePath.appending(component: "Package.swift"),
@@ -545,7 +546,11 @@ private struct BasicTests {
         try withTemporaryDirectory { tempDir in
             let packagePath = tempDir.appending(component: "swiftPlayExecute")
             try localFileSystem.createDirectory(packagePath)
-            try await executeSwiftPackage(packagePath, extraArgs: ["init", "--type", "library"])
+            try await executeSwiftPackage(
+                packagePath,
+                extraArgs: ["init", "--type", "library"],
+                buildSystem: .native
+            )
             try localFileSystem.writeFileContents(
                 packagePath.appending(component: "Package.swift"),
                 bytes: ByteString(
