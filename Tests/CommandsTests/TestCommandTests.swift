1865ccaab9ddca0763759e953a0705494aab85c8
Paul LeMarquand <plemarquand@apple.com>
2025-09-03 09:38:53 -0400

    Fixup tests on Linux/Windows

--- Tests/CommandsTests/TestCommandTests.swift
+++ Tests/CommandsTests/TestCommandTests.swift
@@ -1918,8 +1918,8 @@ struct TestCommandTests {
                 )
 
                 #expect(
-                    stdout.contains("failbreak breakpoint set -n \"_XCTFailureBreakpoint\""),
-                    "Expected XCTest failure breakpoint setup, got stdout: \(stdout), stderr: \(stderr)",
+                    stdout.contains("failbreak breakpoint set"),
+                    "Expected a failure breakpoint to be setup, got stdout: \(stdout), stderr: \(stderr)",
                 )
             }
         }
@@ -1946,7 +1946,7 @@ struct TestCommandTests {
                 )
 
                 #expect(
-                    stdout.contains("failbreak breakpoint set -s Testing -n \"failureBreakpoint()\""),
+                    stdout.contains("failbreak breakpoint set"),
                     "Expected Swift Testing failure breakpoint setup, got stdout: \(stdout), stderr: \(stderr)",
                 )
             }
@@ -1974,10 +1974,7 @@ struct TestCommandTests {
                 )
 
                 #expect(
-                    stdout.contains("failbreak") && (
-                        stdout.contains("_XCTFailureBreakpoint") ||
-                        stdout.contains("failureBreakpoint()")
-                    ),
+                    getNumberOfMatches(of: "breakpoint set", in: stdout) == 2,
                     "Expected combined failure breakpoint setup, got stdout: \(stdout), stderr: \(stderr)",
                 )
 
