96bc469ae3d26a060762537785e4c73595fdda40
Bri Peticca <briannap_22@apple.com>
2025-09-03 11:49:11 -0400

    Cleanup

--- Fixtures/Traits/PackageConditionalDeps/Package.swift
+++ Fixtures/Traits/PackageConditionalDeps/Package.swift
@@ -1,12 +1,10 @@
-// swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.1
 
 import PackageDescription
 
 let package = Package(
     name: "PackageConditionalDeps",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PackageConditionalDeps",
             targets: ["PackageConditionalDeps"]
@@ -22,8 +20,6 @@ let package = Package(
         .package(path: "../Package2"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "PackageConditionalDeps",
             dependencies: [ 
--- Fixtures/Traits/PackageConditionalDeps/Sources/PackageConditionalDeps/PackageConditionalDeps.swift
+++ Fixtures/Traits/PackageConditionalDeps/Sources/PackageConditionalDeps/PackageConditionalDeps.swift
@@ -1,2 +1,3 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+public func nothingHappens() {
+    // Do nothing.
+}
