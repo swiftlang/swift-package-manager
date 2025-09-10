dc32bc9c9db5c324c92c603ac01683a13d1d3405
Paul LeMarquand <plemarquand@apple.com>
2025-09-10 15:31:17 -0400

    Consolidate BuildConfiguration.buildArgs into one spot

--- Tests/CommandsTests/TestCommandTests.swift
+++ Tests/CommandsTests/TestCommandTests.swift
@@ -1715,7 +1715,7 @@ struct TestCommandTests {
         }
 
         func args(_ args: [String], for buildSystem: BuildSystemProvider.Kind, buildConfiguration: BuildConfiguration = .debug) -> [String] {
-            return args + buildConfiguration.args + getBuildSystemArgs(for: buildSystem)
+            return args + buildConfiguration.buildArgs + getBuildSystemArgs(for: buildSystem)
         }
 
         func commandState() throws -> (SwiftCommandState, BufferedOutputByteStream) {
@@ -1743,19 +1743,6 @@ struct TestCommandTests {
             )
             return (state, outputStream)
         }
-    }
-}
-
-fileprivate extension BuildConfiguration {
-    var args: [String] {
-        var args = ["--configuration"]
-        switch self {
-        case .debug:
-            args.append("debug")
-        case .release:
-            args.append("release")
-        }
-        return args
     }
 }
 
