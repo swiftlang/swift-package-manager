28a78b15fc81ea44d981f463403a7cef36b565bd
Sarunas <3808892+vsarunas@users.noreply.github.com>
2025-10-15 17:02:23 +0300

    move to memoize and move android check

--- Sources/Build/BuildPlan/BuildPlan.swift
+++ Sources/Build/BuildPlan/BuildPlan.swift
@@ -654,12 +654,11 @@ public class BuildPlan: SPMBuildCore.BuildPlan {
 
     /// Extracts the library information from an XCFramework.
     func parseXCFramework(for binaryTarget: BinaryModule, triple: Basics.Triple, enableXCFrameworksOnLinux: Bool) throws -> [LibraryInfo] {
-        if !enableXCFrameworksOnLinux && triple.os == .linux && triple.environment != .android {
-            return []
-        }
-
-        return try self.externalLibrariesCache.memoize(key: binaryTarget) {
-            try binaryTarget.parseXCFrameworks(for: triple, fileSystem: self.fileSystem)
+        try self.externalLibrariesCache.memoize(key: binaryTarget) {
+            if !enableXCFrameworksOnLinux && triple.os == .linux {
+                return []
+            }
+            return try binaryTarget.parseXCFrameworks(for: triple, fileSystem: self.fileSystem)
         }
     }
 
