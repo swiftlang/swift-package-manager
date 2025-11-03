a5bb468ab3897679fc85de513294cf5a5366b30b
Robert Connell <robert_connell@apple.com>
2025-11-03 11:57:05 -0500

    Specify the platform version as the fixtures depend on swift concurrency

--- Fixtures/SwiftMigrate/ExistentialAnyWithPluginMigration/Package.swift
+++ Fixtures/SwiftMigrate/ExistentialAnyWithPluginMigration/Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "ExistentialAnyMigration",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     targets: [
         .target(name: "Library", plugins: [.plugin(name: "Plugin")]),
         .plugin(name: "Plugin", capability: .buildTool, dependencies: ["Tool"]),
