93258e34d5db0b255a4ee4384a5ab46a99eb26f5
Doug Schaefer <dschaefer2@apple.com>
2025-11-10 15:03:16 -0500

    Fix copyright

--- Sources/PackageModel/Module/PrebuiltLibrary.swift
+++ Sources/PackageModel/Module/PrebuiltLibrary.swift
@@ -1,9 +1,14 @@
+//===----------------------------------------------------------------------===//
 //
-//  PrebuiltLibrary.swift
-//  SwiftPM
+// This source file is part of the Swift open source project
 //
-//  Created by Doug Schaefer on 2025-10-16.
+// Copyright (c) 1994-2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
 
 import Basics
 
