a918bd235d794233627c55ab96c2c31bc8d2cc11
Jonathan Grynspan <grynspan@me.com>
2025-11-26 10:50:50 -0500

    Add an experimental interface for custom test libraries.
    This PR adds command-line experimental support for custom test libraries that don't sit on top of XCTest or Swift Testing (irony inbound). Developers can specify additional libraries to invoke using the `--experimental-testing-library` argument.
    
    In a separate PR, I will teach Swift Testing how to read the `--experimental-testing-library` argument (forwarded as `--testing-library` once per, er, library.) A developer can write a small shim function that gets added to the `__swift5_tests` section as a special test content record and which, when called, invokes its corresponding testing library and translates output to the "language" Swift Testing speaks.
    
    The above description is light on details because we haven't designed out the mechanisms in question. In particular, we don't have a stable configuration/input mechanism, so for the moment I'm just forwarding command-line arguments. We also need to teach some part of the stack how to collate results from multiple libraries to produce a single unified test report in various formats. This PR is just one step.

--- Sources/Basics/TestingLibrary.swift
+++ Sources/Basics/TestingLibrary.swift
@@ -11,23 +11,58 @@
 //===----------------------------------------------------------------------===//
 
 /// The testing libraries supported by the package manager.
-public enum TestingLibrary: Sendable, CustomStringConvertible {
-  /// The XCTest library.
-  ///
-  /// This case represents both the open-source swift-corelibs-xctest
-  /// package and Apple's XCTest framework that ships with Xcode.
-  case xctest
+public enum TestingLibrary: Sendable, Equatable, Hashable, CustomStringConvertible {
+    /// The XCTest library.
+    ///
+    /// This case represents both the open-source swift-corelibs-xctest
+    /// package and Apple's XCTest framework that ships with Xcode.
+    case xctest
 
-  /// The swift-testing library.
-  case swiftTesting
+    /// The swift-testing library.
+    case swiftTesting
 
-  public var description: String {
-    switch self {
-    case .xctest:
-      "XCTest"
-    case .swiftTesting:
-      "Swift Testing"
+    /// A library specified by name _other than_ XCTest or Swift Testing.
+    ///
+    /// - Parameters:
+    ///   - name: The name of the library. The formatting of this string is
+    ///     unspecified.
+    case other(_ name: String)
+
+    public init(_ name: String) {
+        switch name.filter(\.isLetter).lowercased() {
+        case "xctest":
+            self = .xctest
+        case "swifttesting":
+            self = .swiftTesting
+        default:
+            self = .other(name)
+        }
     }
-  }
+
+    public init(argument: String) {
+        self.init(argument)
+    }
+
+    public var shortName: String {
+        switch self {
+        case .xctest:
+            "xctest"
+        case .swiftTesting:
+            "swift-testing"
+        case let .other(name):
+            name
+        }
+    }
+
+    public var description: String {
+        switch self {
+        case .xctest:
+            "XCTest"
+        case .swiftTesting:
+            "Swift Testing"
+        case let .other(name):
+            name
+        }
+    }
 }
 
