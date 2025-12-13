466cadc697b96a8ba516d5f12f70b332d4524748
Robert Connell <robert_connell@apple.com>
2025-12-12 22:15:39 -0500

    More logging

--- Sources/_InternalTestSupport/misc.swift
+++ Sources/_InternalTestSupport/misc.swift
@@ -100,7 +100,7 @@ public func testWithTemporaryDirectory<Result>(
     return try await withTemporaryDirectory(prefix: "spm-tests-\(cleanedFunction)") { tmpDirPath in
         defer {
             let cwd = FileManager.default.currentDirectoryPath
-            print("TWT-FMCWD: \(cwd)")
+            print("TWT-FMCWD: \(cwd) - \(cleanedFunction)")
             // Unblock and remove the tmp dir on deinit.
             try? localFileSystem.chmod(.userWritable, path: tmpDirPath, options: [.recursive])
             try? localFileSystem.removeFileTree(tmpDirPath)
@@ -134,7 +134,7 @@ public func testWithTemporaryDirectory<Result>(
             defer {
                 // Unblock and remove the tmp dir on deinit.
                 let cwd = FileManager.default.currentDirectoryPath
-                print("FXT-FMCWD: \(cwd)")
+                print("FXT-FMCWD: \(cwd) - \(copyName)")
                 try? localFileSystem.chmod(.userWritable, path: tmpDirPath, options: [.recursive])
                 try? localFileSystem.removeFileTree(tmpDirPath)
             }
@@ -268,7 +268,7 @@ public enum TestError: Error {
                 if removeFixturePathOnDeinit {
                     // Unblock and remove the tmp dir on deinit.
                     let cwd = FileManager.default.currentDirectoryPath
-                    print("FIX-FMCWD: \(cwd)")
+                    print("FIX-FMCWD: \(cwd) - \(name)")
                     try? localFileSystem.chmod(.userWritable, path: tmpDirPath, options: [.recursive])
                     try? localFileSystem.removeFileTree(tmpDirPath)
                 }
