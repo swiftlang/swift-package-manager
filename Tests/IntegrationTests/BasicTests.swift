b60528e60b576bf42fa14be9c230a7ac1f212d28
Dave Inglis <dinglis@apple.com>
2025-12-14 09:35:00 -0500

    Remove a couple areas where tests where changing cwd (fixes AL2 crashes) (#9505)
    Fix crashes on AL2 where tests where changing current working directory

--- Tests/IntegrationTests/BasicTests.swift
+++ Tests/IntegrationTests/BasicTests.swift
@@ -69,7 +69,7 @@ private struct BasicTests {
             #expect(dealerOutput.filter(\.isPlayingCardSuit).count == 10)
 
             // Verify that the 'git status' is clean after a build.
-            let gitOutput = try sh("git\(ProcessInfo.exeSuffix)", "status", workingDirectory: packagePath).stdout
+            let gitOutput = try sh("git\(ProcessInfo.exeSuffix)", "-C", packagePath, "status").stdout
             #expect(gitOutput.contains("nothing to commit, working tree clean"))
 
             // Verify that another 'swift build' does nothing.
