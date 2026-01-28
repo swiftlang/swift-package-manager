ec8cbb0b5b9c72c94956a875d81a9a32e5f0c42e
Ev Cheng <ev_cheng@apple.com>
2026-01-28 12:00:37 -0800

    fix: revert changed code by accident

--- Sources/SwiftBuildSupport/PackagePIFBuilder.swift
+++ Sources/SwiftBuildSupport/PackagePIFBuilder.swift
@@ -409,6 +409,7 @@ public final class PackagePIFBuilder {
         init(from pifProductType: ProjectModel.Target.ProductType) {
             self = switch pifProductType {
             case .application: .application
+            case .commonStaticArchive: .staticArchive
             case .staticArchive: .staticArchive
             case .commonObject: .commonObject
             case .objectFile: .commonObject
