f1457bdefc8f79f9c664f25513d6f45f8718b1d4
Ev Cheng <ev_cheng@apple.com>
2026-02-06 15:13:05 -0800

    refactor: remove unused url field

--- Sources/SBOMModel/Core/SBOMCommit.swift
+++ Sources/SBOMModel/Core/SBOMCommit.swift
@@ -13,20 +13,17 @@
 internal struct SBOMCommit: Hashable, Codable, Equatable {
     internal let sha: String
     internal let repository: String
-    internal let url: String?
     internal let authors: [SBOMPerson]?
     internal let message: String?
 
     internal init(
         sha: String,
         repository: String,
-        url: String? = nil, // url to the commit
         authors: [SBOMPerson]? = nil,
         message: String? = nil
     ) {
         self.sha = sha
         self.repository = repository
-        self.url = url
         self.authors = authors
         self.message = message
     }
