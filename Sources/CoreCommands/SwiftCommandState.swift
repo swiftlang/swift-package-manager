a6540a2ae1b6d6dc8284b4b1ba66842004ad961c
Doug Gregor <dgregor@apple.com>
2026-02-08 11:29:00 -0800

    Add ParsingManifestLoader, which uses swift-syntax to parse the package manifest directly, and then walks the resulting syntax tree to find the package declaration, its targets, dependencies, and so on. For anything in the syntax tree that the parsing manifest loader does not recognize, it records a "limitation" associated with that syntax tree node. The presence of any limitations means that the result of the parsing manifest loader should not be trusted, in which case we can fall back to the existing manifest loader. One can request that the limitations, if found, are printed to the terminal using the experimental flag --experimental-show-manifest-parser-limitations. This can be used to determine why a particular package manifest isn't handled by the parsing manifest loader. We could use this information to expand the capabilities of the parsing manifest loader in the future, so that more manifests would benefit from it as a fast path.
    The parsing manifest loader takes advantage of the SwiftIfConfig
    library to handle #if in the package manifest. It queries the compiler
    frontend to determine the configuration of the compiler that would be
    used to build the manifest so we get the same results from #if.
    Additionally, the parsing manifest loader maintains a static version
    of the Context that is available to the package manifest, so it can
    substitute (e.g.) the package directory, Git information, or
    environment information into the package manifest.
    
    The large amount of churn in the package loading tests are because
    most of the tests have been switched from just using the existing
    manifest loader to iterating over both loaders (using
    forEachManifestLoader). This ensures that both loaders pass the same
    set of tests. Additionally, we check that the resulting manifests from
    both loaders matches exactly (by encoding them into JSON) to ensure
    that the results are the same. This way, we can keep the manifest
    loaders in sync for the subset of package manifests that are meant to work.
    
    This also adds an experimental command-line option,
    --experimental-manifest-processing-mode, that enables the parsing
    manifest loader. It must be provided with one of the following options:
    
    - only-parsed: only use the parsing loader. This is not correct, because
    manifests can require execution, but can be used for testing.
    - only-executed: only using the executing loader, which is the normal
    one. This is the default for now.
    - parsed-with-fallback: try using the parsing loader. If it encounters
    limitations, fall back to the executing loader without failing. This
    option is what can provide build-time performance improvements when
    the parsing manifest loader takes over.
    - crosscheck: try both the parsing and the executing loaders and compare
    the results to ensure they match. If the parsing loader encounters
    limitations, no further checking will be done. On success, it will
    print the relative times. If the results differ, it will print the
    JSON form of the manifest from each parser.

--- Sources/CoreCommands/SwiftCommandState.swift
+++ Sources/CoreCommands/SwiftCommandState.swift
@@ -848,7 +848,7 @@ public final class SwiftCommandState {
         return rootManifests.values.map { $0.toolsVersion }.min()
     }
 
-    func getManifestLoader() throws -> ManifestLoader {
+    func getManifestLoader() throws -> any ManifestLoaderProtocol {
         try self._manifestLoader.get()
     }
 
@@ -1113,7 +1113,16 @@ public final class SwiftCommandState {
         )
     })
 
-    private lazy var _manifestLoader: Result<ManifestLoader, Swift.Error> = Result(catching: {
+    private func createParsingManifestLoader() throws -> ParsingManifestLoader {
+        return try ParsingManifestLoader(
+            toolchain: self.getHostToolchain(),
+            pruneDependencies: self.options.resolver.pruneDependencies,
+            extraManifestFlags: self.options.build.manifestFlags,
+            environment: nil
+        )
+    }
+
+    private func createExecutingManifestLoader() throws -> ManifestLoader {
         let cachePath: AbsolutePath? = switch (
             self.options.caching.shouldDisableManifestCaching,
             self.options.caching.manifestCachingMode
@@ -1143,6 +1152,21 @@ public final class SwiftCommandState {
             importRestrictions: .none,
             pruneDependencies: self.options.resolver.pruneDependencies
         )
+    }
+
+    private lazy var _manifestLoader: Result<any ManifestLoaderProtocol, Swift.Error> = Result(
+        catching: {
+            switch self.options.manifest.manifestProcessingMode {
+            case .onlyParsed: try createParsingManifestLoader()
+            case .onlyExecuted: try createExecutingManifestLoader()
+            case .crosscheck, .parsedWithFallback:
+                ChainedParsingManifestLoader(
+                    parsingLoader: try createParsingManifestLoader(),
+                    executingLoader: try createExecutingManifestLoader(),
+                    showLimitations: self.options.manifest.showManifestParserLimitations,
+                    crosscheck: self.options.manifest.manifestProcessingMode == .crosscheck
+                )
+        }
     })
 
     /// An enum indicating the execution status of run commands.
