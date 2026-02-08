1f0551391e996dd774979878ed664700b724b740
Doug Gregor <dgregor@apple.com>
2026-02-08 11:29:00 -0800

    Add ParsingManifestLoader, which conforms to the existing ManifestLoaderProtocol. ParsingManifestLoader uses swift-syntax to parse the manifest directly, and then walks the resulting syntax tree to find the package declaration, its targets, dependencies, and so on. For anything in the syntax tree that the parsing manifest loader does not recognize, it records a "limitation" associated with that syntax tree node. The presence of any limitations means that the result of the parsing manifest loader should not be trusted, in which case we can fall back to the existing manifest loader. One can request that the limitations, if found, are printed to the terminal using the experimental flag --experimental-show-manifest-parser-limitations. This can be used to determine why a particular package manifest isn't handled by the parsing manifest loader. We could use this information to expand the capabilities of the parsing manifest loader in the future, so that more manifests would benefit from it as a fast path.
    The parsing manifest loader takes advantage of the SwiftIfConfig
    library to handle #if in the package manifest. It queries the compiler
    frontend to determine the configuration of the compiler that would be
    used to build the manifest so we get the same results from #if.
    Additionally, the parsing manifest loader maintains a static version
    of the Context that is available to the package manifest, so it can
    substitute (e.g.) the package directory, Git information, or
    environment information into the package manifest.
    
    The large amount of churn in the package loading tests are because
    most of the tests have been switched from just using the existing
    manifest loader to iterating over both loaders (using
    forEachManifestLoader). This ensures that both loaders pass the same
    set of tests. Additionally, we check that the resulting manifests from
    both loaders matches exactly (by encoding them into JSON) to ensure
    that the results are the same. This way, we can keep the manifest
    loaders in sync for the subset of package manifests that are meant to work.
    
    This also adds an experimental command-line option,
    --experimental-manifest-processing-mode, that enables the parsing
    manifest loader. It must be provided with one of the following options:
    
    - only-parsed: only use the parsing loader. This is not correct, because
    manifests can require execution, but can be used for testing.
    - only-executed: only using the executing loader, which is the normal
    one. This is the default for now.
    - parsed-with-fallback: try using the parsing loader. If it encounters
    limitations, fall back to the executing loader without failing. This
    option is what can provide build-time performance improvements when
    the parsing manifest loader takes over.
    - crosscheck: try both the parsing and the executing loaders and compare
    the results to ensure they match. If the parsing loader encounters
    limitations, no further checking will be done. On success, it will
    print the relative times. If the results differ, it will print the
    JSON form of the manifest from each parser.

---
+++ Sources/PackageLoading/ManifestParsing/ChainedParsingManifestLoader.swift
@@ -0,0 +1,230 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2014-2026 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Basics
+import Dispatch
+import Foundation
+import PackageModel
+import SourceControl
+
+public import SwiftDiagnostics
+import SwiftParser
+import SwiftParserDiagnostics
+import SwiftSyntax
+
+import struct TSCBasic.ByteString
+
+import struct TSCUtility.Version
+
+/// Manifest loader that chains together a parsing manifest loader (which
+/// parses the manifest source directly) and an another manifest loader
+/// (e.g., that executes the manifest). It can use the parsing manifest
+/// loader when that succeeds, or cross-check the results of the parsing
+/// manifest loader against the other manifest loader to verify that they
+/// produce the same results.
+public final class ChainedParsingManifestLoader: ManifestLoaderProtocol {
+    let parsingLoader: ParsingManifestLoader
+    let executingLoader: any ManifestLoaderProtocol
+
+    /// Whether to show the limitations that prevent us from using the
+    /// results of the parsing loader.
+    let showLimitations: Bool
+
+    /// Whether to cross-check the results of the two loaders. Otherwise,
+    /// the results will be taken from the parsing loader if it succeeds,
+    /// and the executing loader otherwise.
+    let crosscheck: Bool
+
+    public init(
+        parsingLoader: ParsingManifestLoader,
+        executingLoader: any ManifestLoaderProtocol,
+        showLimitations: Bool,
+        crosscheck: Bool
+    ) {
+        self.parsingLoader = parsingLoader
+        self.executingLoader = executingLoader
+        self.showLimitations = showLimitations
+        self.crosscheck = crosscheck
+    }
+
+    public func load(
+        manifestPath: AbsolutePath,
+        manifestToolsVersion: ToolsVersion,
+        packageIdentity: PackageIdentity,
+        packageKind: PackageReference.Kind,
+        packageLocation: String,
+        packageVersion: (version: Version?, revision: String?)?,
+        identityResolver: IdentityResolver,
+        dependencyMapper: DependencyMapper,
+        fileSystem: FileSystem,
+        observabilityScope: ObservabilityScope,
+        delegateQueue: DispatchQueue
+    ) async throws -> Manifest {
+        if crosscheck {
+            // When cross-checking, we're doing timings. Pre-load the file
+            // to warm the filesystem cache rather than charging it to
+            // a particular loader.
+            let manifestContents: ByteString
+            do {
+                manifestContents = try fileSystem.readFileContents(manifestPath)
+            } catch {
+                throw ManifestParserError.inaccessibleManifest(path: manifestPath, reason: String(describing: error))
+            }
+            _ = manifestContents
+        }
+
+        // Parse the manifest directly with the parsing loader.
+        let parsedManifest: Manifest?
+        let parsingStartTime = DispatchTime.now()
+        let parsingEndTime: DispatchTime
+        do {
+            let manifest = try parsingLoader.load(
+                manifestPath: manifestPath,
+                manifestToolsVersion: manifestToolsVersion,
+                packageIdentity: packageIdentity,
+                packageKind: packageKind,
+                packageLocation: packageLocation,
+                packageVersion: packageVersion,
+                identityResolver: identityResolver,
+                dependencyMapper: dependencyMapper,
+                fileSystem: fileSystem,
+                observabilityScope: observabilityScope,
+                delegateQueue: delegateQueue
+            )
+
+            // We successfully parsed the manifest. If we aren't
+            // cross-checking the results, we're done.
+            if !crosscheck {
+                return manifest
+            }
+
+            parsedManifest = manifest
+            parsingEndTime = DispatchTime.now()
+        } catch {
+            guard case .limitations(let limitations) = error else {
+                throw error
+            }
+
+            // We hit a limitation of the parsed approach, which can either
+            // be a missing feature or something executable in the manifest.
+            parsingEndTime = DispatchTime.now()
+
+            if showLimitations {
+                print("Manifest parser encountered \(limitations.count) limitations that prevent its use for '\(manifestPath.pathString)'")
+
+                let formatter = DiagnosticsFormatter()
+                let filename = manifestPath.pathString
+                let locationConverter = SourceLocationConverter(
+                    fileName: filename,
+                    tree: limitations[0].syntax.root
+                )
+                for limitation in limitations {
+                    let diagLoc = locationConverter.location(
+                        for: limitation.syntax.position
+                    )
+                    let prefix = "\(filename):\(diagLoc.line):\(diagLoc.column):"
+                    let message = formatter.formattedMessage(limitation)
+
+                    let source = formatter.annotatedSource(
+                        tree: limitation.syntax.root,
+                        diags: [limitation.asDiagnostic()]
+                    )
+
+                    print(
+                        "\(prefix) \(message)\n\(source)"
+                    )
+                }
+            }
+            parsedManifest = nil
+        }
+
+        // Use the executing loader to process the manifest.
+        let executingStartTime = DispatchTime.now()
+        let executedManifest = try await executingLoader.load(
+            manifestPath: manifestPath,
+            manifestToolsVersion: manifestToolsVersion,
+            packageIdentity: packageIdentity,
+            packageKind: packageKind,
+            packageLocation: packageLocation,
+            packageVersion: packageVersion,
+            identityResolver: identityResolver,
+            dependencyMapper: dependencyMapper,
+            fileSystem: fileSystem,
+            observabilityScope: observabilityScope,
+            delegateQueue: delegateQueue
+        )
+        let executingEndTime = DispatchTime.now()
+
+        let parsingDuration = parsingStartTime.distance(to: parsingEndTime)
+        let executingDuration = executingStartTime.distance(to: executingEndTime)
+
+        // If we have a parsed manifest, it means that we want to
+        // cross-check the results. Do so now.
+        if let parsedManifest {
+            precondition(crosscheck)
+
+            let parsedJSON = try parsedManifest.toJSON()
+            let executedJSON = try executedManifest.toJSON()
+            guard parsedJSON == executedJSON else {
+                print("""
+                    Manifest loading cross-check failed for '\(manifestPath.pathString)':
+                      - Parsing took \(parsingDuration)
+                      - Executing took \(executingDuration)
+                    """
+                )
+                throw ChainedParsingError.manifestMismatch(
+                    manifestPath: manifestPath.pathString,
+                    parsed: parsedJSON,
+                    executed: executedJSON
+                )
+            }
+
+            print("""
+                Manifest loading cross-check succeeded for '\(manifestPath.pathString)':
+                  - Parsing took \(parsingDuration)
+                  - Executing took \(executingDuration)
+                """
+            )
+            return parsedManifest
+        } else {
+            print("""
+                Manifest loading encountered limitations for '\(manifestPath.pathString)':
+                  - Parsing took \(parsingDuration)
+                  - Executing took \(executingDuration)
+                """
+            )
+        }
+
+        return executedManifest
+    }
+
+    public func resetCache(observabilityScope: Basics.ObservabilityScope) async {
+        await parsingLoader.resetCache(observabilityScope: observabilityScope)
+        await executingLoader.resetCache(observabilityScope: observabilityScope)
+    }
+
+    public func purgeCache(observabilityScope: Basics.ObservabilityScope) async {
+        await parsingLoader.purgeCache(observabilityScope: observabilityScope)
+        await executingLoader.purgeCache(observabilityScope: observabilityScope)
+    }
+
+    enum ChainedParsingError: Error, CustomStringConvertible {
+        case manifestMismatch(manifestPath: String, parsed: String, executed: String)
+
+        var description: String {
+            switch self {
+            case .manifestMismatch(manifestPath: let manifestPath, parsed: let parsed, executed: let expected):
+                "The manifest produced by parsing '\(manifestPath)' does not match the one produced by executing: \(parsed) != \(expected)"
+            }
+        }
+    }
+}
