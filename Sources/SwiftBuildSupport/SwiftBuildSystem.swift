74a51e15967fec70335b9617240b1a6790017dcd
Bassam Khouri <bkhouri@apple.com>
2026-03-07 09:18:41 -0500

    Add Umbrella in the umbrella product name to avoid collision with test targets that are nanames the same as the manifest display name

--- Sources/SwiftBuildSupport/SwiftBuildSystem.swift
+++ Sources/SwiftBuildSupport/SwiftBuildSystem.swift
@@ -246,7 +246,7 @@ public final class SwiftBuildSystem: SPMBuildCore.BuildSystem {
                         builtProducts.append(
                             BuiltTestProduct(
                                 productName: product.name,
-                                umbrellaProductName: "\(package.manifest.displayName)PackageTests",
+                                umbrellaProductName: "\(package.manifest.displayName)UmbrellaPackageTests",
                                 binaryPath: binaryPath,
                                 packagePath: package.path,
                                 testEntryPointPath: product.underlying.testEntryPointPath
