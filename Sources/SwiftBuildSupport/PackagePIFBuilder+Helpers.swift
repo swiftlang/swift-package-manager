3fa2c55da040eb854193a70000279d4b741c9096
Ev Cheng <ev_cheng@apple.com>
2026-03-11 10:03:59 -0700

    comment out some stuff

--- Sources/SwiftBuildSupport/PackagePIFBuilder+Helpers.swift
+++ Sources/SwiftBuildSupport/PackagePIFBuilder+Helpers.swift
@@ -207,7 +207,8 @@ extension PackagePIFBuilder {
         // e.g., swift-nio_NIOPosix, swift-crypto__CryptoExtras
         // So should be ignored by moduleName()
         // But moduleName can start with _, like _CryptoExtras and __AsyncFileSystem
-        if targetName.contains("_") { // && !targetName.starts(with: "_") {
+        // if targetName.contains("_") {
+        if !targetName.starts(with: "_") {
             return nil
         }
         return removeSuffix(from: targetName)
