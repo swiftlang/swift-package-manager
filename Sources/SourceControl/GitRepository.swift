532a7066a3edf10c01c830930585ede5bf00692c
Meet Balani <74920401+shrudge@users.noreply.github.com>
2026-03-22 21:08:15 +0530

    Improve error reporting when git executable fails or is missing

--- Sources/SourceControl/GitRepository.swift
+++ Sources/SourceControl/GitRepository.swift
@@ -1202,11 +1202,8 @@ package struct GitShellError: Error, CustomStringConvertible {
     let result: AsyncProcessResult
 
     public var description: String {
-        let stdout = (try? self.result.utf8Output()) ?? ""
-        let stderr = (try? self.result.utf8stderrOutput()) ?? ""
-        let output = (stdout + stderr).spm_chomp()
         let command = self.result.arguments.joined(separator: " ")
-        return "Git command '\(command)' failed: \(output)"
+        return formatErrorDescription(result: self.result, message: "Git command '\(command)' failed")
     }
 }
 
@@ -1235,10 +1232,7 @@ public struct GitRepositoryError: Error, CustomStringConvertible, DiagnosticLoca
     }
 
     public var description: String {
-        let stdout = (try? self.result.utf8Output()) ?? ""
-        let stderr = (try? self.result.utf8stderrOutput()) ?? ""
-        let output = (stdout + stderr).spm_chomp().spm_multilineIndent(count: 4)
-        return "\(self.message):\n\(output)"
+        formatErrorDescription(result: self.result, message: self.message)
     }
 }
 
@@ -1259,10 +1253,7 @@ public struct GitCloneError: Error, CustomStringConvertible, DiagnosticLocationP
     }
 
     public var description: String {
-        let stdout = (try? self.result.utf8Output()) ?? ""
-        let stderr = (try? self.result.utf8stderrOutput()) ?? ""
-        let output = (stdout + stderr).spm_chomp().spm_multilineIndent(count: 4)
-        return "\(self.message):\n\(output)"
+        formatErrorDescription(result: self.result, message: self.message)
     }
 }
 
@@ -1450,4 +1441,21 @@ extension RepositorySpecifier.Location {
             return url.absoluteString
         }
     }
+}
+
+fileprivate func formatErrorDescription(result: AsyncProcessResult, message: String) -> String {
+    var output = ""
+    switch (result.output, result.stderrOutput) {
+    case (.failure(let error), _), (_, .failure(let error)):
+        output = String(describing: error)
+    default:
+        let stdout = (try? result.utf8Output()) ?? ""
+        let stderr = (try? result.utf8stderrOutput()) ?? ""
+        output = (stdout + stderr).spm_chomp()
+    }
+    let indentedOutput = output.spm_multilineIndent(count: 4)
+    if indentedOutput.isEmpty {
+        return message
+    }
+    return "\(message):\n\(indentedOutput)"
 }
