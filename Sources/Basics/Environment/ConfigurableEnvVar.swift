823a74be9a07517c26b6d67d6113339d988d52ce
Bassam Khouri <bkhouri@apple.com>
2026-04-17 11:58:19 -0400

    Consolidate enviroment variables used to configure SwiftPM
    PR #9925 added new environment variables that would control
    authentication with the Package registry using environment variables.
    
    Move the newly added environment variables to the `ConfigurableEnvVar`
    enum, to have all configurable environment variables in a single
    location, so we can eventually generated documentation from this source.

---
+++ Sources/Basics/Environment/ConfigurableEnvVar.swift
@@ -0,0 +1,71 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+import struct Basics.EnvironmentKey
+
+package enum ConfigurableEnvVar: String, CaseIterable {
+    /// SBOM specification(s) to generate (comma-separated list)
+    case SWIFTPM_BUILD_SBOM_SPEC
+
+    /// Directory path to generate SBOM(s) in
+    case SWIFTPM_BUILD_SBOM_OUTPUT_DIR
+
+    /// Filter SBOM components and dependencies by entity (package, product, or both)
+    case SWIFTPM_BUILD_SBOM_FILTER
+
+    /// Whether to treat SBOM generation errors as warnings
+    case SWIFTPM_BUILD_SBOM_WARNING_ONLY
+
+    /// Bearer token to be provided for all package registry requests
+    case SWIFTPM_REGISTRY_TOKEN
+
+    /// Username for Basic authentication credentials provided to all package registries
+    case SWIFTPM_REGISTRY_LOGIN
+
+    /// Password for Basic authentication credentials provided to all package registries
+    case SWIFTPM_REGISTRY_PASSWORD
+
+    /// Token for HTTP downloads of binary artifacts and prebuilts; does not affect git operations
+    case SWIFTPM_SOURCE_CONTROL_TOKEN
+
+    /// Inline netrc-formatted content for per-host credentials
+    case SWIFTPM_NETRC_DATA
+
+    private var isCacheable : Bool {
+        switch self {
+        case .SWIFTPM_BUILD_SBOM_SPEC: false
+        case .SWIFTPM_BUILD_SBOM_OUTPUT_DIR: false
+        case .SWIFTPM_BUILD_SBOM_FILTER: false
+        case .SWIFTPM_BUILD_SBOM_WARNING_ONLY: false
+        case .SWIFTPM_REGISTRY_TOKEN: false
+        case .SWIFTPM_REGISTRY_LOGIN: false
+        case .SWIFTPM_REGISTRY_PASSWORD: false
+        case .SWIFTPM_SOURCE_CONTROL_TOKEN: false
+        case .SWIFTPM_NETRC_DATA: false
+
+        }
+    }
+
+    package static func nonCachableEnvVars() -> Set<EnvironmentKey> {
+        return Set(
+            Self.allCases.filter { !$0.isCacheable }
+                .map { variable in
+                    EnvironmentKey(variable.rawValue)
+                }
+        )
+    }
+
+    package func value(from environment: Environment) -> String? {
+        environment[EnvironmentKey(self.rawValue)]
+    }
+}
