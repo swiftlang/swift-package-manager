70f138727c39797650717120806cafb956117bc9
Matt <85322+mattmassicotte@users.noreply.github.com>
2026-05-12 14:26:55 -0400

    WIP - define tests

--- Sources/PackageModel/Manifest/TargetBuildSettingDescription.swift
+++ Sources/PackageModel/Manifest/TargetBuildSettingDescription.swift
@@ -119,6 +119,8 @@ public enum TargetBuildSettingDescription {
                 return true
             case (.unsafeFlags, .unsafeFlags):
                 return true
+            case (.define, .define):
+                return true
             case (.treatWarning(let value, _), .treatWarning(let defaultValue, _)):
                 return value == defaultValue
             case (.enableWarning(let value), .disableWarning(let defaultValue)):
