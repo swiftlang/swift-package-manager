1259ec49ce10b285c8d2f60bac8fc487353ba510
Bri Peticca <briannap_22@apple.com>
2026-07-09 10:32:40 -0400

    Bump swift-syntax in macro template to 605.0.0-latest

--- Sources/PackageModel/InstalledSwiftPMConfiguration.swift
+++ Sources/PackageModel/InstalledSwiftPMConfiguration.swift
@@ -37,7 +37,7 @@ public struct InstalledSwiftPMConfiguration {
         return .init(
             version: 0,
             swiftSyntaxVersionForMacroTemplate: .init(
-                major: 604,
+                major: 605,
                 minor: 0,
                 patch: 0,
                 prereleaseIdentifier: "latest"
