abe5a595a74738a224fb8a4a68f8e2573e202be3
Doug Schaefer <dschaefer2@apple.com>
2026-09-01 19:15:14 -0400

    Make sure SDL can access C functions

--- Examples/swift-sdl/Tests/SDLTests/SDLTests.swift
+++ Examples/swift-sdl/Tests/SDLTests/SDLTests.swift
@@ -3,5 +3,6 @@ import SDL
 
 @Test func example() async throws {
     // SDL_GetVersion renamed in API notes
-    print(SwiftSDL_GetVersion())
+    print(SwiftSDL_GetVersion(), "Tests")
+    sayVersion()
 }
