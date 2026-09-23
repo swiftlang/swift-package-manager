1680874fbc2d763d13ab50f862a04e4756ce2727
Paul LeMarquand <plemarquand@apple.com>
2026-09-23 19:42:22 -0400

    [6.4.x] Scope registry downloads by the registry that served them (#10573)

--- Tests/WorkspaceTests/WorkspaceTests.swift
+++ Tests/WorkspaceTests/WorkspaceTests.swift
@@ -16487,7 +16487,7 @@ final class WorkspaceTests: XCTestCase {
             testDiagnostics(diagnostics) { result in
                 result.check(
                     diagnostic: .regex(
-                        "failed extracting '.*[\\\\/]registry[\\\\/]downloads[\\\\/]org[\\\\/]foo[\\\\/]1.0.0.zip' to '.*[\\\\/]registry[\\\\/]downloads[\\\\/]org[\\\\/]foo[\\\\/]1.0.0': boom"
+                        "failed extracting '.*[\\\\/]registry[\\\\/]downloads[\\\\/][^\\\\/]+[\\\\/]org[\\\\/]foo[\\\\/]1.0.0.zip' to '.*[\\\\/]registry[\\\\/]downloads[\\\\/][^\\\\/]+[\\\\/]org[\\\\/]foo[\\\\/]1.0.0': boom"
                     ),
                     severity: .error
                 )
@@ -16542,7 +16542,9 @@ final class WorkspaceTests: XCTestCase {
         // for mock manifest loader to work with an actual registry download
         // we populate the mock manifest with a pointer to the correct download location
         let defaultLocations = try Workspace.Location(forRootPackage: sandbox, fileSystem: fs)
-        let packagePath = defaultLocations.registryDownloadDirectory.appending(components: ["org", "foo", "1.5.1"])
+        let packagePath = defaultLocations.registryDownloadDirectory
+            .appending(component: Registry(url: registryURL, supportsAvailability: false).storageKey)
+            .appending(components: ["org", "foo", "1.5.1"])
         workspace.manifestLoader.manifests[.init(url: "org.foo", version: "1.5.1")] =
             try Manifest.createManifest(
                 displayName: "Foo",
